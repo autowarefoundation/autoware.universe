c5190668e61c2439f8d0d42d90793588cad8f9eb
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2020-07-30 11:24:44 +0900

    Add nodelets of tlr nodes (#715)
    * Add classifier nodelet
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * Replace boost::shared_ptr into std::shared_ptr
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * Add lock guard
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * Add detetcor nodelet
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * Integrate main into node
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * Add SubscriberStatusCallback
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * add image_transport_decompresser nodelet
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * Add visualizer nodelet
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * fixed bug
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * Fix plugin name
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * Launch nodelet
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * Fix classifier constructor
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * add decompresser node
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * fix typo
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * fixed bug
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * fixed bug
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * cosmetic change
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * add param
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * fix bug
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * Fix build warning
    
    Signed-off-by: Daisuke Nishimatsu <border_goldenmarket@yahoo.co.jp>
    
    * change rgb
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    * change rgb
    
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>
    
    Co-authored-by: Yukihiro Saito <yukky.saito@gmail.com>

---
+++ sensing/image_transport_decompressor/include/image_transport_decompressor/nodelet.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 Tier IV, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CompressedImage.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <limits>
+#include <vector>
+
+namespace image_preprocessor
+{
+class ImageTransportDecompressorNodelet : public nodelet::Nodelet
+{
+private:
+  virtual void onInit() override;
+
+  void compressedImageCallback(
+    const sensor_msgs::CompressedImageConstPtr & input_compressed_image_msg);
+
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Subscriber compressed_image_sub_;
+  ros::Publisher raw_image_pub_;
+  std::string enc_;
+};
+
+}  // namespace image_preprocessor
