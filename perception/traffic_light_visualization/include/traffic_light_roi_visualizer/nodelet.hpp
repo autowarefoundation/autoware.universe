bb1a48d97da03698dc61f931b220c1d1b240a473
Servando <43142004+sgermanserrano@users.noreply.github.com>
2020-12-08 01:58:21 +0000

    Port traffic light visualization (#121)
    * Port
    
    Signed-off-by: Servando German Serrano <servando.german.serrano@linaro.org>
    
    * Fix package.xml
    
    Signed-off-by: Servando German Serrano <servando.german.serrano@linaro.org>
    
    * Removed unused mutex
    
    Signed-off-by: Servando German Serrano <servando.german.serrano@linaro.org>

--- perception/traffic_light_visualization/include/traffic_light_roi_visualizer/nodelet.hpp
+++ perception/traffic_light_visualization/include/traffic_light_roi_visualizer/nodelet.hpp
@@ -12,17 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #pragma once
-#include "ros/ros.h"
 
-#include "image_transport/image_transport.h"
-#include "image_transport/subscriber_filter.h"
-#include "autoware_perception_msgs/TrafficLightRoiArray.h"
+#include "rclcpp/rclcpp.hpp"
+
+#include "image_transport/image_transport.hpp"
+#include "image_transport/subscriber_filter.hpp"
+#include "autoware_perception_msgs/msg/traffic_light_roi_array.hpp"
 #include "message_filters/subscriber.h"
 #include "message_filters/sync_policies/approximate_time.h"
 #include "message_filters/synchronizer.h"
-#include "sensor_msgs/Image.h"
-#include "nodelet/nodelet.h"
-#include "pluginlib/class_list_macros.h"
+#include "sensor_msgs/msg/image.hpp"
 
 #include "cv_bridge/cv_bridge.h"
 #include "opencv2/imgproc/imgproc_c.h"
@@ -33,48 +32,46 @@
 
 namespace traffic_light
 {
-class TrafficLightRoiVisualizerNodelet : public nodelet::Nodelet
+class TrafficLightRoiVisualizerNodelet : public rclcpp::Node
 {
 public:
-  virtual void onInit();
+  TrafficLightRoiVisualizerNodelet(const rclcpp::NodeOptions & options);
   void connectCb();
 
   void imageRoiCallback(
-    const sensor_msgs::ImageConstPtr & input_image_msg,
-    const autoware_perception_msgs::TrafficLightRoiArray::ConstPtr & input_tl_roi_msg);
+    const sensor_msgs::msg::Image::ConstSharedPtr & input_image_msg,
+    const autoware_perception_msgs::msg::TrafficLightRoiArray::ConstSharedPtr & input_tl_roi_msg);
 
   void imageRoughRoiCallback(
-    const sensor_msgs::ImageConstPtr & input_image_msg,
-    const autoware_perception_msgs::TrafficLightRoiArrayConstPtr & input_tl_roi_msg,
-    const autoware_perception_msgs::TrafficLightRoiArrayConstPtr & input_tl_rough_roi_msg);
+    const sensor_msgs::msg::Image::ConstSharedPtr & input_image_msg,
+    const autoware_perception_msgs::msg::TrafficLightRoiArray::ConstSharedPtr & input_tl_roi_msg,
+    const autoware_perception_msgs::msg::TrafficLightRoiArray::ConstSharedPtr & input_tl_rough_roi_msg);
 
 private:
   bool createRect(
     cv::Mat& image,
-    const autoware_perception_msgs::TrafficLightRoi& tl_roi,
+    const autoware_perception_msgs::msg::TrafficLightRoi& tl_roi,
     const cv::Scalar& color);
 
-  ros::NodeHandle nh_, pnh_;
-  std::shared_ptr<image_transport::ImageTransport> image_transport_;
+  rclcpp::TimerBase::SharedPtr timer_;
   image_transport::SubscriberFilter image_sub_;
-  message_filters::Subscriber<autoware_perception_msgs::TrafficLightRoiArray> roi_sub_;
-  message_filters::Subscriber<autoware_perception_msgs::TrafficLightRoiArray> rough_roi_sub_;
+  message_filters::Subscriber<autoware_perception_msgs::msg::TrafficLightRoiArray> roi_sub_;
+  message_filters::Subscriber<autoware_perception_msgs::msg::TrafficLightRoiArray> rough_roi_sub_;
   image_transport::Publisher image_pub_;
   typedef message_filters::sync_policies::ApproximateTime<
-    sensor_msgs::Image, autoware_perception_msgs::TrafficLightRoiArray>
+    sensor_msgs::msg::Image, autoware_perception_msgs::msg::TrafficLightRoiArray>
     SyncPolicy;
   typedef message_filters::Synchronizer<SyncPolicy> Sync;
   std::shared_ptr<Sync> sync_;
 
   typedef message_filters::sync_policies::ApproximateTime<
-    sensor_msgs::Image,
-    autoware_perception_msgs::TrafficLightRoiArray,
-    autoware_perception_msgs::TrafficLightRoiArray>
+    sensor_msgs::msg::Image,
+    autoware_perception_msgs::msg::TrafficLightRoiArray,
+    autoware_perception_msgs::msg::TrafficLightRoiArray>
     SyncPolicyWithRoughRoi;
   typedef message_filters::Synchronizer<SyncPolicyWithRoughRoi> SyncWithRoughRoi;
   std::shared_ptr<SyncWithRoughRoi> sync_with_rough_roi_;
 
-  std::mutex connect_mutex_;
   bool enable_fine_detection_;
 };
 
--- perception/traffic_light_visualization/src/nodelet.cpp
+++ perception/traffic_light_visualization/src/nodelet.cpp
@@ -16,43 +16,52 @@
 
 namespace traffic_light
 {
-void TrafficLightRoiVisualizerNodelet::onInit()
-{
-  nh_ = getNodeHandle();
-  pnh_ = getPrivateNodeHandle();
-  image_transport_.reset(new image_transport::ImageTransport(pnh_));
-
-  pnh_.param<bool>("enable_fine_detection", enable_fine_detection_, false);
+TrafficLightRoiVisualizerNodelet::TrafficLightRoiVisualizerNodelet(const rclcpp::NodeOptions & options)
+: Node("traffic_light_roi_visualizer_node", options)
+{  
+  using std::placeholders::_1;
+  using std::placeholders::_2;
+  using std::placeholders::_3;
+  enable_fine_detection_ = this->declare_parameter("enable_fine_detection", false);
 
   if (enable_fine_detection_) {
     sync_with_rough_roi_.reset(new SyncWithRoughRoi(SyncPolicyWithRoughRoi(10), image_sub_, roi_sub_,  rough_roi_sub_));
-    sync_with_rough_roi_->registerCallback(boost::bind(&TrafficLightRoiVisualizerNodelet::imageRoughRoiCallback, this, _1, _2, _3));
+    sync_with_rough_roi_->registerCallback(std::bind(&TrafficLightRoiVisualizerNodelet::imageRoughRoiCallback, this, _1, _2, _3));
   } else {
     sync_.reset(new Sync(SyncPolicy(10), image_sub_, roi_sub_));
-    sync_->registerCallback(boost::bind(&TrafficLightRoiVisualizerNodelet::imageRoiCallback, this, _1, _2));
+    sync_->registerCallback(std::bind(&TrafficLightRoiVisualizerNodelet::imageRoiCallback, this, _1, _2));
   }
-  image_transport::SubscriberStatusCallback connect_cb = boost::bind(&TrafficLightRoiVisualizerNodelet::connectCb, this);
-  std::lock_guard<std::mutex> lock(connect_mutex_);
-  image_pub_ = image_transport_->advertise("output/image", 1, connect_cb, connect_cb);
+  
+  auto timer_callback = std::bind(&TrafficLightRoiVisualizerNodelet::connectCb, this);
+  const auto period_s = 0.1;
+  const auto period_ns =
+    std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(period_s));
+  timer_ = std::make_shared<rclcpp::GenericTimer<decltype(timer_callback)>>(
+    this->get_clock(), period_ns, std::move(timer_callback),
+    this->get_node_base_interface()->get_context());
+  this->get_node_timers_interface()->add_timer(timer_, nullptr);
+
+  image_pub_ = image_transport::create_publisher(this, "output/image", rclcpp::QoS{1}.get_rmw_qos_profile());
 }
 
 void TrafficLightRoiVisualizerNodelet::connectCb()
 {
-  std::lock_guard<std::mutex> lock(connect_mutex_);
   if (image_pub_.getNumSubscribers() == 0) {
     image_sub_.unsubscribe();
     roi_sub_.unsubscribe();
     if (enable_fine_detection_) rough_roi_sub_.unsubscribe();
   } else if (!image_sub_.getSubscriber()) {
-    image_sub_.subscribe(*image_transport_, "input/image", 1);
-    roi_sub_.subscribe(pnh_, "input/rois", 1);
-    if (enable_fine_detection_) rough_roi_sub_.subscribe(pnh_, "input/rough/rois", 1);
+    image_sub_.subscribe(this, "input/image", "raw", rclcpp::QoS{1}.get_rmw_qos_profile());
+    roi_sub_.subscribe(this, "input/rois", rclcpp::QoS{1}.get_rmw_qos_profile());
+    if (enable_fine_detection_) {
+      rough_roi_sub_.subscribe(this, "input/rough/rois", rclcpp::QoS{1}.get_rmw_qos_profile());
+    }
   }
 }
 
 bool TrafficLightRoiVisualizerNodelet::createRect(
   cv::Mat& image,
-  const autoware_perception_msgs::TrafficLightRoi& tl_roi,
+  const autoware_perception_msgs::msg::TrafficLightRoi& tl_roi,
   const cv::Scalar& color)
 {
   cv::rectangle(
@@ -69,8 +78,8 @@ bool TrafficLightRoiVisualizerNodelet::createRect(
 }
 
 void TrafficLightRoiVisualizerNodelet::imageRoiCallback(
-  const sensor_msgs::ImageConstPtr & input_image_msg,
-  const autoware_perception_msgs::TrafficLightRoiArrayConstPtr & input_tl_roi_msg)
+  const sensor_msgs::msg::Image::ConstSharedPtr & input_image_msg,
+  const autoware_perception_msgs::msg::TrafficLightRoiArray::ConstSharedPtr & input_tl_roi_msg)
 {
   cv_bridge::CvImagePtr cv_ptr;
   try {
@@ -79,15 +88,15 @@ void TrafficLightRoiVisualizerNodelet::imageRoiCallback(
       createRect(cv_ptr->image, tl_roi, cv::Scalar(0,255,0));
     }
   } catch (cv_bridge::Exception & e) {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", input_image_msg->encoding.c_str());
+    RCLCPP_ERROR(get_logger(), "Could not convert from '%s' to 'bgr8'.", input_image_msg->encoding.c_str());
   }
   image_pub_.publish(cv_ptr->toImageMsg());
 }
 
 void TrafficLightRoiVisualizerNodelet::imageRoughRoiCallback(
-  const sensor_msgs::ImageConstPtr & input_image_msg,
-  const autoware_perception_msgs::TrafficLightRoiArrayConstPtr & input_tl_roi_msg,
-  const autoware_perception_msgs::TrafficLightRoiArrayConstPtr & input_tl_rough_roi_msg)
+  const sensor_msgs::msg::Image::ConstSharedPtr & input_image_msg,
+  const autoware_perception_msgs::msg::TrafficLightRoiArray::ConstSharedPtr & input_tl_roi_msg,
+  const autoware_perception_msgs::msg::TrafficLightRoiArray::ConstSharedPtr & input_tl_rough_roi_msg)
 {
   cv_bridge::CvImagePtr cv_ptr;
   try {
@@ -99,12 +108,12 @@ void TrafficLightRoiVisualizerNodelet::imageRoughRoiCallback(
       createRect(cv_ptr->image, tl_rough_roi, cv::Scalar(0,255,0));
     }
   } catch (cv_bridge::Exception & e) {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", input_image_msg->encoding.c_str());
+    RCLCPP_ERROR(get_logger(), "Could not convert from '%s' to 'bgr8'.", input_image_msg->encoding.c_str());
   }
   image_pub_.publish(cv_ptr->toImageMsg());
 }
 
 }  // namespace traffic_light
 
-#include "pluginlib/class_list_macros.h"
-PLUGINLIB_EXPORT_CLASS(traffic_light::TrafficLightRoiVisualizerNodelet, nodelet::Nodelet)
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(traffic_light::TrafficLightRoiVisualizerNodelet)
