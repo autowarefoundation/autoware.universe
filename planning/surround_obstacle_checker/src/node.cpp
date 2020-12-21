222f58e14aab4ee3b93b5d80b82cad4591204378
Nikolai Morin <nnmmgit@gmail.com>
2020-12-21 14:02:27 +0100

    Replace waitForTransform by lookupTransform in surround_obstacle_checker (#212)
    * Fix erroneous waitForTransform in surround_obstacle_checker
    
    * Line length fix

--- planning/surround_obstacle_checker/src/node.cpp
+++ planning/surround_obstacle_checker/src/node.cpp
@@ -194,22 +194,11 @@ bool SurroundObstacleCheckerNode::convertPose(
   const geometry_msgs::msg::Pose & pose, const std::string & source, const std::string & target,
   const rclcpp::Time & time, geometry_msgs::msg::Pose & conv_pose)
 {
-  geometry_msgs::msg::TransformStamped ros_src2tgt;
   tf2::Transform src2tgt;
-  tf2::Transform src2obj;
-  tf2::Transform tgt2obj;
-
   try {
     // get transform from source to target
-    auto tf_future =
-      tf_buffer_.waitForTransform(source, target, time, tf2::durationFromSec(0.0), [](auto &) {});
-    auto status = tf_future.wait_for(tf2::durationFromSec(0.1));
-    if (status != std::future_status::ready) {
-      RCLCPP_WARN_STREAM_THROTTLE(
-        get_logger(), *this->get_clock(), 0.5, "cannot get tf from " << source << " to " << target);
-      return false;
-    }
-
+    geometry_msgs::msg::TransformStamped ros_src2tgt = tf_buffer_.lookupTransform(
+      source, target, time, tf2::durationFromSec(0.1));
     tf2::fromMsg(ros_src2tgt.transform, src2tgt);
   } catch (tf2::TransformException & ex) {
     RCLCPP_WARN_STREAM_THROTTLE(
@@ -217,8 +206,9 @@ bool SurroundObstacleCheckerNode::convertPose(
     return false;
   }
 
+  tf2::Transform src2obj;
   tf2::fromMsg(pose, src2obj);
-  tgt2obj = src2tgt.inverse() * src2obj;
+  tf2::Transform tgt2obj = src2tgt.inverse() * src2obj;
   tf2::toMsg(tgt2obj, conv_pose);
   return true;
 }
@@ -258,16 +248,9 @@ void SurroundObstacleCheckerNode::getNearestObstacleByPointCloud(
   // waint to transform pointcloud
   geometry_msgs::msg::TransformStamped transform_stamped;
   try {
-    auto tf_future = tf_buffer_.waitForTransform(
+    transform_stamped = tf_buffer_.lookupTransform(
       "base_link", pointcloud_ptr_->header.frame_id, pointcloud_ptr_->header.stamp,
-      tf2::durationFromSec(0.0), [](auto &) {});
-    auto status = tf_future.wait_for(tf2::durationFromSec(0.5));
-    if (status != std::future_status::ready) {
-      RCLCPP_WARN_STREAM_THROTTLE(
-        get_logger(), *this->get_clock(), 0.5,
-        "failed to get base_link to " << pointcloud_ptr_->header.frame_id << " transform.");
-      return;
-    }
+      tf2::durationFromSec(0.5));
   } catch (tf2::TransformException & ex) {
     RCLCPP_WARN_STREAM_THROTTLE(
       get_logger(), *this->get_clock(), 0.5,
