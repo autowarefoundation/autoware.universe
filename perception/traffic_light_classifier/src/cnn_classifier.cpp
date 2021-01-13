bb9aad11578a19ee292d27117fef578afbe65a28
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-01-13 13:10:49 +0900

    traffic_light_classifier: Fix engine save dir (#250)
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- perception/traffic_light_classifier/src/cnn_classifier.cpp
+++ perception/traffic_light_classifier/src/cnn_classifier.cpp
@@ -14,6 +14,7 @@
 #include "traffic_light_classifier/cnn_classifier.hpp"
 #include "boost/algorithm/string/split.hpp"
 #include "boost/algorithm/string/classification.hpp"
+#include "ament_index_cpp/get_package_share_directory.hpp"
 
 namespace traffic_light
 {
@@ -33,7 +34,8 @@ CNNClassifier::CNNClassifier(rclcpp::Node * node_ptr) : node_ptr_(node_ptr)
 
   readLabelfile(label_file_path, labels_);
 
-  std::string cache_dir = "/tmp/traffic_light_classifier/data";
+  std::string cache_dir =
+    ament_index_cpp::get_package_share_directory("traffic_light_classifier") + "/data";
   trt_ = std::make_shared<Tn::TrtCommon>(model_file_path, cache_dir, precision);
   trt_->setup();
 }
