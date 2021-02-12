528cfede94dd894c0148a2310450c16846af6fc8
Shinnosuke Hirakawa <8327162+0x126@users.noreply.github.com>
2021-02-12 21:29:09 +0900

    Ros2 v0.8.0 obstacle collision checker (#316)
    * Feature/obstacle collision checker (#1063)
    
    * Add template
    
    * Remove unnecessary code
    
    * Add obstacle_collision_checker
    
    * add braking distance
    
    * delete unuse file
    
    * change resample traj
    
    * Format files
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Add author
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Remove vehicle_footprint visualization
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Move package under control/trajectory_follower
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Add space
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix visualization error
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Remove comment out lines
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    Co-authored-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * ros2 porting
    
    * remove "dynamic_reconfigure"
    
    * fix CMAKELists.txt & apply lint
    
    * fix paramCallback
    
    * remove unnecessary comment
    
    * fix include statement
    
    Co-authored-by: Satoshi Tanaka <st14.828soccer@gmail.com>
    Co-authored-by: Kenji Miyake <kenji.miyake@tier4.jp>
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

---
+++ control/trajectory_follower/obstacle_collision_checker/README.md
@@ -0,0 +1,1 @@
+# obstacle_collision_checker
---
+++ control/trajectory_follower/obstacle_collision_checker/include/obstacle_collision_checker/obstacle_collision_checker_node.hpp
@@ -0,0 +1,113 @@
+// Copyright 2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OBSTACLE_COLLISION_CHECKER__OBSTACLE_COLLISION_CHECKER_NODE_HPP_
+#define OBSTACLE_COLLISION_CHECKER__OBSTACLE_COLLISION_CHECKER_NODE_HPP_
+
+#include <memory>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "diagnostic_updater/diagnostic_updater.hpp"
+#include "autoware_utils/geometry/geometry.hpp"
+#include "autoware_utils/ros/debug_publisher.hpp"
+#include "autoware_utils/ros/processing_time_publisher.hpp"
+#include "autoware_utils/ros/self_pose_listener.hpp"
+#include "autoware_utils/ros/transform_listener.hpp"
+#include "autoware_planning_msgs/msg/route.hpp"
+#include "autoware_planning_msgs/msg/trajectory.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
+
+#include "obstacle_collision_checker/obstacle_collision_checker.hpp"
+
+namespace obstacle_collision_checker
+{
+struct NodeParam
+{
+  double update_rate;
+};
+
+class ObstacleCollisionCheckerNode : public rclcpp::Node
+{
+public:
+  ObstacleCollisionCheckerNode();
+
+private:
+  // NodeHandle
+  // ros::NodeHandle nh_{""};
+  // ros::NodeHandle private_nh_{"~"};
+
+  // Subscriber
+  std::shared_ptr<autoware_utils::SelfPoseListener> self_pose_listener_;
+  std::shared_ptr<TransformListener> transform_listener_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_obstacle_pointcloud_;
+  rclcpp::Subscription<autoware_planning_msgs::msg::Trajectory>::SharedPtr
+    sub_reference_trajectory_;
+  rclcpp::Subscription<autoware_planning_msgs::msg::Trajectory>::SharedPtr
+    sub_predicted_trajectory_;
+  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr sub_twist_;
+
+  // Data Buffer
+  geometry_msgs::msg::PoseStamped::ConstSharedPtr current_pose_;
+  geometry_msgs::msg::TwistStamped::ConstSharedPtr current_twist_;
+  sensor_msgs::msg::PointCloud2::ConstSharedPtr obstacle_pointcloud_;
+  geometry_msgs::msg::TransformStamped::ConstSharedPtr obstacle_transform_;
+  autoware_planning_msgs::msg::Trajectory::ConstSharedPtr reference_trajectory_;
+  autoware_planning_msgs::msg::Trajectory::ConstSharedPtr predicted_trajectory_;
+
+  // Callback
+  void onObstaclePointcloud(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+  void onReferenceTrajectory(const autoware_planning_msgs::msg::Trajectory::SharedPtr msg);
+  void onPredictedTrajectory(const autoware_planning_msgs::msg::Trajectory::SharedPtr msg);
+  void onTwist(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
+
+  // Publisher
+  std::shared_ptr<autoware_utils::DebugPublisher> debug_publisher_;
+  std::shared_ptr<autoware_utils::ProcessingTimePublisher> time_publisher_;
+
+  // Timer
+  rclcpp::TimerBase::SharedPtr timer_;
+  void initTimer(double period_s);
+
+  bool isDataReady();
+  bool isDataTimeout();
+  void onTimer();
+
+  // Parameter
+  NodeParam node_param_;
+  Param param_;
+
+  // Dynamic Reconfigure
+  OnSetParametersCallbackHandle::SharedPtr set_param_res_;
+  rcl_interfaces::msg::SetParametersResult paramCallback(
+    const std::vector<rclcpp::Parameter> & parameters);
+
+  // Core
+  Input input_;
+  Output output_;
+  std::unique_ptr<ObstacleCollisionChecker> obstacle_collision_checker_;
+
+  // Diagnostic Updater
+  diagnostic_updater::Updater updater_;
+
+  void checkLaneDeparture(diagnostic_updater::DiagnosticStatusWrapper & stat);
+
+  // Visualization
+  visualization_msgs::msg::MarkerArray createMarkerArray() const;
+};
+}  // namespace obstacle_collision_checker
+
+#endif  // OBSTACLE_COLLISION_CHECKER__OBSTACLE_COLLISION_CHECKER_NODE_HPP_
