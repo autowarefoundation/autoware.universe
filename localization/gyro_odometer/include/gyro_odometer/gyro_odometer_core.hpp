37bed8fe7eccf4f5fcfbea7f122968005e50a3f3
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-04-05 11:05:54 +0900

    Fix rolling build errors (#1225)
    * Add missing include files
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Replace rclcpp::Duration
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Use reference for exceptions
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Use from_seconds
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- localization/gyro_odometer/include/gyro_odometer/gyro_odometer_core.hpp
+++ localization/gyro_odometer/include/gyro_odometer/gyro_odometer_core.hpp
@@ -21,6 +21,7 @@
 
 #include "tf2/transform_datatypes.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
 
 #include "rclcpp/rclcpp.hpp"
