a82572f573f1ce3026da159debe11f1e3a9d0487
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-04-12 14:59:29 +0900

    Awapi relay component (#1237)
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

---
+++ awapi/awapi_awiv_adapter/launch/awapi_relay_container.launch.py
@@ -0,0 +1,377 @@
+# Copyright 2020 Tier IV, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import launch
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition, LaunchConfigurationNotEquals
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from launch.substitutions import EnvironmentVariable
+
+
+def generate_launch_description():
+    relay_components = []
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='route_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('input_route'),
+            'output_topic': LaunchConfiguration('get_route'),
+            'type': 'autoware_planning_msgs/msg/Route',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='predict_object_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('input_object'),
+            'output_topic': LaunchConfiguration('get_predicted_object'),
+            'type': 'autoware_perception_msgs/msg/DynamicObjectArray',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='nearest_traffic_light_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('input_nearest_traffic_light_state'),
+            'output_topic': LaunchConfiguration('get_nearest_traffic_light_status'),
+            'type': 'autoware_perception_msgs/msg/TrafficLightStateStamped',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='gate_mode_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_gate_mode'),
+            'output_topic': LaunchConfiguration('output_gate_mode'),
+            'type': 'autoware_control_msgs/msg/GateMode',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='emergency_stop_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_emergency_stop'),
+            'output_topic': LaunchConfiguration('output_emergency_stop'),
+            'type': 'autoware_control_msgs/msg/EmergencyMode',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='autoware_engage_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_engage'),
+            'output_topic': LaunchConfiguration('output_autoware_engage'),
+            'type': 'autoware_vehicle_msgs/msg/Engage',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='vehicle_engage_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_engage'),
+            'output_topic': LaunchConfiguration('output_vehicle_engage'),
+            'type': 'autoware_vehicle_msgs/msg/Engage',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='put_route_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_route'),
+            'output_topic': LaunchConfiguration('output_route'),
+            'type': 'autoware_planning_msgs/msg/Route',
+            'durability': 'transient_local',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='put_goal_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_goal'),
+            'output_topic': LaunchConfiguration('output_goal'),
+            'type': 'geometry_msgs/msg/PoseStamped',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='lane_change_approval_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_lane_change_approval'),
+            'output_topic': LaunchConfiguration('output_lane_change_approval'),
+            'type': 'autoware_planning_msgs/msg/LaneChangeCommand',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='force_lane_change_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_force_lane_change'),
+            'output_topic': LaunchConfiguration('output_force_lane_change'),
+            'type': 'autoware_planning_msgs/msg/LaneChangeCommand',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='obstacle_avoid_approval_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_obstacle_avoid_approval'),
+            'output_topic': LaunchConfiguration('output_obstacle_avoid_approval'),
+            'type': 'autoware_planning_msgs/msg/EnableAvoidance',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='traffic_light_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('input_traffic_light_state'),
+            'output_topic': LaunchConfiguration('get_traffic_light_status'),
+            'type': 'autoware_perception_msgs/msg/TrafficLightStateArray',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='overwrite_traffic_light_state_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_overwrite_traffic_light_state'),
+            'output_topic': LaunchConfiguration('output_overwrite_traffic_light_state'),
+            'type': 'autoware_perception_msgs/msg/TrafficLightStateArray',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='speed_exceeded_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('input_stop_speed_exceeded'),
+            'output_topic': LaunchConfiguration('get_stop_speed_exceeded'),
+            'type': 'autoware_planning_msgs/msg/StopSpeedExceeded',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='crosswalk_status_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_crosswalk_status'),
+            'output_topic': LaunchConfiguration('input_external_crosswalk_status'),
+            'type': 'autoware_api_msgs/msg/CrosswalkStatus',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='intersection_status_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_intersection_status'),
+            'output_topic': LaunchConfiguration('input_external_intersection_status'),
+            'type': 'autoware_api_msgs/msg/IntersectionStatus',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='expand_stop_range_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_expand_stop_range'),
+            'output_topic': LaunchConfiguration('input_expand_stop_range'),
+            'type': 'autoware_planning_msgs/msg/ExpandStopRange',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    relay_components.append(ComposableNode(
+        package='topic_tools',
+        plugin='topic_tools::RelayNode',
+        name='pose_initialization_request_relay',
+        namespace='awapi',
+        parameters=[{
+            'input_topic': LaunchConfiguration('set_pose_initialization_request'),
+            'output_topic': LaunchConfiguration('input_pose_initialization_request'),
+            'type': 'autoware_localization_msgs/msg/PoseInitializationRequest',
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME',
+                                                default_value='False')
+        }],
+        extra_arguments=[{
+            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
+        }],
+    ))
+
+    container = ComposableNodeContainer(
+        name='awapi_relay_container',
+        namespace='awapi',
+        package='rclcpp_components',
+        executable=LaunchConfiguration('container_executable'),
+        composable_node_descriptions=relay_components,
+        output='screen',
+        parameters=[{
+            'use_sim_time': EnvironmentVariable(name='AW_ROS2_USE_SIM_TIME', default_value='False')
+        }],
+    )
+
+    set_container_executable = SetLaunchConfiguration(
+        'container_executable',
+        'component_container',
+        condition=UnlessCondition(LaunchConfiguration('use_multithread'))
+    )
+
+    set_container_mt_executable = SetLaunchConfiguration(
+        'container_executable',
+        'component_container_mt',
+        condition=IfCondition(LaunchConfiguration('use_multithread'))
+    )
+
+    return launch.LaunchDescription([set_container_executable,
+                                     set_container_mt_executable] +
+                                    [container])
