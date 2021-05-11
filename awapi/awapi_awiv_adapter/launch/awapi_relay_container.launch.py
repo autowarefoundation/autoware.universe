5e5b7615662ba5fc2126927c3aa2c20755692180
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2021-05-11 13:22:24 +0900

    fix flake8 (#1306)
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

--- awapi/awapi_awiv_adapter/launch/awapi_relay_container.launch.py
+++ awapi/awapi_awiv_adapter/launch/awapi_relay_container.launch.py
@@ -13,12 +13,11 @@
 # limitations under the License.
 
 import launch
-from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetLaunchConfiguration
-from launch.conditions import IfCondition, UnlessCondition, LaunchConfigurationNotEquals
+from launch.actions import SetLaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
+from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
-from launch.substitutions import EnvironmentVariable
 
 
 def generate_launch_description():
