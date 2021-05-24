c232e05a4bff7b577cc29c8c95d2ac9225ecebf8
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-05-25 01:24:11 +0900

    Format launch files (#228)
    * Format launch files
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Format launch.py
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix lint
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- launch/perception_launch/launch/traffic_light_recognition/traffic_light_node_container.launch.py
+++ launch/perception_launch/launch/traffic_light_recognition/traffic_light_node_container.launch.py
@@ -15,12 +15,12 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 import launch
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import ComposableNodeContainer, LoadComposableNodes
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 
 
