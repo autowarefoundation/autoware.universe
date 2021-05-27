dc313687bf7d7541eefe8d147cdf614f41aff4c2
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-05-27 14:30:35 +0900

    Make a curvature calculation function in autoware_utils' library (#1388) (#1393)
    * add curvature calculation function
    
    * add two functions
    
    * integrate 2 functions into 1 function by template function
    
    * add unit test for curvature calculation
    
    * modify spell
    
    * Update common/util/autoware_utils/include/autoware_utils/math/curvature.h
    
    Co-authored-by: Kazuki Miyahara <kmiya@outlook.com>
    
    * add modifications
    
    * modify format
    
    * update
    
    * update
    
    Co-authored-by: Kazuki Miyahara <kmiya@outlook.com>
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    Co-authored-by: purewater0901 <43805014+purewater0901@users.noreply.github.com>
    Co-authored-by: Kazuki Miyahara <kmiya@outlook.com>

--- common/autoware_utils/include/autoware_utils/geometry/geometry.hpp
+++ common/autoware_utils/include/autoware_utils/geometry/geometry.hpp
@@ -155,6 +155,20 @@ T transformVector(const T & points, const geometry_msgs::msg::Transform & transf
   }
   return transformed;
 }
+
+inline double calculateCurvature(
+  const geometry_msgs::msg::Point & p1, const geometry_msgs::msg::Point & p2,
+  const geometry_msgs::msg::Point & p3)
+{
+  // Calculation details are described in the following page
+  // https://en.wikipedia.org/wiki/Menger_curvature
+  const double denominator =
+    calcDistance2d(p1, p2) * calcDistance2d(p2, p3) * calcDistance2d(p3, p1);
+  if (std::fabs(denominator) < 1e-10) {
+    throw std::runtime_error("points are too close for curvature calculation.");
+  }
+  return 2.0 * ((p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x)) / denominator;
+}
 }  // namespace autoware_utils
 
 #endif  // AUTOWARE_UTILS__GEOMETRY__GEOMETRY_HPP_
--- common/autoware_utils/test/src/geometry/test_geometry.cpp
+++ common/autoware_utils/test/src/geometry/test_geometry.cpp
@@ -323,3 +323,57 @@ TEST(geometry, transformVector)
     EXPECT_DOUBLE_EQ(ps_transformed.at(1).z(), 6.299038105676658);
   }
 }
+
+TEST(geometry, calculateCurvature)
+{
+  using autoware_utils::calculateCurvature;
+  // Straight Line
+  {
+    geometry_msgs::msg::Point p1 = autoware_utils::createPoint(0.0, 0.0, 0.0);
+    geometry_msgs::msg::Point p2 = autoware_utils::createPoint(1.0, 0.0, 0.0);
+    geometry_msgs::msg::Point p3 = autoware_utils::createPoint(2.0, 0.0, 0.0);
+    EXPECT_DOUBLE_EQ(calculateCurvature(p1, p2, p3), 0.0);
+  }
+
+  // Clockwise Curved Road with a 1[m] radius
+  {
+    geometry_msgs::msg::Point p1 = autoware_utils::createPoint(0.0, 0.0, 0.0);
+    geometry_msgs::msg::Point p2 = autoware_utils::createPoint(1.0, 1.0, 0.0);
+    geometry_msgs::msg::Point p3 = autoware_utils::createPoint(2.0, 0.0, 0.0);
+    EXPECT_DOUBLE_EQ(calculateCurvature(p1, p2, p3), -1.0);
+  }
+
+  // Clockwise Curved Road with a 5[m] radius
+  {
+    geometry_msgs::msg::Point p1 = autoware_utils::createPoint(0.0, 0.0, 0.0);
+    geometry_msgs::msg::Point p2 = autoware_utils::createPoint(5.0, 5.0, 0.0);
+    geometry_msgs::msg::Point p3 = autoware_utils::createPoint(10.0, 0.0, 0.0);
+    EXPECT_DOUBLE_EQ(calculateCurvature(p1, p2, p3), -0.2);
+  }
+
+  // Counter-Clockwise Curved Road with a 1[m] radius
+  {
+    geometry_msgs::msg::Point p1 = autoware_utils::createPoint(0.0, 0.0, 0.0);
+    geometry_msgs::msg::Point p2 = autoware_utils::createPoint(-1.0, 1.0, 0.0);
+    geometry_msgs::msg::Point p3 = autoware_utils::createPoint(-2.0, 0.0, 0.0);
+    EXPECT_DOUBLE_EQ(calculateCurvature(p1, p2, p3), 1.0);
+  }
+
+  // Counter-Clockwise Curved Road with a 5[m] radius
+  {
+    geometry_msgs::msg::Point p1 = autoware_utils::createPoint(0.0, 0.0, 0.0);
+    geometry_msgs::msg::Point p2 = autoware_utils::createPoint(-5.0, 5.0, 0.0);
+    geometry_msgs::msg::Point p3 = autoware_utils::createPoint(-10.0, 0.0, 0.0);
+    EXPECT_DOUBLE_EQ(calculateCurvature(p1, p2, p3), 0.2);
+  }
+
+  // Give same points
+  {
+    geometry_msgs::msg::Point p1 = autoware_utils::createPoint(0.0, 0.0, 0.0);
+    geometry_msgs::msg::Point p2 = autoware_utils::createPoint(1.0, 0.0, 0.0);
+    ASSERT_ANY_THROW(calculateCurvature(p1, p1, p1));
+    ASSERT_ANY_THROW(calculateCurvature(p1, p1, p2));
+    ASSERT_ANY_THROW(calculateCurvature(p1, p2, p1));
+    ASSERT_ANY_THROW(calculateCurvature(p1, p2, p2));
+  }
+}
