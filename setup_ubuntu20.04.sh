20abfade800032d74b8121b59eebb3cec65a9ab5
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-05-31 10:09:49 +0900

    Fix shellcheck warnings of setup_ubuntu20.04.sh (#119)
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- setup_ubuntu20.04.sh
+++ setup_ubuntu20.04.sh
@@ -5,15 +5,19 @@ while getopts c OPT
 do
   case $OPT in
     "c" ) noninteractive=1
-          echo "run setup with a noninteractive mode";;
+      echo "run setup with a noninteractive mode"
+      ;;
+    "*" )
+      ;;
   esac
 done
 
+# shellcheck disable=SC2046,SC2086,SC2164
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # Prevent root execution
 if [ $noninteractive -eq 0 ]; then
-  if [ $(id -u) -eq 0 ]; then
+  if [ "$(id -u)" -eq 0 ]; then
     echo "Do not run setup as root!" >&2
     exit 1
   fi
@@ -23,11 +27,11 @@ if [ $noninteractive -eq 1 ]; then
   answer="y"
 else
   echo "Setting up the pre-build environment for AutowareArchitectureProposal.IV can take up to 45 minutes."
-  read -p ">  Are you sure you want to run setup? [y/N] " answer
+  read -rp ">  Are you sure you want to run setup? [y/N] " answer
 fi
 
 if [[ $answer = [cC] ]]; then
-  if !(command -v cowsay  > /dev/null 2>&1); then
+  if ! (command -v cowsay  > /dev/null 2>&1); then
     sudo apt install -y cowsay
   fi
 
@@ -36,17 +40,18 @@ fi
 
 case $answer in
   [yY]* )
-    if !(command -v ansible-playbook  > /dev/null 2>&1); then
+    if ! (command -v ansible-playbook  > /dev/null 2>&1); then
       sudo apt install -y ansible
     fi
 
-    cd $SCRIPT_DIR/ansible
+    cd "$SCRIPT_DIR/ansible" || exit
     if [ $noninteractive -eq 1 ]; then
-      ansible-playbook -i localhost, $SCRIPT_DIR/ansible/localhost-setup-ubuntu20.04-devpc.yml -i $SCRIPT_DIR/inventories/local-dev.ini -e AUTOWARE_DIR=$SCRIPT_DIR --extra-vars "yn_gpu=y"
+      ansible-playbook -i localhost, "$SCRIPT_DIR/ansible/localhost-setup-ubuntu20.04-devpc.yml" -i "$SCRIPT_DIR/inventories/local-dev.ini -e AUTOWARE_DIR=$SCRIPT_DIR" --extra-vars "yn_gpu=y"
     else
-      ansible-playbook -i localhost, $SCRIPT_DIR/ansible/localhost-setup-ubuntu20.04-devpc.yml -i $SCRIPT_DIR/inventories/local-dev.ini -e AUTOWARE_DIR=$SCRIPT_DIR --ask-become-pass
+      ansible-playbook -i localhost, "$SCRIPT_DIR/ansible/localhost-setup-ubuntu20.04-devpc.yml" -i "$SCRIPT_DIR/inventories/local-dev.ini -e AUTOWARE_DIR=$SCRIPT_DIR" --ask-become-pass
     fi
 
+    # shellcheck disable=SC2181
     if [ "$?" = "0" ]; then
       echo -e "\e[32mComplete \e[0m"
       exit 0
