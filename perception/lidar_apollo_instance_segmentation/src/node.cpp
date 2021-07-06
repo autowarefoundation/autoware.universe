814fccc2ed235bd43c14bf5e8dfbbb37b9039581
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-07-06 17:11:59 +0900

    [lidar_apollo_instance_segmentation]: Change subscriber queue size(5 to 1) (#1570)
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- perception/lidar_apollo_instance_segmentation/src/node.cpp
+++ perception/lidar_apollo_instance_segmentation/src/node.cpp
@@ -24,7 +24,7 @@ LidarInstanceSegmentationNode::LidarInstanceSegmentationNode(
   debugger_ptr_ = std::make_shared<Debugger>(this);
   pointcloud_sub_ =
     this->create_subscription<sensor_msgs::msg::PointCloud2>(
-    "input/pointcloud", rclcpp::SensorDataQoS(),
+    "input/pointcloud", rclcpp::SensorDataQoS().keep_last(1),
     std::bind(&LidarInstanceSegmentationNode::pointCloudCallback, this, _1));
   dynamic_objects_pub_ =
     this->create_publisher<autoware_perception_msgs::msg::DynamicObjectWithFeatureArray>(
