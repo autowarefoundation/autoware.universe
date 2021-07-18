31ba1d4b1b59ede640f3b48a983ef2826f879635
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-07-19 00:31:22 +0900

    Update repos2workspace.py (#173)
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- scripts/repos2workspace.py
+++ scripts/repos2workspace.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
 import argparse
-import yaml
 import json
-import sys
 from pathlib import Path
+import sys
+
+import yaml
 
 
-def repos2workspace(repos_file, output_file):
+def repos2workspace(repos_file: Path, output_file: Path):
     with open(repos_file, 'r') as f:
         repos = yaml.load(f, Loader=yaml.SafeLoader)
 
     paths = [f'src/{path}' for path in repos['repositories']]
-    folders = [{'path': '.'}]
+    folders = [{'path': path} for path in paths]
+    folders += [{'path': '.'}]
 
     workspace = {
         'folders': folders,
-        'settings': {'git.ignoredRepositories': ['.'], 'git.scanRepositories': paths},
     }
 
     with open(output_file, 'w') as f:
@@ -26,12 +27,12 @@ def repos2workspace(repos_file, output_file):
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('repos_file', type=Path)
-    parser.add_argument('-o', '--output', dest='output_file',
-                        type=Path, default=None)
+    parser.add_argument('-o', '--output', dest='output_file', type=Path, default=None)
     ns = parser.parse_args(args)
 
     if not ns.output_file:
-        ns.output_file = f'{ns.repos_file.absolute().parent.name}.code-workspace'
+        parent_dir = ns.repos_file.absolute().parent
+        ns.output_file = parent_dir / f'{parent_dir.name}.code-workspace'
 
     repos2workspace(ns.repos_file, ns.output_file)
 
