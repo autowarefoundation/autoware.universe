4e1eae4c26c50ce3cca859d442ffe80fa6dbfacd
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-08-15 14:13:39 +0900

    Fix compiler warnings (#1837)
    * Fix -Wunused-private-field
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Wunused-variable
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Wformat-security
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Winvalid-constexpr
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Wdelete-non-abstract-non-virtual-dtor
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Wdelete-abstract-non-virtual-dtor
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Winconsistent-missing-override
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Wrange-loop-construct
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix "invalid application of 'sizeof' to an incomplete type"
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Ignore -Wgnu-anonymous-struct and -Wnested-anon-types
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix lint
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Ignore -Wno-deprecated-declarations in CUDA-related packages
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix mistake
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Fix -Wunused-parameter
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- common/autoware_utils/include/autoware_utils/math/normalization.hpp
+++ common/autoware_utils/include/autoware_utils/math/normalization.hpp
@@ -21,7 +21,7 @@
 
 namespace autoware_utils
 {
-constexpr double normalizeDegree(const double deg, const double min_deg = -180)
+inline double normalizeDegree(const double deg, const double min_deg = -180)
 {
   const auto max_deg = min_deg + 360.0;
 
@@ -33,7 +33,7 @@ constexpr double normalizeDegree(const double deg, const double min_deg = -180)
   }
 }
 
-constexpr double normalizeRadian(const double rad, const double min_rad = -pi)
+inline double normalizeRadian(const double rad, const double min_rad = -pi)
 {
   const auto max_rad = min_rad + 2 * pi;
 
