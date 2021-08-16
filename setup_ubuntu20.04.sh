ec9f1ea8445ac53af67202866266dd385664b998
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-08-16 17:01:24 +0900

    Simplify ansible files (#122)
    * Simplify ansible files
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Add build-pr.yml
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- setup_ubuntu20.04.sh
+++ setup_ubuntu20.04.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
+
+# Parse args
 noninteractive=0
 while getopts c OPT
 do
@@ -12,56 +15,55 @@ do
   esac
 done
 
-# shellcheck disable=SC2046,SC2086,SC2164
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-# Prevent root execution
+# Confirm to start installation
 if [ $noninteractive -eq 0 ]; then
+  # Prevent root execution in interactive mode
   if [ "$(id -u)" -eq 0 ]; then
     echo "Do not run setup as root!" >&2
     exit 1
   fi
-fi
 
-if [ $noninteractive -eq 1 ]; then
-  answer="y"
-else
-  echo "Setting up the pre-build environment for AutowareArchitectureProposal.IV can take up to 45 minutes."
+  echo "Setting up the build environment take up to 45 minutes."
   read -rp ">  Are you sure you want to run setup? [y/N] " answer
+else
+  answer="y"
 fi
 
+# Use "Super Cow Powers"!
 if [[ $answer = [cC] ]]; then
   if ! (command -v cowsay  > /dev/null 2>&1); then
-    sudo apt install -y cowsay
+    sudo apt-get install -y cowsay
   fi
 
-  export answer=y
+  answer="y"
 fi
 
+# Run ansible if confirmed
 case $answer in
   [yY]* )
+    # Install ansible
     if ! (command -v ansible-playbook  > /dev/null 2>&1); then
-      sudo apt install -y ansible
+      sudo apt-get install -y ansible
     fi
 
-    cd "$SCRIPT_DIR/ansible" || exit
-    if [ $noninteractive -eq 1 ]; then
-      ansible-playbook -i localhost, "$SCRIPT_DIR/ansible/localhost-setup-ubuntu20.04-devpc.yml" -i "$SCRIPT_DIR/inventories/local-dev.ini" -e AUTOWARE_DIR="$SCRIPT_DIR" --extra-vars "yn_gpu=y"
+    # Set options
+    if [ $noninteractive -eq 0 ]; then
+      options=("--ask-become-pass")
     else
-      ansible-playbook -i localhost, "$SCRIPT_DIR/ansible/localhost-setup-ubuntu20.04-devpc.yml" -i "$SCRIPT_DIR/inventories/local-dev.ini" -e AUTOWARE_DIR="$SCRIPT_DIR" --ask-become-pass
+      options=("--extra-vars" "yn_gpu=y")
     fi
 
-    # shellcheck disable=SC2181
-    if [ "$?" = "0" ]; then
-      echo -e "\e[32mComplete \e[0m"
+    # Run ansible
+    if ansible-playbook "$SCRIPT_DIR/ansible/playbook.yml" -e WORKSPACE_ROOT="$SCRIPT_DIR" "${options[@]}"; then
+      echo -e "\e[32mCompleted.\e[0m"
       exit 0
     else
-      echo -e "\e[31mFailed \e[0m"
+      echo -e "\e[31mFailed.\e[0m"
       exit 1
     fi
     ;;
   * )
-    echo -e "\e[33mCancelled \e[0m"
+    echo -e "\e[33mCancelled.\e[0m"
     exit 1
     ;;
 esac
