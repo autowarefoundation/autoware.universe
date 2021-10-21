dc93ca4ae3634bfbb06613e1f5b4dd983a590141
Daisuke Nishimatsu <42202095+wep21@users.noreply.github.com>
2021-10-21 19:27:49 +0900

    Feature/porting control performance analysis (#1671)
    * Feature/control performance analysis (#1212)
    
    * First commit of kinematic_controller
    
    * First commit.
    
    * second commit
    
    * Just setup updated Autoware.
    
    * changed package name.
    
    * Messages variables are created.
    
    * Writing subscribers and publishers.
    
    * Writing subscribers. Traj, pose and control_values are read into the node.
    
    * Computing control performance variables.
    
    * Computing control performance variables.
    
    * Current velocity subscribed.
    
    * Acceleration performance is computed.
    
    * Publishing completed. Will start rqt_multiplot
    
    * Publishing completed. Decay rate fixed. Will start rqt_multiplot
    
    * rqt_multiplot first configuration.
    
    * Update pure_pursuit.launch
    
    * Update pure_pursuit.launch
    
    * Update .gitignore
    
    * Update Error.msg
    
    * Update control_performance_utils.cpp
    
    * Update ErrorStamped.msg
    
    * Update package.xml
    
    * rqt_multiplot first configuration.
    
    * Update controller_performance_core.cpp
    
    * Update controller_performance_core.cpp
    
    * Update CMakeLists.txt
    
    * Update control_performance_analysis_param.yaml
    
    * EPS is added for value_decay_rate.
    
    * There is a  bug.
    
    * Bug removed.
    
    * Bug removed.
    
    * lateral_acceleration is published.
    
    * Interpolated pose is added.
    
    * Update controller_performance_node.cpp
    
    * find Curve index bug is removed.
    
    * dot product on projection is updated.
    
    * Vehicle measured steering is included in the node and rqt_graph.
    
    * Review will be requested.
    
    * After the test:
    Three point curvature module is added. Std:vector will be fixed.
    
    * After the test:
    Curvature plot is added.
    
    * After the test:
    Fine tuned.
    
    * rqt curvature is modified.
    
    * Pure pursuit curvature is implemented and tested. Results are fine.
    
    * addressed some code review issues. Will replace get_pose.
    
    * GetPose is removed.
    
    * All the core review issues have been addressed.
    
    * Rename files
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Porting control performance analysis
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Apply lint
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Add boost dependency for optional
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Remove confusing abbreviation
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Fix dependency in packages.xml
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Add missing new line
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * Add comment for eigen macro
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    * pre-commit fixes
    
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>
    
    Co-authored-by: Ali BOYALI <boyali@users.noreply.github.com>

---
+++ control/control_performance_analysis/include/control_performance_analysis/control_performance_analysis_node.hpp
@@ -0,0 +1,104 @@
+// Copyright 2021 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONTROL_PERFORMANCE_ANALYSIS__CONTROL_PERFORMANCE_ANALYSIS_NODE_HPP_
+#define CONTROL_PERFORMANCE_ANALYSIS__CONTROL_PERFORMANCE_ANALYSIS_NODE_HPP_
+
+#include <memory>
+
+#include "boost/optional.hpp"
+
+#include "autoware_control_msgs/msg/control_command_stamped.hpp"
+#include "autoware_planning_msgs/msg/trajectory.hpp"
+#include "autoware_utils/ros/self_pose_listener.hpp"
+#include "autoware_vehicle_msgs/msg/steering.hpp"
+#include "control_performance_analysis/msg/error_stamped.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include "control_performance_analysis/control_performance_analysis_core.hpp"
+
+namespace control_performance_analysis
+{
+using autoware_control_msgs::msg::ControlCommandStamped;
+using autoware_planning_msgs::msg::Trajectory;
+using autoware_vehicle_msgs::msg::Steering;
+using control_performance_analysis::msg::ErrorStamped;
+using geometry_msgs::msg::PoseStamped;
+using geometry_msgs::msg::TwistStamped;
+
+// Parameters Struct
+struct Param
+{
+  // Global parameters
+  double wheel_base;
+  double curvature_interval_length;
+
+  // Control Method Parameters
+  double control_period;
+};
+
+class ControlPerformanceAnalysisNode : public rclcpp::Node
+{
+public:
+  explicit ControlPerformanceAnalysisNode(const rclcpp::NodeOptions & node_options);
+
+private:
+  // Subscribers and Local Variable Assignment
+  rclcpp::Subscription<Trajectory>::SharedPtr sub_trajectory_;        // subscribe to trajectory
+  rclcpp::Subscription<ControlCommandStamped>::SharedPtr
+    sub_control_steering_;  // subscribe to steering control value
+  rclcpp::Subscription<TwistStamped>::SharedPtr sub_velocity_;          // subscribe to velocity
+  rclcpp::Subscription<Steering>::SharedPtr sub_vehicle_steering_;
+
+  // Self Pose listener.
+  autoware_utils::SelfPoseListener self_pose_listener_{this};  // subscribe to pose listener.
+
+  // Publishers
+  rclcpp::Publisher<ErrorStamped>::SharedPtr pub_error_msg_;  // publish error message
+
+  // Node Methods
+  bool isDataReady() const;  // check if data arrive
+  static bool isValidTrajectory(const Trajectory & traj);
+  boost::optional<TargetPerformanceMsgVars> computeTargetPerformanceMsgVars() const;
+
+  // Callback Methods
+  void onTrajectory(const Trajectory::ConstSharedPtr msg);
+  void publishErrorMsg(const TargetPerformanceMsgVars & control_performance_vars);
+  void onControlRaw(const ControlCommandStamped::ConstSharedPtr control_msg);
+  void onVecSteeringMeasured(const Steering::ConstSharedPtr meas_steer_msg);
+  void onVelocity(const TwistStamped::ConstSharedPtr msg);
+
+  // Timer - To Publish In Control Period
+  rclcpp::TimerBase::SharedPtr timer_publish_;
+  void onTimer();
+
+  // Parameters
+  Param param_{};  // wheelbase, control period and feedback coefficients.
+  TargetPerformanceMsgVars target_error_vars_{};
+
+  // Subscriber Parameters
+  Trajectory::ConstSharedPtr current_trajectory_ptr_;  // ConstPtr to local traj.
+  ControlCommandStamped::ConstSharedPtr current_control_msg_ptr_;
+  Steering::ConstSharedPtr current_vec_steering_msg_ptr_;
+  TwistStamped::ConstSharedPtr current_velocity_ptr_;
+  PoseStamped::ConstSharedPtr current_pose_;  // pose of the vehicle, x, y, heading
+
+  // Algorithm
+  std::unique_ptr<ControlPerformanceAnalysisCore> control_performance_core_ptr_;
+};
+}  // namespace control_performance_analysis
+
+#endif  // CONTROL_PERFORMANCE_ANALYSIS__CONTROL_PERFORMANCE_ANALYSIS_NODE_HPP_
