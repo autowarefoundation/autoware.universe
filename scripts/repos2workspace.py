2d489ecae38c68b2fc72d701e15883ef2960f9b3
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-11-02 21:10:44 +0900

    Change formatter to black (#340)
    * Update pre-commit hooks versions
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Update pre-commit settings
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Apply Black
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- scripts/repos2workspace.py
+++ scripts/repos2workspace.py
@@ -9,33 +9,33 @@ import yaml
 
 
 def repos2workspace(repos_file: Path, output_file: Path):
-    with open(repos_file, 'r') as f:
+    with open(repos_file, "r") as f:
         repos = yaml.load(f, Loader=yaml.SafeLoader)
 
-    paths = [f'src/{path}' for path in repos['repositories']]
-    folders = [{'path': path} for path in paths]
-    folders += [{'path': '.'}]
+    paths = [f"src/{path}" for path in repos["repositories"]]
+    folders = [{"path": path} for path in paths]
+    folders += [{"path": "."}]
 
     workspace = {
-        'folders': folders,
+        "folders": folders,
     }
 
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         json.dump(workspace, f, indent=2, sort_keys=False)
 
 
 def main(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('repos_file', type=Path)
-    parser.add_argument('-o', '--output', dest='output_file', type=Path, default=None)
+    parser.add_argument("repos_file", type=Path)
+    parser.add_argument("-o", "--output", dest="output_file", type=Path, default=None)
     ns = parser.parse_args(args)
 
     if not ns.output_file:
         parent_dir = ns.repos_file.absolute().parent
-        ns.output_file = parent_dir / f'{parent_dir.name}.code-workspace'
+        ns.output_file = parent_dir / f"{parent_dir.name}.code-workspace"
 
     repos2workspace(ns.repos_file, ns.output_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
