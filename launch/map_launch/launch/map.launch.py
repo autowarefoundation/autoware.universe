d4b7166f4b6c3acdb523cdb8b46c1917ba751836
Kenji Miyake <kenji.miyake@tier4.jp>
2021-11-02 19:37:45 +0900

    Change formatter to black (#488)
    * Update pre-commit settings
    
    * Apply Black
    
    * Replace ament_lint_common with autoware_lint_common
    
    * Update build_depends.repos
    
    * Fix build_depends

--- launch/map_launch/launch/map.launch.py
+++ launch/map_launch/launch/map.launch.py
@@ -27,113 +27,115 @@ from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     map_hash_generator = Node(
-        package='map_loader',
-        executable='map_hash_generator',
-        name='map_hash_generator',
+        package="map_loader",
+        executable="map_hash_generator",
+        name="map_hash_generator",
         parameters=[
             {
-                'lanelet2_map_path': LaunchConfiguration('lanelet2_map_path'),
+                "lanelet2_map_path": LaunchConfiguration("lanelet2_map_path"),
             }
         ],
     )
 
     lanelet2_map_loader = ComposableNode(
-        package='map_loader',
-        plugin='Lanelet2MapLoaderNode',
-        name='lanelet2_map_loader',
-        remappings=[('output/lanelet2_map', 'vector_map')],
+        package="map_loader",
+        plugin="Lanelet2MapLoaderNode",
+        name="lanelet2_map_loader",
+        remappings=[("output/lanelet2_map", "vector_map")],
         parameters=[
             {
-                'center_line_resolution': 5.0,
-                'lanelet2_map_path': LaunchConfiguration('lanelet2_map_path'),
+                "center_line_resolution": 5.0,
+                "lanelet2_map_path": LaunchConfiguration("lanelet2_map_path"),
             }
         ],
-        extra_arguments=[{
-            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
-        }],
+        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
     )
 
     lanelet2_map_visualization = ComposableNode(
-        package='map_loader',
-        plugin='Lanelet2MapVisualizationNode',
-        name='lanelet2_map_visualization',
-        remappings=[('input/lanelet2_map', 'vector_map'),
-                    ('output/lanelet2_map_marker', 'vector_map_marker')],
-        extra_arguments=[{
-            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
-        }],
+        package="map_loader",
+        plugin="Lanelet2MapVisualizationNode",
+        name="lanelet2_map_visualization",
+        remappings=[
+            ("input/lanelet2_map", "vector_map"),
+            ("output/lanelet2_map_marker", "vector_map_marker"),
+        ],
+        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
     )
 
     pointcloud_map_loader = ComposableNode(
-        package='map_loader',
-        plugin='PointCloudMapLoaderNode',
-        name='pointcloud_map_loader',
-        remappings=[('output/pointcloud_map', 'pointcloud_map')],
+        package="map_loader",
+        plugin="PointCloudMapLoaderNode",
+        name="pointcloud_map_loader",
+        remappings=[("output/pointcloud_map", "pointcloud_map")],
         parameters=[
-            {
-                'pcd_paths_or_directory': ['[', LaunchConfiguration('pointcloud_map_path'), ']']
-            }
+            {"pcd_paths_or_directory": ["[", LaunchConfiguration("pointcloud_map_path"), "]"]}
         ],
-        extra_arguments=[{
-            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
-        }],
+        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
     )
 
     map_tf_generator = ComposableNode(
-        package='map_tf_generator',
-        plugin='MapTFGeneratorNode',
-        name='map_tf_generator',
+        package="map_tf_generator",
+        plugin="MapTFGeneratorNode",
+        name="map_tf_generator",
         parameters=[
             {
-                'map_frame': 'map',
-                'viewer_frame': 'viewer',
+                "map_frame": "map",
+                "viewer_frame": "viewer",
             }
         ],
-        extra_arguments=[{
-            'use_intra_process_comms': LaunchConfiguration('use_intra_process')
-        }],
+        extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
     )
 
     container = ComposableNodeContainer(
-        name='map_container',
-        namespace='',
-        package='rclcpp_components',
-        executable=LaunchConfiguration('container_executable'),
+        name="map_container",
+        namespace="",
+        package="rclcpp_components",
+        executable=LaunchConfiguration("container_executable"),
         composable_node_descriptions=[
             lanelet2_map_loader,
             lanelet2_map_visualization,
             pointcloud_map_loader,
             map_tf_generator,
         ],
-        output='screen',
+        output="screen",
     )
 
     def add_launch_arg(name: str, default_value=None, description=None):
         return DeclareLaunchArgument(name, default_value=default_value, description=description)
 
-    return launch.LaunchDescription([
-        add_launch_arg('map_path', '', 'path to map directory'),
-        add_launch_arg('lanelet2_map_path', [
-                       LaunchConfiguration('map_path'), '/lanelet2_map.osm'],
-                       'path to lanelet2 map file'),
-        add_launch_arg('pointcloud_map_path', [
-                       LaunchConfiguration('map_path'), '/pointcloud_map.pcd'],
-                       'path to pointcloud map file'),
-        add_launch_arg('use_intra_process', 'false', 'use ROS2 component container communication'),
-        add_launch_arg('use_multithread', 'false', 'use multithread'),
-        SetLaunchConfiguration(
-            'container_executable',
-            'component_container',
-            condition=UnlessCondition(LaunchConfiguration('use_multithread'))
-        ),
-        SetLaunchConfiguration(
-            'container_executable',
-            'component_container_mt',
-            condition=IfCondition(LaunchConfiguration('use_multithread'))
-        ),
-        GroupAction([
-            PushRosNamespace('map'),
-            container,
-            map_hash_generator,
-        ])
-    ])
+    return launch.LaunchDescription(
+        [
+            add_launch_arg("map_path", "", "path to map directory"),
+            add_launch_arg(
+                "lanelet2_map_path",
+                [LaunchConfiguration("map_path"), "/lanelet2_map.osm"],
+                "path to lanelet2 map file",
+            ),
+            add_launch_arg(
+                "pointcloud_map_path",
+                [LaunchConfiguration("map_path"), "/pointcloud_map.pcd"],
+                "path to pointcloud map file",
+            ),
+            add_launch_arg(
+                "use_intra_process", "false", "use ROS2 component container communication"
+            ),
+            add_launch_arg("use_multithread", "false", "use multithread"),
+            SetLaunchConfiguration(
+                "container_executable",
+                "component_container",
+                condition=UnlessCondition(LaunchConfiguration("use_multithread")),
+            ),
+            SetLaunchConfiguration(
+                "container_executable",
+                "component_container_mt",
+                condition=IfCondition(LaunchConfiguration("use_multithread")),
+            ),
+            GroupAction(
+                [
+                    PushRosNamespace("map"),
+                    container,
+                    map_hash_generator,
+                ]
+            ),
+        ]
+    )
