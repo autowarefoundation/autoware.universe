4ff26371f1232d7f9a2c0a397db6a74fb9065ffd
Kosuke Murakami <kosuke.murakami@tier4.jp>
2021-11-12 19:02:37 +0900

    port part of external_cmd_selector (#497)
    * port part of external_cmd_selector
    
    Signed-off-by: Kosuke Murakami <kosuke.murakami@tier4.jp>
    
    * add convert function
    
    Signed-off-by: kosuke murakami <kosuke.murakami@tier4.jp>
    
    * port signal
    
    Signed-off-by: kosuke murakami <kosuke.murakami@tier4.jp>
    
    * Update control/external_cmd_selector/include/external_cmd_selector/external_cmd_selector_node.hpp
    
    Co-authored-by: tkimura4 <tomoya.kimura@tier4.jp>

--- control/external_cmd_selector/include/external_cmd_selector/external_cmd_selector_node.hpp
+++ control/external_cmd_selector/include/external_cmd_selector/external_cmd_selector_node.hpp
@@ -19,6 +19,9 @@
 #include <diagnostic_updater/update_functions.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include <autoware_auto_vehicle_msgs/msg/gear_command.hpp>
+#include <autoware_auto_vehicle_msgs/msg/hazard_lights_command.hpp>
+#include <autoware_auto_vehicle_msgs/msg/turn_indicators_command.hpp>
 #include <autoware_control_msgs/msg/external_command_selector_mode.hpp>
 #include <autoware_control_msgs/srv/external_command_select.hpp>
 #include <autoware_external_api_msgs/msg/control_command_stamped.hpp>
@@ -38,8 +41,9 @@ public:
 private:
   using CommandSourceSelect = autoware_control_msgs::srv::ExternalCommandSelect;
   using CommandSourceMode = autoware_control_msgs::msg::ExternalCommandSelectorMode;
-  using InternalGearShift = autoware_vehicle_msgs::msg::ShiftStamped;
-  using InternalTurnSignal = autoware_vehicle_msgs::msg::TurnSignal;
+  using InternalGearShift = autoware_auto_vehicle_msgs::msg::GearCommand;
+  using InternalTurnSignal = autoware_auto_vehicle_msgs::msg::TurnIndicatorsCommand;
+  using InternalHazardSignal = autoware_auto_vehicle_msgs::msg::HazardLightsCommand;
   using InternalHeartbeat = autoware_external_api_msgs::msg::Heartbeat;
   using ExternalControlCommand = autoware_external_api_msgs::msg::ControlCommandStamped;
   using ExternalGearShift = autoware_external_api_msgs::msg::GearShiftStamped;
@@ -55,6 +59,7 @@ private:
   rclcpp::Publisher<ExternalControlCommand>::SharedPtr pub_control_cmd_;
   rclcpp::Publisher<InternalGearShift>::SharedPtr pub_shift_cmd_;
   rclcpp::Publisher<InternalTurnSignal>::SharedPtr pub_turn_signal_cmd_;
+  rclcpp::Publisher<InternalHazardSignal>::SharedPtr pub_hazard_signal_cmd_;
   rclcpp::Publisher<InternalHeartbeat>::SharedPtr pub_heartbeat_;
 
   // Subscriber
@@ -93,7 +98,6 @@ private:
 
   // Converter
   static InternalGearShift convert(const ExternalGearShift & command);
-  static InternalTurnSignal convert(const ExternalTurnSignal & command);
   static InternalHeartbeat convert(const ExternalHeartbeat & command);
 
   // Diagnostics Updater
--- control/external_cmd_selector/src/external_cmd_selector/external_cmd_selector_node.cpp
+++ control/external_cmd_selector/src/external_cmd_selector/external_cmd_selector_node.cpp
@@ -14,6 +14,8 @@
 
 #include "external_cmd_selector/external_cmd_selector_node.hpp"
 
+#include <autoware_iv_auto_msgs_converter/autoware_iv_auto_msgs_converter.hpp>
+
 #include <chrono>
 #include <memory>
 #include <string>
@@ -127,7 +129,9 @@ void ExternalCmdSelector::onLocalTurnSignalCmd(const ExternalTurnSignal::ConstSh
   if (current_selector_mode_.data != CommandSourceMode::LOCAL) {
     return;
   }
-  pub_turn_signal_cmd_->publish(convert(*msg));
+  auto light_signal = autoware_iv_auto_msgs_converter::convert(*msg);
+  pub_turn_signal_cmd_->publish(light_signal.turn_signal);
+  pub_hazard_signal_cmd_->publish(light_signal.hazard_signal);
 }
 
 void ExternalCmdSelector::onLocalHeartbeat(const ExternalHeartbeat::ConstSharedPtr msg)
@@ -159,7 +163,9 @@ void ExternalCmdSelector::onRemoteTurnSignalCmd(const ExternalTurnSignal::ConstS
   if (current_selector_mode_.data != CommandSourceMode::REMOTE) {
     return;
   }
-  pub_turn_signal_cmd_->publish(convert(*msg));
+  auto light_signal = autoware_iv_auto_msgs_converter::convert(*msg);
+  pub_turn_signal_cmd_->publish(light_signal.turn_signal);
+  pub_hazard_signal_cmd_->publish(light_signal.hazard_signal);
 }
 
 void ExternalCmdSelector::onRemoteHeartbeat(const ExternalHeartbeat::ConstSharedPtr msg)
@@ -185,21 +191,7 @@ void ExternalCmdSelector::onTimer() { pub_current_selector_mode_->publish(curren
 ExternalCmdSelector::InternalGearShift ExternalCmdSelector::convert(
   const ExternalGearShift & command)
 {
-  InternalGearShift message;
-  message.header.stamp = command.stamp;
-  message.header.frame_id = "base_link";  // dummy
-  message.shift.data = command.gear_shift.data;
-  return message;
-}
-
-ExternalCmdSelector::InternalTurnSignal ExternalCmdSelector::convert(
-  const ExternalTurnSignal & command)
-{
-  InternalTurnSignal message;
-  message.header.stamp = command.stamp;
-  message.header.frame_id = "base_link";  // dummy
-  message.data = command.turn_signal.data;
-  return message;
+  return autoware_iv_auto_msgs_converter::convert(command);
 }
 
 ExternalCmdSelector::InternalHeartbeat ExternalCmdSelector::convert(
