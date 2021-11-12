93dc74ea0482939d940aad643a781005cbf3a5f1
Maxime CLEMENT <78338830+maxime-clem@users.noreply.github.com>
2021-11-12 14:14:23 +0900

    Port parking planner packages from .Auto (#600)
    * Copy code of 'vehicle_constants_manager'
    
    * Fix vehicle_constants_manager for ROS galactic
    
    * Rm .iv costmap_generator freespace_planner freespace_planning_aglorihtms
    
    * Add astar_search (from .Auto)
    
    * Copy freespace_planner from .Auto
    
    * Update freespace_planner for .IV
    
    * Copy costmap_generator from .Auto
    
    * Copy and update had_map_utils from .Auto
    
    * Update costmap_generator
    
    * Copy costmap_generator_nodes
    
    * Update costmap_generator_nodes
    
    * Comment out all tests
    
    * Move vehicle_constant_managers to tmp_autoware_auto_dependencies
    
    * ignore pre-commit for back-ported packages
    
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>
    
    * ignore testing
    
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>
    
    Co-authored-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- planning/costmap_generator/src/costmap_generator/object_map_utils.cpp
+++ planning/costmap_generator/src/costmap_generator/object_map_utils.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Tier IV, Inc.
+// Copyright 2021 The Autoware Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Co-developed by Tier IV, Inc. and Robotec.AI sp. z o.o.
 
 /*
  * Copyright 2018-2019 Autoware Foundation. All rights reserved.
@@ -37,33 +39,12 @@
 
 namespace object_map
 {
-void PublishGridMap(
-  const grid_map::GridMap & in_gridmap,
-  const rclcpp::Publisher<grid_map_msgs::msg::GridMap>::SharedPtr in_publisher)
-{
-  auto message = grid_map::GridMapRosConverter::toMessage(in_gridmap);
-  in_publisher->publish(*message);
-}
-
-void PublishOccupancyGrid(
-  const grid_map::GridMap & in_gridmap,
-  const rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr in_publisher,
-  const std::string & in_layer, double in_min_value, double in_max_value, double in_height)
-{
-  nav_msgs::msg::OccupancyGrid message;
-  grid_map::GridMapRosConverter::toOccupancyGrid(
-    in_gridmap, in_layer, in_min_value, in_max_value, message);
-  message.info.origin.position.z = in_height;
-  in_publisher->publish(message);
-}
-
-void FillPolygonAreas(
+void fillPolygonAreas(
   grid_map::GridMap & out_grid_map,
   const std::vector<std::vector<geometry_msgs::msg::Point>> & in_area_points,
   const std::string & in_grid_layer_name, const int in_layer_background_value,
   const int in_layer_min_value, const int in_fill_color, const int in_layer_max_value,
-  const std::string & in_tf_target_frame, const std::string & in_tf_source_frame,
-  const tf2_ros::Buffer & in_tf_buffer)
+  const geometry_msgs::msg::TransformStamped & in_transform)
 {
   if (!out_grid_map.exists(in_grid_layer_name)) {
     out_grid_map.add(in_grid_layer_name);
@@ -76,10 +57,6 @@ void FillPolygonAreas(
     original_image);
 
   cv::Mat merged_filled_image = original_image.clone();
-
-  geometry_msgs::msg::TransformStamped transform;
-  transform = in_tf_buffer.lookupTransform(
-    in_tf_target_frame, in_tf_source_frame, rclcpp::Time(0), rclcpp::Duration::from_seconds(1.0));
 
   // calculate out_grid_map position
   grid_map::Position map_pos = out_grid_map.getPosition();
@@ -94,14 +71,14 @@ void FillPolygonAreas(
       geometry_msgs::msg::Point transformed_point;
       geometry_msgs::msg::PointStamped output_stamped, input_stamped;
       input_stamped.point = p;
-      tf2::doTransform(input_stamped, output_stamped, transform);
+      tf2::doTransform(input_stamped, output_stamped, in_transform);
       transformed_point = output_stamped.point;
 
       // coordinate conversion for cv image
       const double cv_x = (out_grid_map.getLength().y() - origin_y_offset - transformed_point.y) /
-                          out_grid_map.getResolution();
+        out_grid_map.getResolution();
       const double cv_y = (out_grid_map.getLength().x() - origin_x_offset - transformed_point.x) /
-                          out_grid_map.getResolution();
+        out_grid_map.getResolution();
       cv_polygon.emplace_back(cv_x, cv_y);
     }
 
