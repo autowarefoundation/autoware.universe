39cf3626107090099fe33446eba3276e5862daec
Taichi Higashide <taichi.higashide@tier4.jp>
2021-11-16 19:43:57 +0900

    add tf2_geometry_msgs include to distorction_filter (#664)
    * add tf2_geometry_msgs include
    
    * add tf2_geometry_msgs pkg depend

--- sensing/pointcloud_preprocessor/include/pointcloud_preprocessor/distortion_corrector/distortion_corrector.hpp
+++ sensing/pointcloud_preprocessor/include/pointcloud_preprocessor/distortion_corrector/distortion_corrector.hpp
@@ -23,6 +23,7 @@
 
 #include <tf2/convert.h>
 #include <tf2/transform_datatypes.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 
