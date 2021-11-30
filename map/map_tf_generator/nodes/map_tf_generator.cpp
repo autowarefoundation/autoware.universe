ea3bcb37c9fd657de41439a528b6f872dfed1e5a
Takamasa Horibe <horibe.takamasa@gmail.com>
2021-11-30 13:58:17 +0900

    port map_tf_generator (#32)
    * port map_tf_generator
    
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>
    
    * add missing dependency
    
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>
    
    * fix pointor, tf_broadcaster, add compile option
    
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>
    
    * use ament_auto
    
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- map/map_tf_generator/nodes/map_tf_generator.cpp
+++ map/map_tf_generator/nodes/map_tf_generator.cpp
@@ -13,65 +13,83 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl_ros/point_cloud.h>
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/static_transform_broadcaster.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+#include <pcl_conversions/pcl_conversions.h>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
-std::string map_frame_ = "map";
-std::string viewer_frame_ = "viewer";
 
-void Callback(const PointCloud::ConstPtr & clouds)
+class MapTFGenerator : public rclcpp::Node
 {
-  const unsigned int sum = clouds->points.size();
-  double coordinate[3] = {0, 0, 0};
-  for (int i = 0; i < sum; i++) {
-    coordinate[0] += clouds->points[i].x;
-    coordinate[1] += clouds->points[i].y;
-    coordinate[2] += clouds->points[i].z;
+public:
+  MapTFGenerator() : Node("map_tf_generator")
+  {
+    map_frame_ = declare_parameter("map_frame", "map");
+    viewer_frame_ = declare_parameter("viewer_frame", "viewer");
+
+    sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
+      "pointcloud_map", rclcpp::QoS{1},
+      std::bind(&MapTFGenerator::Callback, this, std::placeholders::_1));
+
+    static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
   }
-  coordinate[0] = coordinate[0] / sum;
-  coordinate[1] = coordinate[1] / sum;
-  coordinate[2] = coordinate[2] / sum;
 
-  geometry_msgs::TransformStamped static_transformStamped;
-  static_transformStamped.header.stamp = ros::Time::now();
-  static_transformStamped.header.frame_id = map_frame_;
-  static_transformStamped.child_frame_id = viewer_frame_;
-  static_transformStamped.transform.translation.x = coordinate[0];
-  static_transformStamped.transform.translation.y = coordinate[1];
-  static_transformStamped.transform.translation.z = coordinate[2];
-  tf2::Quaternion quat;
-  quat.setRPY(0, 0, 0);
-  static_transformStamped.transform.rotation.x = quat.x();
-  static_transformStamped.transform.rotation.y = quat.y();
-  static_transformStamped.transform.rotation.z = quat.z();
-  static_transformStamped.transform.rotation.w = quat.w();
+private:
+  std::string map_frame_;
+  std::string viewer_frame_;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_;
 
-  static tf2_ros::StaticTransformBroadcaster static_broadcaster;
-  static_broadcaster.sendTransform(static_transformStamped);
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_broadcaster_;
 
-  ROS_INFO_STREAM(
-    "broadcast static tf. map_frame:" << map_frame_ << ", viewer_frame:" << viewer_frame_
-                                      << ", x:" << coordinate[0] << ", y:" << coordinate[1]
-                                      << ", z:" << coordinate[2]);
-}
+  void Callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr clouds_ros)
+  {
+    PointCloud clouds;
+    pcl::fromROSMsg<pcl::PointXYZ>(*clouds_ros, clouds);
+
+    const unsigned int sum = clouds.points.size();
+    double coordinate[3] = {0, 0, 0};
+    for (int i = 0; i < sum; i++) {
+      coordinate[0] += clouds.points[i].x;
+      coordinate[1] += clouds.points[i].y;
+      coordinate[2] += clouds.points[i].z;
+    }
+    coordinate[0] = coordinate[0] / sum;
+    coordinate[1] = coordinate[1] / sum;
+    coordinate[2] = coordinate[2] / sum;
+
+    geometry_msgs::msg::TransformStamped static_transformStamped;
+    static_transformStamped.header.stamp = get_clock()->now();
+    static_transformStamped.header.frame_id = map_frame_;
+    static_transformStamped.child_frame_id = viewer_frame_;
+    static_transformStamped.transform.translation.x = coordinate[0];
+    static_transformStamped.transform.translation.y = coordinate[1];
+    static_transformStamped.transform.translation.z = coordinate[2];
+    tf2::Quaternion quat;
+    quat.setRPY(0, 0, 0);
+    static_transformStamped.transform.rotation.x = quat.x();
+    static_transformStamped.transform.rotation.y = quat.y();
+    static_transformStamped.transform.rotation.z = quat.z();
+    static_transformStamped.transform.rotation.w = quat.w();
+
+    static_broadcaster_->sendTransform(static_transformStamped);
+
+    RCLCPP_INFO_STREAM(
+      get_logger(), "broadcast static tf. map_frame:"
+                      << map_frame_ << ", viewer_frame:" << viewer_frame_ << ", x:" << coordinate[0]
+                      << ", y:" << coordinate[1] << ", z:" << coordinate[2]);
+  }
+};
 
 int main(int argc, char ** argv)
 {
-  ros::init(argc, argv, "map_tf_generator");
-  ros::NodeHandle nh;
-  ros::NodeHandle nh_private("~");
-
-  nh_private.getParam("map_frame", map_frame_);
-  nh_private.getParam("viewer_frame", viewer_frame_);
-
-  ros::Subscriber sub = nh.subscribe<PointCloud>("pointcloud_map", 1, &Callback);
-
-  ros::spin();
-
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<MapTFGenerator>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
-};
+}
