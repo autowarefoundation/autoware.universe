7dad3567fcf0dfa1db0cdcab063c06d0ae1792c0
tanaka3 <ttatcoder@outlook.jp>
2022-02-07 20:14:09 +0900

    fix(behavior_velocity): fix stop dist
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

--- planning/behavior_velocity_planner/src/scene_module/occlusion_spot/occlusion_spot_utils.cpp
+++ planning/behavior_velocity_planner/src/scene_module/occlusion_spot/occlusion_spot_utils.cpp
@@ -320,15 +320,15 @@ PossibleCollisionInfo calculateCollisionPathPointFromOcclusionSpot(
   PossibleCollisionInfo pc;
   const double ttc = std::abs(arc_coord_occlusion_with_offset.distance / param.pedestrian_vel);
   SafeMotion sm = calculateSafeMotion(param.v, ttc, param.safety_time_buffer);
-  double stop_dist = arc_coord_occlusion_with_offset.length - sm.stop_dist;
+  double distance_to_stop = arc_coord_occlusion_with_offset.length - sm.stop_dist;
   const double eps = 1e-3;
-  if (stop_dist <= param.baselink_to_front) stop_dist = param.baselink_to_front + eps;
-  pc.arc_lane_dist_at_collision = {stop_dist, arc_coord_occlusion_with_offset.distance};
-  pc.obstacle_info.sm = {sm.safe_velocity, stop_dist};
+  if (distance_to_stop <= param.baselink_to_front) distance_to_stop = param.baselink_to_front + eps;
+  pc.arc_lane_dist_at_collision = {distance_to_stop, arc_coord_occlusion_with_offset.distance};
+  pc.obstacle_info.sm = sm;
   pc.obstacle_info.ttc = ttc;
   pc.obstacle_info.position = setPose(path_lanelet, arc_coord_occlusion).position;
   pc.obstacle_info.max_velocity = param.pedestrian_vel;
-  pc.collision_path_point.pose = setPose(path_lanelet, {stop_dist, 0.0});
+  pc.collision_path_point.pose = setPose(path_lanelet, {distance_to_stop, 0.0});
   pc.intersection_pose = setPose(path_lanelet, {arc_coord_occlusion.length, 0.0});
   return pc;
 }
