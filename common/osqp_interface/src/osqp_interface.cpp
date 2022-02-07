8f85fc15e7506eb75e823ffa3675aee0aee290a4
Takayuki Murooka <takayuki5168@gmail.com>
2022-02-07 12:16:31 +0900

    remove unnecessary includes
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- common/osqp_interface/src/osqp_interface.cpp
+++ common/osqp_interface/src/osqp_interface.cpp
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "rclcpp/rclcpp.hpp"
 #include "osqp_interface/osqp_interface.hpp"
 
 #include "osqp/osqp.h"
