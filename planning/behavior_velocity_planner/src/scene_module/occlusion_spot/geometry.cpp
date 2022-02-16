dd6e794f203df479c7c31c6a6950c0f2063370da
tanaka3 <ttatcoder@outlook.jp>
2022-02-16 10:42:02 +0900

    chore(behavior_velocity): better visualization
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

--- planning/behavior_velocity_planner/src/scene_module/occlusion_spot/geometry.cpp
+++ planning/behavior_velocity_planner/src/scene_module/occlusion_spot/geometry.cpp
@@ -59,7 +59,10 @@ void buildSlices(
    * +--------------------------+ - +---+(max_length,min_distance) = inner_polygons
    */
   const double min_length = offset;  // + p.baselink_to_front;
-  const double max_length = std::min(p.detection_area_max_length, p.detection_area_length);
+  // Note: min_detection_area_length is for occlusion spot visualization but not effective for planning
+  const double min_detection_area_length = 10.0;
+  const double max_length = std::max(
+    min_detection_area_length, std::min(p.detection_area_max_length, p.detection_area_length));
   const double min_distance = (is_on_right) ? -p.half_vehicle_width : p.half_vehicle_width;
   const double slice_length = p.detection_area.slice_length;
   const int num_step = static_cast<int>(slice_length);
