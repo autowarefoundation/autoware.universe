ab3bdddac6420d0573cbd4aa8a72de7e0564a296
Yukihiro Saito <yukky.saito@gmail.com>
2022-03-03 20:40:12 +0900

    add debugger
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>

--- perception/detection_by_tracker/src/detection_by_tracker_core.cpp
+++ perception/detection_by_tracker/src/detection_by_tracker_core.cpp
@@ -215,6 +215,7 @@ DetectionByTracker::DetectionByTracker(const rclcpp::NodeOptions & node_options)
   shape_estimator_ = std::make_shared<ShapeEstimator>(true, true);
   cluster_ = std::make_shared<euclidean_cluster::VoxelGridBasedEuclideanCluster>(
     false, 10, 10000, 0.7, 0.3, 0);
+  debugger_ = std::make_shared<Debugger>(this);
 }
 
 void DetectionByTracker::onObjects(
@@ -224,34 +225,36 @@ void DetectionByTracker::onObjects(
   detected_objects.header = input_msg->header;
 
   // get objects from tracking module
-  autoware_auto_perception_msgs::msg::TrackedObjects tracked_objects;
+  autoware_auto_perception_msgs::msg::DetectedObjects tracked_objects;
   {
-    autoware_auto_perception_msgs::msg::TrackedObjects objects;
+    autoware_auto_perception_msgs::msg::TrackedObjects objects, transformed_objects;
     const bool available_trackers =
       tracker_handler_.estimateTrackedObjects(input_msg->header.stamp, objects);
     if (
       !available_trackers ||
-      !transformTrackedObjects(objects, input_msg->header.frame_id, tf_buffer_, tracked_objects)) {
+      !transformTrackedObjects(
+        objects, input_msg->header.frame_id, tf_buffer_, transformed_objects)) {
       objects_pub_->publish(detected_objects);
       return;
     }
+    // to simplify post processes, convert tracked_objects to DetectedObjects message.
+    tracked_objects = toDetectedObjects(transformed_objects);
   }
-
-  // to simplify post processes, convert tracked_objects to DetectedObjects message.
-  autoware_auto_perception_msgs::msg::DetectedObjects tracked_objects_dt;
-  tracked_objects_dt = toDetectedObjects(tracked_objects);
+  debugger_->publishInitialObjects(*input_msg);
+  debugger_->publishTrackedObjects(tracked_objects);
 
   // merge over segmented objects
   tier4_perception_msgs::msg::DetectedObjectsWithFeature merged_objects;
   autoware_auto_perception_msgs::msg::DetectedObjects no_found_tracked_objects;
-  mergeOverSegmentedObjects(
-    tracked_objects_dt, *input_msg, no_found_tracked_objects, merged_objects);
+  mergeOverSegmentedObjects(tracked_objects, *input_msg, no_found_tracked_objects, merged_objects);
+  debugger_->publishMergedObjects(merged_objects);
 
   // divide under segmented objects
   tier4_perception_msgs::msg::DetectedObjectsWithFeature divided_objects;
   autoware_auto_perception_msgs::msg::DetectedObjects temp_no_found_tracked_objects;
   divideUnderSegmentedObjects(
     no_found_tracked_objects, *input_msg, temp_no_found_tracked_objects, divided_objects);
+  debugger_->publishDividedObjects(divided_objects);
 
   // merge under/over segmented objects to build output objects
   for (const auto & merged_object : merged_objects.feature_objects) {
