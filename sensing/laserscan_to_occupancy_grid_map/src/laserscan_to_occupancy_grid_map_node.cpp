070512fbdcdcfb691a7c778164fc45093c4583f7
Yukihiro Saito <yukky.saito@gmail.com>
2022-03-19 16:04:38 +0900

    feat: detected object validation (#507)

--- sensing/laserscan_to_occupancy_grid_map/src/laserscan_to_occupancy_grid_map_node.cpp
+++ sensing/laserscan_to_occupancy_grid_map/src/laserscan_to_occupancy_grid_map_node.cpp
@@ -120,6 +120,7 @@ OccupancyGridMapNode::OccupancyGridMapNode(const rclcpp::NodeOptions & node_opti
   base_link_frame_ = declare_parameter("base_link_frame", "base_link");
   use_height_filter_ = declare_parameter("use_height_filter", true);
   const double map_length{declare_parameter("map_length", 100.0)};
+  const double map_width{declare_parameter("map_width", 100.0)};
   const double map_resolution{declare_parameter("map_resolution", 0.5)};
   const bool input_obstacle_pointcloud{declare_parameter("input_obstacle_pointcloud", true)};
   const bool input_obstacle_and_raw_pointcloud{
@@ -151,7 +152,7 @@ OccupancyGridMapNode::OccupancyGridMapNode(const rclcpp::NodeOptions & node_opti
 
   /* Occupancy grid */
   occupancy_grid_map_updater_ptr_ = std::make_shared<OccupancyGridMapBBFUpdater>(
-    map_length / map_resolution, map_length / map_resolution, map_resolution);
+    map_length / map_resolution, map_width / map_resolution, map_resolution);
 }
 
 PointCloud2::SharedPtr OccupancyGridMapNode::convertLaserscanToPointCLoud2(
