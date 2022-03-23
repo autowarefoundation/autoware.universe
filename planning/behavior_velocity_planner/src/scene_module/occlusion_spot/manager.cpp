a874bd08ca42e80e3407fd341ae27a59060115ea
tanaka3 <ttatcoder@outlook.jp>
2022-03-23 12:06:20 +0900

    chore(behavior_velocity): minor change
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

--- planning/behavior_velocity_planner/src/scene_module/occlusion_spot/manager.cpp
+++ planning/behavior_velocity_planner/src/scene_module/occlusion_spot/manager.cpp
@@ -58,18 +58,21 @@ OcclusionSpotModuleManager::OcclusionSpotModuleManager(rclcpp::Node & node)
         "[behavior_velocity]: occlusion spot pass judge method has invalid argument"};
     }
   }
-  pp.is_show_occlusion = node.declare_parameter(ns + "debug.is_show_occlusion", false);
-  pp.is_show_cv_window = node.declare_parameter(ns + "debug.is_show_cv_window", false);
-  pp.is_show_processing_time = node.declare_parameter(ns + "debug.is_show_processing_time", false);
   pp.filter_occupancy_grid = node.declare_parameter(ns + ".filter_occupancy_grid", false);
   pp.use_object_info = node.declare_parameter(ns + ".use_object_info", false);
   pp.use_partition_lanelet = node.declare_parameter(ns + ".use_partition_lanelet", false);
   pp.pedestrian_vel = node.declare_parameter(ns + ".pedestrian_vel", 1.5);
   pp.pedestrian_radius = node.declare_parameter(ns + ".pedestrian_radius", 0.5);
+
+  // debug
+  pp.is_show_occlusion = node.declare_parameter(ns + ".debug.is_show_occlusion", false);
+  pp.is_show_cv_window = node.declare_parameter(ns + ".debug.is_show_cv_window", false);
+  pp.is_show_processing_time = node.declare_parameter(ns + ".debug.is_show_processing_time", false);
+
+  // threshold
   pp.detection_area_length = node.declare_parameter(ns + ".threshold.detection_area_length", 200.0);
   pp.stuck_vehicle_vel = node.declare_parameter(ns + ".threshold.stuck_vehicle_vel", 1.0);
   pp.lateral_distance_thr = node.declare_parameter(ns + ".threshold.lateral_distance", 10.0);
-
   pp.dist_thr = node.declare_parameter(ns + ".threshold.search_dist", 10.0);
   pp.angle_thr = node.declare_parameter(ns + ".threshold.search_angle", M_PI / 5.0);
 
