b0b80426b9493a9e5a53d2ca7fb620a3f49413db
Makoto Kurihara <mkuri8m@gmail.com>
2022-03-31 13:53:13 +0900

    Refactor param variables
    Signed-off-by: Makoto Kurihara <mkuri8m@gmail.com>

--- planning/motion_velocity_smoother/src/smoother/smoother_base.cpp
+++ planning/motion_velocity_smoother/src/smoother/smoother_base.cpp
@@ -25,27 +25,28 @@ namespace motion_velocity_smoother
 {
 SmootherBase::SmootherBase(rclcpp::Node & node)
 {
-  base_param_.max_accel = node.declare_parameter("normal.max_acc", 2.0);
-  base_param_.min_decel = node.declare_parameter("normal.min_acc", -3.0);
-  base_param_.stop_decel = node.declare_parameter("stop_decel", 0.0);
-  base_param_.max_jerk = node.declare_parameter("normal.max_jerk", 0.3);
-  base_param_.min_jerk = node.declare_parameter("normal.min_jerk", -0.1);
-  base_param_.max_lateral_accel = node.declare_parameter("max_lateral_accel", 0.2);
-  base_param_.decel_distance_before_curve =
+  auto & p = base_param_;
+  p.max_accel = node.declare_parameter("normal.max_acc", 2.0);
+  p.min_decel = node.declare_parameter("normal.min_acc", -3.0);
+  p.stop_decel = node.declare_parameter("stop_decel", 0.0);
+  p.max_jerk = node.declare_parameter("normal.max_jerk", 0.3);
+  p.min_jerk = node.declare_parameter("normal.min_jerk", -0.1);
+  p.max_lateral_accel = node.declare_parameter("max_lateral_accel", 0.2);
+  p.decel_distance_before_curve =
     node.declare_parameter("decel_distance_before_curve", 3.5);
-  base_param_.decel_distance_after_curve =
+  p.decel_distance_after_curve =
     node.declare_parameter("decel_distance_after_curve", 0.0);
-  base_param_.min_curve_velocity = node.declare_parameter("min_curve_velocity", 1.38);
-  base_param_.resample_param.max_trajectory_length =
+  p.min_curve_velocity = node.declare_parameter("min_curve_velocity", 1.38);
+  p.resample_param.max_trajectory_length =
     node.declare_parameter("max_trajectory_length", 200.0);
-  base_param_.resample_param.min_trajectory_length =
+  p.resample_param.min_trajectory_length =
     node.declare_parameter("min_trajectory_length", 30.0);
-  base_param_.resample_param.resample_time = node.declare_parameter("resample_time", 10.0);
-  base_param_.resample_param.dense_resample_dt = node.declare_parameter("dense_resample_dt", 0.1);
-  base_param_.resample_param.dense_min_interval_distance =
+  p.resample_param.resample_time = node.declare_parameter("resample_time", 10.0);
+  p.resample_param.dense_resample_dt = node.declare_parameter("dense_resample_dt", 0.1);
+  p.resample_param.dense_min_interval_distance =
     node.declare_parameter("dense_min_interval_distance", 0.1);
-  base_param_.resample_param.sparse_resample_dt = node.declare_parameter("sparse_resample_dt", 0.5);
-  base_param_.resample_param.sparse_min_interval_distance =
+  p.resample_param.sparse_resample_dt = node.declare_parameter("sparse_resample_dt", 0.5);
+  p.resample_param.sparse_min_interval_distance =
     node.declare_parameter("sparse_min_interval_distance", 4.0);
 }
 
