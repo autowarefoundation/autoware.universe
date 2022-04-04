b0299f29bf5dd5a3df7a22ff847782ac109539d2
melike <melike@leodrive.ai>
2022-04-04 12:41:34 +0300

    fix(map_loader): add UTM projector to map_loader package
    Signed-off-by: melike <melike@leodrive.ai>

--- map/map_loader/src/lanelet2_map_loader/lanelet2_map_loader_node.cpp
+++ map/map_loader/src/lanelet2_map_loader/lanelet2_map_loader_node.cpp
@@ -38,20 +38,32 @@
 #include <lanelet2_extension/utility/message_conversion.hpp>
 #include <lanelet2_extension/utility/utilities.hpp>
 #include <rclcpp/rclcpp.hpp>
-
+#include <ament_index_cpp/get_package_prefix.hpp>
 #include <lanelet2_core/LaneletMap.h>
 #include <lanelet2_io/Io.h>
 #include <lanelet2_projection/UTM.h>
-
 #include <string>
 
 Lanelet2MapLoaderNode::Lanelet2MapLoaderNode(const rclcpp::NodeOptions & options)
 : Node("lanelet2_map_loader", options)
 {
   const auto lanelet2_filename = declare_parameter("lanelet2_map_path", "");
+  const auto lanelet2_map_projector_type = declare_parameter("lanelet2_map_projector_type", "MGRS");
   lanelet::ErrorMessages errors{};
-  lanelet::projection::MGRSProjector projector{};
-  lanelet::LaneletMapPtr map = lanelet::load(lanelet2_filename, projector, &errors);
+  lanelet::LaneletMapPtr map;
+  if (lanelet2_map_projector_type == "MGRS") {
+    lanelet::projection::MGRSProjector projector{};
+    map = lanelet::load(lanelet2_filename, projector, &errors);
+  } else if (lanelet2_map_projector_type == "UTM") {
+    double map_origin_lat = this->declare_parameter("latitude", 0.0);
+    double map_origin_lon = declare_parameter("longitude", 0.0);
+    lanelet::GPSPoint position{map_origin_lat, map_origin_lon};
+    lanelet::Origin origin{position};
+    lanelet::projection::UtmProjector projector{origin};
+    map = lanelet::load(lanelet2_filename, projector, &errors);
+  } else {
+    RCLCPP_ERROR(this->get_logger(), "lanelet2_map_projector_type is not supported");
+  }
 
   for (const auto & error : errors) {
     RCLCPP_ERROR_STREAM(this->get_logger(), error);
