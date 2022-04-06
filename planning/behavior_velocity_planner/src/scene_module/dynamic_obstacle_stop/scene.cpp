22dd97fc86331aeb8ca159248decf190bac53b40
Tomohito Ando <tomohito.ando@tier4.jp>
2022-04-06 23:20:53 +0900

    modify stopping velocity
    Signed-off-by: Tomohito Ando <tomohito.ando@tier4.jp>

--- planning/behavior_velocity_planner/src/scene_module/dynamic_obstacle_stop/scene.cpp
+++ planning/behavior_velocity_planner/src/scene_module/dynamic_obstacle_stop/scene.cpp
@@ -1018,10 +1018,10 @@ boost::optional<geometry_msgs::msg::Pose> DynamicObstacleStopModule::calcStopPoi
   const bool deceleration_needed =
     *stop_dist > dist_to_collision - planner_param_.dynamic_obstacle_stop.stop_margin;
   // avoid acceleration when ego is decelerating
+  // TODO(Tomohito Ando): replace with more appropriate method
   constexpr float epsilon = 1.0e-2;
-  const bool is_stopping =
-    current_vel < planner_param_.dynamic_obstacle_stop.min_vel_ego_kmph / 3.6 &&
-    current_acc < epsilon;
+  constexpr float stopping_vel_mps = 2.5 / 3.6;
+  const bool is_stopping = current_vel < stopping_vel_mps && current_acc < epsilon;
   if (!deceleration_needed && !is_stopping) {
     debug_ptr_->setAccelReason(AccelReason::LOW_JERK);
     return {};
