584e14bb414b09659802732bde6ce2205e1d3740
Maxime CLEMENT <maxime.clement@tier4.jp>
2022-04-14 17:56:36 +0900

    Add docstring + Small code cleanup
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/safe_velocity_adjustor/include/safe_velocity_adjustor/collision_distance.hpp
+++ planning/safe_velocity_adjustor/include/safe_velocity_adjustor/collision_distance.hpp
@@ -44,9 +44,12 @@ using segment_t = bg::model::segment<point_t>;
 using linestring_t = bg::model::linestring<point_t>;
 using multilinestring_t = bg::model::multi_linestring<linestring_t>;
 
-/// @brief generate a segment to where the vehicle body would be after some duration assuming a
-/// constant velocity and heading
-inline segment_t forwardSimulatedVector(
+/// @brief generate a segment to where ego would reach with constant velocity and heading
+/// @param [in] trajectory_point origin of the segment
+/// @param [in] duration duration of the forward projection
+/// @param [in] extra_distance distance to add ahead of the projected point
+/// @return segment from the trajectory_point to its position after duration + the extra_distance
+inline segment_t forwardSimulatedSegment(
   const autoware_auto_planning_msgs::msg::TrajectoryPoint & trajectory_point, const double duration,
   const double extra_distance)
 {
@@ -59,23 +62,31 @@ inline segment_t forwardSimulatedVector(
   return segment_t{from, to};
 }
 
-/// @brief generate a footprint from a segment and a vehicle width
-inline polygon_t forwardSimulatedFootprint(const segment_t & vector, const double lateral_offset)
+/// @brief generate a footprint from a segment and a lateral offset
+/// @param [in] segment segment from which to create the footprint
+/// @param [in] lateral_offset offset around the segment used to create the footprint
+/// @return footprint polygon
+inline polygon_t forwardSimulatedFootprint(const segment_t & segment, const double lateral_offset)
 {
   multipolygon_t footprint;
   namespace strategy = bg::strategy::buffer;
   bg::buffer(
-    linestring_t{vector.first, vector.second}, footprint,
+    linestring_t{segment.first, segment.second}, footprint,
     strategy::distance_symmetric<double>(lateral_offset), strategy::side_straight(),
     strategy::join_miter(), strategy::end_flat(), strategy::point_square());
   return footprint[0];
 }
 
+/// @brief calculate the closest distance to a collision
+/// @param [in] segment forard projection of a trajectory point
+/// @param [in] footprint footprint of the segment
+/// @param [in] obstacles set of obstacles as linestrings
+/// @return distance to the closest collision if any
 inline std::optional<double> distanceToClosestCollision(
-  const segment_t & vector, const polygon_t & footprint, const multilinestring_t & obstacles)
+  const segment_t & segment, const polygon_t & footprint, const multilinestring_t & obstacles)
 {
-  const auto vector_heading =
-    std::atan2(vector.second.y() - vector.first.y(), vector.second.x() - vector.first.x());
+  const auto segment_heading =
+    std::atan2(segment.second.y() - segment.first.y(), segment.second.x() - segment.first.x());
   double min_dist = std::numeric_limits<double>::max();
   for (const auto & obstacle : obstacles) {
     multilinestring_t intersection_lines;
@@ -84,9 +95,9 @@ inline std::optional<double> distanceToClosestCollision(
         for (const auto & obs_point : intersection_line) {
           // Calculate longitudinal distance to the collision point along the segment
           const auto collision_heading =
-            std::atan2(obs_point.y() - vector.first.y(), obs_point.x() - vector.first.x());
-          const auto angle = vector_heading - collision_heading;
-          const auto hypot_length = bg::distance(obs_point, vector.first);
+            std::atan2(obs_point.y() - segment.first.y(), obs_point.x() - segment.first.x());
+          const auto angle = segment_heading - collision_heading;
+          const auto hypot_length = bg::distance(obs_point, segment.first);
           const auto long_dist = std::abs(std::cos(angle)) * hypot_length;
           min_dist = std::min(min_dist, long_dist);
         }
@@ -98,16 +109,21 @@ inline std::optional<double> distanceToClosestCollision(
   return distance;
 }
 
-inline polygon_t createObjPolygon(
-  const geometry_msgs::msg::Pose & pose, const geometry_msgs::msg::Vector3 & size,
+/// @brief create a polygon from an object represented by a pose and a size
+/// @param [in] pose pose of the object
+/// @param [in] dimensions dimensions of the object
+/// @param [in] buffer buffer to add to the dimensions of the object
+/// @return polygon of the object
+inline polygon_t createObjectPolygon(
+  const geometry_msgs::msg::Pose & pose, const geometry_msgs::msg::Vector3 & dimensions,
   const double buffer)
 {
   // (objects.kinematics.initial_pose_with_covariance.pose, object.shape.dimensions);
   // rename
   const double x = pose.position.x;
   const double y = pose.position.y;
-  const double h = size.x + buffer;
-  const double w = size.y + buffer;
+  const double h = dimensions.x + buffer;
+  const double w = dimensions.y + buffer;
   const double yaw = tf2::getYaw(pose.orientation);
 
   // create base polygon
@@ -128,7 +144,12 @@ inline polygon_t createObjPolygon(
   return translate_obj_poly;
 }
 
-inline multipolygon_t createObjPolygons(
+/// @brief create polygons from a set of predicted object
+/// @param [in] objects objects from which to create polygons
+/// @param [in] buffer buffer to add to the objects dimensions
+/// @param [in] min_velocity objects with velocity lower will be ignored
+/// @return polygons of the objects
+inline multipolygon_t createObjectPolygons(
   const autoware_auto_perception_msgs::msg::PredictedObjects & objects, const double buffer,
   const double min_velocity)
 {
@@ -137,7 +158,7 @@ inline multipolygon_t createObjPolygons(
     if (
       object.kinematics.initial_twist_with_covariance.twist.linear.x >= min_velocity ||
       object.kinematics.initial_twist_with_covariance.twist.linear.x <= -min_velocity) {
-      polygons.push_back(createObjPolygon(
+      polygons.push_back(createObjectPolygon(
         object.kinematics.initial_pose_with_covariance.pose, object.shape.dimensions, buffer));
     }
   }
