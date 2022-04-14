f881193ef2428031b5fd3346edd1c4bab608fa19
Maxime CLEMENT <maxime.clement@tier4.jp>
2022-04-14 18:30:11 +0900

    Improve test of occupancy_grid_utils
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/safe_velocity_adjustor/test/test_occupancy_grid_utils.cpp
+++ planning/safe_velocity_adjustor/test/test_occupancy_grid_utils.cpp
@@ -20,7 +20,8 @@
 TEST(TestOccupancyGridUtils, extractStaticObstaclePolygons)
 {
   using safe_velocity_adjustor::extractStaticObstaclePolygons;
-  constexpr int8_t occupied_threshold = 10;
+  using safe_velocity_adjustor::polygon_t;
+  constexpr int8_t occupied_thr = 10;
   nav_msgs::msg::OccupancyGrid occupancy_grid;
   occupancy_grid.info.height = 5;
   occupancy_grid.info.width = 5;
@@ -28,15 +29,22 @@ TEST(TestOccupancyGridUtils, extractStaticObstaclePolygons)
     std::vector<signed char>(occupancy_grid.info.height * occupancy_grid.info.width);
   occupancy_grid.info.resolution = 1.0;
 
-  auto polygons = extractStaticObstaclePolygons(occupancy_grid, {}, occupied_threshold);
+  auto polygons = extractStaticObstaclePolygons(occupancy_grid, {}, occupied_thr);
   EXPECT_TRUE(polygons.empty());
 
   occupancy_grid.data.at(5) = 10;
-  polygons = extractStaticObstaclePolygons(occupancy_grid, {}, occupied_threshold);
+  polygons = extractStaticObstaclePolygons(occupancy_grid, {}, occupied_thr);
   EXPECT_EQ(polygons.size(), 1ul);
 
   for (auto i = 1; i < 4; ++i)
     for (auto j = 1; j < 4; ++j) occupancy_grid.data[j + i * occupancy_grid.info.width] = 10;
-  polygons = extractStaticObstaclePolygons(occupancy_grid, {}, occupied_threshold);
+  polygons = extractStaticObstaclePolygons(occupancy_grid, {}, occupied_thr);
   EXPECT_EQ(polygons.size(), 1ul);
+
+  polygon_t full_mask;
+  full_mask.outer() = {{0.0, 0.0}, {0.0, 5.0}, {5.0, 5.0}, {5.0, 0.0}};
+  boost::geometry::correct(full_mask);
+  std::cout << boost::geometry::wkt(full_mask) << std::endl;
+  polygons = extractStaticObstaclePolygons(occupancy_grid, {full_mask}, occupied_thr);
+  EXPECT_EQ(polygons.size(), 0ul);
 }
