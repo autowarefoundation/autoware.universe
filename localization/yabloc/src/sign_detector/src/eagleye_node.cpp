f58949206a66beab7823844def1e4670f87b6d4a
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2022-04-22 14:31:23 +0900

    convert /eagleye/fix to path for visualization

--- localization/yabloc/src/sign_detector/src/eagleye_node.cpp
+++ localization/yabloc/src/sign_detector/src/eagleye_node.cpp
@@ -2,9 +2,13 @@
 #include <GeographicLib/MGRS.hpp>
 #include <GeographicLib/UTMUPS.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
+#include <string>
 
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
 class EagleyeSubscriber : public rclcpp::Node
 {
 public:
@@ -12,13 +16,19 @@ public:
   {
     sub_fix_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(fix_topic, 10, std::bind(&EagleyeSubscriber::fixCallback, this, std::placeholders::_1));
     sub_twist_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(twist_topic, 10, std::bind(&EagleyeSubscriber::twistCallback, this, std::placeholders::_1));
+    pub_path_ = this->create_publisher<nav_msgs::msg::Path>("/path", 10);
+
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
   }
 
 private:
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr sub_fix_;
   rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr sub_twist_;
+  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pub_path_;
+  nav_msgs::msg::Path path_;
 
-  void fixCallback(const sensor_msgs::msg::NavSatFix& msg) const
+  void fixCallback(const sensor_msgs::msg::NavSatFix& msg)
   {
     using namespace GeographicLib;
     double x, y;
@@ -26,9 +36,38 @@ private:
     bool northp;
     std::string mgrs;
     UTMUPS::Forward(msg.latitude, msg.longitude, zone, northp, x, y);
-    MGRS::Forward(zone, northp, x, y, msg.latitude, 5, mgrs);
-    std::cout << msg.latitude << " " << msg.longitude << " " << msg.altitude << std::endl;
-    std::cout << mgrs << " " << x << " " << y << std::endl;
+    MGRS::Forward(zone, northp, x, y, msg.latitude, 6, mgrs);
+
+    double local_x = std::stoi(mgrs.substr(5, 6)) * 0.1;
+    double local_y = std::stoi(mgrs.substr(11, 6)) * 0.1;
+
+    RCLCPP_INFO_STREAM(this->get_logger(), mgrs.substr(0, 5) << " " << local_x << " " << local_y << " ( " << msg.latitude << ", " << msg.longitude << " )");
+
+    path_.header.frame_id = "map";
+    path_.header.stamp = this->get_clock()->now();
+    geometry_msgs::msg::PoseStamped ps;
+    ps.pose.orientation.w = 1;
+    ps.pose.position.x = local_x;
+    ps.pose.position.y = local_y;
+    ps.pose.position.z = 0;
+    path_.poses.push_back(ps);
+    pub_path_->publish(path_);
+
+    publishTf(ps);
+  }
+
+  void publishTf(const geometry_msgs::msg::PoseStamped& ps)
+  {
+    geometry_msgs::msg::TransformStamped t;
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "map";
+    t.child_frame_id = "eagleye";
+    t.transform.translation.x = ps.pose.position.x;
+    t.transform.translation.y = ps.pose.position.y;
+    t.transform.translation.z = ps.pose.position.z;
+    t.transform.rotation.w = 1;
+
+    tf_broadcaster_->sendTransform(t);
   }
 
   void twistCallback(const geometry_msgs::msg::TwistStamped&) const
