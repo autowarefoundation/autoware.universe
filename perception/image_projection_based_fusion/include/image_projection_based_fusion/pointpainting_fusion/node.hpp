9ebc13ff4b629020d0bd85c02cd41dfe3cdca434
tzhong518 <sworgun@gmail.com>
2022-04-26 16:12:49 +0900

    pointpainting_fusion: fix uncommited node.cpp and node.hpp

--- perception/image_projection_based_fusion/include/image_projection_based_fusion/pointpainting_fusion/node.hpp
+++ perception/image_projection_based_fusion/include/image_projection_based_fusion/pointpainting_fusion/node.hpp
@@ -16,11 +16,11 @@
 #define IMAGE_PROJECTION_BASED_FUSION__POINTPAINTING__NODE_HPP_
 
 #include "image_projection_based_fusion/fusion_node.hpp"
+#include "image_projection_based_fusion/pointpainting_fusion/pointpainting_trt.hpp"
 
-#include <centerpoint_trt.hpp>
-#include <config.hpp>
 #include <image_projection_based_fusion/utils/geometry.hpp>
 #include <image_projection_based_fusion/utils/utils.hpp>
+#include <lidar_centerpoint/centerpoint_trt.hpp>
 
 #include <map>
 #include <memory>
@@ -29,6 +29,7 @@
 namespace image_projection_based_fusion
 {
 using Label = autoware_auto_perception_msgs::msg::ObjectClassification;
+
 class PointpaintingFusionNode : public FusionNode<sensor_msgs::msg::PointCloud2>
 {
 public:
@@ -43,27 +44,16 @@ protected:
     const sensor_msgs::msg::CameraInfo & camera_info,
     sensor_msgs::msg::PointCloud2 & painted_pointcloud_msg) override;
 
-  void paintPoints(
-    const sensor_msgs::msg::PointCloud2 & input_pointcloud_msg,
-    const DetectedObjectsWithFeature & input_roi_msg, const double image_width,
-    const double image_height, const geometry_msgs::msg::TransformStamped transform_stamped,
-    const Eigen::Matrix4d & camera_projection,
-    sensor_msgs::msg::PointCloud2 & painted_pointcloud_msg);
-
   void postprocess(sensor_msgs::msg::PointCloud2 & painted_pointcloud_msg) override;
-
-  void box3DToDetectedObject(
-    const Box3D & box3d, autoware_auto_perception_msgs::msg::DetectedObject & obj);
-  static uint8_t getSemanticType(const std::string & class_name);
-  static bool isCarLikeVehicleLabel(const uint8_t label);
 
   rclcpp::Publisher<autoware_auto_perception_msgs::msg::DetectedObjects>::SharedPtr obj_pub_ptr_;
 
   float score_threshold_{0.0};
   std::vector<std::string> class_names_;
   bool rename_car_to_truck_and_bus_{false};
+  bool has_twist_{false};
 
-  std::unique_ptr<CenterPointTRT> detector_ptr_{nullptr};
+  std::unique_ptr<centerpoint::PointPaintingTRT> detector_ptr_{nullptr};
 };
 
 }  // namespace image_projection_based_fusion
