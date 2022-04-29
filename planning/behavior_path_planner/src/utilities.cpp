bf4da00370674bb7cf91613ede2e80817d609563
Takayuki Murooka <takayuki5168@gmail.com>
2022-04-29 23:42:05 +0900

    feat(behavior_path_planner): Weakened noise filtering of drivable area
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/behavior_path_planner/src/utilities.cpp
+++ planning/behavior_path_planner/src/utilities.cpp
@@ -1227,7 +1227,7 @@ OccupancyGrid generateDrivableArea(
     }
 
     // Closing
-    constexpr int num_iter = 10;  // TODO(Horibe) Think later.
+    constexpr int num_iter = 1;  // TODO(Horibe) Think later.
     cv::Mat cv_erode, cv_dilate;
     cv::erode(cv_image, cv_erode, cv::Mat(), cv::Point(-1, -1), num_iter);
     cv::dilate(cv_erode, cv_dilate, cv::Mat(), cv::Point(-1, -1), num_iter);
