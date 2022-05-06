9e28270ebbf6f2562838be188adb9ed364a0a782
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2022-05-06 15:43:22 +0900

    subscribe static_tf about base_link to camera

--- localization/yabloc/src/sign_detector/include/sign_detector/lsd.hpp
+++ localization/yabloc/src/sign_detector/include/sign_detector/lsd.hpp
@@ -12,19 +12,75 @@
 #include <opencv4/opencv2/opencv.hpp>
 #include <optional>
 
+#include <eigen3/Eigen/Geometry>
+#include <eigen3/Eigen/StdVector>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
 class LineSegmentDetector : public rclcpp::Node
 {
 public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
   LineSegmentDetector(const std::string& image_topic, const std::string& info_topic) : Node("line_detector"), info_(std::nullopt)
   {
     sub_image_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(image_topic, 10, std::bind(&LineSegmentDetector::imageCallback, this, std::placeholders::_1));
     sub_info_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(info_topic, 10, std::bind(&LineSegmentDetector::infoCallback, this, std::placeholders::_1));
     pub_image_ = this->create_publisher<sensor_msgs::msg::Image>("/lsd", 10);
     lsd = cv::lsd::createLineSegmentDetector(cv::lsd::LSD_REFINE_ADV);
+
+    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
   }
 
 private:
-  void infoCallback(const sensor_msgs::msg::CameraInfo& msg) { info_ = msg; }
+  std::shared_ptr<tf2_ros::TransformListener> transform_listener_{nullptr};
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  std::optional<Eigen::Affine3f> camera_extrinsic_{std::nullopt};
+
+  cv::Ptr<cv::lsd::LineSegmentDetector> lsd;
+  std::optional<sensor_msgs::msg::CameraInfo> info_;
+  rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr sub_image_;
+  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr sub_info_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image_;
+
+  void projectEdgeOnPlane() const
+  {
+    if (!camera_extrinsic_.has_value()) {
+      RCLCPP_WARN_STREAM(this->get_logger(), "camera_extrinsic_ has not been initialized");
+      return;
+    }
+    Eigen::Vector3f t = camera_extrinsic_->translation();
+    Eigen::Quaternionf q(camera_extrinsic_->rotation());
+    RCLCPP_INFO_STREAM(this->get_logger(), "transform: " << t.transpose() << " " << q.coeffs().transpose());
+  }
+
+  void listenExtrinsicTf(const std::string& frame_id)
+  {
+    try {
+      geometry_msgs::msg::TransformStamped ts = tf_buffer_->lookupTransform("base_link", frame_id, tf2::TimePointZero);
+      Eigen::Vector3f p;
+      p.x() = ts.transform.translation.x;
+      p.y() = ts.transform.translation.y;
+      p.z() = ts.transform.translation.z;
+
+      Eigen::Quaternionf q;
+      q.w() = ts.transform.rotation.w;
+      q.x() = ts.transform.rotation.x;
+      q.y() = ts.transform.rotation.y;
+      q.z() = ts.transform.rotation.z;
+      camera_extrinsic_ = Eigen::Affine3f::Identity();
+      camera_extrinsic_->translation() = p;
+      camera_extrinsic_->matrix().topLeftCorner(3, 3) = q.toRotationMatrix();
+    } catch (tf2::TransformException& ex) {
+    }
+  }
+
+  void infoCallback(const sensor_msgs::msg::CameraInfo& msg)
+  {
+    info_ = msg;
+    listenExtrinsicTf(info_->header.frame_id);
+  }
 
   void imageCallback(const sensor_msgs::msg::CompressedImage& msg) const
   {
@@ -62,13 +118,10 @@ private:
       raw_image.image = image;
       pub_image_->publish(*raw_image.toImageMsg());
     }
+
+    projectEdgeOnPlane();
   }
 
-  cv::Ptr<cv::lsd::LineSegmentDetector> lsd;
-  std::optional<sensor_msgs::msg::CameraInfo> info_;
-  rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr sub_image_;
-  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr sub_info_;
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image_;
 
   sensor_msgs::msg::Image::ConstSharedPtr decompressImage(const sensor_msgs::msg::CompressedImage& compressed_img) const
   {
