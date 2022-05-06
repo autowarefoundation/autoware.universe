28beed89396918707cfdff98bbff2d725610e229
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2022-05-06 17:14:23 +0900

    project line segment from LSD like bird eye view

--- localization/yabloc/src/sign_detector/include/sign_detector/lsd.hpp
+++ localization/yabloc/src/sign_detector/include/sign_detector/lsd.hpp
@@ -14,6 +14,7 @@
 
 #include <eigen3/Eigen/Geometry>
 #include <eigen3/Eigen/StdVector>
+#include <opencv4/opencv2/core/eigen.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 
@@ -26,7 +27,8 @@ public:
   {
     sub_image_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(image_topic, 10, std::bind(&LineSegmentDetector::imageCallback, this, std::placeholders::_1));
     sub_info_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(info_topic, 10, std::bind(&LineSegmentDetector::infoCallback, this, std::placeholders::_1));
-    pub_image_ = this->create_publisher<sensor_msgs::msg::Image>("/lsd", 10);
+    pub_image_ = this->create_publisher<sensor_msgs::msg::Image>("/projected", 10);
+    pub_image_lsd_ = this->create_publisher<sensor_msgs::msg::Image>("/lsd", 10);
     lsd = cv::lsd::createLineSegmentDetector(cv::lsd::LSD_REFINE_ADV);
 
     tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
@@ -42,9 +44,9 @@ private:
   std::optional<sensor_msgs::msg::CameraInfo> info_;
   rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr sub_image_;
   rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr sub_info_;
-  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image_, pub_image_lsd_;
 
-  void projectEdgeOnPlane() const
+  void projectEdgeOnPlane(const cv::Mat& lines, const cv::Mat& K_cv, const rclcpp::Time& stamp) const
   {
     if (!camera_extrinsic_.has_value()) {
       RCLCPP_WARN_STREAM(this->get_logger(), "camera_extrinsic_ has not been initialized");
@@ -53,6 +55,68 @@ private:
     Eigen::Vector3f t = camera_extrinsic_->translation();
     Eigen::Quaternionf q(camera_extrinsic_->rotation());
     RCLCPP_INFO_STREAM(this->get_logger(), "transform: " << t.transpose() << " " << q.coeffs().transpose());
+
+    struct Edge {
+      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+      Edge(const Eigen::Vector3f& p, const Eigen::Vector3f& q) : p(p), q(q) {}
+      Eigen::Vector3f p, q;
+    };
+
+    // Convert to projected coordinate
+    Eigen::Matrix3f K, K_inv;
+    cv::cv2eigen(K_cv, K);
+    K_inv = K.inverse();
+
+    // NOTE: reference capture is better?
+    auto conv = [t, q, K_inv](const Eigen::Vector2f& u) -> Eigen::Vector3f {
+      Eigen::Vector3f u3(u.x(), u.y(), 1);
+      Eigen::Vector3f bearing = (q * K_inv * u3).normalized();
+      if (bearing.z() > -0.1) return Eigen::Vector3f::Zero();
+
+      float l = -t.z() / bearing.z();
+      float projected_x = t.x() + bearing.x() * l;
+      float projected_y = t.y() + bearing.y() * l;
+      Eigen::Vector3f tmp(projected_x, projected_y, 0);
+      return tmp;
+    };
+
+    std::vector<Edge> edges;
+    const int N = lines.rows;
+    for (int i = 0; i < N; i++) {
+      cv::Mat xyxy = lines.row(i);
+      Eigen::Vector2f xy1, xy2;
+      xy1 << xyxy.at<float>(0), xyxy.at<float>(1);
+      xy2 << xyxy.at<float>(2), xyxy.at<float>(3);
+      edges.emplace_back(conv(xy1), conv(xy2));
+    }
+
+    // Draw projected edge image
+    const float max_image_size = 600;
+    const float max_range = 20;  // [m]
+    cv::Mat image = cv::Mat::zeros(cv::Size{max_image_size, max_image_size}, CV_8UC3);
+    {
+      const cv::Size center(image.cols / 2, image.rows / 2);
+      auto toCvPoint = [center, max_range](const Eigen::Vector3f& v) -> cv::Point {
+        cv::Point pt;
+        pt.x = -v.y() / max_range * center.width + center.width;
+        pt.y = -v.x() / max_range * center.height + 2 * center.height;
+        return pt;
+      };
+      for (const auto e : edges) {
+        if (e.p.isZero() || e.q.isZero()) continue;
+        cv::line(image, toCvPoint(e.p), toCvPoint(e.q), cv::Scalar(0, 255, 255), 2, cv::LineTypes::LINE_8);
+      }
+    }
+
+    // Publish image
+    {
+      cv_bridge::CvImage raw_image;
+      raw_image.header.stamp = stamp;
+      raw_image.header.frame_id = "map";
+      raw_image.encoding = "bgr8";
+      raw_image.image = image;
+      pub_image_->publish(*raw_image.toImageMsg());
+    }
   }
 
   void listenExtrinsicTf(const std::string& frame_id)
@@ -88,16 +152,17 @@ private:
     cv::Mat image = cv_bridge::toCvCopy(*image_ptr, "rgb8")->image;
     cv::Size size = image.size();
 
-    if (info_.has_value()) {
-      cv::Mat K(cv::Size(3, 3), CV_64FC1, (void*)(info_->k.data()));
-      cv::Mat D(cv::Size(5, 1), CV_64FC1, (void*)(info_->d.data()));
-      cv::Mat undistorted;
-      cv::undistort(image, undistorted, K, D, K);
-      image = undistorted;
-    }
+    if (!info_.has_value()) return;
+    cv::Mat K = cv::Mat(cv::Size(3, 3), CV_64FC1, (void*)(info_->k.data()));
+    cv::Mat D = cv::Mat(cv::Size(5, 1), CV_64FC1, (void*)(info_->d.data()));
+    cv::Mat undistorted;
+    cv::undistort(image, undistorted, K, D, K);
+    image = undistorted;
+
 
     const int WIDTH = 800;
-    int HEIGHT = 1.0f * WIDTH / size.width * size.height;
+    const float SCALE = 1.0f * WIDTH / size.width;
+    int HEIGHT = SCALE * size.height;
     cv::resize(image, image, cv::Size(WIDTH, HEIGHT));
     cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
 
@@ -112,14 +177,16 @@ private:
 
     {
       cv_bridge::CvImage raw_image;
-      raw_image.header = msg.header;
+      raw_image.header.stamp = msg.header.stamp;
       raw_image.header.frame_id = "map";
       raw_image.encoding = "bgr8";
       raw_image.image = image;
-      pub_image_->publish(*raw_image.toImageMsg());
+      pub_image_lsd_->publish(*raw_image.toImageMsg());
     }
 
-    projectEdgeOnPlane();
+    cv::Mat scaled_K = SCALE * K;
+    scaled_K.at<double>(2, 2) = 1;
+    projectEdgeOnPlane(lines, scaled_K, msg.header.stamp);
   }
 
 
