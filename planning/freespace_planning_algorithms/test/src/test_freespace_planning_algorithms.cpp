8ce9c3923e0c77e448a75fc37ec17580b62d0ba3
Hirokazu Ishida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2022-05-06 23:22:06 +0900

    refactor: dump and load rosbag for plotting
    Signed-off-by: Hirokazu Ishida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>

--- planning/freespace_planning_algorithms/test/src/test_freespace_planning_algorithms.cpp
+++ planning/freespace_planning_algorithms/test/src/test_freespace_planning_algorithms.cpp
@@ -15,8 +15,13 @@
 #include "freespace_planning_algorithms/astar_search.hpp"
 
 #include <rclcpp/rclcpp.hpp>
+#include <rcpputils/filesystem_helper.hpp>
+#include <rosbag2_cpp/writer.hpp>
+
+#include <std_msgs/msg/float64.hpp>
 
 #include <gtest/gtest.h>
+#include <rcutils/time.h>
 #include <tf2/utils.h>
 
 #include <array>
@@ -26,7 +31,7 @@
 
 namespace fpa = freespace_planning_algorithms;
 
-geometry_msgs::msg::Pose construct_pose_msg(std::array<double, 3> pose3d)
+geometry_msgs::msg::Pose create_pose_msg(std::array<double, 3> pose3d)
 {
   geometry_msgs::msg::Pose pose{};
   tf2::Quaternion quat{};
@@ -36,6 +41,13 @@ geometry_msgs::msg::Pose construct_pose_msg(std::array<double, 3> pose3d)
   pose.position.y = pose3d[1];
   pose.position.z = 0.0;
   return pose;
+}
+
+std_msgs::msg::Float64 create_float_msg(double val)
+{
+  std_msgs::msg::Float64 msg;
+  msg.data = val;
+  return msg;
 }
 
 nav_msgs::msg::OccupancyGrid construct_cost_map(
@@ -74,8 +86,35 @@ nav_msgs::msg::OccupancyGrid construct_cost_map(
   return costmap_msg;
 }
 
+template <typename MessageT>
+void add_message_to_rosbag(
+  rosbag2_cpp::Writer & writer, const MessageT & message, const std::string & name,
+  const std::string & type)
+{
+  rclcpp::SerializedMessage serialized_msg;
+  rclcpp::Serialization<MessageT> serialization;
+  serialization.serialize_message(&message, &serialized_msg);
+
+  rosbag2_storage::TopicMetadata tm;
+  tm.name = name;
+  tm.type = type;
+  tm.serialization_format = "cdr";
+  writer.create_topic(tm);
+
+  auto bag_message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
+  auto ret = rcutils_system_time_now(&bag_message->time_stamp);
+  if (ret != RCL_RET_OK) {
+    RCLCPP_ERROR(rclcpp::get_logger("saveToBag"), "couldn't assign time rosbag message");
+  }
+
+  bag_message->topic_name = tm.name;
+  bag_message->serialized_data = std::shared_ptr<rcutils_uint8_array_t>(
+    &serialized_msg.get_rcl_serialized_message(), [](rcutils_uint8_array_t * /* data */) {});
+  writer.write(bag_message);
+}
+
 bool test_astar(
-  std::array<double, 3> start, std::array<double, 3> goal, std::string file_name,
+  std::array<double, 3> start, std::array<double, 3> goal, std::string dir_name,
   double maximum_turning_radius = 9.0, int turning_radius_size = 1)
 {
   // set problem configuration
@@ -119,7 +158,7 @@ bool test_astar(
 
   rclcpp::Clock clock{RCL_SYSTEM_TIME};
   const rclcpp::Time begin = clock.now();
-  bool success = astar.makePlan(construct_pose_msg(start), construct_pose_msg(goal));
+  bool success = astar.makePlan(create_pose_msg(start), create_pose_msg(goal));
   const rclcpp::Time now = clock.now();
   const double msec = (now - begin).seconds() * 1000.0;
   if (success) {
@@ -129,17 +168,36 @@ bool test_astar(
   }
   auto result = astar.getWaypoints();
 
-  // dump waypoints (will be used for debugging. the dumped file can be loaded by debug_plot.py)
-  std::ofstream file(file_name);
-  file << msec << std::endl;
-  file << start[0] << ", " << start[1] << ", " << start[2] << std::endl;
-  file << goal[0] << ", " << goal[1] << ", " << goal[2] << std::endl;
-  for (auto & point : result.waypoints) {
-    auto & pos = point.pose.pose.position;
-    auto & ori = point.pose.pose.orientation;
-    file << pos.x << ", " << pos.y << ", " << tf2::getYaw(ori) << std::endl;
+  geometry_msgs::msg::PoseArray result_trajectory;
+  for (const auto & pose : result.waypoints) {
+    result_trajectory.poses.push_back(pose.pose.pose);
   }
-  file.close();
+
+  rcpputils::fs::remove_all(dir_name);
+
+  rosbag2_storage::StorageOptions storage_options;
+  storage_options.uri = dir_name;
+  storage_options.storage_id = "sqlite3";
+
+  rosbag2_cpp::ConverterOptions converter_options;
+  converter_options.input_serialization_format = "cdr";
+  converter_options.output_serialization_format = "cdr";
+
+  rosbag2_cpp::Writer writer(std::make_unique<rosbag2_cpp::writers::SequentialWriter>());
+  writer.open(storage_options, converter_options);
+
+  add_message_to_rosbag(
+    writer, create_float_msg(shape.length), "vehicle_length", "std_msgs/msg/Float64");
+  add_message_to_rosbag(
+    writer, create_float_msg(shape.width), "vehicle_width", "std_msgs/msg/Float64");
+  add_message_to_rosbag(
+    writer, create_float_msg(shape.base2back), "vehicle_base2back", "std_msgs/msg/Float64");
+
+  add_message_to_rosbag(writer, costmap_msg, "costmap", "nav_msgs/msg/OccupancyGrid");
+  add_message_to_rosbag(writer, create_pose_msg(start), "start", "geometry_msgs/msg/Pose");
+  add_message_to_rosbag(writer, create_pose_msg(goal), "goal", "geometry_msgs/msg/Pose");
+  add_message_to_rosbag(writer, result_trajectory, "trajectory", "geometry_msgs/msg/PoseArray");
+  add_message_to_rosbag(writer, create_float_msg(msec), "elapsed_time", "std_msgs/msg/Float64");
 
   // backtrace the path and confirm that the entire path is collision-free
   return success && astar.hasFeasibleSolution();
@@ -151,8 +209,8 @@ TEST(AstarSearchTestSuite, SingleCurvature)
   for (size_t i = 0; i < goal_xs.size(); i++) {
     std::array<double, 3> start{6., 4., 0.5 * 3.1415};
     std::array<double, 3> goal{goal_xs[i], 4., 0.5 * 3.1415};
-    std::string file_name = "/tmp/result_single" + std::to_string(i) + ".txt";
-    EXPECT_TRUE(test_astar(start, goal, file_name));
+    std::string dir_name = "/tmp/result_single" + std::to_string(i);
+    EXPECT_TRUE(test_astar(start, goal, dir_name));
   }
 }
 
@@ -164,8 +222,8 @@ TEST(AstarSearchTestSuite, MultiCurvature)
   for (size_t i = 0; i < goal_xs.size(); i++) {
     std::array<double, 3> start{6., 4., 0.5 * 3.1415};
     std::array<double, 3> goal{goal_xs[i], 4., 0.5 * 3.1415};
-    std::string file_name = "/tmp/result_multi" + std::to_string(i) + ".txt";
-    EXPECT_TRUE(test_astar(start, goal, file_name, maximum_turning_radius, turning_radius_size));
+    std::string dir_name = "/tmp/result_multi" + std::to_string(i);
+    EXPECT_TRUE(test_astar(start, goal, dir_name, maximum_turning_radius, turning_radius_size));
   }
 }
 
