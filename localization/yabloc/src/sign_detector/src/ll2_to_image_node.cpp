cc318f6352a5087b389e2e7ddb27db8c5f595127
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2022-05-06 11:43:35 +0900

    draw visible lanelet2 elements as image

--- localization/yabloc/src/sign_detector/src/ll2_to_image_node.cpp
+++ localization/yabloc/src/sign_detector/src/ll2_to_image_node.cpp
@@ -1,5 +1,8 @@
 #include "sign_detector/ll2_util.hpp"
+#include <cv_bridge/cv_bridge.h>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <lanelet2_core/LaneletMap.h>
+#include <opencv4/opencv2/core.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 class Ll2ImageConverter : public rclcpp::Node
@@ -14,33 +17,115 @@ public:
     this->get_parameter("map_topic", map_topic);
     this->get_parameter("pose_topic", pose_topic);
 
+    pub_image_ = this->create_publisher<sensor_msgs::msg::Image>("/ll2_image", 10);
     sub_map_ = this->create_subscription<autoware_auto_mapping_msgs::msg::HADMapBin>(map_topic, rclcpp::QoS(10).transient_local().reliable(), std::bind(&Ll2ImageConverter::mapCallback, this, std::placeholders::_1));
     sub_pose_stamped_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(pose_topic, 10, std::bind(&Ll2ImageConverter::poseCallback, this, std::placeholders::_1));
   }
 
 private:
-  void poseCallback(const geometry_msgs::msg::PoseStamped&)
+  void poseCallback(const geometry_msgs::msg::PoseStamped& pose_stamped)
   {
+    if (!visible_linestrings_.has_value()) {
+      RCLCPP_WARN_STREAM_THROTTLE(this->get_logger(), *this->get_clock(), 500, "wating for /map/vector_map");
+      return;
+    }
+
+    RCLCPP_INFO_STREAM(this->get_logger(), "try to make image");
+
+    Eigen::Vector3f pose;
+    {
+      const auto p = pose_stamped.pose.position;
+      pose << p.x, p.y, p.z;
+    }
+
+    auto toVector3f = [&pose](const lanelet::ConstPoint3d& p) -> Eigen::Vector3f {
+      Eigen::Vector3f v;
+      v << p.x(), p.y(), p.z();
+      return v - pose;
+    };
+
+    const float max_range = 50;  // [m]
+    auto checkIntersection = [max_range](const Eigen::Vector3f& from, const Eigen::Vector3f& to) -> bool {
+      float mu = to.squaredNorm() / (from.dot(to));
+      Eigen::Vector3f nearest = from + to * mu;
+      return nearest.norm() < max_range;
+    };
+
+    struct LineString {
+      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+      LineString(const Eigen::Vector3f& from, const Eigen::Vector3f& to) : from(from), to(to) {}
+      Eigen::Vector3f from, to;
+    };
+
+    std::vector<LineString> near_linestring;
+
+    for (const lanelet::LineString3d& line : *visible_linestrings_) {
+      // compute distance between pose and linesegment of linestring
+      std::optional<Eigen::Vector3f> from = std::nullopt;
+      for (const lanelet::ConstPoint3d p : line) {
+        Eigen::Vector3f to = toVector3f(p);
+        if (!from.has_value()) {
+          from = to;
+          continue;
+        }
+
+        if (checkIntersection(*from, to))
+          near_linestring.push_back({*from, to});
+        from = to;
+      }
+    }
+
+
+    cv::Mat image = cv::Mat::zeros(cv::Size{600, 600}, CV_8UC3);
+    // Draw image
+    {
+      const cv::Size center(image.cols / 2, image.rows / 2);
+      auto toCvPoint = [center](const Eigen::Vector3f v) -> cv::Point {
+        cv::Point pt;
+        pt.x = -v.x() / 30 * 600 + center.width;
+        pt.y = -v.y() / 30 * 600 + center.height;
+        return pt;
+      };
+
+      for (const LineString& ls : near_linestring) {
+        cv::line(image, toCvPoint(ls.from), toCvPoint(ls.to), cv::Scalar(0, 0, 255), 2, cv::LineTypes::LINE_8);
+      }
+    }
+
+    // Publish
+    publishImage(image, this->get_clock()->now());
+  }
+
+  void publishImage(const cv::Mat& image, const rclcpp::Time& stamp)
+  {
+    cv_bridge::CvImage raw_image;
+    raw_image.header.stamp = stamp;
+    raw_image.header.frame_id = "map";
+    raw_image.encoding = "bgr8";
+    raw_image.image = image;
+    pub_image_->publish(*raw_image.toImageMsg());
   }
 
   void mapCallback(const autoware_auto_mapping_msgs::msg::HADMapBin& msg)
   {
     lanelet::LaneletMapPtr viz_lanelet_map = fromBinMsg(msg);
-
     RCLCPP_INFO_STREAM(this->get_logger(), "lanelet: " << viz_lanelet_map->laneletLayer.size());
     RCLCPP_INFO_STREAM(this->get_logger(), "line: " << viz_lanelet_map->lineStringLayer.size());
     RCLCPP_INFO_STREAM(this->get_logger(), "point: " << viz_lanelet_map->pointLayer.size());
 
+    visible_linestrings_ = lanelet::LineStrings3d{};
+
     for (lanelet::LineString3d& line : viz_lanelet_map->lineStringLayer) {
       if (!line.hasAttribute(lanelet::AttributeName::Type)) continue;
       lanelet::Attribute attr = line.attribute(lanelet::AttributeName::Type);
-      if (attr.value() != "line_thin") continue;
+      if (attr.value() != "line_thin" && attr.value() != "pedestrian_marking") continue;
 
-      // for (const lanelet::ConstPoint3d& p : line) {
-      // }
+      visible_linestrings_->push_back(line);
     }
   }
 
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image_;
+  std::optional<lanelet::LineStrings3d> visible_linestrings_;
   rclcpp::Subscription<autoware_auto_mapping_msgs::msg::HADMapBin>::SharedPtr sub_map_;
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_pose_stamped_;
 };
