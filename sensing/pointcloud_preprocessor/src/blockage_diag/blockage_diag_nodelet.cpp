74c24fee49d06fdb357fc1e16289065f9ee72152
YusukeMizoguchi <sky.y.m.318@gmail.com>
2022-05-10 15:55:16 +0900

    update: change the number of frames to variable
    Signed-off-by: YusukeMizoguchi <sky.y.m.318@gmail.com>
    Signed-off-by: swift_file <sky.y.m.318@gmail.com>

--- sensing/pointcloud_preprocessor/src/blockage_diag/blockage_diag_nodelet.cpp
+++ sensing/pointcloud_preprocessor/src/blockage_diag/blockage_diag_nodelet.cpp
@@ -169,9 +169,8 @@ void BlockageDiagComponent::filter(
       cv::Rect(0, horizontal_ring_id_, horizontal_bins, vertical_bins - horizontal_ring_id_))
       .copyTo(ground_no_return_mask);
     cv::Mat time_series_blockage_mask(cv::Size(horizontal_bins, vertical_bins), CV_8UC1, cv::Scalar(0));
-    // TODO(yusuke-mizoguchi):  to adjust the number of frames" [todo]
     cv::Mat no_return_mask_binarized(cv::Size(horizontal_bins, vertical_bins), CV_8UC1, cv::Scalar(0));
-    static boost::circular_buffer<cv::Mat> no_return_mask_buffer(10);
+    static boost::circular_buffer<cv::Mat> no_return_mask_buffer(time_series_blockage_frames_);
       no_return_mask_binarized= no_return_mask / 255;
     no_return_mask_buffer.push_back(no_return_mask_binarized);
       for (const auto& binary_mask:no_return_mask_buffer) {
