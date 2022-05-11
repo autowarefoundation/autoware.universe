ac5b4c3e309dd8ca167adb1019b91cca8dfa3129
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2022-05-11 10:22:56 +0900

    feat(rviz_plugins): add velocity limit to autoware state panel (#879)
    * feat(rviz_plugins): add velocity limit to autoware state panel
    
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>
    
    * chore(rviz_plugin): change ms to kmh
    
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>
    Signed-off-by: Nobuo TAKAMASA <nobuo.takamasa@tier4.jp>

--- common/tier4_state_rviz_plugin/src/autoware_state_panel.hpp
+++ common/tier4_state_rviz_plugin/src/autoware_state_panel.hpp
@@ -20,6 +20,7 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QSpinBox>
+#include <QSpinBox>
 #include <rclcpp/rclcpp.hpp>
 #include <rviz_common/panel.hpp>
 
@@ -29,8 +30,6 @@
 #include <tier4_control_msgs/msg/external_command_selector_mode.hpp>
 #include <tier4_control_msgs/msg/gate_mode.hpp>
 #include <tier4_external_api_msgs/srv/engage.hpp>
-#include <tier4_external_api_msgs/srv/set_emergency.hpp>
-#include <tier4_planning_msgs/msg/approval.hpp>
 #include <tier4_planning_msgs/msg/velocity_limit.hpp>
 
 namespace rviz_plugins
@@ -46,9 +45,6 @@ public:
 public Q_SLOTS:  // NOLINT for Qt
   void onClickAutowareEngage();
   void onClickVelocityLimit();
-  void onClickGateMode();
-  void onClickPathChangeApproval();
-  void onClickEmergencyButton();
 
 protected:
   void onGateMode(const tier4_control_msgs::msg::GateMode::ConstSharedPtr msg);
@@ -80,6 +76,10 @@ protected:
   rclcpp::Publisher<tier4_control_msgs::msg::GateMode>::SharedPtr pub_gate_mode_;
   rclcpp::Publisher<tier4_planning_msgs::msg::Approval>::SharedPtr pub_path_change_approval_;
 
+  rclcpp::Publisher<tier4_planning_msgs::msg::VelocityLimit>::SharedPtr pub_velocity_limit_;
+
+  rclcpp::Publisher<tier4_planning_msgs::msg::VelocityLimit>::SharedPtr pub_velocity_limit_;
+
   QLabel * gate_mode_label_ptr_;
   QLabel * selector_mode_label_ptr_;
   QLabel * autoware_state_label_ptr_;
@@ -87,10 +87,7 @@ protected:
   QLabel * engage_status_label_ptr_;
   QPushButton * engage_button_ptr_;
   QPushButton * velocity_limit_button_ptr_;
-  QPushButton * gate_mode_button_ptr_;
-  QPushButton * path_change_approval_button_ptr_;
   QSpinBox * pub_velocity_limit_input_;
-  QPushButton * emergency_button_ptr_;
 
   bool current_engage_{false};
   bool current_emergency_{false};
