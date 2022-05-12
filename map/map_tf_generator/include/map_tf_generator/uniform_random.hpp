0a2a601ac7906ad6d444a0c56fc37221ec11b3dd
IshitaTakeshi <ishitah.takeshi@gmail.com>
2022-05-12 17:27:54 +0900

    Change argument types 'int' -> 'size_t'
    Signed-off-by: IshitaTakeshi <ishitah.takeshi@gmail.com>

--- map/map_tf_generator/include/map_tf_generator/uniform_random.hpp
+++ map/map_tf_generator/include/map_tf_generator/uniform_random.hpp
@@ -18,13 +18,13 @@
 #include <random>
 #include <vector>
 
-std::vector<size_t> UniformRandom(const int max_excluded, const int n)
+std::vector<size_t> UniformRandom(const size_t max_exclusive, const size_t n)
 {
   std::default_random_engine generator;
-  std::uniform_int_distribution<size_t> distribution(0, max_excluded - 1);
+  std::uniform_int_distribution<size_t> distribution(0, max_exclusive - 1);
 
   std::vector<size_t> v(n);
-  for (int i = 0; i < n; ++i) {
+  for (size_t i = 0; i < n; i++) {
     v[i] = distribution(generator);
   }
   return v;
