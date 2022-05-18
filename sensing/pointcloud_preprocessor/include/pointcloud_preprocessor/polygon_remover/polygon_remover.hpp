44e79247b0630cdf29c0832ad25dbfc3ae7416c4
liangliang fan <beginning.fan@autocore.ai>
2022-05-18 18:40:09 +0800

    feat(pointcloud_preprocessor): add polygon remover
    Signed-off-by: liangliang fan <beginning.fan@autocore.ai>

---
+++ sensing/pointcloud_preprocessor/include/pointcloud_preprocessor/polygon_remover/polygon_remover.hpp
@@ -0,0 +1,53 @@
+// Copyright 2022 The Autoware Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef POINTCLOUD_PREPROCESSOR__CROP_BOX_FILTER__CROP_BOX_FILTER_NODELET_HPP_
+#define POINTCLOUD_PREPROCESSOR__CROP_BOX_FILTER__CROP_BOX_FILTER_NODELET_HPP_
+
+#include "pointcloud_preprocessor/filter.hpp"
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+#include <geometry_msgs/msg/polygon_stamped.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_2_algorithms.h>
+
+using K = CGAL::Exact_predicates_inexact_constructions_kernel;
+using PointCgal = K::Point_2;
+
+namespace pointcloud_preprocessor
+{
+class PolygonRemoverComponent : public pointcloud_preprocessor::Filter
+{
+protected:
+  virtual void filter(const PointCloud2ConstPtr & input, const IndicesPtr & indices, PointCloud2 & output);
+
+  void publishRemovedPolygon();
+
+  void update_polygon(const geometry_msgs::msg::Polygon::ConstSharedPtr & polygon_in);
+  static std::vector<PointCgal> polygon_geometry_to_cgal(
+          const geometry_msgs::msg::Polygon::ConstSharedPtr & polygon_in);
+  PointCloud2 remove_updated_polygon_from_cloud(const PointCloud2ConstPtr & cloud_in);
+  PointCloud2 remove_polygon_cgal_from_cloud(
+    const PointCloud2::ConstSharedPtr & cloud_in_ptr,
+    const std::vector<PointCgal> & polyline_polygon);
+
+private:
+  rclcpp::Parameter param;
+  std::vector<double> polygon_vertices_;
+  geometry_msgs::msg::Polygon::SharedPtr polygon_;
+
+  bool polygon_is_initialized_;
+  bool will_visualize_;
+  std::vector<PointCgal> polygon_cgal_;
+  visualization_msgs::msg::Marker marker_;
+
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub_marker_ptr_;
+
+public:
+  PCL_MAKE_ALIGNED_OPERATOR_NEW
+  explicit PolygonRemoverComponent(const rclcpp::NodeOptions & options);
+};
+}  // namespace pointcloud_preprocessor
+
+#endif  // POINTCLOUD_PREPROCESSOR__CROP_BOX_FILTER__POLYGON_REMOVER_NODELET_HPP_
---
+++ sensing/pointcloud_preprocessor/launch/polygon_remover.launch.py
@@ -0,0 +1,44 @@
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+import os
+import yaml
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    ns = "pointcloud_preprocessor"
+    pkg = "pointcloud_preprocessor"
+
+    param_file = os.path.join(get_package_share_directory("vehicle_info_util"), "config/polygon_remover.yaml")
+        
+    with open(param_file, "r") as f:
+        polygon_remover_param = yaml.safe_load(f)["/**"]["ros__parameters"]
+
+    my_component = ComposableNode(
+        package=pkg,
+        plugin="pointcloud_preprocessor::PolygonRemoverComponent",
+        name="polygon_remover",
+        parameters=[
+            {
+                "polygon_vertices": polygon_remover_param["polygon_vertices"],
+                "will_visualize": polygon_remover_param["will_visualize"],
+            }
+        ],
+    )
+
+    # set container to run all required components in the same process
+    container = ComposableNodeContainer(
+        name="pointcloud_preprocessor_container",
+        namespace=ns,
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[my_component],
+        output="screen",
+    )
+
+    return launch.LaunchDescription(
+        [
+            container,
+        ]
+    )
