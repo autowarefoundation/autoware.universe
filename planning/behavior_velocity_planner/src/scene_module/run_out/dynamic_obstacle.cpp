effe284059e4e10d371b2b4d8c5311aae732df2c
Tomohito Ando <tomohito.ando@tier4.jp>
2022-05-31 13:38:15 +0900

    fix build error in humble
    Signed-off-by: Tomohito Ando <tomohito.ando@tier4.jp>

--- planning/behavior_velocity_planner/src/scene_module/run_out/dynamic_obstacle.cpp
+++ planning/behavior_velocity_planner/src/scene_module/run_out/dynamic_obstacle.cpp
@@ -63,7 +63,7 @@ pcl::PointCloud<pcl::PointXYZ> applyVoxelGridFilter(
 
   // use boost::makeshared instead of std beacause filter.setInputCloud requires boost shared ptr
   pcl::VoxelGrid<pcl::PointXYZ> filter;
-  filter.setInputCloud(boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>(no_height_points));
+  filter.setInputCloud(pcl::make_shared<pcl::PointCloud<pcl::PointXYZ>>(no_height_points));
   filter.setLeafSize(0.05f, 0.05f, 100000.0f);
 
   pcl::PointCloud<pcl::PointXYZ> output_points;
