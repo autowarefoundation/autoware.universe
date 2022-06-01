b8db5bf69d471d917a98d65ed23e51d3680558b2
ali <boyali@gmail.com>
2022-06-01 17:56:03 +0900

    added TF from tf_factor construciton.

--- control/autoware_control_toolbox/include/utils_act/tf_algebra.hpp
+++ control/autoware_control_toolbox/include/utils_act/tf_algebra.hpp
@@ -33,238 +33,235 @@
 #include "utils_act/act_utils.hpp"
 #include "visibility_control.hpp"
 #include "utils_act/act_definitions.hpp"
-#include "utils_act/transfer_functions.hpp"
-#include "utils_act/state_space.hpp"
 
 
 namespace ns_control_toolbox
-{
-
-
-	//using namespace boost::math;
-	using boost_polynomial = boost::math::tools::polynomial<double>; // for polynomial
-
-	/**
-	 * @brief Structs equipped with polynomial addition and multiplication property for the denominator and numerator
-	 * operations of the transfer function objects.
-	 * */
-	struct tf_factor_base
 	{
-	public:
-
-		tf_factor_base() = default;
-
-		explicit tf_factor_base(std::vector<double> factor) : factor_{ std::move(factor) }
-		{
-		}
-
-		std::vector<double> operator()() const
-		{
-			return factor_;
-		}
-
-		void power(unsigned int const& n)
-		{
-
-			if (n > 10)
+		
+		//using namespace boost::math;
+		using boost_polynomial = boost::math::tools::polynomial<double>; // for polynomial
+		
+		/**
+		 * @brief Structs equipped with polynomial addition and multiplication property for the denominator and numerator
+		 * operations of the transfer function objects.
+		 * */
+		struct tf_factor_base
 			{
-				throw std::invalid_argument("the power is too big than expected ...");
-			}
-			auto&& v1 = factor_; // we store in tf classes as x^3, x^2 ... 1
-
-
-			boost_polynomial p0{ 1. };
-			boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
-
-			for (size_t k = 0; k < n; ++k)
+		public:
+			
+			tf_factor_base() = default;
+			
+			explicit tf_factor_base(std::vector<double> factor) : factor_{ std::move(factor) }
+				{
+				}
+			
+			std::vector<double> operator()() const
+				{
+					return factor_;
+				}
+			
+			void power(unsigned int const& n)
+				{
+					
+					if (n > 10)
+						{
+							throw std::invalid_argument("the power is too big than expected ...");
+						}
+					auto&& v1 = factor_; // we store in tf classes as x^3, x^2 ... 1
+					
+					
+					boost_polynomial p0{ 1. };
+					boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
+					
+					for (size_t k = 0; k < n; ++k)
+						{
+							p0 *= p1;
+						}
+					
+					// Create a vector from the multiplication data.
+					std::vector<double> vresults(p0.data());
+					
+					// Reverse
+					std::reverse(vresults.begin(), vresults.end());
+					
+					factor_ = vresults;
+				}
+		
+		private:
+			std::vector<double> factor_{ 1. };
+			};
+		
+		
+		// OPERATORS
+		struct addition_tf_factors
 			{
-				p0 *= p1;
-			}
-
-			// Create a vector from the multiplication data.
-			std::vector<double> vresults(p0.data());
-
-			// Reverse
-			std::reverse(vresults.begin(), vresults.end());
-
-			factor_ = vresults;
-		}
-
-	private:
-		std::vector<double> factor_{ 1. };
-	};
-
-
-	// OPERATORS
-	struct addition_tf_factors
-	{
-
-		friend tf_factor_base operator+(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-			// zero pad shorter vector from left.
-			auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
-			auto&& v1 = tf_factor_vec_zpadded[0];
-			auto&& v2 = tf_factor_vec_zpadded[1];
-
-			std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+			
+			friend tf_factor_base operator+(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					// zero pad shorter vector from left.
+					auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
+					auto&& v1 = tf_factor_vec_zpadded[0];
+					auto&& v2 = tf_factor_vec_zpadded[1];
+					
+					std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+						{
+							return it1 + it2;
+						});
+					return tf_factor_base(v1);;
+				}
+			
+			
+			friend tf_factor_base operator+=(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					// zero pad shorter vector from left.
+					auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
+					auto&& v1 = tf_factor_vec_zpadded[0];
+					auto&& v2 = tf_factor_vec_zpadded[1];
+					
+					std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+						{
+							return it1 + it2;
+						});
+					return tf_factor_base(v1);;
+				}
+			};
+		
+		
+		struct subtraction_tf_factors
 			{
-				return it1 + it2;
-			});
-			return tf_factor_base(v1);;
-		}
-
-
-		friend tf_factor_base operator+=(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-			// zero pad shorter vector from left.
-			auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
-			auto&& v1 = tf_factor_vec_zpadded[0];
-			auto&& v2 = tf_factor_vec_zpadded[1];
-
-			std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+			
+			friend tf_factor_base operator-(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					// zero pad shorter vector from left.
+					auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
+					auto&& v1 = tf_factor_vec_zpadded[0];
+					auto&& v2 = tf_factor_vec_zpadded[1];
+					
+					std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+						{
+							return it1 - it2;
+						});
+					return tf_factor_base(v1);;
+				}
+			
+			
+			friend tf_factor_base operator-=(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					// zero pad shorter vector from left.
+					auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
+					auto&& v1 = tf_factor_vec_zpadded[0];
+					auto&& v2 = tf_factor_vec_zpadded[1];
+					
+					std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+						{
+							return it1 - it2;
+						});
+					return tf_factor_base(v1);;
+				}
+			};
+		
+		struct multiplication_tf_factors
 			{
-				return it1 + it2;
-			});
-			return tf_factor_base(v1);;
-		}
-	};
-
-
-	struct subtraction_tf_factors
-	{
-
-		friend tf_factor_base operator-(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-			// zero pad shorter vector from left.
-			auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
-			auto&& v1 = tf_factor_vec_zpadded[0];
-			auto&& v2 = tf_factor_vec_zpadded[1];
-
-			std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+			
+			friend tf_factor_base operator*(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
+					auto&& v2 = rhs();
+					
+					boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
+					boost_polynomial p2(v2.rbegin(), v2.rend());
+					
+					auto&& p3 = p1 * p2;
+					
+					// Create a vector from the multiplication data.
+					std::vector<double> vresults(p3.data());
+					
+					// Reverse
+					std::reverse(vresults.begin(), vresults.end());
+					
+					return tf_factor_base(vresults);
+				}
+			
+			
+			friend tf_factor_base operator*=(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					
+					auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
+					auto&& v2 = rhs();
+					
+					boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
+					boost_polynomial p2(v2.rbegin(), v2.rend());
+					
+					auto&& p3 = p1 * p2;
+					
+					// Create a vector from the multiplication data.
+					std::vector<double> vresults(p3.data());
+					
+					// Reverse
+					std::reverse(vresults.begin(), vresults.end());
+					
+					return tf_factor_base(vresults);
+				}
+			};
+		
+		struct division_tf_factors
 			{
-				return it1 - it2;
-			});
-			return tf_factor_base(v1);;
-		}
-
-
-		friend tf_factor_base operator-=(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-			// zero pad shorter vector from left.
-			auto tf_factor_vec_zpadded = ns_utils::zero_pad_left_make_equal(lhs(), rhs());
-			auto&& v1 = tf_factor_vec_zpadded[0];
-			auto&& v2 = tf_factor_vec_zpadded[1];
-
-			std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), v1.begin(), [](auto it1, auto const& it2)
+			
+			friend tf_factor_base operator/(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
+					auto&& v2 = rhs();
+					
+					boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
+					boost_polynomial p2(v2.rbegin(), v2.rend());
+					
+					auto&& p3 = p1 / p2;
+					
+					// Create a vector from the multiplication data.
+					std::vector<double> vresults(p3.data());
+					
+					// Reverse
+					std::reverse(vresults.begin(), vresults.end());
+					
+					return tf_factor_base(vresults);
+				}
+			
+			
+			friend tf_factor_base operator/=(tf_factor_base const& lhs, tf_factor_base const& rhs)
+				{
+					
+					auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
+					auto&& v2 = rhs();
+					
+					boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
+					boost_polynomial p2(v2.rbegin(), v2.rend());
+					
+					auto&& p3 = p1 / p2;
+					
+					// Create a vector from the multiplication data.
+					std::vector<double> vresults(p3.data());
+					
+					// Reverse
+					std::reverse(vresults.begin(), vresults.end());
+					
+					return tf_factor_base(vresults);
+				}
+			};
+		
+		
+		struct tf_arithmetic : addition_tf_factors,
+		                       subtraction_tf_factors,
+		                       multiplication_tf_factors //, division_tf_factors
 			{
-				return it1 - it2;
-			});
-			return tf_factor_base(v1);;
-		}
-	};
-
-	struct multiplication_tf_factors
-	{
-
-		friend tf_factor_base operator*(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-			auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
-			auto&& v2 = rhs();
-
-			boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
-			boost_polynomial p2(v2.rbegin(), v2.rend());
-
-			auto&& p3 = p1 * p2;
-
-			// Create a vector from the multiplication data.
-			std::vector<double> vresults(p3.data());
-
-			// Reverse
-			std::reverse(vresults.begin(), vresults.end());
-
-			return tf_factor_base(vresults);
-		}
-
-
-		friend tf_factor_base operator*=(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-
-			auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
-			auto&& v2 = rhs();
-
-			boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
-			boost_polynomial p2(v2.rbegin(), v2.rend());
-
-			auto&& p3 = p1 * p2;
-
-			// Create a vector from the multiplication data.
-			std::vector<double> vresults(p3.data());
-
-			// Reverse
-			std::reverse(vresults.begin(), vresults.end());
-
-			return tf_factor_base(vresults);
-		}
-	};
-
-	struct division_tf_factors
-	{
-
-		friend tf_factor_base operator/(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-			auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
-			auto&& v2 = rhs();
-
-			boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
-			boost_polynomial p2(v2.rbegin(), v2.rend());
-
-			auto&& p3 = p1 / p2;
-
-			// Create a vector from the multiplication data.
-			std::vector<double> vresults(p3.data());
-
-			// Reverse
-			std::reverse(vresults.begin(), vresults.end());
-
-			return tf_factor_base(vresults);
-		}
-
-
-		friend tf_factor_base operator/=(tf_factor_base const& lhs, tf_factor_base const& rhs)
-		{
-
-			auto&& v1 = lhs(); // we store in tf classes as x^3, x^2 ... 1
-			auto&& v2 = rhs();
-
-			boost_polynomial p1(v1.rbegin(), v1.rend()); // Boost stores 1, x, x^2 ... x^n
-			boost_polynomial p2(v2.rbegin(), v2.rend());
-
-			auto&& p3 = p1 / p2;
-
-			// Create a vector from the multiplication data.
-			std::vector<double> vresults(p3.data());
-
-			// Reverse
-			std::reverse(vresults.begin(), vresults.end());
-
-			return tf_factor_base(vresults);
-		}
-	};
-
-
-	struct tf_arithmetic : addition_tf_factors,
-	                       subtraction_tf_factors,
-	                       multiplication_tf_factors //, division_tf_factors
-	{
-	};
-
-
-	struct tf_factor : tf_factor_base, tf_arithmetic
-	{
-		using tf_factor_base::tf_factor_base;
-	};
-
-
-} // namespace ns_control_toolbox
+			};
+		
+		
+		struct tf_factor : tf_factor_base, tf_arithmetic
+			{
+			using tf_factor_base::tf_factor_base;
+			};
+		
+		
+	} // namespace ns_control_toolbox
 
 #endif //AUTOWARE_CONTROL_TOOLBOX_TF_ALGEBRA_HPP
--- control/autoware_control_toolbox/usage_examples/main_tf.cpp
+++ control/autoware_control_toolbox/usage_examples/main_tf.cpp
@@ -9,26 +9,34 @@
 
 
 int main()
-{
-
-	std::vector<double> num{ 1. };
-	std::vector<double> den{ 5.039e-07, 0.00019, 0.02387, 1 };
-
-	// With a num, den
-	ns_control_toolbox::tf sys(num, den);
-
-	// Print sys
-	sys.print();
-
-
-	// With a default constructor
-	ns_utils::print("\n\n");
-	ns_control_toolbox::tf sys_default;
-	sys_default.print();
-
-	// Inverse
-	sys.inv();
-	sys.print();
-
-	return 0;
-}
+	{
+		
+		std::vector<double> num{ 1. };
+		std::vector<double> den{ 5.039e-07, 0.00019, 0.02387, 1 };
+		
+		// With a num, den
+		ns_control_toolbox::tf sys(num, den);
+		
+		// Print sys
+		sys.print();
+		
+		
+		// With a default constructor
+		ns_utils::print("\n\n");
+		ns_control_toolbox::tf sys_default;
+		sys_default.print();
+		
+		// Inverse
+		sys.inv();
+		sys.print();
+		
+		// test construction from tf factors.
+		ns_control_toolbox::tf_factor ntf1{{ 1, 0, 0 }};
+		ns_control_toolbox::tf_factor dtf2{{ 1, 0, 0.2 }};
+		
+		auto tf_from_tf_factors = ns_control_toolbox::tf(ntf1, dtf2);
+		tf_from_tf_factors.print();
+		
+		
+		return 0;
+	}
