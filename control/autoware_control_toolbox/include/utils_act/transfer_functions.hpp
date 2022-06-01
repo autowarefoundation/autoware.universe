c409aca04aa64aaa8d2ccaff91ec4fe2bd23ddea
ali <boyali@gmail.com>
2022-06-01 17:56:03 +0900

    added TF from tf_factor construciton.

--- control/autoware_control_toolbox/include/utils_act/transfer_functions.hpp
+++ control/autoware_control_toolbox/include/utils_act/transfer_functions.hpp
@@ -22,80 +22,89 @@
 #include "act_utils.hpp"
 #include "act_utils_eigen.hpp"
 #include "act_definitions.hpp"
+#include "tf_algebra.hpp"
 
 namespace ns_control_toolbox
-{
-
-
-/**
- * @brief Transfer Function representation in descending power of  Laplace variable "s".
- * [s^n, s^{n-1} ... s^0]
- * @param Nn	: size of the numerator array.
- * @param Nd	: size of the denominator array
- * */
-
-	// For visibility we can set for each object : __attribute__((visibility("default"))) class_name
-	struct ACT_PUBLIC tf
 	{
-
-		// Constructors.
-		tf() : num_{ 1. }, den_{ 1. }
-		{
-		}
-
-		// Constructor from non-empty numerator and denominator std::vectors.
-		tf(std::vector<double> num, std::vector<double> den)
-				: num_{ std::move(num) }, den_{ std::move(den) }
-		{
-
-			ns_utils::stripVectorZerosFromLeft(num_); // remove zeros from the left.
-			ns_utils::stripVectorZerosFromLeft(den_);
-		}
-
-
-		// Member functions.
 		/**
-		 * @brief : Creates a string stream object of polynomial representation of given the vector,
+		 * @brief Transfer Function representation in descending power of  Laplace variable "s".
+		 * [s^n, s^{n-1} ... s^0]
+		 * @param Nn	: size of the numerator array.
+		 * @param Nd	: size of the denominator array
 		 * */
-		static size_t getPolynomialStringAndSize(std::vector<double> const& num_or_den,
-		                                         std::ostringstream& string_stream);
-
-
-		void print() const;
-
-		[[nodiscard]] std::vector<double> num() const
-		{
-			return num_;
-		}
-
-		[[nodiscard]] std::vector<double> den() const
-		{
-			return den_;
-		}
-
-		// Invert the transfer function.
-		void inv();
-
-
-	private:
-
-		// Data members
-		std::vector<double> num_;   // <-@brief numerator
-		std::vector<double> den_;   // <-@brief denominator
-	};
-
-/**
- * @param Td	: time delay value in seconds.
- * @param N		: Order of Pade approximation.
- * */
-	tf ACT_PUBLIC pade(double const& Td, size_t const& order);
-
-/**
- * @bried see pade()
- * @refitem Golub and Van Loan, Matrix Computations, 4rd edition, Chapter 9., Section 9.3.1 pp 530
- * */
-	tf ACT_PUBLIC padecoeff(double const& Td, size_t const& order);
-
-} // namespace ns_control_toolbox
+		
+		// For visibility, we can set for each object : __attribute__((visibility("default"))) class_name
+		struct ACT_PUBLIC tf
+			{
+			
+			// Constructors.
+			tf() : num_{ 1. }, den_{ 1. }
+				{
+				}
+			
+			// Constructor from non-empty numerator and denominator std::vectors.
+			tf(std::vector<double> num, std::vector<double> den)
+					: num_{ std::move(num) }, den_{ std::move(den) }
+				{
+					
+					ns_utils::stripVectorZerosFromLeft(num_); // remove zeros from the left.
+					ns_utils::stripVectorZerosFromLeft(den_);
+				}
+			
+			// Constructor from tf_factors
+			tf(tf_factor const& num, tf_factor const& den);
+			
+			// Member functions.
+			/**
+			 * @brief : Creates a string stream object of polynomial representation of given the vector,
+			 * */
+			static size_t getPolynomialStringAndSize(std::vector<double> const& num_or_den,
+			                                         std::ostringstream& string_stream);
+			
+			
+			void print() const;
+			
+			[[nodiscard]] std::vector<double> num() const
+				{
+					return num_;
+				}
+			
+			[[nodiscard]] std::vector<double> den() const
+				{
+					return den_;
+				}
+			
+			// Invert the transfer function.
+			void inv();
+			
+			// Update num and den
+			void update_num(std::vector<double> const& num);
+			
+			void update_num(tf_factor const& num);
+			
+			void update_den(std::vector<double> const& den);
+			
+			void update_den(tf_factor const& den);
+		
+		private:
+			
+			// Data members
+			std::vector<double> num_;   // <-@brief numerator
+			std::vector<double> den_;   // <-@brief denominator
+			};
+		
+		/**
+		 * @param Td	: time delay value in seconds.
+		 * @param N		: Order of Pade approximation.
+		 * */
+		tf ACT_PUBLIC pade(double const& Td, size_t const& order);
+		
+		/**
+		 * @bried see pade()
+		 * @refitem Golub and Van Loan, Matrix Computations, 4rd edition, Chapter 9., Section 9.3.1 pp 530
+		 * */
+		tf ACT_PUBLIC padecoeff(double const& Td, size_t const& order);
+		
+	} // namespace ns_control_toolbox
 
 #endif //AUTOWARE_CONTROL_TOOLBOX_TRANSFER_FUNCTIONS_HPP
--- control/autoware_control_toolbox/src/utils_act/transfer_functions.cpp
+++ control/autoware_control_toolbox/src/utils_act/transfer_functions.cpp
@@ -18,155 +18,185 @@
 
 size_t ns_control_toolbox::tf::getPolynomialStringAndSize(std::vector<double> const& num_or_den,
                                                           std::ostringstream& string_stream)
-{
-
-	auto Nn = num_or_den.size();
-	int precision = 4;
-
-	for (size_t k = 0; k < Nn; ++k)
 	{
-
-		auto&& power_of_term = Nn - 1 - k;
-		auto&& coeff_abs = std::abs(num_or_den.at(k));
-
-		if (std::fabs(num_or_den.at(k)) > EPS)
-		{
-			// to put "+" or "-" char in front of "s"
-			auto sign_str = num_or_den.at(k) > 0 ? " + " : " - ";
-
-			// k is not the last idx.
-			if (k != Nn - 1)
+		
+		auto Nn        = num_or_den.size();
+		int  precision = 4;
+		
+		for (size_t k = 0; k < Nn; ++k)
 			{
-				// if k==0, do not use +s for the first term of the polynomial.
-				if (k == 0 && num_or_den.at(k) > 0)
-				{
-					sign_str = "";
-				}
-
-				// If the coefficient is 1 do not print 1s --> instead print s
-				if (std::fabs(std::fabs(num_or_den.at(k)) - 1) > EPS)
-				{
-					string_stream << sign_str << std::setprecision(precision) << coeff_abs;
-				}
-				else
-				{
-					string_stream << sign_str;
-				}
-
-				// if power is 1 do not print s^1 --> instead s
-				if (power_of_term != 1)
-				{
-					string_stream << " s^" << power_of_term;
-				}
-				else
-				{
-					string_stream << " s";
-				}
-
+				
+				auto&& power_of_term = Nn - 1 - k;
+				auto&& coeff_abs     = std::abs(num_or_den.at(k));
+				
+				if (std::fabs(num_or_den.at(k)) > EPS)
+					{
+						// to put "+" or "-" char in front of "s"
+						auto sign_str = num_or_den.at(k) > 0 ? " + " : " - ";
+						
+						// k is not the last idx.
+						if (k != Nn - 1)
+							{
+								// if k==0, do not use +s for the first term of the polynomial.
+								if (k == 0 && num_or_den.at(k) > 0)
+									{
+										sign_str = "";
+									}
+								
+								// If the coefficient is 1 do not print 1s --> instead print s
+								if (std::fabs(std::fabs(num_or_den.at(k)) - 1) > EPS)
+									{
+										string_stream << sign_str << std::setprecision(precision) << coeff_abs;
+									}
+								else
+									{
+										string_stream << sign_str;
+									}
+								
+								// if power is 1 do not print s^1 --> instead s
+								if (power_of_term != 1)
+									{
+										string_stream << " s^" << power_of_term;
+									}
+								else
+									{
+										string_stream << " s";
+									}
+								
+							}
+						else
+							{               // if k is the last index
+								if (Nn > 1)
+									{
+										string_stream << sign_str << std::setprecision(precision) << coeff_abs;
+									}
+								else
+									{
+										string_stream << num_or_den.at(k);
+									}
+								
+							}
+					}
 			}
-			else
-			{               // if k is the last index
-				if (Nn > 1)
-				{
-					string_stream << sign_str << std::setprecision(precision) << coeff_abs;
-				}
-				else
-				{
-					string_stream << num_or_den.at(k);
-				}
-
-			}
-		}
+		return string_stream.str().length();
 	}
-	return string_stream.str().length();
-}
 
 void ns_control_toolbox::tf::print() const
-{
-
-	std::ostringstream num_string;
-	std::ostringstream den_string;
-
-	auto num_string_size = getPolynomialStringAndSize(num_, num_string);
-	auto den_string_size = getPolynomialStringAndSize(den_, den_string);
-
-	if (num_string_size > den_string_size)
 	{
-		size_t num_of_tab = (num_string_size - den_string_size) / 4;
-		ns_utils::print(num_string.str());
-		ns_utils::print(std::string(std::max(num_string_size, den_string_size), '-'));
-		ns_utils::print(std::string(num_of_tab, ' '), den_string.str());
-
+		
+		std::ostringstream num_string;
+		std::ostringstream den_string;
+		
+		auto num_string_size = getPolynomialStringAndSize(num_, num_string);
+		auto den_string_size = getPolynomialStringAndSize(den_, den_string);
+		
+		if (num_string_size > den_string_size)
+			{
+				size_t num_of_tab = (num_string_size - den_string_size) / 4;
+				ns_utils::print(num_string.str());
+				ns_utils::print(std::string(std::max(num_string_size, den_string_size), '-'));
+				ns_utils::print(std::string(num_of_tab, ' '), den_string.str());
+				
+			}
+		else if (num_string_size < den_string_size)
+			{
+				size_t num_of_tab = (den_string_size - num_string_size) / 4;
+				ns_utils::print(std::string(num_of_tab, ' '), num_string.str());
+				ns_utils::print(std::string(std::max(num_string_size, den_string_size), '-'));
+				ns_utils::print(den_string.str());
+				
+			}
+		else
+			{
+				
+				ns_utils::print(num_string.str());
+				ns_utils::print(std::string(std::max(num_string_size, den_string_size), '-'));
+				ns_utils::print(den_string.str());
+			}
+		
+		ns_utils::print("\nContinuous-time transfer function.\n");
+		
 	}
-	else if (num_string_size < den_string_size)
-	{
-		size_t num_of_tab = (den_string_size - num_string_size) / 4;
-		ns_utils::print(std::string(num_of_tab, ' '), num_string.str());
-		ns_utils::print(std::string(std::max(num_string_size, den_string_size), '-'));
-		ns_utils::print(den_string.str());
-
-	}
-	else
-	{
-
-		ns_utils::print(num_string.str());
-		ns_utils::print(std::string(std::max(num_string_size, den_string_size), '-'));
-		ns_utils::print(den_string.str());
-	}
-
-	ns_utils::print("\nContinuous-time transfer function.\n");
-
-}
 
 void ns_control_toolbox::tf::inv()
-{
-	std::vector<double> temp_den{ std::move(den_) };
-	den_ = std::move(num_);
-	num_ = std::move(temp_den);
+	{
+		std::vector<double> temp_den{ std::move(den_) };
+		den_ = std::move(num_);
+		num_ = std::move(temp_den);
+		
+	}
 
-}
+ns_control_toolbox::tf::tf(const ns_control_toolbox::tf_factor& num, const ns_control_toolbox::tf_factor& den)
+	{
+		num_ = num();
+		den_ = den();
+		
+	}
+
+void ns_control_toolbox::tf::update_num(std::vector<double> const& num)
+	{
+		
+		num_ = num;
+	}
+
+void ns_control_toolbox::tf::update_num(tf_factor const& num)
+	{
+		
+		num_ = num();
+	}
+
+void ns_control_toolbox::tf::update_den(const std::vector<double>& den)
+	{
+		den_ = den;
+		
+	}
+
+void ns_control_toolbox::tf::update_den(const ns_control_toolbox::tf_factor& den)
+	{
+		den_ = den();
+	}
 
 
 ns_control_toolbox::tf ns_control_toolbox::padecoeff(const double& Td, const size_t& order)
-{
-	if (std::fabs(Td) <= EPS)
 	{
-		return {};             // constant tf.
+		if (std::fabs(Td) <= EPS)
+			{
+				return {};             // constant tf.
+			}
+		
+		auto&& n = order + 1;
+		std::vector<double> num(n, 0.0);
+		std::vector<double> den(n, 0.0);
+		
+		num.back() = 1.;
+		den.back() = 1.;
+		
+		// Double conversion of order
+		double order_double_t{ static_cast<double>(order) };
+		
+		for (size_t k = 1; k < n; ++k)
+			{
+				double k_double_t{ static_cast<double>(k) };
+				double fact = Td * (order_double_t - k_double_t + 1) / (2. * order_double_t - k_double_t + 1) /
+				              k_double_t;
+				num[n - 1 - k] = -fact * num[n - k];
+				den[n - 1 - k] = fact * den[n - k];
+			}
+		
+		auto const den0 = den[0];
+		std::transform(num.begin(), num.end(), num.begin(),
+		               [&den0](auto& x)
+			               { return x / den0; });
+		
+		std::transform(den.begin(), den.end(), den.begin(),
+		               [&den0](auto& x)
+			               { return x / den0; });
+		
+		return { num, den };
 	}
-
-	auto&& n = order + 1;
-	std::vector<double> num(n, 0.0);
-	std::vector<double> den(n, 0.0);
-
-	num.back() = 1.;
-	den.back() = 1.;
-
-	// Double conversion of order
-	double order_double_t{ static_cast<double>(order) };
-
-	for (size_t k = 1; k < n; ++k)
-	{
-		double k_double_t{ static_cast<double>(k) };
-		double fact = Td * (order_double_t - k_double_t + 1) / (2. * order_double_t - k_double_t + 1) /
-		              k_double_t;
-		num[n - 1 - k] = -fact * num[n - k];
-		den[n - 1 - k] = fact * den[n - k];
-	}
-
-	auto const den0 = den[0];
-	std::transform(num.begin(), num.end(), num.begin(),
-	               [&den0](auto& x)
-	               { return x / den0; });
-
-	std::transform(den.begin(), den.end(), den.begin(),
-	               [&den0](auto& x)
-	               { return x / den0; });
-
-	return { num, den };
-}
 
 ns_control_toolbox::tf ns_control_toolbox::pade(const double& Td, const size_t& order)
-{
-	return padecoeff(Td, order);
-
-}
+	{
+		return padecoeff(Td, order);
+		
+	}
