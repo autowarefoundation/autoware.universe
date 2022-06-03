79406720b50dfc9e2cf1177c0624d162333b7267
h-ohta <hiroki.ota@tier4.jp>
2022-06-03 12:13:54 +0900

    fix(lanelet2_extension): fix low FPS when visualizing center_line_arrows at a large map
    Signed-off-by: h-ohta <hiroki.ota@tier4.jp>

--- map/lanelet2_extension/include/lanelet2_extension/visualization/visualization.hpp
+++ map/lanelet2_extension/include/lanelet2_extension/visualization/visualization.hpp
@@ -84,14 +84,26 @@ void pushLineStringMarker(
   const std_msgs::msg::ColorRGBA c, const float lss = 0.1);
 
 /**
- * [pushArrowMarkerArray pushes marker to visualize arrows]
- * @param marker_array     [output marker array message]
- * @param ls         [input linestring]
+ * [initArrowsMarker initializes marker to visualize arrows with TRIANGLE_LIST]
+ * @param marker     [output marker message]
+ * @param frame_id   [frame id of the marker]
+ * @param ns         [namespace of the marker]
  * @param c          [color of the marker]
  */
-void pushArrowMarkerArray(
-  visualization_msgs::msg::MarkerArray * marker_array, const lanelet::ConstLineString3d & ls,
-  const std::string frame_id, const std::string ns, const std_msgs::msg::ColorRGBA c);
+void initArrowsMarker(
+  visualization_msgs::msg::Marker * marker, const std::string frame_id, const std::string ns,
+  const std_msgs::msg::ColorRGBA c);
+
+/**
+ * [pushArrowsMarker pushes marker to visualize arrows with TRIANGLE_LIST]
+ * @param marker     [output marker message]
+ * @param ls         [input linestring]
+ * @param c          [color of the marker]
+ * @param lss        [thickness of the marker]
+ */
+void pushArrowsMarker(
+  visualization_msgs::msg::Marker * marker, const lanelet::ConstLineString3d & ls,
+  const std_msgs::msg::ColorRGBA c, const float lss = 0.1);
 
 /**
  * [initTrafficLightTriangleMarker initializes marker to visualize shape of traffic
