4dfdc5fcdf76cb02ceb60c1c2130e6a319eebf29
ali <boyali@gmail.com>
2022-06-05 22:17:08 +0900

    Verified sinusoidal signal productions.

--- control/autoware_control_toolbox/usage_examples/main_signal_generator.cpp
+++ control/autoware_control_toolbox/usage_examples/main_signal_generator.cpp
@@ -6,11 +6,23 @@
 
 int main()
 {
-	auto time_vec = ns_control_toolbox::make_time_signal(0.1, 10);
+	double tfinal = 10.;
+	double dt = 1. / 40.;
+	auto time_vec = ns_control_toolbox::make_time_signal(dt, tfinal);
+
+	ns_utils::print("Time points");
 	ns_eigen_utils::printEigenMat(time_vec);
 
-	double frequency_hz = 2;
+
+	// Creating a sinusoidal points.
+	double frequency_hz = 1;
+
+	auto sin_signal = ns_control_toolbox::make_sinus_signal(time_vec, frequency_hz);
+	ns_utils::print("Sinusoidal Signal");
+	ns_eigen_utils::printEigenMat(sin_signal);
+
 	// test triangle wave
+
 	auto triangle_wave = ns_control_toolbox::make_triangle_signal(time_vec, frequency_hz);
 	ns_eigen_utils::printEigenMat(triangle_wave);
 	return 0;
