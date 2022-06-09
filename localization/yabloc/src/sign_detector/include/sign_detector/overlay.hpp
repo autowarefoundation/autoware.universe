714e4c91bca3898e561f6338df03e3b46734d6a9
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2022-06-09 16:27:37 +0900

    interactive changing extrinsic parameter
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/src/sign_detector/include/sign_detector/overlay.hpp
+++ localization/yabloc/src/sign_detector/include/sign_detector/overlay.hpp
@@ -32,29 +32,51 @@ public:
   {
     using std::placeholders::_1;
     using namespace std::literals::chrono_literals;
+    // Subscriber
+    {
+      const rclcpp::QoS qos = rclcpp::QoS(10);
+      sub_info_ = create_subscription<sensor_msgs::msg::CameraInfo>(
+        "/sensing/camera/traffic_light/camera_info", 10,
+        std::bind(&Overlay::infoCallback, this, _1));
+      sub_image_ = create_subscription<sensor_msgs::msg::Image>(
+        "/sensing/camera/traffic_light/image_raw/compressed", qos,
+        std::bind(&Overlay::imageCallback, this, _1));
+      sub_pose_ = create_subscription<geometry_msgs::msg::PoseStamped>(
+        "/particle_pose", 10, std::bind(&Overlay::poseCallback, this, _1));
+      sub_cloud_pose_ = create_subscription<CloudWithPose>(
+        "/ll2_cloud", 10, std::bind(&Overlay::cloudPoseCallback, this, _1));
+      sub_cloud_ = create_subscription<PointCloud2>(
+        "/lsd_cloud", 10, std::bind(&Overlay::cloudCallback, this, _1));
 
-    const rclcpp::QoS qos = rclcpp::QoS(10);
-    sub_info_ = create_subscription<sensor_msgs::msg::CameraInfo>(
-      "/sensing/camera/traffic_light/camera_info", 10, std::bind(&Overlay::infoCallback, this, _1));
-    sub_image_ = create_subscription<sensor_msgs::msg::Image>(
-      "/sensing/camera/traffic_light/image_raw/compressed", qos,
-      std::bind(&Overlay::imageCallback, this, _1));
-    sub_pose_ = create_subscription<geometry_msgs::msg::PoseStamped>(
-      "/particle_pose", 10, std::bind(&Overlay::poseCallback, this, _1));
-    sub_cloud_pose_ = create_subscription<CloudWithPose>(
-      "/ll2_cloud", 10, std::bind(&Overlay::cloudPoseCallback, this, _1));
-    sub_cloud_ = create_subscription<PointCloud2>(
-      "/lsd_cloud", 10, std::bind(&Overlay::cloudCallback, this, _1));
+      tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+      transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    }
+    // Publisher
+    {
+      pub_vis_ = create_publisher<Marker>("/marker", 10);
+      pub_image_ = create_publisher<sensor_msgs::msg::Image>("/overlay_image", 10);
+    }
 
-    pub_vis_ = create_publisher<Marker>("/marker", 10);
-
-    pub_image_ = create_publisher<sensor_msgs::msg::Image>("/overlay_image", 10);
-
-    tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-    transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    {
+      declare_parameter("offset", std::vector<double>{0, 0, 0, 0, 0, 0});
+      param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
+      auto cb = [this](const rclcpp::Parameter & p) {
+        if (p.as_double_array().size() == 6) {
+          auto v = p.as_double_array();
+          this->ex_extrinsic_ = buildAffine(v);
+          RCLCPP_WARN_STREAM(get_logger(), "update: " << this->ex_extrinsic_.matrix());
+        }
+      };
+      cb_handle_ = param_subscriber_->add_parameter_callback("offset", cb);
+    }
   }
 
 private:
+  std::shared_ptr<rclcpp::ParameterEventHandler> param_subscriber_;
+  std::shared_ptr<rclcpp::ParameterCallbackHandle> cb_handle_;
+
+  Eigen::Affine3f ex_extrinsic_;
+
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_image_;
   rclcpp::Subscription<CloudWithPose>::SharedPtr sub_cloud_pose_;
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr sub_pose_;
@@ -81,4 +103,15 @@ private:
 
   void overlay(const cv::Mat & image, const Pose & pose, const rclcpp::Time & stamp);
   void makeVisMarker(const LineSegments & ls, const Pose & pose, const rclcpp::Time & stamp);
+
+  Eigen::Affine3f buildAffine(const std::vector<double> & v)
+  {
+    Eigen::Affine3f a;
+    double r = v[3], p = v[4], y = v[5];
+    Eigen::Translation3f t(v[0], v[1], v[2]);
+    Eigen::Quaternionf R = Eigen::AngleAxisf(y, Eigen::Vector3f::UnitZ()) *
+                           Eigen::AngleAxisf(p, Eigen::Vector3f::UnitY()) *
+                           Eigen::AngleAxisf(r, Eigen::Vector3f::UnitX());
+    return t * R;
+  }
 };
