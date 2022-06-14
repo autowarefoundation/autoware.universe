71fd5f980c40d342b1d7146e38db4fea62a715fe
Muhammad Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2022-06-15 02:37:56 +0900

    feat(behavior_path_planner): add debug marker to side shift module
    Signed-off-by: Muhammad Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/behavior_path_planner/src/scene_module/side_shift/side_shift_module.cpp
+++ planning/behavior_path_planner/src/scene_module/side_shift/side_shift_module.cpp
@@ -14,6 +14,7 @@
 
 #include "behavior_path_planner/scene_module/side_shift/side_shift_module.hpp"
 
+#include "behavior_path_planner/debug_utilities.hpp"
 #include "behavior_path_planner/path_utilities.hpp"
 #include "behavior_path_planner/scene_module/side_shift/util.hpp"
 #include "behavior_path_planner/utilities.hpp"
@@ -70,6 +71,7 @@ SideShiftModule::SideShiftModule(
 void SideShiftModule::initVariables()
 {
   reference_path_ = std::make_shared<PathWithLaneId>();
+  debug_data_.path_shifter = nullptr;
   start_pose_reset_request_ = false;
   lateral_offset_ = 0.0;
   prev_output_ = ShiftedPath{};
@@ -243,6 +245,7 @@ bool SideShiftModule::addShiftPoint()
 
   // set to path_shifter
   path_shifter_.setShiftPoints(shift_points);
+
   lateral_offset_change_request_ = false;
 
   return true;
@@ -257,6 +260,8 @@ BehaviorModuleOutput SideShiftModule::plan()
     RCLCPP_DEBUG(getLogger(), "change is not requested");
   }
 
+  // *debug_data_.path_shifter = path_shifter_;
+
   // Refine path
   ShiftedPath shifted_path;
   path_shifter_.generate(&shifted_path);
@@ -270,6 +275,9 @@ BehaviorModuleOutput SideShiftModule::plan()
   output.path = std::make_shared<PathWithLaneId>(shifted_path.path);
 
   prev_output_ = shifted_path;
+
+  *debug_data_.path_shifter = path_shifter_;
+  setDebugMarkersVisualization();
 
   return output;
 }
@@ -452,4 +460,24 @@ PathWithLaneId SideShiftModule::calcCenterLinePath(
   return centerline_path;
 }
 
+void SideShiftModule::setDebugMarkersVisualization() const
+{
+  using marker_utils::createShiftPointMarkerArray;
+
+  debug_marker_.markers.clear();
+
+  const auto add = [this](const MarkerArray & added) {
+    tier4_autoware_utils::appendMarkerArray(added, &debug_marker_);
+  };
+
+  const auto addShiftPoint = [this, add](const auto & ns, auto r, auto g, auto b, double w = 0.1) {
+    add(createShiftPointMarkerArray(
+      debug_data_.path_shifter->getShiftPoints(), debug_data_.path_shifter->getBaseOffset(), ns, r,
+      g, b, w));
+  };
+
+  if (debug_data_.path_shifter) {
+    addShiftPoint("side_shift_shift_points", 0.7, 0.7, 0.7, 0.4);
+  }
+}
 }  // namespace behavior_path_planner
