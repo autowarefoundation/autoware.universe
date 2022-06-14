cfcbf40ca20fe1daaf8156055b9470f4ec716c99
kminoda <koji.m.minoda@gmail.com>
2022-06-14 11:07:40 +0900

    reflected some review comments
    Signed-off-by: kminoda <koji.m.minoda@gmail.com>

--- localization/ekf_localizer/src/ekf_localizer.cpp
+++ localization/ekf_localizer/src/ekf_localizer.cpp
@@ -169,8 +169,8 @@ void EKFLocalizer::timerCallback()
         current_pose_info_queue_.push(pose_info);
       }
     }
-    DEBUG_INFO(get_logger(), "[EKF] measurementUpdatePose calc time = %f [ms]", stop_watch_.toc()); DEBUG_INFO(get_logger(), "------------------------- end Pose
-    -------------------------\n");
+    DEBUG_INFO(get_logger(), "[EKF] measurementUpdatePose calc time = %f [ms]", stop_watch_.toc());
+    DEBUG_INFO(get_logger(), "------------------------- end Pose -------------------------\n");
   }
 
   /* twist measurement update */
