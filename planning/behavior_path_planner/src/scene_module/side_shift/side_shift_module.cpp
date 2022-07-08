653c6ea5fadae168d8c194ed7e673281a39f1a52
Muhammad Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2022-07-08 11:09:05 +0900

    fix pointer error due to direct assignment
    added make_shared
    
    Signed-off-by: Muhammad Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/behavior_path_planner/src/scene_module/side_shift/side_shift_module.cpp
+++ planning/behavior_path_planner/src/scene_module/side_shift/side_shift_module.cpp
@@ -276,8 +276,6 @@ BehaviorModuleOutput SideShiftModule::plan()
     RCLCPP_DEBUG(getLogger(), "change is not requested");
   }
 
-  // *debug_data_.path_shifter = path_shifter_;
-
   // Refine path
   ShiftedPath shifted_path;
   path_shifter_.generate(&shifted_path);
@@ -294,7 +292,7 @@ BehaviorModuleOutput SideShiftModule::plan()
   prev_output_ = shifted_path;
   path_reference_ = getPreviousModuleOutput().reference_path;
 
-  *debug_data_.path_shifter = path_shifter_;
+  *debug_data_.path_shifter = std::make_shared<PathShifter>(path_shifter_);
   setDebugMarkersVisualization();
 
   return output;
