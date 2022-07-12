4eddeace54746ad298baa36575d692c6d672328c
tomoya.kimura <tomoya.kimura@tier4.jp>
2022-07-12 22:23:24 +0900

    use same function
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

--- planning/behavior_velocity_planner/src/utilization/util.cpp
+++ planning/behavior_velocity_planner/src/utilization/util.cpp
@@ -593,15 +593,16 @@ LineString2d extendLine(
     {(p1 - length * t).x(), (p1 - length * t).y()}, {(p2 + length * t).x(), (p2 + length * t).y()}};
 }
 
-std::vector<lanelet::ConstLanelet> getLaneletsOnPath(
+boost::optional<int64_t> getNearestLaneId(
   const autoware_auto_planning_msgs::msg::PathWithLaneId & path,
-  const lanelet::LaneletMapPtr lanelet_map, const geometry_msgs::msg::Pose & current_pose)
+  const lanelet::LaneletMapPtr lanelet_map, const geometry_msgs::msg::Pose & current_pose,
+  boost::optional<size_t> & nearest_segment_idx)
 {
-  std::vector<int64_t> unique_lane_ids;
-  auto nearest_segment_idx = tier4_autoware_utils::findNearestSegmentIndex(
+  boost::optional<int64_t> nearest_lane_id;
+
+  nearest_segment_idx = tier4_autoware_utils::findNearestSegmentIndex(
     path.points, current_pose, std::numeric_limits<double>::max(), M_PI_2);
 
-  // Add current lane id
   lanelet::ConstLanelets current_lanes;
   if (
     lanelet::utils::query::getCurrentLanelets(
@@ -609,22 +610,35 @@ std::vector<lanelet::ConstLanelet> getLaneletsOnPath(
     nearest_segment_idx) {
     for (const auto & ll : current_lanes) {
       if (ll.id() == path.points.at(*nearest_segment_idx).lane_ids.at(0)) {
-        unique_lane_ids.emplace_back(ll.id());
-        break;
+        *nearest_lane_id = ll.id();
+        return nearest_lane_id;
       }
     }
 
     // if the lane_id of nearest_segment_idx does not involved in current_lanes,
     // search the lane_id of nearest_segment_idx + 1
-    if (unique_lane_ids.empty()) {
-      *nearest_segment_idx += 1;
-      for (const auto & ll : current_lanes) {
-        if (ll.id() == path.points.at(*nearest_segment_idx).lane_ids.at(0)) {
-          unique_lane_ids.emplace_back(ll.id());
-          break;
-        }
+    *nearest_segment_idx += 1;
+    for (const auto & ll : current_lanes) {
+      if (ll.id() == path.points.at(*nearest_segment_idx).lane_ids.at(0)) {
+        *nearest_lane_id = ll.id();
+        return nearest_lane_id;
       }
     }
+  }
+  return boost::none;
+}
+
+std::vector<lanelet::ConstLanelet> getLaneletsOnPath(
+  const autoware_auto_planning_msgs::msg::PathWithLaneId & path,
+  const lanelet::LaneletMapPtr lanelet_map, const geometry_msgs::msg::Pose & current_pose)
+{
+  boost::optional<size_t> nearest_segment_idx;
+  const auto nearest_lane_id =
+    getNearestLaneId(path, lanelet_map, current_pose, nearest_segment_idx);
+
+  std::vector<int64_t> unique_lane_ids;
+  if (nearest_lane_id) {
+    unique_lane_ids.emplace_back(*nearest_lane_id);
   }
 
   // Add forward path lane_id
