82ace54a240b8e52731aa8586613a8de2e174a34
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2022-07-22 14:14:21 +0900

    load multiple reference rosbags
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/src/vector_map_visual_localizer/include/validation/ape.hpp
+++ localization/yabloc/src/vector_map_visual_localizer/include/validation/ape.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <rclcpp/rclcpp.hpp>
 
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <std_msgs/msg/string.hpp>
@@ -13,14 +14,24 @@ public:
   using String = std_msgs::msg::String;
   using Odometry = nav_msgs::msg::Odometry;
   using PoseCovStamped = geometry_msgs::msg::PoseWithCovarianceStamped;
+  using PoseStamped = geometry_msgs::msg::PoseStamped;
 
   AbsolutePoseError();
 
 private:
-  const std::string reference_bags_path_;
+  struct Reference
+  {
+    Reference() { poses_.reserve(3e4); }
+    rclcpp::Time start_stamp_;
+    rclcpp::Time end_stamp_;
+    std::vector<PoseStamped> poses_;
+  };
+
   rclcpp::Publisher<String>::SharedPtr pub_string_;
   rclcpp::Subscription<PoseCovStamped>::SharedPtr sub_pose_cov_stamped_;
+  std::unordered_map<std::string, Reference> references_;
 
   void poseCallback(const PoseCovStamped & pose_cov);
+  void loadRosbag(const std::string & bag_file);
 };
 }  // namespace validation
--- localization/yabloc/src/vector_map_visual_localizer/src/validation/ape_node.cpp
+++ localization/yabloc/src/vector_map_visual_localizer/src/validation/ape_node.cpp
@@ -2,35 +2,53 @@
 #include "rosbag2_cpp/readers/sequential_reader.hpp"
 #include "validation/ape.hpp"
 
+#include <filesystem>
 #include <sstream>
-
 namespace validation
 {
-AbsolutePoseError::AbsolutePoseError()
-: Node("ape_node"), reference_bags_path_(declare_parameter<std::string>("reference_bags_path", ""))
+AbsolutePoseError::AbsolutePoseError() : Node("ape_node")
 {
   using std::placeholders::_1;
   auto cb_pose = std::bind(&AbsolutePoseError::poseCallback, this, _1);
   sub_pose_cov_stamped_ = create_subscription<PoseCovStamped>("/pose_with_covariance", 1, cb_pose);
   pub_string_ = create_publisher<String>("/ape_diag", 10);
 
-  RCLCPP_INFO_STREAM(get_logger(), "reference bag path: " << reference_bags_path_);
+  const std::string reference_bags_path = declare_parameter<std::string>("reference_bags_path", "");
+  RCLCPP_INFO_STREAM(get_logger(), "reference bag path: " << reference_bags_path);
 
-  {
-    std::cout << "try to read rosbag2" << std::endl;
-    rosbag2_cpp::Reader reader;
-    reader.open(reference_bags_path_);
-    while (reader.has_next()) {
-      auto bag_message = reader.read_next();
-      if (bag_message->topic_name == "/localization/kinematic_state") {
-        rclcpp::SerializedMessage serialized_msg(*bag_message->serialized_data);
-        Odometry msg;
-        rclcpp::Serialization<Odometry> serialization;
-        serialization.deserialize_message(&serialized_msg, &msg);
-      }
-    }
-    std::cout << "successed to read rosbag2" << std::endl;
+  namespace fs = std::filesystem;
+  for (const fs::directory_entry & x : fs::directory_iterator(reference_bags_path)) {
+    loadRosbag(x.path());
   }
+  RCLCPP_INFO_STREAM(get_logger(), "successed to read " << references_.size());
+}
+
+void AbsolutePoseError::loadRosbag(const std::string & bag_file)
+{
+  RCLCPP_INFO_STREAM(get_logger(), "opeing " << bag_file.size());
+
+  Reference reference;
+  rosbag2_cpp::Reader reader;
+  reader.open(bag_file);
+  while (reader.has_next()) {
+    auto bag_message = reader.read_next();
+    if (bag_message->topic_name == "/localization/kinematic_state") {
+      rclcpp::SerializedMessage serialized_msg(*bag_message->serialized_data);
+      Odometry odom_msg;
+      rclcpp::Serialization<Odometry> serialization;
+      serialization.deserialize_message(&serialized_msg, &odom_msg);
+
+      PoseStamped pose_msg;
+      pose_msg.header = odom_msg.header;
+      pose_msg.pose = odom_msg.pose.pose;
+      reference.poses_.push_back(pose_msg);
+    }
+  }
+  if (reference.poses_.empty()) return;
+
+  reference.start_stamp_ = reference.poses_.front().header.stamp;
+  reference.end_stamp_ = reference.poses_.back().header.stamp;
+  references_[bag_file] = reference;
 }
 
 void AbsolutePoseError::poseCallback(const PoseCovStamped & pose_cov)
