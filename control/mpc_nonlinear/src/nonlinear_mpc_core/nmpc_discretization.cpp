72aa3eb08db74834ee5ed99df3fc3b18331a1cc7
ali <boyali@gmail.com>
2022-07-25 12:53:40 +0900

    Checked profiling, put some const in the discretization modules.

--- control/mpc_nonlinear/src/nonlinear_mpc_core/nmpc_discretization.cpp
+++ control/mpc_nonlinear/src/nonlinear_mpc_core/nmpc_discretization.cpp
@@ -38,14 +38,14 @@ ns_discretization::ODEzoh::ODEzoh(Model::model_ptr_t model,
 
 // --------- Integration Operator -------------------
 
-void ns_discretization::ODEfoh::operator()(const ode_matrix_t &V, ode_matrix_t &dVdt, const double t)
+void ns_discretization::ODEfoh::operator()(const ode_matrix_t &V, ode_matrix_t &dVdt, const double t) const
 {
-  const Model::state_vector_t x = V.col(0);
+  const Model::state_vector_t x{V.col(0)};
   Model::input_vector_t u(Model::input_vector_t::Zero());
 
   // InterpolateInCoordinates the inputs and the parameters.
   u = u0_ + t / dt_ * (u1_ - u0_);
-  auto const &&params = params0_ + t / dt_ * (params1_ - params0_);
+  auto const &params = params0_ + t / dt_ * (params1_ - params0_);
 
   /** @brief  Declare A, B, f */
   Model::state_matrix_t A(Model::state_matrix_t::Zero());
@@ -72,12 +72,11 @@ void ns_discretization::ODEfoh::operator()(const ode_matrix_t &V, ode_matrix_t &
   Model::state_vector_t z = (f - A * x - B * u);
 
   // Compute Ak, B0k and B1k.
-  Eigen::Index cols = 0;
+  Eigen::Index cols{0};
 
   // Phi in Fundamental matrix.
-  const Model::state_matrix_t Phi_A_xi = V.template block<Model::state_dim, Model::state_dim>(0, 1);
-  const Model::state_matrix_t Phi_A_xi_inverse = Phi_A_xi.inverse();
-  // ns_eigen_utils::printEigenMat(Phi_A_xi_inverse);
+  const Model::state_matrix_t &Phi_A_xi = V.template block<Model::state_dim, Model::state_dim>(0, 1);
+  const Model::state_matrix_t &Phi_A_xi_inverse = Phi_A_xi.inverse();
 
   // x[k+1]
   dVdt.template block<Model::state_dim, 1>(0, cols) = f;
@@ -108,9 +107,9 @@ void ns_discretization::ODEfoh::operator()(const ode_matrix_t &V, ode_matrix_t &
   assert(cols == ode_matrix_t::ColsAtCompileTime);
 }
 
-void ns_discretization::ODEzoh::operator()(const ode_matrix_t &V, ode_matrix_t &dVdt, const double t)
+void ns_discretization::ODEzoh::operator()(const ode_matrix_t &V, ode_matrix_t &dVdt, const double t) const
 {
-  const Model::state_vector_t x = V.col(0);
+  const Model::state_vector_t x{V.col(0)};
   Model::input_vector_t u(Model::input_vector_t::Zero());
 
   /** @brief  Declare A, B, f */
@@ -126,11 +125,11 @@ void ns_discretization::ODEzoh::operator()(const ode_matrix_t &V, ode_matrix_t &
   Model::state_vector_t z(f - A * x - B * u0_);
 
   // Compute Ak, B0k and B1k.
-  Eigen::Index cols = 0;
+  Eigen::Index cols{0};
 
   // Phi in Fundamental matrix.
-  const Model::state_matrix_t Phi_A_xi = V.template block<Model::state_dim, Model::state_dim>(0, 1);
-  const Model::state_matrix_t Phi_A_xi_inverse = Phi_A_xi.inverse();
+  const Model::state_matrix_t &Phi_A_xi = V.template block<Model::state_dim, Model::state_dim>(0, 1);
+  const Model::state_matrix_t &Phi_A_xi_inverse = Phi_A_xi.inverse();
 
   // x[k+1]
   dVdt.template block<Model::state_dim, 1>(0, cols) = f;
@@ -160,7 +159,7 @@ bool ns_discretization::multipleShootingTrajectory(Model::model_ptr_t const &mod
                                                    discretization_data_t &dd)
 {
 
-  size_t const &&K = dd.nX();  // number of matrices in the state matrix container.
+  size_t const &K = dd.nX();  // number of matrices in the state matrix container.
   double const num_of_tsteps{2};  // number of time steps for the RK integration. 1 for a single step.
 
   // Start computing Ak, B0k, B1k and zk where zk is the residual in the Taylor
@@ -194,16 +193,17 @@ bool ns_discretization::multipleShootingTrajectory(Model::model_ptr_t const &mod
   for (size_t k = 0; k < K; ++k)
   {
     // Compute kappa0, kappa1.
-    auto const x0 = trajectory_data.X.at(k);
-    auto const x1 = trajectory_data.X.at(k + 1);
+    auto const &x0 = trajectory_data.X[k];
+    auto const &x1 = trajectory_data.X[k + 1];
 
-    auto const u0 = trajectory_data.U.at(k);
-    auto const u1 = trajectory_data.U.at(k + 1);
+    auto const &u0 = trajectory_data.U[k];
+    auto const &u1 = trajectory_data.U[k + 1];
 
     auto const &s0 = x0(3);  //  ['xw', 'yw', 'psi', 's', 'ey', 'epsi', 'Vx', 'delta']
     auto const &s1 = x1(3);
     std::vector<double> s0s1{s0, s1};  // we interpolate at two coordinates
     std::vector<double> kappa0_kappa1;
+    kappa0_kappa1.reserve(2);
 
     // InterpolateInCoordinates wit PCG setting reusing_param true.
     if (auto const &&could_interpolate = piecewise_interpolator.Interpolate(s0s1, kappa0_kappa1);!could_interpolate)
@@ -228,7 +228,7 @@ bool ns_discretization::multipleShootingTrajectory(Model::model_ptr_t const &mod
     V.setZero();
 
     // Assign ODEzoh matrix V columns.
-    V.col(0) = trajectory_data.X.at(k);
+    V.col(0) << trajectory_data.X[k];
     V.template block<Model::state_dim, Model::state_dim>(0, 1).setIdentity();
 
     // Set the rest of the columns zero.
@@ -352,8 +352,7 @@ bool ns_discretization::multipleShootingSingleStep(Model::model_ptr_t const &mod
                                        double,
                                        ode_matrix_t,
                                        double,
-                                       boost::numeric::odeint::vector_space_algebra>
-    stepper;
+                                       boost::numeric::odeint::vector_space_algebra> stepper;
 
   // runge_kutta_fehlberg78<ode_matrix_t, double, ode_matrix_t,
   // double, vector_space_algebra> stepper;
@@ -374,16 +373,15 @@ bool ns_discretization::multipleShootingSingleStep(Model::model_ptr_t const &mod
 
   // Set the rest of the columns zero.
   /**
-     *  Number of columns in V; total= 1 for x0, nx for states, 2 * nu for the
-     * controls. total - 1- nx is the rest of the number of columns.
-     * */
+   *  Number of columns in V; total= 1 for x0, nx for states, 2 * nu for the
+   * controls. total - 1- nx is the rest of the number of columns.
+   * */
 
   V.template rightCols<ode_matrix_t::ColsAtCompileTime - 1 - Model::state_dim>().setZero();
 
   // Integrate ODEzoh matrices. We can increase the integration accuracy
   // dt/ nRKorder defines the number of integration steps.
-  boost::numeric::odeint::integrate_adaptive(
-    stepper, ode_multipleshooting, V, 0., dt, dt / num_of_tsteps);
+  boost::numeric::odeint::integrate_adaptive(stepper, ode_multipleshooting, V, 0., dt, dt / num_of_tsteps);
 
   // Assign the discrete matrices. Vectorized matrices start at column 1.
   Eigen::Index cols = 1;
@@ -401,10 +399,13 @@ bool ns_discretization::multipleShootingSingleStep(Model::model_ptr_t const &mod
 }
 
 // We can also use the Bi-linear transformation method for discretization.
-bool ns_discretization::bilinearTransformationOneStep(
-  Model::model_ptr_t const &model_ptr, Model::state_vector_t const &x0,
-  Model::input_vector_t const &u0, Model::param_vector_t const &params0, double const &dt,
-  Model::state_matrix_t &Ad, Model::control_matrix_t &Bd)
+bool ns_discretization::bilinearTransformationOneStep(Model::model_ptr_t const &model_ptr,
+                                                      Model::state_vector_t const &x0,
+                                                      Model::input_vector_t const &u0,
+                                                      Model::param_vector_t const &params0,
+                                                      double const &dt,
+                                                      Model::state_matrix_t &Ad,
+                                                      Model::control_matrix_t &Bd)
 {
   // Start computing Ak, B0k, B1k and zk where zk is the residual in the Taylor
   // expansion; x[k+1]  = Ax + Bu + f(xk, uk) - Axk - Buk and zk = f(xk, uk) -
