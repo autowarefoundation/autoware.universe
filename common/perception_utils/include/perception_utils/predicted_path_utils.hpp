15157f4baac9024beba5d28a0a748342cea39f44
yutaka <purewater0901@gmail.com>
2022-07-25 18:49:04 +0900

    feat(perception_utils): add predicted_path utils

---
+++ common/perception_utils/include/perception_utils/predicted_path_utils.hpp
@@ -0,0 +1,120 @@
+// Copyright 2022 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PERCEPTION_UTILS__PREDICTED_PATH_UTILS_HPP_
+#define PERCEPTION_UTILS__PREDICTED_PATH_UTILS_HPP_
+
+#include "perception_utils/geometry.hpp"
+#include "tier4_autoware_utils/geometry/geometry.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "autoware_auto_perception_msgs/msg/predicted_path.hpp"
+#include <geometry_msgs/msg/pose.hpp>
+
+#include <boost/optional.hpp>
+
+#include <vector>
+
+namespace perception_utils
+{
+/**
+ * @brief Calculate Interpolated Pose from predicted path by time
+ * @param path Input predicted path
+ * @param relative_time time at interpolated point. This should be within [0.0,
+ * time_step*(num_of_path_points)]
+ * @return interpolated pose
+ */
+inline boost::optional<geometry_msgs::msg::Pose> calcInterpolatedPose(
+  const autoware_auto_perception_msgs::msg::PredictedPath & path, const double relative_time)
+{
+  // Check if relative time is in the valid range
+  if (path.path.empty() || relative_time < 0.0) {
+    return boost::none;
+  }
+
+  const auto & time_step = rclcpp::Duration(path.time_step).seconds();
+  for (size_t path_idx = 1; path_idx < path.path.size(); ++path_idx) {
+    const auto & pt = path.path.at(path_idx);
+    const auto & prev_pt = path.path.at(path_idx - 1);
+    if (relative_time <= time_step * static_cast<double>(path_idx)) {
+      const auto offset = relative_time - time_step * static_cast<double>(path_idx - 1);
+      const auto ratio = offset / time_step;
+      return tier4_autoware_utils::calcInterpolatedPose(prev_pt, pt, ratio);
+    }
+  }
+
+  return boost::none;
+}
+
+/**
+ * @brief Resampling predicted path by time step vector
+ * @param path Input predicted path
+ * @param relative_time_vec time at each resampling point. Each time should be within [0.0,
+ * time_step*(num_of_path_points)]
+ * @return resampled path
+ */
+autoware_auto_perception_msgs::msg::PredictedPath resamplePredictedPath(
+  const autoware_auto_perception_msgs::msg::PredictedPath & path,
+  const std::vector<double> & relative_time_vec)
+{
+  autoware_auto_perception_msgs::msg::PredictedPath resampled_path;
+  resampled_path.confidence = path.confidence;
+  resampled_path.time_step = path.time_step;
+
+  for (const auto & rel_time : relative_time_vec) {
+    const auto opt_pose = calcInterpolatedPose(path, rel_time);
+    if (!opt_pose) {
+      continue;
+    }
+
+    resampled_path.path.push_back(*opt_pose);
+  }
+
+  return resampled_path;
+}
+
+/**
+ * @brief Resampling predicted path by sampling time interval. Note that this function samples
+ * terminal point as well as points by sampling time interval
+ * @param path Input predicted path
+ * @param sampling_time_interval sampling time interval for each point
+ * @param enable_sampling_terminal_point flag if to sample terminal point
+ * @return resampled path
+ */
+autoware_auto_perception_msgs::msg::PredictedPath resamplePredictedPath(
+  const autoware_auto_perception_msgs::msg::PredictedPath & path,
+  const double sampling_time_interval, bool enable_sampling_terminal_point = true)
+{
+  std::vector<double> sampling_time_vector;
+  const double predicted_horizon =
+    rclcpp::Duration(path.time_step).seconds() * static_cast<double>(path.path.size() - 1);
+  for (double t = 0.0; t < predicted_horizon; t += sampling_time_interval) {
+    sampling_time_vector.push_back(t);
+  }
+
+  // Insert Predicted Horizon(Terminal Predicted Point)
+  if (enable_sampling_terminal_point) {
+    if (predicted_horizon - sampling_time_vector.back() > 1e-3) {
+      sampling_time_vector.push_back(predicted_horizon);
+    } else {
+      sampling_time_vector.back() = predicted_horizon;
+    }
+  }
+
+  return resamplePredictedPath(path, sampling_time_vector);
+}
+}  // namespace perception_utils
+
+#endif  // PERCEPTION_UTILS__PREDICTED_PATH_UTILS_HPP_
---
+++ common/perception_utils/test/src/test_predicted_path_utils.cpp
@@ -0,0 +1,87 @@
+// Copyright 2020 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "perception_utils/predicted_path_utils.hpp"
+#include "tier4_autoware_utils/geometry/geometry.hpp"
+#include "tier4_autoware_utils/math/unit_conversion.hpp"
+
+#include <gtest/gtest.h>
+
+using tier4_autoware_utils::Point2d;
+using tier4_autoware_utils::Point3d;
+
+constexpr double epsilon = 1e-06;
+
+namespace
+{
+using autoware_auto_perception_msgs::msg::PredictedPath;
+using tier4_autoware_utils::createPoint;
+using tier4_autoware_utils::createQuaternionFromRPY;
+using tier4_autoware_utils::transformPoint;
+
+geometry_msgs::msg::Pose createPose(
+  double x, double y, double z, double roll, double pitch, double yaw)
+{
+  geometry_msgs::msg::Pose p;
+  p.position = createPoint(x, y, z);
+  p.orientation = createQuaternionFromRPY(roll, pitch, yaw);
+  return p;
+}
+
+PredictedPath createTestPredictedPath(
+  const size_t num_points, const double point_time_interval, const double vel,
+  const double init_theta = 0.0, const double delta_theta = 0.0)
+{
+  PredictedPath path;
+  path.confidence = 1.0;
+  path.time_step = rclcpp::Duration::from_seconds(point_time_interval);
+
+  const double point_interval = vel * point_time_interval;
+  for (size_t i = 0; i < num_points; ++i) {
+    const double theta = init_theta + i * delta_theta;
+    const double x = i * point_interval * std::cos(theta);
+    const double y = i * point_interval * std::sin(theta);
+
+    const auto p = createPose(x, y, 0.0, 0.0, 0.0, theta);
+    path.path.push_back(p);
+  }
+  return path;
+}
+}  // namespace
+
+TEST(predicted_path_utils, testCalcInterpolatedPose)
+{
+  using perception_utils::calcInterpolatedPose;
+  using tier4_autoware_utils::createQuaternionFromRPY;
+  using tier4_autoware_utils::createQuaternionFromYaw;
+  using tier4_autoware_utils::deg2rad;
+
+  const auto path = createTestPredictedPath(100, 1.0, 1.0);
+
+  {
+    const auto ans_quat = createQuaternionFromRPY(deg2rad(0.0), deg2rad(0.0), deg2rad(0.0));
+    for (double t = 0.0; t < 10.0; t += 1.0) {
+      const auto p = calcInterpolatedPose(path, t);
+
+      EXPECT_NE(p, boost::none);
+      EXPECT_NEAR(p->position.x, t * 1.0, epsilon);
+      EXPECT_NEAR(p->position.y, 0.0, epsilon);
+      EXPECT_NEAR(p->position.z, 0.0, epsilon);
+      EXPECT_NEAR(p->orientation.x, ans_quat.x, epsilon);
+      EXPECT_NEAR(p->orientation.y, ans_quat.y, epsilon);
+      EXPECT_NEAR(p->orientation.z, ans_quat.z, epsilon);
+      EXPECT_NEAR(p->orientation.w, ans_quat.w, epsilon);
+    }
+  }
+}
