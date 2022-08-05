75e0f925adcd6008140d08f7b48839c55d5c772a
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2022-08-05 12:14:55 +0900

    add test for resampler with time regression
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

---
+++ localization/yabloc/src/particle_filter/modularized_particle_filter/test/src/test_predictor.cpp
@@ -0,0 +1,26 @@
+#include "modularized_particle_filter/prediction/predictor.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <gtest/gtest.h>
+
+namespace mpf = modularized_particle_filter;
+
+class PredictorTestSuite : public ::testing::Test
+{
+protected:
+  static void SetUpTestCase() { rclcpp::init(0, nullptr); }
+  static void TearDownTestCase() { rclcpp::shutdown(); }
+};
+
+// class TestPredictor : public mpf::Predictor
+// {
+// public:
+//   TestPredictor() : Predictor() {}
+// };
+
+// TEST_F(PredictorTestSuite, predictionConstruct)
+// {
+//   auto node = std::make_shared<TestPredictor>();
+//   EXPECT_DOUBLE_EQ(3.5, 3.5);
+// }
--- localization/yabloc/src/particle_filter/modularized_particle_filter/test/src/test_resampler.cpp
+++ localization/yabloc/src/particle_filter/modularized_particle_filter/test/src/test_resampler.cpp
@@ -1,32 +1,11 @@
-#include "modularized_particle_filter/prediction/predictor.hpp"
+#include "modularized_particle_filter/prediction/resampler.hpp"
 
-#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
 
 #include <gtest/gtest.h>
 
-class PredictorTestSuite : public ::testing::Test
-{
-protected:
-  static void SetUpTestCase() { rclcpp::init(0, nullptr); }
-  static void TearDownTestCase() { rclcpp::shutdown(); }
-};
-
 namespace mpf = modularized_particle_filter;
-
-// class TestPredictor : public mpf::Predictor
-// {
-// public:
-//   TestPredictor() : Predictor() {}
-
-// private:
-// };
-
-// TEST_F(PredictorTestSuite, predictionConstruct)
-// {
-//   auto node = std::make_shared<TestPredictor>();
-//   EXPECT_DOUBLE_EQ(3.5, 3.5);
-// }
-
+using Particle = modularized_particle_filter_msgs::msg::Particle;
 using ParticleArray = modularized_particle_filter_msgs::msg::ParticleArray;
 using OptParticleArray = std::optional<ParticleArray>;
 
@@ -34,7 +13,57 @@ constexpr int RESAMPLE_INTERVAL = 5;
 constexpr int PARTICLE_COUNT = 10;
 constexpr int HISTORY_SIZE = 10;
 
-// Simple weighting without time retrogression
+TEST(ResamplerTestSuite, ourOfHistory)
+{
+  mpf::RetroactiveResampler resampler(RESAMPLE_INTERVAL, PARTICLE_COUNT, HISTORY_SIZE);
+  ParticleArray predicted;
+  ParticleArray::SharedPtr weighted = std::make_shared<ParticleArray>();
+
+  predicted.header.stamp = rclcpp::Time(0);
+  predicted.id = 0;
+  weighted->id = 0;
+  predicted.particles.resize(PARTICLE_COUNT);
+  weighted->particles.resize(PARTICLE_COUNT);
+  for (auto & p : predicted.particles) p.weight = 1;
+  for (auto & p : weighted->particles) p.weight = 1;
+
+  // First resampling must fail
+  {
+    OptParticleArray opt_array = resampler.resampling(predicted);
+    EXPECT_FALSE(opt_array.has_value());
+  }
+
+  // Invalid generation
+  {
+    weighted->id = -1;
+    auto opt = resampler.retroactiveWeighting(predicted, weighted);
+    EXPECT_FALSE(opt.has_value());
+  }
+
+  // Future generation
+  {
+    weighted->id = 1;
+    auto opt = resampler.retroactiveWeighting(predicted, weighted);
+    EXPECT_FALSE(opt.has_value());
+  }
+
+  // Repease resampling to fill all history
+  for (int t = 0; t < HISTORY_SIZE; ++t) {
+    predicted.header.stamp = rclcpp::Time((RESAMPLE_INTERVAL + 1) * (t + 1), 0);
+    auto opt = resampler.resampling(predicted);
+    EXPECT_TRUE(opt.has_value());
+    EXPECT_EQ(opt->id, t + 1);
+    predicted = opt.value();
+  }
+
+  // Too old generation
+  {
+    weighted->id = 0;
+    auto opt = resampler.retroactiveWeighting(predicted, weighted);
+    EXPECT_FALSE(opt.has_value());
+  }
+}
+
 TEST(ResamplerTestSuite, simpleResampling)
 {
   mpf::RetroactiveResampler resampler(RESAMPLE_INTERVAL, PARTICLE_COUNT, HISTORY_SIZE);
@@ -92,7 +121,7 @@ TEST(ResamplerTestSuite, simpleResampling)
     OptParticleArray opt_array1 = resampler.retroactiveWeighting(predicted, weighted);
     EXPECT_TRUE(opt_array1.has_value());
 
-    // All weights must be equal
+    // All weight must match with following exepection
     for (int i = 0; i < PARTICLE_COUNT; ++i) {
       const auto & p = opt_array1->particles.at(i);
       if (i < PARTICLE_COUNT / 2) {
@@ -116,19 +145,23 @@ TEST(ResamplerTestSuite, simpleResampling)
   }
 }
 
-TEST(ResamplerTestSuite, ourOfHistory)
+TEST(ResamplerTestSuite, resamplingWithRetrogression)
 {
   mpf::RetroactiveResampler resampler(RESAMPLE_INTERVAL, PARTICLE_COUNT, HISTORY_SIZE);
-  ParticleArray predicted;
-  ParticleArray::SharedPtr weighted = std::make_shared<ParticleArray>();
 
+  ParticleArray predicted;
   predicted.header.stamp = rclcpp::Time(0);
-  predicted.id = 0;
-  weighted->id = 0;
   predicted.particles.resize(PARTICLE_COUNT);
-  weighted->particles.resize(PARTICLE_COUNT);
-  for (auto & p : predicted.particles) p.weight = 1;
-  for (auto & p : weighted->particles) p.weight = 1;
+  predicted.id = 0;
+
+  for (int i = 0; i < PARTICLE_COUNT; ++i) {
+    auto & p = predicted.particles.at(i);
+    p.weight = 1.0;
+    if (i < PARTICLE_COUNT / 2)
+      p.pose.position.x = 1;
+    else
+      p.pose.position.x = -1;
+  }
 
   // First resampling must fail
   {
@@ -136,33 +169,42 @@ TEST(ResamplerTestSuite, ourOfHistory)
     EXPECT_FALSE(opt_array.has_value());
   }
 
-  // Invalid generation
-  {
-    weighted->id = -1;
-    auto opt = resampler.retroactiveWeighting(predicted, weighted);
-    EXPECT_FALSE(opt.has_value());
+  // Fill all history with biased weighted particles
+  for (int p = 0; p < HISTORY_SIZE; ++p) {
+    predicted.header.stamp = rclcpp::Time((RESAMPLE_INTERVAL + 1) * (p + 1), 0);
+    auto opt_array = resampler.resampling(predicted);
+    EXPECT_TRUE(opt_array.has_value());
+    predicted = opt_array.value();
+    EXPECT_EQ(predicted.id, p + 1);
   }
 
-  // Future generation
+  // Update by ancient measurement
   {
-    weighted->id = 1;
-    auto opt = resampler.retroactiveWeighting(predicted, weighted);
-    EXPECT_FALSE(opt.has_value());
-  }
+    double before_centroid = 0;
+    for (const auto & p : predicted.particles) {
+      before_centroid += p.pose.position.x * p.weight;
+    }
 
-  // Repease resampling to fill all history
-  for (int t = 0; t < HISTORY_SIZE; ++t) {
-    predicted.header.stamp = rclcpp::Time((RESAMPLE_INTERVAL + 1) * (t + 1), 0);
-    auto opt = resampler.resampling(predicted);
-    EXPECT_TRUE(opt.has_value());
-    EXPECT_EQ(opt->id, t + 1);
-    predicted = opt.value();
-  }
+    // Weight
+    ParticleArray::SharedPtr weighted = std::make_shared<ParticleArray>();
+    weighted->particles.resize(PARTICLE_COUNT);
+    weighted->id = 1;  // ancient generation id
+    for (int i = 0; i < PARTICLE_COUNT; ++i) {
+      auto & q = weighted->particles.at(i);
+      if (i < PARTICLE_COUNT / 2) {
+        q.weight = 2.0;
+      } else {
+        q.weight = 1.0;
+      }
+    }
+    OptParticleArray opt_array = resampler.retroactiveWeighting(predicted, weighted);
+    EXPECT_TRUE(opt_array.has_value());
+    predicted = opt_array.value();
 
-  // Too old generation
-  {
-    weighted->id = 0;
-    auto opt = resampler.retroactiveWeighting(predicted, weighted);
-    EXPECT_FALSE(opt.has_value());
+    double after_centroid = 0;
+    for (const auto & p : predicted.particles) {
+      after_centroid += p.pose.position.x * p.weight;
+    }
+    EXPECT_TRUE(after_centroid > before_centroid);
   }
 }
