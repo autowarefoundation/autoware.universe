768154c7475bf797027b7d6cc869a0a5083c86e3
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2022-08-08 11:51:17 +0900

    semseg_node works well
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/src/semantic_segmentation/src/semseg_core.py
+++ localization/yabloc/src/semantic_segmentation/src/semseg_core.py
@@ -1,4 +1,57 @@
 #!/usr/bin/env python3
+import copy
+import numpy as np
+import cv2
+import os
+
+
+class SemSeg:
+    def __init__(self, model_path):
+        self.net_ = cv2.dnn.readNet(model_path)
+        self.net_.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        self.net_.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+    def makeBlob(self, image: np.ndarray) -> np.ndarray:
+        scale = 1.0
+        size = (896, 512)
+        mean = (0, 0, 0)
+        swap = True
+        crop = False
+        depth = cv2.CV_32F
+        return cv2.dnn.blobFromImage(image, scale, size, mean, swap, crop, depth)
+
+    def inference(self, image: np.ndarray) -> np.ndarray:
+        blob = self.makeBlob(image)
+        self.net_.setInput(blob)
+        output_layers = self.net_.getUnconnectedOutLayersNames()
+        mask = self.net_.forward(output_layers)[0]
+        mask = np.squeeze(mask).transpose(1, 2, 0)
+
+        mask = cv2.resize(
+            mask,
+            dsize=(image.shape[1], image.shape[0]),
+            interpolation=cv2.INTER_LINEAR
+        )
+
+        return mask
+
+    def drawOverlayMask(self, image, segmentation, score_threshold=0.5) -> np.ndarray:
+        overlay_image = copy.deepcopy(image)
+
+        masks = cv2.split(segmentation)[1:]
+        bin_masks = []
+        for mask in masks:
+            bin_mask = np.where(mask > score_threshold, 0, 1).astype('uint8')
+            bin_masks.append(255-255*bin_mask)
+        mask_image = cv2.merge(bin_masks)
+        overlay_image = cv2.addWeighted(overlay_image, 0.5, mask_image, 0.5, 1.0)
+        return overlay_image
+
+
+def main():
+    dirname = os.path.dirname(__file__)
+    path = dirname+'/../data/model_32.pb'
+
 
 if __name__ == '__main__':
-    print('This is a library')
+    main()
--- localization/yabloc/src/semantic_segmentation/src/semseg_node.py
+++ localization/yabloc/src/semantic_segmentation/src/semseg_node.py
@@ -1,9 +1,46 @@
 #!/usr/bin/env python3
 import semseg_core
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+import sys
+import cv2
+from cv_bridge import CvBridge
+
+
+class SemsegNode(Node):
+    def __init__(self):
+        super().__init__('initial_pose_latch')
+
+        model_path = self.declare_parameter('model_path', '').value
+        self.get_logger().info('model path: ' + model_path)
+
+        self.sub_image_ = self.create_subscription(
+            Image, '/in/image_raw',  self.image_callback, 10)
+
+        self.pub_image_ = self.create_publisher(Image, '/out/image_raw', 10)
+        self.dnn_ = semseg_core.SemSeg(model_path)
+        self.bridge_ = CvBridge()
+
+    def image_callback(self, msg: Image):
+        stamp = msg.header.stamp
+        self.get_logger().info('Subscribed image: ' + str(stamp))
+
+        image = self.bridge_.imgmsg_to_cv2(msg)
+        mask = self.dnn_.inference(image)
+        show_image = self.dnn_.drawOverlayMask(image, mask)
+
+        cv2.imshow('semseg',  show_image)
+        cv2.waitKey(1)
 
 
 def main():
-    print('hello world')
+    rclpy.init(args=sys.argv)
+
+    semseg_node = SemsegNode()
+    rclpy.spin(semseg_node)
+    semseg_node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
