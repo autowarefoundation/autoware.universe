d351d7555ee91556c4727e2bfb91d4643b2f5cdb
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2022-08-09 16:52:08 +0900

    implement initialpose latch plugin
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/src/rviz2_initialpose_plugins/src/initialpose_panel.cpp
+++ localization/yabloc/src/rviz2_initialpose_plugins/src/initialpose_panel.cpp
@@ -1,46 +1,141 @@
 #include "initialpose_panel.hpp"
 
-#include <QPushButton>
-#include <QtWidgets>
+#include <QHBoxLayout>
+#include <QWidget>
 #include <rviz_common/display_context.hpp>
-#include <rviz_common/panel.hpp>
 
-#include <memory>
-#include <string>
-#include <vector>
+#include <sstream>
 
 namespace initialpose_plugins
 {
 
-InitialPosePanel::InitialPosePanel(QWidget * parent) : Panel(parent)
+InitialPosePanel::InitialPosePanel(QWidget * parent)
+: Panel(parent), logger_(rclcpp::get_logger("initialpose_plugins"))
 {
-  toggle_button_ = new QPushButton;
-  toggle_button_->setText("PUSH");
+  // Button
+  toggle_button_ = new QPushButton();
+  toggle_button_->setText("CLEAR");
+  toggle_button_->setFixedSize(QSize(60, 30));
   QObject::connect(toggle_button_, SIGNAL(clicked()), this, SLOT(toggle()));
 
-  // Add button
-  QVBoxLayout * main_layout = new QVBoxLayout;
-  main_layout->addWidget(toggle_button_);
+  // Label
+  pose_label_ = new QLabel();
+  time_label_ = new QLabel();
+  pose_label_->setText("");
+  time_label_->setText("remaining time: ");
+
+  // Layout
+  QHBoxLayout * main_layout = new QHBoxLayout;
   main_layout->setContentsMargins(10, 10, 10, 10);
+
+  // Add widges
+  main_layout->addWidget(toggle_button_);
+  main_layout->addWidget(pose_label_);
+  main_layout->addWidget(time_label_);
 
   setLayout(main_layout);
 }
 
-InitialPosePanel::~InitialPosePanel() {}
+InitialPosePanel::~InitialPosePanel()
+{
+  timer_.reset();
+  pose_sub_.reset();
+  pose_pub_.reset();
+}
 
 void InitialPosePanel::onInitialize()
 {
   auto node = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
+  clock_ = node->get_clock();
+
+  auto sub_cb = std::bind(&InitialPosePanel::poseCallback, this, std::placeholders::_1);
+  pose_sub_ = node->create_subscription<PoseCovStamped>("/initialpose", 10, std::move(sub_cb));
+  pose_pub_ = node->create_publisher<PoseCovStamped>("/initialpose", 10);
+
+  auto timer_cb = std::bind(&InitialPosePanel::timerCallback, this);
+  using namespace std::literals::chrono_literals;
+  timer_ = rclcpp::create_timer(node, clock_, 1000ms, std::move(timer_cb));
 }
 
-void InitialPosePanel::save(rviz_common::Config config) const { Panel::save(config); }
+void InitialPosePanel::save(rviz_common::Config config) const
+{
+  Panel::save(config);
+  if (last_initial_pose_.has_value()) {
+    auto pos = last_initial_pose_->pose.pose.position;
+    auto ori = last_initial_pose_->pose.pose.orientation;
+    config.mapSetValue("last_initial_pose.x", pos.x);
+    config.mapSetValue("last_initial_pose.y", pos.y);
+    config.mapSetValue("last_initial_pose.w", ori.w);
+    config.mapSetValue("last_initial_pose.z", ori.z);
+    ulong t = rclcpp::Time(last_initial_pose_->header.stamp).nanoseconds();
+    QVariant var;
+    var.setValue(t);
+    config.mapSetValue("last_initial_pose.t", var);
+  }
+}
 
-void InitialPosePanel::load(const rviz_common::Config & config) { Panel::load(config); }
+void InitialPosePanel::load(const rviz_common::Config & config)
+{
+  Panel::load(config);
+
+  QVariant var;
+  QVariant x, y, w, z;
+  bool f1 = config.mapGetValue("last_initial_pose.x", &x);
+  bool f2 = config.mapGetValue("last_initial_pose.y", &y);
+  bool f3 = config.mapGetValue("last_initial_pose.w", &w);
+  bool f4 = config.mapGetValue("last_initial_pose.z", &z);
+  bool f5 = config.mapGetValue("last_initial_pose.t", &var);
+
+  if (f1 & f2 & f3 & f4 & f5) {
+    rclcpp::Time stamp(var.value<ulong>());
+    last_initial_pose_ = PoseCovStamped();
+    last_initial_pose_->header.stamp = stamp;
+    last_initial_pose_->pose.pose.position.x = x.value<double>();
+    last_initial_pose_->pose.pose.position.y = y.value<double>();
+    last_initial_pose_->pose.pose.orientation.w = w.value<double>();
+    last_initial_pose_->pose.pose.orientation.z = z.value<double>();
+    poseCallback(last_initial_pose_.value());
+  }
+}
+
+void InitialPosePanel::timerCallback()
+{
+  if (last_initial_pose_.has_value()) {
+    rclcpp::Time stamp = clock_->now();
+
+    rclcpp::Time target = last_initial_pose_->header.stamp;
+    double dt = (target - stamp).seconds();
+    std::stringstream ss;
+    ss << std::setprecision(3) << std::fixed << dt << " sec";
+    time_label_->setText(ss.str().c_str());
+
+    if (std::abs(dt) < 0.55) {
+      pose_pub_->publish(last_initial_pose_.value());
+    }
+  }
+}
+
+void InitialPosePanel::poseCallback(const PoseCovStamped & msg)
+{
+  last_initial_pose_ = msg;
+
+  auto pos = msg.pose.pose.position;
+  auto ori = msg.pose.pose.orientation;
+
+  double theta = 2 * std::atan2(ori.w, ori.z);
+  double deg = theta * 180 / M_PI;
+  std::stringstream ss;
+  ss << std::setprecision(1) << std::fixed;
+  ss << "X:" << pos.x << ", Y=" << pos.y << ", Deg=" << deg;
+  pose_label_->setText(ss.str().c_str());
+  time_label_->setText("0");
+}
 
 void InitialPosePanel::toggle()
 {
-  rclcpp::Logger logger = rclcpp::get_logger("rviz2");
-  RCLCPP_INFO_STREAM(logger, "hoge");
+  last_initial_pose_ = std::nullopt;
+  pose_label_->setText("");
+  time_label_->setText("remaining time: ");
 }
 
 }  // namespace initialpose_plugins
--- localization/yabloc/src/rviz2_initialpose_plugins/src/initialpose_panel.hpp
+++ localization/yabloc/src/rviz2_initialpose_plugins/src/initialpose_panel.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
-#include <QBasicTimer>
-#include <QtWidgets>
+#include <QLabel>
+#include <QPushButton>
+#include <rclcpp/logger.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <rosbag2_interfaces/srv/toggle_paused.hpp>
 #include <rviz_common/display_context.hpp>
 #include <rviz_common/panel.hpp>
 
-#include <memory>
-#include <string>
-#include <vector>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 
-class QPushButton;
+#include <optional>
 
 namespace initialpose_plugins
 {
@@ -21,19 +19,34 @@ class InitialPosePanel : public rviz_common::Panel
   Q_OBJECT
 
 public:
-  explicit InitialPosePanel(QWidget * parent = 0);
+  using PoseCovStamped = geometry_msgs::msg::PoseWithCovarianceStamped;
+  explicit InitialPosePanel(QWidget * parent = nullptr);
   virtual ~InitialPosePanel();
 
   void onInitialize() override;
 
   void load(const rviz_common::Config & config) override;
   void save(rviz_common::Config config) const override;
+  rclcpp::Logger logger_;
 
 private Q_SLOTS:
   void toggle();
 
 private:
+  rclcpp::Publisher<PoseCovStamped>::SharedPtr pose_pub_;
+  rclcpp::Subscription<PoseCovStamped>::SharedPtr pose_sub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
   QPushButton * toggle_button_{nullptr};
+  QLabel * pose_label_{nullptr};
+  QLabel * time_label_{nullptr};
+
+  rclcpp::Clock::SharedPtr clock_{nullptr};
+  std::optional<PoseCovStamped> last_initial_pose_{std::nullopt};
+
+  void poseCallback(const PoseCovStamped & msg);
+
+  void timerCallback();
 };
 
 }  // namespace initialpose_plugins
