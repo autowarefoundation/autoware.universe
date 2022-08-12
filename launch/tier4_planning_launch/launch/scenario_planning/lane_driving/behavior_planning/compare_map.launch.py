20627cc1ba13449f92b1c5e66b47d6a98315b762
Tomohito Ando <tomohito.ando@tier4.jp>
2022-08-12 16:22:53 +0900

    fix container name
    Signed-off-by: Tomohito Ando <tomohito.ando@tier4.jp>

--- launch/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/compare_map.launch.py
+++ launch/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/compare_map.launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
     ]
 
     compare_map_container = ComposableNodeContainer(
-        name=LaunchConfiguration("container_name"),
+        name="compare_map_container",
         namespace="",
         package="rclcpp_components",
         executable=LaunchConfiguration("container_executable"),
