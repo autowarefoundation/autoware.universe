54b1cee6610f6aa446463dcd690b73963300ded7
Berkay Karaman <berkay@leodrive.ai>
2022-08-24 15:12:39 +0300

    add error handler for too close points
    Signed-off-by: Berkay Karaman <berkay@leodrive.ai>

--- planning/motion_velocity_smoother/src/trajectory_utils.cpp
+++ planning/motion_velocity_smoother/src/trajectory_utils.cpp
@@ -190,10 +190,25 @@ std::vector<double> calcTrajectoryCurvatureFrom3Points(
   //  for first point curvature = 0;
   k_arr.push_back(0.0);
   for (size_t i = 1; i + 1 < trajectory.size(); i++) {
+    double curvature = 0.0;
     const auto p0 = getPoint(trajectory.at(i - std::min(idx_dist, i)));
     const auto p1 = getPoint(trajectory.at(i));
     const auto p2 = getPoint(trajectory.at(i + std::min(idx_dist, trajectory.size() - 1 - i)));
-    k_arr.push_back(calcCurvature(p0, p1, p2));
+    try {
+      curvature = calcCurvature(p0, p1, p2);
+    } catch (std::exception const& e) {
+      // ...code that handles the error...
+      RCLCPP_WARN(
+        rclcpp::get_logger("motion_velocity_smoother").get_child("trajectory_utils"),
+        "%s", e.what());
+      if(!k_arr.empty())
+      {
+        curvature = k_arr.back();  // previous curvature
+      } else {
+        curvature = 0.0;
+      }
+    }
+    k_arr.push_back(curvature);
   }
   // for last point curvature = 0;
   k_arr.push_back(0.0);
