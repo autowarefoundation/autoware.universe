a24652f285ec39a62711b76e33c6ddf7c7581da1
scepter914 <scepter914@gmail.com>
2022-08-25 15:45:19 +0900

    fix copyright
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- perception/front_vehicle_velocity_estimator/src/front_vehicle_velocity_estimator_node/front_vehicle_velocity_estimator.cpp
+++ perception/front_vehicle_velocity_estimator/src/front_vehicle_velocity_estimator_node/front_vehicle_velocity_estimator.cpp
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2022 Satoshi Tanaka
+// Copyright 2022 TIER IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
