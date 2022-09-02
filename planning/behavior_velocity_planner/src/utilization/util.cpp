6c733629737b2ebb520a6ffa86b446b1131f627d
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2022-09-02 18:40:04 +0900

    fix(behavior_velocity): fix insertVelocity (autowarefoundation#1768)  (#102)
    fix(behavior_velocity): fix insertVelocity (#1768)
    
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>
    
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>
    
    Signed-off-by: tanaka3 <ttatcoder@outlook.jp>

--- planning/behavior_velocity_planner/src/utilization/util.cpp
+++ planning/behavior_velocity_planner/src/utilization/util.cpp
@@ -217,7 +217,7 @@ void insertVelocity(
     if (
       tier4_autoware_utils::calcDistance2d(path.points.at(static_cast<size_t>(i)), path_point) <
       min_distance) {
-      path.points.at(i).point.longitudinal_velocity_mps = 0;
+      path.points.at(i).point.longitudinal_velocity_mps = v;
       already_has_path_point = true;
       insert_index = static_cast<size_t>(i);
       // set velocity from is going to insert min velocity later
