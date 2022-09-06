da0232ffbdd6a49e62e29aa1510810a65725a3bf
satoshi-ota <satoshi.ota928@gmail.com>
2022-09-07 08:36:51 +0900

    feat(behavior_path_planner): subscribe acceleration from localization module
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- launch/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
+++ launch/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.py
@@ -142,6 +142,7 @@ def launch_setup(context, *args, **kwargs):
             ("~/input/vector_map", LaunchConfiguration("map_topic_name")),
             ("~/input/perception", "/perception/object_recognition/objects"),
             ("~/input/odometry", "/localization/kinematic_state"),
+            ("~/input/accel", "/localization/acceleration"),
             ("~/input/scenario", "/planning/scenario_planning/scenario"),
             ("~/output/path", "path_with_lane_id"),
             ("~/output/turn_indicators_cmd", "/planning/turn_indicators_cmd"),
