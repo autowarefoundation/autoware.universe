387db142b2cb994cc5215840650ab00dc5973e04
ali <boyali@gmail.com>
2022-09-07 15:03:33 +0900

    Linted.

--- control/autoware_control_toolbox/include/splines/bspline_interpolator_templated.hpp
+++ control/autoware_control_toolbox/include/splines/bspline_interpolator_templated.hpp
@@ -1,4 +1,4 @@
-// Copyright 2022 The Autoware Foundation.
+// Copyright 2018-2022 The Autoware Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 
 #ifndef AUTOWARE_CONTROL_TOOLBOX_INCLUDE_SPLINES_BSPLINEINTERPOLATORTEMPLATED_HPP_
 #define AUTOWARE_CONTROL_TOOLBOX_INCLUDE_SPLINES_BSPLINEINTERPOLATORTEMPLATED_HPP_
