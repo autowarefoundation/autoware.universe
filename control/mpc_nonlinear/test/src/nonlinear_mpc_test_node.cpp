587cc3459f9e13fda5d79bbb96a9d0b7b78abf6c
ali <boyali@gmail.com>
2022-09-08 13:16:18 +0900

    Added fake node: adding auto-diff
    Signed-off-by: ali <boyali@gmail.com>
    Signed-off-by: Ali Boyali <boyali@gmail.com>

--- control/mpc_nonlinear/test/src/nonlinear_mpc_test_node.cpp
+++ control/mpc_nonlinear/test/src/nonlinear_mpc_test_node.cpp
@@ -38,3 +38,117 @@ std::shared_ptr<ns_mpc_nonlinear::NonlinearMPCNode> makeNonlinearMPCNode()
 
   return node;
 }
+
+// Helper methods.
+std::vector<double> logisticMap(double const &max_y_value,
+                                double const &center_x_value,
+                                double const &slope,
+                                std::vector<double> const &x_coord)
+{
+  std::vector<double> ycurve_output;
+
+  for (auto &x : x_coord)
+  {
+    auto yval = max_y_value / (1. + std::exp(-slope * (x - center_x_value)));
+    // ns_utils::print("yval : ", yval);
+    ycurve_output.emplace_back(yval);
+  }
+
+  // Debug
+  ns_utils::print_container(ycurve_output);
+  // end of debug.
+
+  return ycurve_output;
+}
+
+std::vector<double> logisticMapDerivative(double const &max_y_value,
+                                          double const &center_x_value,
+                                          double const &slope,
+                                          std::vector<double> const &x_coord)
+{
+  std::vector<double> ycurve_output;
+
+  for (auto &x : x_coord)
+  {
+    auto yval = max_y_value / (1. + std::exp(-slope * (x - center_x_value)));
+
+    // ns_utils::print("yval : ", yval);
+    ycurve_output.emplace_back(yval * (1 - yval));
+  }
+
+  // Debug
+  ns_utils::print_container(ycurve_output);
+  // end of debug.
+
+  return ycurve_output;
+}
+
+void createTrajectoryMessage(TrajectoryMsg &traj_msg)
+{
+  // Generate constant speed logistic function.
+  double trajectory_time_length = 50.;  // !<-@brief [seconds].
+
+  // !<-@brief [meters], how far the vehicle moves in the x-coordinate
+  double trajectory_x_length = 500.;  // !<-@brief [meters].
+
+  // !<-@brief [meters] how far the vehicle moves in the y-direction,
+  double trajectory_max_y = 50.;  // !<-@brief [meters].
+  double max_speed = 10.;         // !<-@brief [m/s] maximum speed the speed trajectory will reach.
+
+  size_t N = 501;      // !<-@brief number of points in the trajectory.
+  double slope = 0.2;  // see the logisticMap method.
+  double t0 = trajectory_time_length / 2.;
+  double x0 = trajectory_x_length / 2.;
+
+  // Create a speed trajectory.
+  std::vector<double> time_vect = ns_utils::linspace(0., trajectory_time_length, N);
+  std::vector<double> x_vect = ns_utils::linspace(0., trajectory_x_length, N);
+  double dx = x_vect[1] - x_vect[0];
+
+  auto vx_vect = logisticMap(max_speed, t0, slope, time_vect);
+  auto y_vect = logisticMap(trajectory_max_y, x0, slope, x_vect);
+  auto dy_vect = logisticMapDerivative(trajectory_max_y, x0, slope, x_vect);
+  std::vector<double> yaw_vect;
+
+  // Compute yaw angles of the trajectory.
+  std::transform(dy_vect.cbegin(), dy_vect.cend(),
+                 std::back_inserter(yaw_vect), [&dx](auto &dy)
+                 {
+                   return std::atan2(dy, dx);
+                 });
+
+  // Create the trajectory points and put in the msg container.
+  for (size_t k = 0; k < x_vect.size(); ++k)
+  {
+    TrajectoryPoint tpoint{};
+    tpoint.pose.position.x = x_vect[k];
+    tpoint.pose.position.y = y_vect[k];
+    tpoint.pose.position.z = 0.;
+
+    // Set the orientation.
+    tf2::Quaternion q;
+    q.setRPY(0, 0, yaw_vect[k]);
+    tpoint.pose.orientation = tf2::toMsg(q);
+
+    // Set the speeds.
+    tpoint.longitudinal_velocity_mps = static_cast<float>(vx_vect[k]);
+    traj_msg.points.emplace_back(tpoint);
+  }
+
+  // Debug
+  ns_utils::print("Generated speed trajectory : ");
+  ns_utils::print_container(vx_vect);
+
+  ns_utils::print("Generated y-vect : ");
+  ns_utils::print_container(y_vect);
+
+  ns_utils::print("created trajectory points : ");
+  for (auto const &point : traj_msg.points)
+  {
+    auto position = point.pose.position;
+    auto yaw = tf2::getYaw(point.pose.orientation);
+    ns_utils::print(position.x, position.y, position.z, yaw);
+  }
+
+  // end of debug
+}
