d23bc024e94c93c0837d9e1d5fac3fe14a799baf
ali <boyali@gmail.com>
2022-09-12 16:13:19 +0900

    Created NMPCPerf message in the NMPC:  removed the UKF vars from the NMPCperf msg.
    Signed-off-by: ali <boyali@gmail.com>
    Signed-off-by: Ali Boyali <boyali@gmail.com>

--- control/mpc_nonlinear/test/include/nonlinear_mpc_test_node.hpp
+++ control/mpc_nonlinear/test/include/nonlinear_mpc_test_node.hpp
@@ -39,7 +39,7 @@ using VelocityMsg = nav_msgs::msg::Odometry;
 using TrajectoryMsg = autoware_auto_planning_msgs::msg::Trajectory;
 using ControlCmdMsg = autoware_auto_control_msgs::msg::AckermannControlCommand;
 using SteeringReport = autoware_auto_vehicle_msgs::msg::SteeringReport;
-using NonlinearMPCPerformanceMsg = autoware_auto_vehicle_msgs::msg::NonlinearMPCPerformanceReport;
+using NonlinearMPCPerformanceMsg = mpc_nonlinear::msg::NonlinearMPCPerformanceReport;
 using TrajectoryPoint = autoware_auto_planning_msgs::msg::TrajectoryPoint;
 using namespace std::chrono_literals;
 using NonlinearMPCNode = ns_mpc_nonlinear::NonlinearMPCNode;
