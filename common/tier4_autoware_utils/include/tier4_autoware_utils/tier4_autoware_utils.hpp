d27d1a022391037de57563871c037ea0b6fe89d9
scepter914 <scepter914@gmail.com>
2022-09-12 14:13:21 +0900

    add include file
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- common/tier4_autoware_utils/include/tier4_autoware_utils/tier4_autoware_utils.hpp
+++ common/tier4_autoware_utils/include/tier4_autoware_utils/tier4_autoware_utils.hpp
@@ -32,6 +32,7 @@
 #include "tier4_autoware_utils/ros/transform_listener.hpp"
 #include "tier4_autoware_utils/ros/update_param.hpp"
 #include "tier4_autoware_utils/ros/wait_for_param.hpp"
+#include "tier4_autoware_utils/ros/msg_covariance.hpp"
 #include "tier4_autoware_utils/system/stop_watch.hpp"
 
 #endif  // TIER4_AUTOWARE_UTILS__TIER4_AUTOWARE_UTILS_HPP_
