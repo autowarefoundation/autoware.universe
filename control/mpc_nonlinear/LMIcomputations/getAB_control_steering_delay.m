d21104453fb0a6ad2e34d89f4088e603de6051a7
ali <boyali@gmail.com>
2022-09-21 21:56:16 +0900

    NMPC: LPV Lyap matrices are recomputed. Lyap test passed. Matlab files updated.

--- control/mpc_nonlinear/LMIcomputations/getAB_control_steering_delay.m
+++ control/mpc_nonlinear/LMIcomputations/getAB_control_steering_delay.m
@@ -19,21 +19,23 @@ function [A, B, C, D] = getAB_control_steering_delay(x,kappai, tau, L)
    
     
     A(1, 2) = v*cos(epsi);
-    A(1, 3) = 0;    
+    A(1, 3) = sin(epsi);    
     
     A(2, 1) = -kp1^2*A(1, 2);
-    A(2, 2) = kp1*v*sin(epsi);
-    A(2, 3) = v/(L*cos(delta)^2);    
-    A(3, 3) = -1/tau; 
+    A(2, 2) = kp1*v*sin(epsi);    
+    A(2, 3) =  tan(delta)/L -kp1*cos(epsi);    
+    A(2, 4) = v*(tan(delta)^2 + 1)/L;
+    
+    A(4, 4) = -1/tau; 
         
     
     % controls
-    B(3,1)=1/tau;    
-    B(4,2)=1;    
+    B(3,1)=1;    
+    B(4,2)=1/tau;    
+    
     
     C = eye(n);
     D = 0 ;  
-    
  
     
     
