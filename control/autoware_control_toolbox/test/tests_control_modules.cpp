fc1d90d8d90c73cb1c70a45bd475c3a080156eb6
ali <boyali@gmail.com>
2022-09-24 01:09:02 +0900

    TESTING ACT: ss and tf factor power is tested.

--- control/autoware_control_toolbox/test/tests_control_modules.cpp
+++ control/autoware_control_toolbox/test/tests_control_modules.cpp
@@ -15,7 +15,7 @@
 #include "act_test_suite.hpp"
 
 /**
- * Test transfer function constructor from TF factors.
+ * ------------------ Test transfer function constructor from TF factors. ----------
  * */
 TEST(ACTcontrol, tffactorConstruction)
 {
@@ -141,4 +141,71 @@ TEST(ACTcontrol, vectorScalarMultiplicationOverloading)
   }
 
   ASSERT_TRUE(true);
+}
+
+/**
+ * ------------------ Test state space methods -------------------------------
+ * */
+
+/**
+ *  Matlab code
+ *  clc
+ *   s = tf('s');
+ *   tau = 0.008;
+ *   norder = 2;
+ *   temp_tf = 1/(tau*s + 1)^norder
+ *   num = temp_tf.Numerator{1}
+ *   den = temp_tf.Denominator{1}
+ *   temp_ss = ss(temp_tf)
+ *   [A, B, C, D] = tf2ss(1, den)
+ *
+ *   temp_tf =
+ *
+ *               1
+ *   -------------------------
+ *   6.4e-05 s^2 + 0.016 s + 1
+ *
+ *   And its state space representation. (Balanced SS matrices.)
+ *   A =   1.0e+04 *
+ *      [-250   -122.1]
+ *      [ 128      0]
+ *   B = [8, 0]' and C = [0, 15.26]
+ * */
+
+TEST(ACTcontrol, takingSquareOfTFandSS)
+{
+  double const tau = 0.008;
+  size_t const norder = 2;
+
+  auto temp_tfactor_den = ns_control_toolbox::tf_factor{{tau, 1.}};
+  temp_tfactor_den.power(norder);
+
+  auto temp_tf = ns_control_toolbox::tf({1.}, temp_tfactor_den());
+  temp_tf.print();
+
+  auto const &tf_den = temp_tf.den();
+
+  ASSERT_DOUBLE_EQ(tf_den[0], 6.4e-05);
+  ASSERT_DOUBLE_EQ(tf_den[1], 0.016);
+  ASSERT_DOUBLE_EQ(tf_den[2], 1.);
+
+  // STATE-SPACE
+  auto ss_cont = ns_control_toolbox::tf2ss(temp_tf);
+  ss_cont.print();
+
+  auto A = ss_cont.A();
+  auto B = ss_cont.B();
+  auto C = ss_cont.C();
+  auto D = ss_cont.D();
+
+  ASSERT_DOUBLE_EQ(A(0, 0), -250.);
+  ASSERT_DOUBLE_EQ(A(0, 1), -122.0703125);
+  ASSERT_DOUBLE_EQ(A(1, 0), 128.);
+  ASSERT_DOUBLE_EQ(A(1, 1), 0.);
+
+  /**
+   * http://www.ece.northwestern.edu/local-apps/matlabhelp/toolbox/control/ref/ssbal.html
+   * */
+  ASSERT_DOUBLE_EQ(C(1) * B(0), 122.0703125); // due to the special algorithm of matrix balancing.
+  ASSERT_DOUBLE_EQ(D(0), 0.);
 }
