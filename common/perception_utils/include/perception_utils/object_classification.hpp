d5162ea1c31cf8076b52b27249c0ed3159419b32
scepter914 <scepter914@gmail.com>
2022-09-26 15:43:38 +0900

    fix input arg of object classification function
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- common/perception_utils/include/perception_utils/object_classification.hpp
+++ common/perception_utils/include/perception_utils/object_classification.hpp
@@ -21,39 +21,43 @@ namespace perception_utils
 {
 using autoware_auto_perception_msgs::msg::ObjectClassification;
 
-inline bool isVehicle(const uint8_t object_classification)
+inline bool isVehicle(const std::vector<ObjectClassification> & object_classifications)
 {
-  return object_classification == ObjectClassification::BICYCLE ||
-         object_classification == ObjectClassification::BUS ||
-         object_classification == ObjectClassification::CAR ||
-         object_classification == ObjectClassification::MOTORCYCLE ||
-         object_classification == ObjectClassification::TRAILER ||
-         object_classification == ObjectClassification::TRUCK;
+  auto highest_prob_classification = getHighestProbLabel(object_classifications);
+  return highest_prob_classification == ObjectClassification::BICYCLE ||
+         highest_prob_classification == ObjectClassification::BUS ||
+         highest_prob_classification == ObjectClassification::CAR ||
+         highest_prob_classification == ObjectClassification::MOTORCYCLE ||
+         highest_prob_classification == ObjectClassification::TRAILER ||
+         highest_prob_classification == ObjectClassification::TRUCK;
 }
 
-inline bool isCarLikeVehicle(const uint8_t object_classification)
+inline bool isCarLikeVehicle(const std::vector<ObjectClassification> & object_classifications)
 {
-  return object_classification == ObjectClassification::BUS ||
-         object_classification == ObjectClassification::CAR ||
-         object_classification == ObjectClassification::TRAILER ||
-         object_classification == ObjectClassification::TRUCK;
+  auto highest_prob_classification = getHighestProbLabel(object_classifications);
+  return highest_prob_classification == ObjectClassification::BUS ||
+         highest_prob_classification == ObjectClassification::CAR ||
+         highest_prob_classification == ObjectClassification::TRAILER ||
+         highest_prob_classification == ObjectClassification::TRUCK;
 }
 
-inline bool isLargeVehicle(const uint8_t object_classification)
+inline bool isLargeVehicle(const std::vector<ObjectClassification> & object_classifications)
 {
-  return object_classification == ObjectClassification::BUS ||
-         object_classification == ObjectClassification::TRAILER ||
-         object_classification == ObjectClassification::TRUCK;
+  auto highest_prob_classification = getHighestProbLabel(object_classifications);
+  return highest_prob_classification == ObjectClassification::BUS ||
+         highest_prob_classification == ObjectClassification::TRAILER ||
+         highest_prob_classification == ObjectClassification::TRUCK;
 }
 
-inline std::uint8_t getHighestProbLabel(const std::vector<ObjectClassification> & classification)
+inline std::uint8_t getHighestProbLabel(
+  const std::vector<ObjectClassification> & object_classifications)
 {
-  std::uint8_t label = autoware_auto_perception_msgs::msg::ObjectClassification::UNKNOWN;
+  std::uint8_t label = ObjectClassification::UNKNOWN;
   float highest_prob = 0.0;
-  for (const auto & _class : classification) {
-    if (highest_prob < _class.probability) {
-      highest_prob = _class.probability;
-      label = _class.label;
+  for (const auto & object_classification : object_classifications) {
+    if (highest_prob < object_classification.probability) {
+      highest_prob = object_classification.probability;
+      label = object_classification.label;
     }
   }
   return label;
