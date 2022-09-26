9f29e78161bfde597ac7c7ff27a4fe815a82d72e
ali <boyali@gmail.com>
2022-09-26 17:34:12 +0900

    TESTING ACT:  Added BSpline interpolator test.
    Signed-off-by: ali <boyali@gmail.com>

--- control/autoware_control_toolbox/analyze_outputs_py/check_Bspline_interpolator_ROS.py
+++ control/autoware_control_toolbox/analyze_outputs_py/check_Bspline_interpolator_ROS.py
@@ -10,33 +10,22 @@ sys.path.append("..")
 path = "logs/bspline_interp/"
 
 # Load the signals
-sxyz_ref = np.loadtxt(path + 'sxyz_ref.txt')  # interpolated vector
-sxyz_smooth = np.loadtxt(path + 'smoothed_map_ROS.txt')  # interpolated vector
-tvec_base = sxyz_ref[:, 0]
-tvec_smth = sxyz_smooth[:, 0]
+# Down-sampled
+yze = np.loadtxt(path + 'yze.txt')  # original ye
+yzeinterp = np.loadtxt(path + 'yz_interp_newsize.txt')  # original ye
 
-# Curvature ROS
-curvature_original = np.loadtxt(path + 'curvature_original_ROS.txt')  # interpolated vector
-curvature_spline = np.loadtxt(path + 'curvature_bspline_interpolator_ROS.txt')  # interpolated vector
+tbase = np.loadtxt(path + 'tvec_base.txt')  # original time coordinate
+tnew = np.loadtxt(path + 'tvec_new.txt')  # downsampled time coordinate
 
 if __name__ == "__main__":
-    plt.plot(tvec_base, sxyz_ref[:, 1], 'k-.', alpha=0.5, lw=2, label='x coord')
-    plt.plot(tvec_smth, sxyz_smooth[:, 1], 'r-.', alpha=1, lw=1, label='x coord')
+    plt.plot(tbase, yze[:, 0], 'k-.', alpha=0.5, lw=2, label='y original')
+    plt.plot(tnew, yzeinterp[:, 0], 'r-.', alpha=1, lw=1, label='y interpolated')
     plt.legend()
+    plt.grid()
     plt.show()
 
-    plt.plot(tvec_base, sxyz_ref[:, 2], 'k-.', alpha=0.5, lw=2, label='y coord')
-    plt.plot(tvec_smth, sxyz_smooth[:, 2], 'r-.', alpha=1, lw=1, label='x coord')
+    plt.plot(tbase, yze[:, 1], 'k-.', alpha=0.5, lw=2, label='y original')
+    plt.plot(tnew, yzeinterp[:, 1], 'r-.', alpha=1, lw=1, label='y interpolated')
     plt.legend()
-    plt.show()
-
-    plt.plot(tvec_base, sxyz_ref[:, 3], 'k-.', alpha=0.5, lw=2, label='z coord')
-    plt.plot(tvec_smth, sxyz_smooth[:, 3], 'r-.', alpha=1, lw=1, label='x coord')
-    plt.legend()
-    plt.show()
-
-    ## TEST CURVATURE
-    plt.plot(tvec_base, curvature_original, 'r', lw=3, alpha=0.3, label='Original Curvature')
-    plt.plot(tvec_smth, curvature_spline, 'k-.', label='BSpline Interpolator')
-    plt.legend()
+    plt.grid()
     plt.show()
