f1b2cf868a9b658e3a37038411f0bdb2aa07e6a2
ali <boyali@gmail.com>
2022-09-26 22:23:38 +0900

    TESTING ACT:  adding angle distance test.

--- control/autoware_control_toolbox/test/tests_util_modules.cpp
+++ control/autoware_control_toolbox/test/tests_util_modules.cpp
@@ -14,3 +14,62 @@
 
 #include "gtest/gtest.h"
 #include "act_test_suite.hpp"
+
+TEST(ACTutils, linearInterpolateExtrapolate)
+{
+  // Generate a  sinusoidal signal with arc-length parametrization. This is our test signal.
+  size_t const num_of_points = 100; // number of points in the signal.
+
+  // Generate x.
+  std::vector<double> xvec = ns_utils::linspace(0.0, 10.0, num_of_points + 1);
+
+  // Generate y = ax + b.
+  double const a = 5.;
+  double const b = 1.;
+
+  std::vector<double> yvec;
+  std::transform(xvec.cbegin(), xvec.cend(), std::back_inserter(yvec), [&a, &b](auto const &x)
+  {
+    return a * x + b;
+  });
+
+  std::vector<double> yextrapolation;
+  std::vector<double> xnew{-1., 11, 5};
+  ns_utils::interp1d_linear(xvec, yvec, xnew, yextrapolation);
+
+  ns_utils::print_container(yextrapolation);
+  ASSERT_DOUBLE_EQ(yextrapolation[0], a * xnew[0] + b);
+  ASSERT_DOUBLE_EQ(yextrapolation[1], a * xnew[1] + b);
+  ASSERT_DOUBLE_EQ(yextrapolation[2], a * xnew[2] + b);
+}
+
+TEST(ACTutils, angleDistance)
+{
+  auto theta_ref = ns_utils::deg2rad(0.);
+  auto theta_1 = 0.0872665; // ns_utils::deg2rad(5.);
+  auto theta_2 = -0.0872665; // ns_utils::deg2rad(-5.);
+
+  auto theta_3 = 3.12414; // ns_utils::deg2rad(179.);
+  auto theta_4 = 3.15905; //ns_utils::deg2rad(181.);
+  auto theta_5 = 3.15905; //ns_utils::deg2rad(-179.);
+
+  ns_utils::print(theta_ref, theta_1, theta_2, theta_3, theta_4, theta_5);
+  ASSERT_TRUE(true);
+
+  std::vector<double> thetas{theta_1, theta_2, theta_3, theta_4, theta_5};
+  std::vector<double> distances;
+
+  for (auto const &th : thetas)
+  {
+    auto dist = ns_utils::angleDistance(th, theta_ref);
+    distances.emplace_back(dist);
+  }
+
+  ns_utils::print_container(distances);
+//  ASSERT_LE(std::fabs(distances[0] - theta_1), 1e-8);
+//  ASSERT_LE(std::fabs(distances[1] - theta_2), 1e-8);
+//  ASSERT_LE(std::fabs(distances[2] - theta_3), 1e-8);
+//  ASSERT_LE(std::fabs(distances[3] - theta_5), 1e-8);
+//  ASSERT_LE(std::fabs(distances[4] - theta_5), 1e-8);
+
+}
