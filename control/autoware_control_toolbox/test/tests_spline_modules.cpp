e52643edc558ea9ecd036efe5fcd62fbf05c9731
ali <boyali@gmail.com>
2022-09-26 17:34:12 +0900

    TESTING ACT:  Added BSpline interpolator test.
    Signed-off-by: ali <boyali@gmail.com>
    Signed-off-by: Ali Boyali <boyali@gmail.com>

--- control/autoware_control_toolbox/test/tests_spline_modules.cpp
+++ control/autoware_control_toolbox/test/tests_spline_modules.cpp
@@ -313,6 +313,56 @@ TEST(ACTspline, linePCGextrapolation)
 }
 
 /**
- * Test BSpline Interpolator and Smoothers.
+ * Test BSpline Interpolator and Smoothers and Interpolators.
  * */
 
+TEST(ACTspline, bSplineInterpolatorDownSampling)
+{
+  // Generate a noisy sinusoidal signal with arc-length parametrization. This is our test signal.
+  size_t const base_size = 100; // number of points in the signal.
+  size_t const new_size = 50; // data will be expanded to this size.
+
+
+  // Generate x.
+  double const kx = 8;
+
+  // Generate y = sin(x).
+  double const cy = 10;
+
+  auto tbase = Eigen::VectorXd::LinSpaced(base_size, 0.0, kx);
+  auto tnew = Eigen::VectorXd::LinSpaced(new_size, 0.0, kx);
+
+  Eigen::MatrixXd ye(tbase.unaryExpr([&cy](auto const &t)
+                                     { return cy * sin(t); }));
+
+  Eigen::MatrixXd ze(tbase.unaryExpr([](auto const &t)
+                                     { return 2 * cos(t) - 3 * sin(t); }));
+
+  // Create a matrix to be interpolated into a new size.
+  auto const &yze = ns_eigen_utils::hstack<double>(ye, ze);
+  Eigen::MatrixXd yz_interp_newsize;
+
+  // Create a new smoothing spline.
+  ns_splines::BSplineInterpolator interpolating_bspline(base_size, new_size, 0.3);
+
+  // Different size multi-column interpolation. Expanding the data points.
+  interpolating_bspline.InterpolateInCoordinates(yze, yz_interp_newsize);
+
+  /**
+   * Create validating sets for y and z vectors.
+   * */
+  Eigen::MatrixXd ye_validation(tnew.unaryExpr([&cy](auto const &t)
+                                               { return cy * sin(t); }));
+
+  Eigen::MatrixXd ze_validation(tnew.unaryExpr([&](auto x)
+                                               { return 2 * cos(x) - 3 * sin(x); }));
+
+  auto error_y = yz_interp_newsize.col(0) - ye_validation;
+  auto error_y_rms = std::sqrt(error_y.array().square().sum() / new_size) / cy; // normalized to 1.
+
+  ns_utils::print("ye_validation size ", ye_validation.rows(), ye_validation.cols());
+  ns_utils::print("yze rows cols ", yz_interp_newsize.rows(), yz_interp_newsize.cols());
+  ns_utils::print("Approximation error for y :", error_y_rms);
+  ASSERT_LE(error_y_rms, 1e-2);  // this is a global curve fitting with down-sampling test.
+
+}
