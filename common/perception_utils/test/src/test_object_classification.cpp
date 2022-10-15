e678ec11dda7cc9de79b7df6106eaa40c2707b97
Yoshi Ri <yoshi.ri@tier4.jp>
2022-10-16 00:40:40 +0900

    add test for isVehicle
    Signed-off-by: Yoshi Ri <yoshi.ri@tier4.jp>

--- common/perception_utils/test/src/test_object_classification.cpp
+++ common/perception_utils/test/src/test_object_classification.cpp
@@ -28,7 +28,6 @@ autoware_auto_perception_msgs::msg::ObjectClassification createObjectClassificat
   return classification;
 }
 
-}  // namespace
 
 TEST(object_classification, test_getHighestProbLabel)
 {
@@ -61,3 +60,55 @@ TEST(object_classification, test_getHighestProbLabel)
     EXPECT_EQ(label, ObjectClassification::CAR);
   }
 }
+
+// Test isVehicle
+TEST(object_classification, test_isVehicle)
+{
+
+  using autoware_auto_perception_msgs::msg::ObjectClassification;
+  using perception_utils::isVehicle;
+
+  {// True Case with uint8_t
+  std::vector<std::uint8_t> vehicle_labels;
+  vehicle_labels.push_back(ObjectClassification::BICYCLE);
+  vehicle_labels.push_back(ObjectClassification::BUS);
+  vehicle_labels.push_back(ObjectClassification::CAR);
+  vehicle_labels.push_back(ObjectClassification::MOTORCYCLE);
+  vehicle_labels.push_back(ObjectClassification::TRAILER);
+  vehicle_labels.push_back(ObjectClassification::TRUCK);
+  
+  for(auto label: vehicle_labels){
+    EXPECT_TRUE(isVehicle(label));
+  }
+  }
+
+  // False Case with uint8_t
+  {
+  std::vector<std::uint8_t> non_vehicle_labels;
+  non_vehicle_labels.push_back(ObjectClassification::UNKNOWN);
+  non_vehicle_labels.push_back(ObjectClassification::PEDESTRIAN);
+  for(auto label: non_vehicle_labels){
+    EXPECT_FALSE(isVehicle(label));
+  }
+  }
+
+  // True Case with object_classifications
+  {  // normal case
+    std::vector<autoware_auto_perception_msgs::msg::ObjectClassification> classification;
+    classification.push_back(createObjectClassification(ObjectClassification::CAR, 0.5));
+    classification.push_back(createObjectClassification(ObjectClassification::TRUCK, 0.8));
+    classification.push_back(createObjectClassification(ObjectClassification::BUS, 0.7));
+    EXPECT_TRUE(isVehicle(classification));
+  }
+
+  // False Case with object_classifications
+  {  // false case
+    std::vector<autoware_auto_perception_msgs::msg::ObjectClassification> classification;
+    classification.push_back(createObjectClassification(ObjectClassification::PEDESTRIAN, 0.8));
+    classification.push_back(createObjectClassification(ObjectClassification::BICYCLE, 0.7));
+    EXPECT_FALSE(isVehicle(classification));
+  }
+
+} // TEST isVehicle
+
+}  // namespace
