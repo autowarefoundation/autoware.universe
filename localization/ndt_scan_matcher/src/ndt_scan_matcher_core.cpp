06dbfd50bae699ff804e4d2c1c2a112344b56003
kminoda <koji.minoda@tier4.jp>
2022-10-20 17:32:11 +0900

    removed 'omp'-related words completely
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- localization/ndt_scan_matcher/src/ndt_scan_matcher_core.cpp
+++ localization/ndt_scan_matcher/src/ndt_scan_matcher_core.cpp
@@ -127,10 +127,10 @@ NDTScanMatcher::NDTScanMatcher()
   ndt_params_.step_size = this->declare_parameter<double>("step_size");
   ndt_params_.resolution = this->declare_parameter<double>("resolution");
   ndt_params_.max_iterations = this->declare_parameter<int>("max_iterations");
-  int search_method = this->declare_parameter<int>("omp_neighborhood_search_method", 
+  int search_method = this->declare_parameter<int>("neighborhood_search_method", 
     static_cast<int>(pclomp::NeighborSearchMethod::KDTREE));
   ndt_params_.search_method = static_cast<pclomp::NeighborSearchMethod>(search_method);
-  ndt_params_.num_threads = this->declare_parameter<int>("omp_num_threads");
+  ndt_params_.num_threads = this->declare_parameter<int>("num_threads");
   ndt_params_.num_threads = std::max(ndt_params_.num_threads, 1);
 
   ndt_ptr_->setTransformationEpsilon(ndt_params_.trans_epsilon);
