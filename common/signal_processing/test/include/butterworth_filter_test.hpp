a68defb54abfbb4b870f07d185abdc03532550be
ali <boyali@gmail.com>
2022-10-21 21:10:17 +0900

    Test fixture is cleaned.
    Signed-off-by: ali <boyali@gmail.com>

--- common/signal_processing/test/include/butterworth_filter_test.hpp
+++ common/signal_processing/test/include/butterworth_filter_test.hpp
@@ -16,40 +16,12 @@
 #include "gtest/gtest.h"
 #include "signal_processing/butterworth.hpp"
 
-// The fixture for testing class Foo.
 class ButterWorthTestFixture : public ::testing::Test
 {
 protected:
-  // You can remove any or all of the following functions if their bodies would
-  // be empty.
+  ButterWorthTestFixture() = default;
 
-  ButterWorthTestFixture()
-  {
-    // You can do set-up work for each test here.
-  }
-
-  ~ButterWorthTestFixture() override
-  {
-    // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
-  void SetUp() override
-  {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
-  }
-
-  void TearDown() override
-  {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
-  }
-
-  // Class members declared here can be used by all tests in the test suite
-  // for Foo.
+  ~ButterWorthTestFixture() override = default;
 };
 
 #endif  // SIGNAL_PROCESSING_TEST_INCLUDE_BUTTERWORTH_FILTER_TEST_HPP_
