5ce350807a78b09adfda98f48dfb04b9bcb0c655
Grzegorz Głowacki <Grzegorz Gowacki gglowacki@autonomous-systems.pl>
2022-11-07 15:25:39 +0100

    replace conditional include ROS_DISTRO_GALACTIC with __has_include in utils.hpp
    Signed-off-by: Grzegorz Głowacki <Grzegorz Głowacki gglowacki@autonomous-systems.pl>

--- perception/image_projection_based_fusion/include/image_projection_based_fusion/utils/utils.hpp
+++ perception/image_projection_based_fusion/include/image_projection_based_fusion/utils/utils.hpp
@@ -21,15 +21,22 @@
 #include <Eigen/Geometry>
 #include <rclcpp/rclcpp.hpp>
 
-#ifdef ROS_DISTRO_GALACTIC
-#include <tf2_eigen/tf2_eigen.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
-#else
+#if __has_include ( <tf2_eigen/tf2_eigen.hpp> )
 #include <tf2_eigen/tf2_eigen.hpp>
+#else
+#include <<tf2_eigen/tf2_eigen.h>
+#endif
 
+#if __has_include ( <tf2_geometry_msgs/tf2_geometry_msgs.hpp> )
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#else
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#endif
+
+#if __has_include ( <tf2_sensor_msgs/tf2_sensor_msgs.hpp> )
 #include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
+#else
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 #endif
 
 #include <optional>
