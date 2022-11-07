4831c6ad2af41506905afc5e5a0716c343ccad71
Grzegorz Głowacki <Grzegorz Gowacki gglowacki@autonomous-systems.pl>
2022-11-07 15:25:39 +0100

    replace conditional include ROS_DISTRO_GALACTIC with __has_include in pointcloud_based_occupancy_grid_map_node.cpp
    Signed-off-by: Grzegorz Głowacki <Grzegorz Głowacki gglowacki@autonomous-systems.pl>

--- perception/probabilistic_occupancy_grid_map/src/pointcloud_based_occupancy_grid_map/pointcloud_based_occupancy_grid_map_node.cpp
+++ perception/probabilistic_occupancy_grid_map/src/pointcloud_based_occupancy_grid_map/pointcloud_based_occupancy_grid_map_node.cpp
@@ -23,13 +23,17 @@
 
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
-#ifdef ROS_DISTRO_GALACTIC
-#include <tf2_eigen/tf2_eigen.h>
-#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
-#else
-#include <tf2_eigen/tf2_eigen.hpp>
 
+#if __has_include ( <tf2_eigen/tf2_eigen.hpp> )
+#include <tf2_eigen/tf2_eigen.hpp>
+#else
+#include <<tf2_eigen/tf2_eigen.h>
+#endif
+
+#if __has_include ( <tf2_sensor_msgs/tf2_sensor_msgs.hpp> )
 #include <tf2_sensor_msgs/tf2_sensor_msgs.hpp>
+#else
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 #endif
 
 #include <memory>
