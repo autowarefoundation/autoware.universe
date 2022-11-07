2d58473021488aedcf3b42f93b955f6e7881940e
kosuke55 <kosuke.tnp@gmail.com>
2022-11-07 18:47:41 +0900

    feat(behavior_path_planner): plan shift pull over/out on curve
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- planning/behavior_path_planner/include/behavior_path_planner/scene_module/pull_out/pull_out_parameters.hpp
+++ planning/behavior_path_planner/include/behavior_path_planner/scene_module/pull_out/pull_out_parameters.hpp
@@ -26,12 +26,12 @@ struct PullOutParameters
   double th_stopped_velocity;
   double th_stopped_time;
   double collision_check_margin;
+  double collision_check_distance_from_end;
   double pull_out_finish_judge_buffer;
   // shift pull out
   bool enable_shift_pull_out;
   double shift_pull_out_velocity;
   int pull_out_sampling_num;
-  double before_pull_out_straight_distance;
   double minimum_shift_pull_out_distance;
   double maximum_lateral_jerk;
   double minimum_lateral_jerk;
