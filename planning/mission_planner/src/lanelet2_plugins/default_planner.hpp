f192ee5e3f5267d2d578e21717b9e0559ec0cb0a
Takayuki Murooka <takayuki5168@gmail.com>
2022-11-10 15:07:48 +0900

    revert mission_planner's change
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/mission_planner/src/lanelet2_plugins/default_planner.hpp
+++ planning/mission_planner/src/lanelet2_plugins/default_planner.hpp
@@ -36,9 +36,6 @@ class DefaultPlanner : public mission_planner::PlannerPlugin
 {
 public:
   void initialize(rclcpp::Node * node) override;
-  void initialize(
-    rclcpp::Node * node,
-    const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr msg) override;
   bool ready() const override;
   HADMapRoute plan(const RoutePoints & points) override;
   MarkerArray visualize(const HADMapRoute & route) const override;
