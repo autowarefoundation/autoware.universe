4a49ea8245b941cbfa8f6b48ac51dede0c155019
Takayuki Murooka <takayuki5168@gmail.com>
2022-11-10 12:10:18 +0900

    Update planning/mission_planner/include/mission_planner/mission_planner_plugin.hpp

--- planning/mission_planner/include/mission_planner/mission_planner_plugin.hpp
+++ planning/mission_planner/include/mission_planner/mission_planner_plugin.hpp
@@ -38,7 +38,7 @@ public:
   virtual ~PlannerPlugin() = default;
   virtual void initialize(rclcpp::Node * node) = 0;
   virtual void initialize(
-    rclcpp::Node * node, const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr msg) = 0;
+    rclcpp::Node * node, const HADMapBin::ConstSharedPtr msg) = 0;
   virtual bool ready() const = 0;
   virtual HADMapRoute plan(const RoutePoints & points) = 0;
   virtual MarkerArray visualize(const HADMapRoute & route) const = 0;
