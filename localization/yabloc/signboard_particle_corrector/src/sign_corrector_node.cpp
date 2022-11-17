893d0902e40816a381a90962207ca5cbd7420f6e
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2022-11-17 10:24:44 +0900

    erase signboard_particle_corrector
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/signboard_particle_corrector/src/sign_corrector_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "particle_filter/sign_corrector.hpp"
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<particle_filter::SignCorrector>());
-  rclcpp::shutdown();
-  return 0;
-}
