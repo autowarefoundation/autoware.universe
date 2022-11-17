6135fa18fb8f662f7d63c00e5b2ae35377c665f1
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2022-11-17 12:30:36 +0900

    remove obsolete files & add standalone directory
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/vmvl_validation/src/freespace_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "vmvl_validation/freespace.hpp"
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<vmvl_validation::FreeSpace>());
-  rclcpp::shutdown();
-  return 0;
-}
