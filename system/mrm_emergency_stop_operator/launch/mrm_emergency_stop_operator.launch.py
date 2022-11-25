1ecf7f10f3cb354cfec201dad7621e472e98c892
Makoto Kurihara <mkuri8m@gmail.com>
2022-11-25 19:33:31 +0900

    fix(mrm_emergency_stop_operator): remove os import
    Signed-off-by: Makoto Kurihara <mkuri8m@gmail.com>

--- system/mrm_emergency_stop_operator/launch/mrm_emergency_stop_operator.launch.py
+++ system/mrm_emergency_stop_operator/launch/mrm_emergency_stop_operator.launch.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from ament_index_python.packages import get_package_share_directory
 import launch
 from launch.actions import DeclareLaunchArgument
