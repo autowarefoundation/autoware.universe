a8e4fd1da8c6a74a0903ab274f71426956a9f550
kminoda <koji.minoda@tier4.jp>
2022-12-01 17:25:23 +0900

    remove unnecessary commentouts
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- localization/gyro_odometer/include/gyro_odometer/gyro_odometer_core.hpp
+++ localization/gyro_odometer/include/gyro_odometer/gyro_odometer_core.hpp
@@ -70,8 +70,6 @@ private:
   std::string output_frame_;
   double message_timeout_sec_;
 
-  // geometry_msgs::msg::TwistWithCovarianceStamped::ConstSharedPtr twist_with_cov_msg_ptr_;
-  // sensor_msgs::msg::Imu::ConstSharedPtr imu_msg_ptr_;
   bool vehicle_twist_arrived_;
   bool imu_arrived_;
   std::deque<geometry_msgs::msg::TwistWithCovarianceStamped> vehicle_twist_queue_;
