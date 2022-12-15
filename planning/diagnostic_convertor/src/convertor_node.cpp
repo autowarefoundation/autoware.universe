8b9d18accc17231760ac8a647f82f3eec979a4ee
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2022-12-15 12:49:16 +0900

    add test
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/diagnostic_convertor/src/convertor_node.cpp
+++ planning/diagnostic_convertor/src/convertor_node.cpp
@@ -12,120 +12,63 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <gtest/gtest.h>
-
-#include <memory>
-#include <string>
-#include <vector>
-
 #include "diagnostic_converter/converter_node.hpp"
-#include "diagnostic_msgs/msg/diagnostic_array.hpp"
-#include "diagnostic_msgs/msg/diagnostic_status.hpp"
-#include "diagnostic_msgs/msg/key_value.hpp"
-#include "openscenario_msgs/msg/parameter_declaration.hpp"
-#include "rclcpp/rclcpp.hpp"
 
-using ConverterNode = diagnostic_converter::DiagnosticConverter;
-using diagnostic_msgs::msg::DiagnosticArray;
-using diagnostic_msgs::msg::DiagnosticStatus;
-using diagnostic_msgs::msg::KeyValue;
-using openscenario_msgs::msg::ParameterDeclaration;
+#include "openscenario_msgs/msg/parameter_type.hpp"
 
-void waitForMsg(
-  bool & flag, const rclcpp::Node::SharedPtr node1, const rclcpp::Node::SharedPtr node2)
+namespace diagnostic_converter
 {
-  while (!flag) {
-    rclcpp::spin_some(node1);
-    rclcpp::spin_some(node2);
-    rclcpp::sleep_for(std::chrono::milliseconds(100));
+DiagnosticConverter::DiagnosticConverter(const rclcpp::NodeOptions & node_options)
+: Node("diagnostic_converter", node_options)
+{
+  using std::placeholders::_1;
+
+  size_t sub_counter = 0;
+  std::vector<std::string> diagnostic_topics;
+  declare_parameter<std::vector<std::string>>("diagnostic_topics", std::vector<std::string>());
+  get_parameter<std::vector<std::string>>("diagnostic_topics", diagnostic_topics);
+  for (const std::string & diagnostic_topic : diagnostic_topics) {
+    // std::function required with multiple arguments https://answers.ros.org/question/289207
+    const std::function<void(const DiagnosticArray::ConstSharedPtr)> fn =
+      std::bind(&DiagnosticConverter::onDiagnostic, this, _1, sub_counter++, diagnostic_topic);
+    diagnostics_sub_.push_back(create_subscription<DiagnosticArray>(diagnostic_topic, 1, fn));
   }
-  flag = false;
+  params_pub_.resize(diagnostics_sub_.size());
 }
 
-std::function<void(ParameterDeclaration::ConstSharedPtr)> generateCallback(
-  bool & flag, ParameterDeclaration & msg)
+void DiagnosticConverter::onDiagnostic(
+  const DiagnosticArray::ConstSharedPtr diag_msg, const size_t diag_idx,
+  const std::string & base_topic)
 {
-  return [&](ParameterDeclaration::ConstSharedPtr received_msg) {
-    flag = true;
-    msg = *received_msg;
-  };
+  for (const auto & status : diag_msg->status) {
+    std::string status_topic = base_topic + (status.name.empty() ? "" : "_" + status.name);
+    for (const auto & key_value : status.values) {
+      getPublisher(status_topic + "_" + key_value.key, diag_idx)
+        ->publish(createParameterDeclaration(key_value));
+    }
+  }
 }
 
-TEST(ConverterNode, ConvertDiagnostics)
+ParameterDeclaration DiagnosticConverter::createParameterDeclaration(
+  const KeyValue & key_value) const
 {
-  const std::vector<std::string> input_topics = {"/test1/diag", "/test2/diag"};
-
-  rclcpp::init(0, nullptr);
-  rclcpp::Node::SharedPtr dummy_node = std::make_shared<rclcpp::Node>("converter_test_node");
-
-  rclcpp::NodeOptions options;
-  options.append_parameter_override("diagnostic_topics", rclcpp::ParameterValue(input_topics));
-  auto node = std::make_shared<ConverterNode>(options);
-
-  {  // Simple case with 1 resulting ParameterDeclaration
-    bool msg_received = false;
-    ParameterDeclaration param;
-    // DiagnosticArray publishers
-    const auto diag_pub = dummy_node->create_publisher<DiagnosticArray>(input_topics[0], 1);
-    // ParameterDeclaration subscribers
-    const auto param_sub_a = dummy_node->create_subscription<ParameterDeclaration>(
-      input_topics[0] + "_a", 1, generateCallback(msg_received, param));
-    DiagnosticArray diag;
-    DiagnosticStatus status;
-    status.name = "";
-    KeyValue key_value = KeyValue().set__key("a").set__value("1");
-    status.values.push_back(key_value);
-    diag.status.push_back(status);
-    diag_pub->publish(diag);
-    waitForMsg(msg_received, node, dummy_node);
-    EXPECT_EQ(param.name, "a");
-    EXPECT_EQ(param.value, "1");
-  }
-  {  // Case with multiple ParameterDeclaration converted from one DiagnosticArray
-    bool msg_received_xa = false;
-    bool msg_received_xb = false;
-    bool msg_received_ya = false;
-    bool msg_received_yb = false;
-    ParameterDeclaration param_xa;
-    ParameterDeclaration param_xb;
-    ParameterDeclaration param_ya;
-    ParameterDeclaration param_yb;
-    // DiagnosticArray publishers
-    const auto diag_pub = dummy_node->create_publisher<DiagnosticArray>(input_topics[1], 1);
-    // ParameterDeclaration subscribers
-    const auto param_sub_xa = dummy_node->create_subscription<ParameterDeclaration>(
-      input_topics[1] + "_x_a", 1, generateCallback(msg_received_xa, param_xa));
-    const auto param_sub_xb = dummy_node->create_subscription<ParameterDeclaration>(
-      input_topics[1] + "_x_b", 1, generateCallback(msg_received_xb, param_xb));
-    const auto param_sub_ya = dummy_node->create_subscription<ParameterDeclaration>(
-      input_topics[1] + "_y_a", 1, generateCallback(msg_received_ya, param_ya));
-    const auto param_sub_yb = dummy_node->create_subscription<ParameterDeclaration>(
-      input_topics[1] + "_y_b", 1, generateCallback(msg_received_yb, param_yb));
-    DiagnosticArray diag;
-    DiagnosticStatus status_x;
-    status_x.name = "x";
-    status_x.values.push_back(KeyValue().set__key("a").set__value("1"));
-    status_x.values.push_back(KeyValue().set__key("b").set__value("10"));
-    diag.status.push_back(status_x);
-    DiagnosticStatus status_y;
-    status_y.name = "y";
-    status_y.values.push_back(KeyValue().set__key("a").set__value("9"));
-    status_y.values.push_back(KeyValue().set__key("b").set__value("6"));
-    diag.status.push_back(status_y);
-    diag_pub->publish(diag);
-    waitForMsg(msg_received_xa, node, dummy_node);
-    EXPECT_EQ(param_xa.name, "a");
-    EXPECT_EQ(param_xa.value, "1");
-    waitForMsg(msg_received_xb, node, dummy_node);
-    EXPECT_EQ(param_xb.name, "b");
-    EXPECT_EQ(param_xb.value, "10");
-    waitForMsg(msg_received_ya, node, dummy_node);
-    EXPECT_EQ(param_ya.name, "a");
-    EXPECT_EQ(param_ya.value, "9");
-    waitForMsg(msg_received_yb, node, dummy_node);
-    EXPECT_EQ(param_yb.name, "b");
-    EXPECT_EQ(param_yb.value, "6");
-  }
-
-  rclcpp::shutdown();
+  ParameterDeclaration param_msg;
+  param_msg.parameter_type.data = openscenario_msgs::msg::ParameterType::DOUBLE;
+  param_msg.name = key_value.key;
+  param_msg.value = key_value.value;
+  return param_msg;
 }
+
+rclcpp::Publisher<ParameterDeclaration>::SharedPtr DiagnosticConverter::getPublisher(
+  const std::string & topic, const size_t pub_idx)
+{
+  auto & pubs = params_pub_[pub_idx];
+  if (pubs.count(topic) == 0) {
+    pubs[topic] = create_publisher<ParameterDeclaration>(topic, 1);
+  }
+  return pubs.at(topic);
+}
+}  // namespace diagnostic_converter
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(diagnostic_converter::DiagnosticConverter)
