9b740b7cb3c6d5a1fd4f157581e4d65d1f4a3e4f
Takamasa Horibe <horibe.takamasa@gmail.com>
2022-12-16 13:31:07 +0900

    enable code
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- perception/multi_object_tracker/src/multi_object_tracker_core.cpp
+++ perception/multi_object_tracker/src/multi_object_tracker_core.cpp
@@ -46,11 +46,11 @@ boost::optional<geometry_msgs::msg::Transform> getTransformAnonymous(
 {
   try {
     // check if the frames are ready
-    // std::string errstr;  // This argument prevents error msg from being displayed in the terminal.
-    // if (!tf_buffer.canTransform(
-    //       target_frame_id, source_frame_id, tf2::TimePointZero, tf2::Duration::zero(), &errstr)) {
-    //   return boost::none;
-    // }
+    std::string errstr;  // This argument prevents error msg from being displayed in the terminal.
+    if (!tf_buffer.canTransform(
+          target_frame_id, source_frame_id, tf2::TimePointZero, tf2::Duration::zero(), &errstr)) {
+      return boost::none;
+    }
 
     geometry_msgs::msg::TransformStamped self_transform_stamped;
     self_transform_stamped = tf_buffer.lookupTransform(
