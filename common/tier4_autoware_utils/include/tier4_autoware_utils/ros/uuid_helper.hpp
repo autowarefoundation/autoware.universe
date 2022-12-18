58e988e038da8abac59c23a313c67d838177e142
Takayuki Murooka <takayuki5168@gmail.com>
2022-12-18 17:21:14 +0900

    fix
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- common/tier4_autoware_utils/include/tier4_autoware_utils/ros/uuid_helper.hpp
+++ common/tier4_autoware_utils/include/tier4_autoware_utils/ros/uuid_helper.hpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Tier IV, Inc.
+// Copyright 2022 Tier IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@
 
 namespace tier4_autoware_utils
 {
-std::string toHexString(const unique_identifier_msgs::msg::UUID & id)
+inline std::string toHexString(const unique_identifier_msgs::msg::UUID & id)
 {
   std::stringstream ss;
   for (auto i = 0; i < 16; ++i) {
