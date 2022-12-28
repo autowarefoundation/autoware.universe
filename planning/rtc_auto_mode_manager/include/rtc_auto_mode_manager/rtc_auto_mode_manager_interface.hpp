780349104531b2d0778f097e199fba9d237b1bd0
tanaka3 <taiki.tanaka@tier4.jp>
2022-12-28 10:19:13 +0900

    fix: member variable
    Signed-off-by: tanaka3 <taiki.tanaka@tier4.jp>

--- planning/rtc_auto_mode_manager/include/rtc_auto_mode_manager/rtc_auto_mode_manager_interface.hpp
+++ planning/rtc_auto_mode_manager/include/rtc_auto_mode_manager/rtc_auto_mode_manager_interface.hpp
@@ -42,6 +42,7 @@ private:
   void onEnableService(
     const AutoMode::Request::SharedPtr request, const AutoMode::Response::SharedPtr response);
   AutoMode::Request createRequest(const AutoMode::Request::SharedPtr request) const;
+  AutoModeStatus auto_mode_status_;
   rclcpp::Client<AutoMode>::SharedPtr enable_cli_;
   rclcpp::Service<AutoMode>::SharedPtr enable_srv_;
 
