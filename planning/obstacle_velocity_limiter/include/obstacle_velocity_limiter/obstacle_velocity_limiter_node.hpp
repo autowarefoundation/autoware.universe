061063652dd92e418669d30757d1fedc5ae7f607
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2022-12-28 09:08:43 +0900

    tf to topic in obstacle velocity limiter node
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/obstacle_velocity_limiter/include/obstacle_velocity_limiter/obstacle_velocity_limiter_node.hpp
+++ planning/obstacle_velocity_limiter/include/obstacle_velocity_limiter/obstacle_velocity_limiter_node.hpp
@@ -51,7 +51,6 @@ public:
 
 private:
   tier4_autoware_utils::TransformListener transform_listener_{this};
-  tier4_autoware_utils::SelfPoseListener self_pose_listener_{this};
   rclcpp::Publisher<Trajectory>::SharedPtr
     pub_trajectory_;  //!< @brief publisher for output trajectory
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr
