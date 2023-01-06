de03c9fc41edb333542fe29bab6a223abc36dc97
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-01-06 16:42:07 +0900

    call service to disable camer_corrector from manager
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/particle_filter/corrector_manager/include/corrector_manager/corrector_manager.hpp
+++ localization/yabloc/particle_filter/corrector_manager/include/corrector_manager/corrector_manager.hpp
@@ -2,6 +2,7 @@
 #include "corrector_manager/init_area.hpp"
 
 #include <rclcpp/rclcpp.hpp>
+#include <std_srvs/srv/set_bool.hpp>
 
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
@@ -13,15 +14,21 @@ class CorrectorManager : public rclcpp::Node
 public:
   using PointCloud2 = sensor_msgs::msg::PointCloud2;
   using PoseStamped = geometry_msgs::msg::PoseStamped;
+  using SetBool = std_srvs::srv::SetBool;
 
   CorrectorManager();
 
 private:
+  rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Subscription<PointCloud2>::SharedPtr sub_init_area_;
   rclcpp::Subscription<PoseStamped>::SharedPtr sub_gnss_pose_;
+  rclcpp::Client<SetBool>::SharedPtr client_;
 
   std::optional<InitArea> init_area_{std::nullopt};
+  rclcpp::CallbackGroup::SharedPtr service_callback_group_;
+  void call_service(bool data);
 
+  void on_timer();
   void on_init_area(const PointCloud2 & msg);
   void on_gnss_pose(const PoseStamped & msg);
 };
--- localization/yabloc/particle_filter/corrector_manager/src/corrector_manager_core.cpp
+++ localization/yabloc/particle_filter/corrector_manager/src/corrector_manager_core.cpp
@@ -10,6 +10,25 @@ CorrectorManager::CorrectorManager() : Node("predictor")
   sub_init_area_ =
     create_subscription<PointCloud2>("/localization/map/ll2_polygon", 10, on_init_area);
   sub_gnss_pose_ = create_subscription<PoseStamped>("/sensing/gnss/ublox/pose", 10, on_gnss_pose);
+
+  // Timer
+  using namespace std::literals::chrono_literals;
+  auto on_timer = std::bind(&CorrectorManager::on_timer, this);
+  timer_ = rclcpp::create_timer(this, get_clock(), 1000ms, std::move(on_timer));
+
+  // Service definition
+  service_callback_group_ = create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  client_ =
+    create_client<SetBool>("/switch", rmw_qos_profile_services_default, service_callback_group_);
+  while (!client_->wait_for_service(1s) && rclcpp::ok()) {
+    RCLCPP_INFO(get_logger(), "Waiting for service...");
+  }
+
+  call_service(declare_parameter<bool>("camera_corrector_enabled_at_first", true));
+}
+
+void CorrectorManager::on_timer()
+{  // TODO:
 }
 
 void CorrectorManager::on_init_area(const PointCloud2 & msg)
@@ -20,12 +39,28 @@ void CorrectorManager::on_init_area(const PointCloud2 & msg)
 
 void CorrectorManager::on_gnss_pose(const PoseStamped & msg)
 {
-  if (init_area_) {
-    auto p = msg.pose.position;
-    if (init_area_->is_inside({p.x, p.y, p.z})) {
-      RCLCPP_WARN_STREAM(get_logger(), "Initialize pose because gnss enters initializable area");
-      // TODO:
-    }
+  if (!init_area_) return;
+
+  const auto p = msg.pose.position;
+  if (init_area_->is_inside({p.x, p.y, p.z})) {
+    RCLCPP_WARN_STREAM(get_logger(), "Initialize pose because gnss enters initializable area");
+    call_service(true);
+  }
+}
+
+void CorrectorManager::call_service(bool data)
+{
+  using namespace std::chrono_literals;
+  auto request = std::make_shared<SetBool::Request>();
+  request->data = data;
+
+  auto result_future = client_->async_send_request(request);
+  std::future_status status = result_future.wait_for(1000ms);
+  if (status == std::future_status::ready) {
+    RCLCPP_WARN_STREAM(get_logger(), "service responce is received successfully");
+    return;
+  } else {
+    RCLCPP_WARN_STREAM(get_logger(), "service responce is time out");
   }
 }
 
