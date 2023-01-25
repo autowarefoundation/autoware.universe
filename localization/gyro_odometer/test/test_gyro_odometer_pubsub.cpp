187ca9202ef7f414b521ca7841c3ea79465d1565
kminoda <koji.minoda@tier4.jp>
2023-01-25 19:23:06 +0900

    update comment
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- localization/gyro_odometer/test/test_gyro_odometer_pubsub.cpp
+++ localization/gyro_odometer/test/test_gyro_odometer_pubsub.cpp
@@ -95,8 +95,7 @@ bool isTwistValid(
 }
 
 // IMU & Velocity test
-// Provide gyro_odometer with minimal velocity and IMU data to verify that the gyro_odometer
-// successfully publishes the output twist message
+// Verify that the gyro_odometer successfully publishes the fused twist message when both IMU and velocity data are provided
 TEST(GyroOdometer, TestGyroOdometerWithImuAndVelocity)
 {
   Imu input_imu = generateSampleImu();
@@ -131,8 +130,7 @@ TEST(GyroOdometer, TestGyroOdometerWithImuAndVelocity)
 }
 
 // IMU-only test
-// Provide gyro_odometer only with IMU data to verify that the gyro_odometer does NOT publish any
-// outputs
+// Verify that the gyro_odometer does NOT publish any outputs when only IMU is provided
 TEST(GyroOdometer, TestGyroOdometerImuOnly)
 {
   Imu input_imu = generateSampleImu();
