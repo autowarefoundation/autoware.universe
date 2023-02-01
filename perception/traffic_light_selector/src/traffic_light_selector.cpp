cba093d3406208ae0783fd2de65fafc67c744254
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-02-01 14:29:15 +0900

    feat: publish signals
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- perception/traffic_light_selector/src/traffic_light_selector.cpp
+++ perception/traffic_light_selector/src/traffic_light_selector.cpp
@@ -40,6 +40,54 @@ std::vector<TrafficLightConstPtr> filter_traffic_signals(const LaneletMapConstPt
 
 }  // namespace lanelet
 
+namespace utils
+{
+
+using Input = autoware_auto_perception_msgs::msg::TrafficLight;
+using Output = autoware_perception_msgs::msg::TrafficLightElement;
+
+template <class K, class V>
+V at_or(const std::unordered_map<K, V> & map, const K & key, const V & value)
+{
+  return map.count(key) ? map.at(key) : value;
+}
+
+Output convert(const Input & input)
+{
+  // clang-format off
+  static const std::unordered_map<Input::_color_type, Output::_color_type> color_map({
+    {Input::RED, Output::RED},
+    {Input::AMBER, Output::AMBER},
+    {Input::GREEN, Output::GREEN},
+    {Input::WHITE, Output::WHITE}
+  });
+  static const std::unordered_map<Input::_shape_type, Output::_shape_type> shape_map({
+    {Input::CIRCLE, Output::CIRCLE},
+    {Input::LEFT_ARROW, Output::LEFT_ARROW},
+    {Input::RIGHT_ARROW, Output::RIGHT_ARROW},
+    {Input::UP_ARROW, Output::UP_ARROW},
+    {Input::DOWN_ARROW, Output::DOWN_ARROW},
+    {Input::DOWN_LEFT_ARROW, Output::DOWN_LEFT_ARROW},
+    {Input::DOWN_RIGHT_ARROW, Output::DOWN_RIGHT_ARROW},
+    {Input::CROSS, Output::CROSS}
+  });
+  static const std::unordered_map<Input::_status_type, Output::_status_type> status_map({
+    {Input::SOLID_OFF, Output::SOLID_OFF},
+    {Input::SOLID_ON, Output::SOLID_ON},
+    {Input::FLASHING, Output::FLASHING}
+  });
+  // clang-format on
+
+  Output output;
+  output.color = at_or(color_map, input.color, Output::UNKNOWN);
+  output.shape = at_or(shape_map, input.shape, Output::UNKNOWN);
+  output.color = at_or(status_map, input.status, Output::UNKNOWN);
+  output.confidence = input.confidence;
+  return output;
+}
+
+}  // namespace utils
+
 TrafficLightSelector::TrafficLightSelector(const rclcpp::NodeOptions & options)
 : Node("traffic_light_selector", options)
 {
@@ -68,13 +116,26 @@ void TrafficLightSelector::on_map(const LaneletMapBin::ConstSharedPtr msg)
 
 void TrafficLightSelector::on_lights(const TrafficLightArray::ConstSharedPtr msg)
 {
+  using TrafficSignal = autoware_perception_msgs::msg::TrafficSignal;
+  using TrafficLightElement = autoware_perception_msgs::msg::TrafficLightElement;
+
+  std::unordered_map<lanelet::Id, std::vector<TrafficLightElement>> elements;
   for (const auto & light : msg->signals) {
-    RCLCPP_INFO_STREAM(get_logger(), "ID: " << light.map_primitive_id);
-    for (const auto & e : light.lights) {
-      RCLCPP_INFO_STREAM(
-        get_logger(), "  - " << (int)e.color << " " << (int)e.shape << " " << e.confidence);
+    const auto id = light_to_signal_[light.map_primitive_id];
+    for (const auto & element : light.lights) {
+      elements[id].push_back(utils::convert(element));
     }
   }
+
+  TrafficSignalArray array;
+  array.stamp = now();
+  for (const auto & [id, elements] : elements) {
+    TrafficSignal signal;
+    signal.traffic_signal_id = id;
+    signal.elements = elements;
+    array.signals.push_back(signal);
+  }
+  pub_signals_->publish(array);
 }
 
 #include <rclcpp_components/register_node_macro.hpp>
