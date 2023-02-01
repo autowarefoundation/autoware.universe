6cc77e1ced4eab6699c44cda91ca1a037eb45d80
Zeynep Akbalık <zeynep@leodrive.ai>
2023-02-01 13:10:10 +0300

    feat(traffic_light_classifier): add params to launch file
    Signed-off-by: Zeynep Akbalık <zeynep@leodrive.ai>

--- perception/traffic_light_classifier/src/cnn_classifier.cpp
+++ perception/traffic_light_classifier/src/cnn_classifier.cpp
@@ -39,9 +39,8 @@ CNNClassifier::CNNClassifier(rclcpp::Node * node_ptr) : node_ptr_(node_ptr)
   input_c_ = node_ptr_->declare_parameter("input_c", 3);
   input_h_ = node_ptr_->declare_parameter("input_h", 224);
   input_w_ = node_ptr_->declare_parameter("input_w", 224);
-  auto input_name = node_ptr_->declare_parameter("input_name", "input_0");
-  auto output_name = node_ptr_->declare_parameter("output_name", "output_0");
-  apply_softmax_ = node_ptr_->declare_parameter("apply_softmax", true);
+  mean_ = node_ptr_->declare_parameter("mean", std::vector<double>({0.242, 0.193, 0.201}));
+  std_ = node_ptr_->declare_parameter("std", std::vector<double>({1.0, 1.0, 1.0}));
 
   readLabelfile(label_file_path, labels_);
 
