a63fd483c39e8f3ae44968e82864ee2929f01510
Mamoru Sobue <mamoru.sobue@tier4.jp>
2023-02-08 21:04:36 +0900

    fix(behavior_velocity_planner): check if over pass judge line after  collision checking
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_velocity_planner/src/scene_module/intersection/scene_intersection.cpp
+++ planning/behavior_velocity_planner/src/scene_module/intersection/scene_intersection.cpp
@@ -160,24 +160,6 @@ bool IntersectionModule::modifyPathVelocity(PathWithLaneId * path, StopReason * 
   }
   const size_t closest_idx = closest_idx_opt.get();
 
-  if (stop_lines_idx_opt.has_value()) {
-    const auto stop_line_idx = stop_lines_idx_opt.value().collision_stop_line;
-    const auto pass_judge_line_idx = stop_lines_idx_opt.value().pass_judge_line;
-
-    const bool is_over_pass_judge_line =
-      util::isOverTargetIndex(*path, closest_idx, current_pose, pass_judge_line_idx);
-
-    /* if ego is over the pass judge line before collision is detected, keep going */
-    if (is_over_pass_judge_line && is_go_out_ && !external_stop) {
-      RCLCPP_DEBUG(logger_, "over the pass judge line. no plan needed.");
-      RCLCPP_DEBUG(logger_, "===== plan end =====");
-      setSafe(true);
-      setDistance(motion_utils::calcSignedArcLength(
-        path->points, planner_data_->current_odometry->pose.position,
-        path->points.at(stop_line_idx).point.pose.position));
-      return true;
-    }
-  }
   /* collision checking */
   bool is_entry_prohibited = false;
 
@@ -229,6 +211,30 @@ bool IntersectionModule::modifyPathVelocity(PathWithLaneId * path, StopReason * 
                       : std::nullopt;
   }
 
+  state_machine_.setStateWithMarginTime(
+    is_entry_prohibited ? StateMachine::State::STOP : StateMachine::State::GO,
+    logger_.get_child("state_machine"), *clock_);
+  setSafe(state_machine_.getState() == StateMachine::State::GO);
+
+  if (stop_lines_idx_opt.has_value()) {
+    const auto stop_line_idx = stop_lines_idx_opt.value().collision_stop_line;
+    const auto pass_judge_line_idx = stop_lines_idx_opt.value().pass_judge_line;
+
+    const bool is_over_pass_judge_line =
+      util::isOverTargetIndex(*path, closest_idx, current_pose, pass_judge_line_idx);
+
+    /* if ego is over the pass judge line before collision is detected, keep going */
+    if (is_over_pass_judge_line && !is_entry_prohibited) {
+      RCLCPP_DEBUG(logger_, "over the pass judge line. no plan needed.");
+      RCLCPP_DEBUG(logger_, "===== plan end =====");
+      setSafe(true);
+      setDistance(motion_utils::calcSignedArcLength(
+        path->points, planner_data_->current_odometry->pose.position,
+        path->points.at(stop_line_idx).point.pose.position));
+      return true;
+    }
+  }
+
   if (!stop_line_idx.has_value()) {
     RCLCPP_DEBUG(logger_, "detection_area is empty, no plan needed");
     RCLCPP_DEBUG(logger_, "===== plan end =====");
@@ -237,11 +243,6 @@ bool IntersectionModule::modifyPathVelocity(PathWithLaneId * path, StopReason * 
     return false;
   }
 
-  state_machine_.setStateWithMarginTime(
-    is_entry_prohibited ? StateMachine::State::STOP : StateMachine::State::GO,
-    logger_.get_child("state_machine"), *clock_);
-
-  setSafe(state_machine_.getState() == StateMachine::State::GO);
   setDistance(motion_utils::calcSignedArcLength(
     path->points, planner_data_->current_odometry->pose.position,
     path->points.at(stop_line_idx.value()).point.pose.position));
