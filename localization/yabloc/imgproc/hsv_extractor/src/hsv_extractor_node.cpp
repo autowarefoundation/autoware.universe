d41bbd4db3ae89650921b164f9482b49b91a9404
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-02-10 10:55:47 +0900

    remove obsolete packages
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/imgproc/hsv_extractor/src/hsv_extractor_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "hsv_extractor/hsv_extractor.hpp"
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<pcdless::hsv_extractor::HsvExtractor>());
-  rclcpp::shutdown();
-  return 0;
-}
--- localization/yabloc/imgproc/segment_accumulator/src/segment_accumulator_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "segment_accumulator/segment_accumulator.hpp"
-
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<pcdless::accumulator::SegmentAccumulator>());
-  rclcpp::shutdown();
-  return 0;
-}
