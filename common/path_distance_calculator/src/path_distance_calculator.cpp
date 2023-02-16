85a3ab85d26b14a01e852d75ac065690e8474a00
tomoya.kimura <tomoya.kimura@tier4.jp>
2023-02-16 15:55:20 +0900

    unify conditions
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

--- common/path_distance_calculator/src/path_distance_calculator.cpp
+++ common/path_distance_calculator/src/path_distance_calculator.cpp
@@ -44,13 +44,8 @@ PathDistanceCalculator::PathDistanceCalculator(const rclcpp::NodeOptions & optio
       RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 5000, "no path");
       return;
     }
-    if (path->points.empty()) {
-      RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 5000, "path empty");
-      return;
-    }
-
-    if (path->points.size() == 1) {
-      RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 5000, "too short path");
+    if (path->points.size() <= 1) {
+      RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 5000, "too short or empty path");
       return;
     }
 
