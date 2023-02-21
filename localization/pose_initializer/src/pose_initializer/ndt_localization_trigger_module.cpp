79b59eb459edca6ab552e64255f6853fe7c89499
ryohei sasaki <rsasaki0109@gmail.com>
2023-02-21 16:01:01 +0900

    Update localization/pose_initializer/src/pose_initializer/ndt_localization_trigger_module.cpp
    Co-authored-by: Takagi, Isamu <43976882+isamu-takagi@users.noreply.github.com>
    Signed-off-by: Ryohei Sasaki <ryohei.sasaki@map4.jp>

--- localization/pose_initializer/src/pose_initializer/ndt_localization_trigger_module.cpp
+++ localization/pose_initializer/src/pose_initializer/ndt_localization_trigger_module.cpp
@@ -10,7 +10,8 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.S
+// limitations under the License.
+
 #include "ndt_localization_trigger_module.hpp"
 
 #include <component_interface_specs/localization.hpp>
