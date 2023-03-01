d4e343752472c3ea425d4ed9c07f887e1b8f26c9
yutaka <purewater0901@gmail.com>
2023-03-01 17:41:38 +0900

    update
    Signed-off-by: yutaka <purewater0901@gmail.com>

--- common/tier4_planning_rviz_plugin/include/path/display_base.hpp
+++ common/tier4_planning_rviz_plugin/include/path/display_base.hpp
@@ -25,6 +25,7 @@
 #include <rviz_common/properties/parse_color.hpp>
 #include <rviz_common/validate_floats.hpp>
 #include <rviz_rendering/objects/movable_text.hpp>
+#include <vehicle_info_util/vehicle_info_util.hpp>
 
 #include <autoware_auto_planning_msgs/msg/path.hpp>
 
@@ -95,6 +96,8 @@ bool validateFloats(const typename T::ConstSharedPtr & msg_ptr)
 
 namespace rviz_plugins
 {
+using vehicle_info_util::VehicleInfo;
+using vehicle_info_util::VehicleInfoUtil;
 template <typename T>
 class AutowarePathBaseDisplay : public rviz_common::MessageFilterDisplay<T>
 {
@@ -115,7 +118,21 @@ public:
     property_velocity_color_{"Color", Qt::black, "", &property_velocity_view_},
     // velocity text
     property_velocity_text_view_{"View Text Velocity", false, "", this},
-    property_velocity_text_scale_{"Scale", 0.3, "", &property_velocity_text_view_}
+    property_velocity_text_scale_{"Scale", 0.3, "", &property_velocity_text_view_},
+    // footprint
+    property_footprint_view_{"View Footprint", false, "", this},
+    property_footprint_alpha_{"Alpha", 1.0, "", &property_footprint_view_},
+    property_footprint_color_{"Color", QColor(230, 230, 50), "", &property_footprint_view_},
+    property_vehicle_length_{"Vehicle Length", 4.77, "", &property_footprint_view_},
+    property_vehicle_width_{"Vehicle Width", 1.83, "", &property_footprint_view_},
+    property_rear_overhang_{"Rear Overhang", 1.03, "", &property_footprint_view_},
+    property_offset_{"Offset from BaseLink", 0.0, "", &property_footprint_view_},
+    // point
+    property_point_view_{"View Point", false, "", this},
+    property_point_alpha_{"Alpha", 1.0, "", &property_point_view_},
+    property_point_color_{"Color", QColor(0, 60, 255), "", &property_point_view_},
+    property_point_radius_{"Radius", 0.1, "", &property_point_view_},
+    property_point_offset_{"Offset", 0.0, "", &property_point_view_}
   {
     // path
     property_path_width_.setMin(0.0);
@@ -128,6 +145,18 @@ public:
     // velocity text
     property_velocity_scale_.setMin(0.1);
     property_velocity_scale_.setMax(10.0);
+
+    // initialize footprint
+    property_footprint_alpha_.setMin(0.0);
+    property_footprint_alpha_.setMax(1.0);
+    property_vehicle_length_.setMin(0.0);
+    property_vehicle_width_.setMin(0.0);
+    property_rear_overhang_.setMin(0.0);
+    // initialize point
+    property_point_alpha_.setMin(0.0);
+    property_point_alpha_.setMax(1.0);
+
+    updateVehicleInfo();
   }
 
   ~AutowarePathBaseDisplay() override
@@ -141,6 +170,8 @@ public:
         node->detachAllObjects();
         this->scene_manager_->destroySceneNode(node);
       }
+      this->scene_manager_->destroyManualObject(footprint_manual_object_);
+      this->scene_manager_->destroyManualObject(point_manual_object_);
     }
   }
   void onInitialize() override
@@ -149,11 +180,18 @@ public:
 
     path_manual_object_ = this->scene_manager_->createManualObject();
     velocity_manual_object_ = this->scene_manager_->createManualObject();
+    footprint_manual_object_ = this->scene_manager_->createManualObject();
+    point_manual_object_ = this->scene_manager_->createManualObject();
     path_manual_object_->setDynamic(true);
     velocity_manual_object_->setDynamic(true);
+    footprint_manual_object_->setDynamic(true);
+    point_manual_object_->setDynamic(true);
     this->scene_node_->attachObject(path_manual_object_);
     this->scene_node_->attachObject(velocity_manual_object_);
+    this->scene_node_->attachObject(footprint_manual_object_);
+    this->scene_node_->attachObject(point_manual_object_);
   }
+
   void reset() override
   {
     rviz_common::MessageFilterDisplay<T>::MFDClass::reset();
@@ -167,9 +205,18 @@ public:
     }
     velocity_text_nodes_.clear();
     velocity_texts_.clear();
+    footprint_manual_object_->clear();
+    point_manual_object_->clear();
   }
 
 protected:
+  virtual void visualizeDrivableArea([[maybe_unused]] const typename T::ConstSharedPtr msg_ptr) {}
+  virtual void preprocessMessageDetail([[maybe_unused]] const typename T::ConstSharedPtr msg_ptr) {}
+  virtual void processMessageDetail(
+    [[maybe_unused]] const typename T::ConstSharedPtr msg_ptr, [[maybe_unused]] const size_t p_idx)
+  {
+  }
+
   void processMessage(const typename T::ConstSharedPtr msg_ptr) override
   {
     if (!validateFloats<T>(msg_ptr)) {
@@ -179,19 +226,6 @@ protected:
       return;
     }
 
-    // visualize Path
-    visualizePath(msg_ptr);
-
-    // visualize drivable area
-    visualizeDrivableArea(msg_ptr);
-
-    last_msg_ptr_ = msg_ptr;
-  }
-
-  virtual void visualizeDrivableArea([[maybe_unused]] const typename T::ConstSharedPtr msg_ptr) {}
-
-  void visualizePath(const typename T::ConstSharedPtr msg_ptr)
-  {
     Ogre::Vector3 position;
     Ogre::Quaternion orientation;
     if (!this->context_->getFrameManager()->getTransform(msg_ptr->header, position, orientation)) {
@@ -200,13 +234,26 @@ protected:
         "Error transforming from frame '%s' to frame '%s'", msg_ptr->header.frame_id.c_str(),
         qPrintable(this->fixed_frame_));
     }
+    this->scene_node_->setPosition(position);
+    this->scene_node_->setOrientation(orientation);
 
+    // visualize Path
+    visualizePath(msg_ptr);
+
+    // visualize drivable area
+    visualizeDrivableArea(msg_ptr);
+
+    // visualize PathFootprint
+    visualizePathFootprint(msg_ptr);
+
+    last_msg_ptr_ = msg_ptr;
+  }
+
+  void visualizePath(const typename T::ConstSharedPtr msg_ptr)
+  {
     if (msg_ptr->points.empty()) {
       return;
     }
-
-    this->scene_node_->setPosition(position);
-    this->scene_node_->setOrientation(orientation);
 
     path_manual_object_->clear();
     velocity_manual_object_->clear();
@@ -339,8 +386,114 @@ protected:
     velocity_manual_object_->end();
   }
 
+  void visualizePathFootprint(const typename T::ConstSharedPtr msg_ptr)
+  {
+    if (msg_ptr->points.empty()) {
+      return;
+    }
+
+    footprint_manual_object_->clear();
+    point_manual_object_->clear();
+
+    Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().getByName(
+      "BaseWhiteNoLighting", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+    material->setSceneBlending(Ogre::SBT_TRANSPARENT_ALPHA);
+    material->setDepthWriteEnabled(false);
+
+    footprint_manual_object_->estimateVertexCount(msg_ptr->points.size() * 4 * 2);
+    footprint_manual_object_->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_LINE_LIST);
+
+    point_manual_object_->estimateVertexCount(msg_ptr->points.size() * 3 * 8);
+    point_manual_object_->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_TRIANGLE_LIST);
+
+    const float offset_from_baselink = property_offset_.getFloat();
+
+    preprocessMessageDetail(msg_ptr);
+
+    for (size_t p_idx = 0; p_idx < msg_ptr->points.size(); p_idx++) {
+      const auto & point = msg_ptr->points.at(p_idx);
+      const auto & pose = tier4_autoware_utils::getPose(point);
+      // footprint
+      if (property_footprint_view_.getBool()) {
+        Ogre::ColourValue color;
+        color = rviz_common::properties::qtToOgre(property_footprint_color_.getColor());
+        color.a = property_footprint_alpha_.getFloat();
+
+        const auto info = vehicle_footprint_info_;
+        const float top = info->length - info->rear_overhang - offset_from_baselink;
+        const float bottom = -info->rear_overhang + offset_from_baselink;
+        const float left = -info->width / 2.0;
+        const float right = info->width / 2.0;
+
+        const std::array<float, 4> lon_offset_vec{top, top, bottom, bottom};
+        const std::array<float, 4> lat_offset_vec{left, right, right, left};
+
+        for (int f_idx = 0; f_idx < 4; ++f_idx) {
+          const Eigen::Quaternionf quat(
+            pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z);
+
+          {
+            const Eigen::Vector3f offset_vec{
+              lon_offset_vec.at(f_idx), lat_offset_vec.at(f_idx), 0.0};
+            const auto offset_to_edge = quat * offset_vec;
+            footprint_manual_object_->position(
+              pose.position.x + offset_to_edge.x(), pose.position.y + offset_to_edge.y(),
+              pose.position.z);
+            footprint_manual_object_->colour(color);
+          }
+          {
+            const Eigen::Vector3f offset_vec{
+              lon_offset_vec.at((f_idx + 1) % 4), lat_offset_vec.at((f_idx + 1) % 4), 0.0};
+            const auto offset_to_edge = quat * offset_vec;
+            footprint_manual_object_->position(
+              pose.position.x + offset_to_edge.x(), pose.position.y + offset_to_edge.y(),
+              pose.position.z);
+            footprint_manual_object_->colour(color);
+          }
+        }
+      }
+
+      // point
+      if (property_point_view_.getBool()) {
+        Ogre::ColourValue color;
+        color = rviz_common::properties::qtToOgre(property_point_color_.getColor());
+        color.a = property_point_alpha_.getFloat();
+
+        const double offset = property_point_offset_.getFloat();
+        const double yaw = tf2::getYaw(pose.orientation);
+        const double base_x = pose.position.x + offset * std::cos(yaw);
+        const double base_y = pose.position.y + offset * std::sin(yaw);
+        const double base_z = pose.position.z;
+
+        const double radius = property_point_radius_.getFloat();
+        for (size_t s_idx = 0; s_idx < 8; ++s_idx) {
+          const double current_angle = static_cast<double>(s_idx) / 8.0 * 2.0 * M_PI;
+          const double next_angle = static_cast<double>(s_idx + 1) / 8.0 * 2.0 * M_PI;
+          point_manual_object_->position(
+            base_x + radius * std::cos(current_angle), base_y + radius * std::sin(current_angle),
+            base_z);
+          point_manual_object_->colour(color);
+
+          point_manual_object_->position(
+            base_x + radius * std::cos(next_angle), base_y + radius * std::sin(next_angle), base_z);
+          point_manual_object_->colour(color);
+
+          point_manual_object_->position(base_x, base_y, base_z);
+          point_manual_object_->colour(color);
+        }
+      }
+
+      processMessageDetail(msg_ptr, p_idx);
+    }
+
+    footprint_manual_object_->end();
+    point_manual_object_->end();
+  }
+
   Ogre::ManualObject * path_manual_object_{nullptr};
   Ogre::ManualObject * velocity_manual_object_{nullptr};
+  Ogre::ManualObject * footprint_manual_object_{nullptr};
+  Ogre::ManualObject * point_manual_object_{nullptr};
   std::vector<rviz_rendering::MovableText *> velocity_texts_;
   std::vector<Ogre::SceneNode *> velocity_text_nodes_;
 
@@ -358,8 +511,50 @@ protected:
   rviz_common::properties::BoolProperty property_velocity_text_view_;
   rviz_common::properties::FloatProperty property_velocity_text_scale_;
 
+  rviz_common::properties::BoolProperty property_footprint_view_;
+  rviz_common::properties::FloatProperty property_footprint_alpha_;
+  rviz_common::properties::ColorProperty property_footprint_color_;
+  rviz_common::properties::FloatProperty property_vehicle_length_;
+  rviz_common::properties::FloatProperty property_vehicle_width_;
+  rviz_common::properties::FloatProperty property_rear_overhang_;
+  rviz_common::properties::FloatProperty property_offset_;
+
+  rviz_common::properties::BoolProperty property_point_view_;
+  rviz_common::properties::FloatProperty property_point_alpha_;
+  rviz_common::properties::ColorProperty property_point_color_;
+  rviz_common::properties::FloatProperty property_point_radius_;
+  rviz_common::properties::FloatProperty property_point_offset_;
+
 private:
   typename T::ConstSharedPtr last_msg_ptr_;
+
+  struct VehicleFootprintInfo
+  {
+    VehicleFootprintInfo(const float l, const float w, const float r)
+    : length(l), width(w), rear_overhang(r)
+    {
+    }
+    float length, width, rear_overhang;
+  };
+
+  std::shared_ptr<VehicleInfo> vehicle_info_;
+  std::shared_ptr<VehicleFootprintInfo> vehicle_footprint_info_;
+
+  void updateVehicleInfo()
+  {
+    if (vehicle_info_) {
+      vehicle_footprint_info_ = std::make_shared<VehicleFootprintInfo>(
+        vehicle_info_->vehicle_length_m, vehicle_info_->vehicle_width_m,
+        vehicle_info_->rear_overhang_m);
+    } else {
+      const float length{property_vehicle_length_.getFloat()};
+      const float width{property_vehicle_width_.getFloat()};
+      const float rear_overhang{property_rear_overhang_.getFloat()};
+
+      vehicle_footprint_info_ =
+        std::make_shared<VehicleFootprintInfo>(length, width, rear_overhang);
+    }
+  }
 };
 }  // namespace rviz_plugins
 
