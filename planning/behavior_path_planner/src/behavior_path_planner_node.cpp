12d088cf2168c3614b6f7c88d655bc8b14c6c14c
satoshi-ota <satoshi.ota928@gmail.com>
2023-03-10 15:07:14 +0900

    fix(pull_over): declaration parameters outside of scene module
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/src/behavior_path_planner_node.cpp
+++ planning/behavior_path_planner/src/behavior_path_planner_node.cpp
@@ -690,97 +690,155 @@ LaneChangeParameters BehaviorPathPlannerNode::getLaneChangeParam()
 
 PullOverParameters BehaviorPathPlannerNode::getPullOverParam()
 {
-  const auto dp = [this](const std::string & str, auto def_val) {
-    std::string name = "pull_over." + str;
-    return this->declare_parameter(name, def_val);
-  };
-
   PullOverParameters p;
-  p.request_length = dp("request_length", 200.0);
-  p.th_stopped_velocity = dp("th_stopped_velocity", 0.01);
-  p.th_arrived_distance = dp("th_arrived_distance", 0.3);
-  p.th_stopped_time = dp("th_stopped_time", 2.0);
-  p.margin_from_boundary = dp("margin_from_boundary", 0.3);
-  p.decide_path_distance = dp("decide_path_distance", 10.0);
-  p.maximum_deceleration = dp("maximum_deceleration", 1.0);
-  // goal research
-  p.enable_goal_research = dp("enable_goal_research", true);
-  p.search_priority = dp("search_priority", "efficient_path");
-  p.forward_goal_search_length = dp("forward_goal_search_length", 20.0);
-  p.backward_goal_search_length = dp("backward_goal_search_length", 20.0);
-  p.goal_search_interval = dp("goal_search_interval", 5.0);
-  p.longitudinal_margin = dp("longitudinal_margin", 3.0);
-  p.max_lateral_offset = dp("max_lateral_offset", 1.0);
-  p.lateral_offset_interval = dp("lateral_offset_interval", 0.25);
-  p.ignore_distance_from_lane_start = dp("ignore_distance_from_lane_start", 15.0);
-  // occupancy grid map
-  p.use_occupancy_grid = dp("use_occupancy_grid", true);
-  p.use_occupancy_grid_for_longitudinal_margin =
-    dp("use_occupancy_grid_for_longitudinal_margin", false);
-  p.occupancy_grid_collision_check_margin = dp("occupancy_grid_collision_check_margin", 0.0);
-  p.theta_size = dp("theta_size", 360);
-  p.obstacle_threshold = dp("obstacle_threshold", 90);
-  // object recognition
-  p.use_object_recognition = dp("use_object_recognition", true);
-  p.object_recognition_collision_check_margin =
-    dp("object_recognition_collision_check_margin", 1.0);
-  // shift path
-  p.enable_shift_parking = dp("enable_shift_parking", true);
-  p.pull_over_sampling_num = dp("pull_over_sampling_num", 4);
-  p.maximum_lateral_jerk = dp("maximum_lateral_jerk", 3.0);
-  p.minimum_lateral_jerk = dp("minimum_lateral_jerk", 1.0);
-  p.deceleration_interval = dp("deceleration_interval", 10.0);
-  p.pull_over_velocity = dp("pull_over_velocity", 8.3);
-  p.pull_over_minimum_velocity = dp("pull_over_minimum_velocity", 0.3);
-  p.after_pull_over_straight_distance = dp("after_pull_over_straight_distance", 3.0);
-  // parallel parking
-  p.enable_arc_forward_parking = dp("enable_arc_forward_parking", true);
-  p.enable_arc_backward_parking = dp("enable_arc_backward_parking", true);
-  p.after_forward_parking_straight_distance = dp("after_forward_parking_straight_distance", 0.5);
-  p.after_backward_parking_straight_distance = dp("after_backward_parking_straight_distance", 0.5);
-  p.forward_parking_velocity = dp("forward_parking_velocity", 1.0);
-  p.backward_parking_velocity = dp("backward_parking_velocity", -0.5);
-  p.forward_parking_lane_departure_margin = dp("forward_parking_lane_departure_margin", 0.0);
-  p.backward_parking_lane_departure_margin = dp("backward_parking_lane_departure_margin", 0.0);
-  p.arc_path_interval = dp("arc_path_interval", 1.0);
-  p.pull_over_max_steer_angle = dp("pull_over_max_steer_angle", 0.35);  // 20deg
-  // freespace parking
-  p.enable_freespace_parking = dp("enable_freespace_parking", true);
-  // hazard
-  p.hazard_on_threshold_distance = dp("hazard_on_threshold_distance", 1.0);
-  p.hazard_on_threshold_velocity = dp("hazard_on_threshold_velocity", 0.5);
-  // safety with dynamic objects. Not used now.
-  p.pull_over_duration = dp("pull_over_duration", 4.0);
-  p.pull_over_prepare_duration = dp("pull_over_prepare_duration", 2.0);
-  p.min_stop_distance = dp("min_stop_distance", 5.0);
-  p.stop_time = dp("stop_time", 2.0);
-  p.hysteresis_buffer_distance = dp("hysteresis_buffer_distance", 2.0);
-  p.prediction_time_resolution = dp("prediction_time_resolution", 0.5);
-  p.enable_collision_check_at_prepare_phase = dp("enable_collision_check_at_prepare_phase", true);
-  p.use_predicted_path_outside_lanelet = dp("use_predicted_path_outside_lanelet", true);
-  p.use_all_predicted_path = dp("use_all_predicted_path", false);
-  // drivable area
-  p.drivable_area_right_bound_offset = dp("drivable_area_right_bound_offset", 0.0);
-  p.drivable_area_left_bound_offset = dp("drivable_area_left_bound_offset", 0.0);
-  p.drivable_area_types_to_skip =
-    dp("drivable_area_types_to_skip", std::vector<std::string>({"road_border"}));
-  // debug
-  p.print_debug_info = dp("print_debug_info", false);
 
-  // validation of parameters
-  if (p.pull_over_sampling_num < 1) {
-    RCLCPP_FATAL_STREAM(
-      get_logger(), "pull_over_sampling_num must be positive integer. Given parameter: "
-                      << p.pull_over_sampling_num << std::endl
-                      << "Terminating the program...");
-    exit(EXIT_FAILURE);
+  {
+    const auto dp = [this](const std::string & str, auto def_val) {
+      std::string name = "pull_over." + str;
+      return this->declare_parameter(name, def_val);
+    };
+
+    p.request_length = dp("request_length", 200.0);
+    p.th_stopped_velocity = dp("th_stopped_velocity", 0.01);
+    p.th_arrived_distance = dp("th_arrived_distance", 0.3);
+    p.th_stopped_time = dp("th_stopped_time", 2.0);
+    p.margin_from_boundary = dp("margin_from_boundary", 0.3);
+    p.decide_path_distance = dp("decide_path_distance", 10.0);
+    p.maximum_deceleration = dp("maximum_deceleration", 1.0);
+    // goal research
+    p.enable_goal_research = dp("enable_goal_research", true);
+    p.search_priority = dp("search_priority", "efficient_path");
+    p.forward_goal_search_length = dp("forward_goal_search_length", 20.0);
+    p.backward_goal_search_length = dp("backward_goal_search_length", 20.0);
+    p.goal_search_interval = dp("goal_search_interval", 5.0);
+    p.longitudinal_margin = dp("longitudinal_margin", 3.0);
+    p.max_lateral_offset = dp("max_lateral_offset", 1.0);
+    p.lateral_offset_interval = dp("lateral_offset_interval", 0.25);
+    p.ignore_distance_from_lane_start = dp("ignore_distance_from_lane_start", 15.0);
+    // occupancy grid map
+    p.use_occupancy_grid = dp("use_occupancy_grid", true);
+    p.use_occupancy_grid_for_longitudinal_margin =
+      dp("use_occupancy_grid_for_longitudinal_margin", false);
+    p.occupancy_grid_collision_check_margin = dp("occupancy_grid_collision_check_margin", 0.0);
+    p.theta_size = dp("theta_size", 360);
+    p.obstacle_threshold = dp("obstacle_threshold", 90);
+    // object recognition
+    p.use_object_recognition = dp("use_object_recognition", true);
+    p.object_recognition_collision_check_margin =
+      dp("object_recognition_collision_check_margin", 1.0);
+    // shift path
+    p.enable_shift_parking = dp("enable_shift_parking", true);
+    p.pull_over_sampling_num = dp("pull_over_sampling_num", 4);
+    p.maximum_lateral_jerk = dp("maximum_lateral_jerk", 3.0);
+    p.minimum_lateral_jerk = dp("minimum_lateral_jerk", 1.0);
+    p.deceleration_interval = dp("deceleration_interval", 10.0);
+    p.pull_over_velocity = dp("pull_over_velocity", 8.3);
+    p.pull_over_minimum_velocity = dp("pull_over_minimum_velocity", 0.3);
+    p.after_pull_over_straight_distance = dp("after_pull_over_straight_distance", 3.0);
+    // parallel parking
+    p.enable_arc_forward_parking = dp("enable_arc_forward_parking", true);
+    p.enable_arc_backward_parking = dp("enable_arc_backward_parking", true);
+    p.after_forward_parking_straight_distance = dp("after_forward_parking_straight_distance", 0.5);
+    p.after_backward_parking_straight_distance =
+      dp("after_backward_parking_straight_distance", 0.5);
+    p.forward_parking_velocity = dp("forward_parking_velocity", 1.0);
+    p.backward_parking_velocity = dp("backward_parking_velocity", -0.5);
+    p.forward_parking_lane_departure_margin = dp("forward_parking_lane_departure_margin", 0.0);
+    p.backward_parking_lane_departure_margin = dp("backward_parking_lane_departure_margin", 0.0);
+    p.arc_path_interval = dp("arc_path_interval", 1.0);
+    p.pull_over_max_steer_angle = dp("pull_over_max_steer_angle", 0.35);  // 20deg
+                                                                          // freespace parking
+    p.enable_freespace_parking = dp("enable_freespace_parking", true);
+    // hazard
+    p.hazard_on_threshold_distance = dp("hazard_on_threshold_distance", 1.0);
+    p.hazard_on_threshold_velocity = dp("hazard_on_threshold_velocity", 0.5);
+    // safety with dynamic objects. Not used now.
+    p.pull_over_duration = dp("pull_over_duration", 4.0);
+    p.pull_over_prepare_duration = dp("pull_over_prepare_duration", 2.0);
+    p.min_stop_distance = dp("min_stop_distance", 5.0);
+    p.stop_time = dp("stop_time", 2.0);
+    p.hysteresis_buffer_distance = dp("hysteresis_buffer_distance", 2.0);
+    p.prediction_time_resolution = dp("prediction_time_resolution", 0.5);
+    p.enable_collision_check_at_prepare_phase = dp("enable_collision_check_at_prepare_phase", true);
+    p.use_predicted_path_outside_lanelet = dp("use_predicted_path_outside_lanelet", true);
+    p.use_all_predicted_path = dp("use_all_predicted_path", false);
+    // drivable area
+    p.drivable_area_right_bound_offset = dp("drivable_area_right_bound_offset", 0.0);
+    p.drivable_area_left_bound_offset = dp("drivable_area_left_bound_offset", 0.0);
+    p.drivable_area_types_to_skip =
+      dp("drivable_area_types_to_skip", std::vector<std::string>({"road_border"}));
+    // debug
+    p.print_debug_info = dp("print_debug_info", false);
+
+    // validation of parameters
+    if (p.pull_over_sampling_num < 1) {
+      RCLCPP_FATAL_STREAM(
+        get_logger(), "pull_over_sampling_num must be positive integer. Given parameter: "
+                        << p.pull_over_sampling_num << std::endl
+                        << "Terminating the program...");
+      exit(EXIT_FAILURE);
+    }
+    if (p.maximum_deceleration < 0.0) {
+      RCLCPP_FATAL_STREAM(
+        get_logger(), "maximum_deceleration cannot be negative value. Given parameter: "
+                        << p.maximum_deceleration << std::endl
+                        << "Terminating the program...");
+      exit(EXIT_FAILURE);
+    }
   }
-  if (p.maximum_deceleration < 0.0) {
-    RCLCPP_FATAL_STREAM(
-      get_logger(), "maximum_deceleration cannot be negative value. Given parameter: "
-                      << p.maximum_deceleration << std::endl
-                      << "Terminating the program...");
-    exit(EXIT_FAILURE);
+
+  {
+    const auto dp = [this](const std::string & str, auto def_val) {
+      std::string name = "pull_over.freespace_parking." + str;
+      return this->declare_parameter(name, def_val);
+    };
+
+    // search configs
+    p.algorithm = dp("planning_algorithm", "astar");
+    p.freespace_parking_velocity = dp("velocity", 1.0);
+    p.vehicle_shape_margin = dp("vehicle_shape_margin", 1.0);
+    p.common_parameters.time_limit = dp("time_limit", 5000.0);
+    p.common_parameters.minimum_turning_radius = dp("minimum_turning_radius", 0.5);
+    p.common_parameters.maximum_turning_radius = dp("maximum_turning_radius", 6.0);
+    p.common_parameters.turning_radius_size = dp("turning_radius_size", 11);
+    p.common_parameters.maximum_turning_radius = std::max(
+      p.common_parameters.maximum_turning_radius, p.common_parameters.minimum_turning_radius);
+    p.common_parameters.turning_radius_size = std::max(p.common_parameters.turning_radius_size, 1);
+
+    p.common_parameters.theta_size = dp("theta_size", 48);
+    p.common_parameters.angle_goal_range = dp("angle_goal_range", 6.0);
+
+    p.common_parameters.curve_weight = dp("curve_weight", 1.2);
+    p.common_parameters.reverse_weight = dp("reverse_weight", 2.00);
+    p.common_parameters.lateral_goal_range = dp("lateral_goal_range", 0.5);
+    p.common_parameters.longitudinal_goal_range = dp("longitudinal_goal_range", 2.0);
+
+    // costmap configs
+    p.common_parameters.obstacle_threshold = dp("obstacle_threshold", 100);
+  }
+
+  {
+    const auto dp = [this](const std::string & str, auto def_val) {
+      std::string name = "pull_over.freespace_parking.astar." + str;
+      return this->declare_parameter(name, def_val);
+    };
+
+    p.astar_parameters.only_behind_solutions = dp("only_behind_solutions", false);
+    p.astar_parameters.use_back = dp("use_back", true);
+    p.astar_parameters.distance_heuristic_weight = dp("distance_heuristic_weight", 1.0);
+  }
+
+  {
+    const auto dp = [this](const std::string & str, auto def_val) {
+      std::string name = "pull_over.freespace_parking.rrtstar." + str;
+      return this->declare_parameter(name, def_val);
+    };
+
+    p.rrt_star_parameters.enable_update = dp("enable_update", true);
+    p.rrt_star_parameters.use_informed_sampling = dp("use_informed_sampling", true);
+    p.rrt_star_parameters.max_planning_time = dp("max_planning_time", 150.0);
+    p.rrt_star_parameters.neighbor_radius = dp("neighbor_radius", 8.0);
+    p.rrt_star_parameters.margin = dp("margin", 0.1);
   }
 
   return p;
