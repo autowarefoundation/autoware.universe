6cd13f82868f34140fa1538d43f99d99b9648df3
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2023-03-14 14:08:43 +0900

    relete unnecessary definition
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/surround_obstacle_checker/test/test_surround_obstacle_checker_node_interface.cpp
+++ planning/surround_obstacle_checker/test/test_surround_obstacle_checker_node_interface.cpp
@@ -23,6 +23,7 @@
 
 TEST(PlanningModuleInterfaceTest, testPlanningInterfaceWithVariousTrajectoryInput)
 {
+  using autoware_auto_planning_msgs::msg::Trajectory;
   rclcpp::init(0, nullptr);
 
   auto test_manager = std::make_shared<planning_test_manager::PlanningIntefaceTestManager>();
