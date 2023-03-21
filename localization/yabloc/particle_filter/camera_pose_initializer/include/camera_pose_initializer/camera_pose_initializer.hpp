b39eb9cbc7a7796a613daeb694a707589d71c0e2
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-03-21 18:06:22 +0900

    subscribe camera_info&tf and prepare semantic projection
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/yabloc/particle_filter/camera_pose_initializer/include/camera_pose_initializer/camera_pose_initializer.hpp
+++ localization/yabloc/particle_filter/camera_pose_initializer/include/camera_pose_initializer/camera_pose_initializer.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <Eigen/Geometry>
+#include <pcdless_common/camera_info_subscriber.hpp>
+#include <pcdless_common/static_tf_subscriber.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <autoware_auto_mapping_msgs/msg/had_map_bin.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <ground_msgs/srv/ground.hpp>
+#include <sensor_msgs/msg/image.hpp>
 #include <visualization_msgs/msg/marker.hpp>
 
 namespace pcdless
@@ -17,14 +20,19 @@ public:
   using Marker = visualization_msgs::msg::Marker;
   using HADMapBin = autoware_auto_mapping_msgs::msg::HADMapBin;
   using Ground = ground_msgs::srv::Ground;
+  using Image = sensor_msgs::msg::Image;
 
   CameraPoseInitializer();
 
 private:
   const Eigen::Vector2d cov_xx_yy_;
 
+  common::CameraInfoSubscriber info_;
+  common::StaticTfSubscriber tf_subscriber_;
+
   rclcpp::Subscription<PoseCovStamped>::SharedPtr sub_initialpose_;
   rclcpp::Subscription<HADMapBin>::SharedPtr sub_map_;
+  rclcpp::Subscription<Image>::SharedPtr sub_image_;
 
   rclcpp::Publisher<PoseCovStamped>::SharedPtr pub_initialpose_;
   rclcpp::Publisher<Marker>::SharedPtr pub_marker_;
@@ -32,11 +40,14 @@ private:
   rclcpp::Client<Ground>::SharedPtr ground_cli_;
   rclcpp::CallbackGroup::SharedPtr service_callback_group_;
 
+  std::optional<Image::ConstSharedPtr> latest_image_msg_{std::nullopt};
+
   void on_map(const HADMapBin & msg);
   void on_initial_pose(const PoseCovStamped & initialpose);
 
+  bool estimate_pose(const Eigen::Vector3f & position, Eigen::Vector3f & tangent);
+
   void publish_rectified_initial_pose(
-    const Eigen::Vector3f & pos, const Eigen::Vector3f & tangent,
-    const PoseCovStamped & raw_initialpose);
+    const Eigen::Vector3f & pos, const Eigen::Vector3f & tangent, const rclcpp::Time & stamp);
 };
 }  // namespace pcdless
--- localization/yabloc/particle_filter/camera_pose_initializer/src/camera_pose_initializer_core.cpp
+++ localization/yabloc/particle_filter/camera_pose_initializer/src/camera_pose_initializer_core.cpp
@@ -1,6 +1,9 @@
 #include "camera_pose_initializer/camera_pose_initializer.hpp"
 
 #include <ll2_decomposer/from_bin_msg.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <pcdless_common/cv_decompress.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -9,7 +12,10 @@ namespace pcdless
 {
 CameraPoseInitializer::CameraPoseInitializer()
 : Node("camera_pose_initializer"),
-  cov_xx_yy_{declare_parameter("cov_xx_yy", std::vector<double>{4.0, 0.25}).data()}
+  cov_xx_yy_{declare_parameter("cov_xx_yy", std::vector<double>{4.0, 0.25}).data()},
+  info_(this),
+  tf_subscriber_(this->get_clock())
+
 {
   using std::placeholders::_1;
   const rclcpp::QoS map_qos = rclcpp::QoS(1).transient_local().reliable();
@@ -21,10 +27,13 @@ CameraPoseInitializer::CameraPoseInitializer()
 
   // Subscriber
   auto on_initialpose = std::bind(&CameraPoseInitializer::on_initial_pose, this, _1);
-  sub_initialpose_ =
-    create_subscription<PoseCovStamped>("initialpose", 10, std::move(on_initialpose));
   auto on_map = std::bind(&CameraPoseInitializer::on_map, this, _1);
+
+  sub_initialpose_ = create_subscription<PoseCovStamped>("initialpose", 10, on_initialpose);
   sub_map_ = create_subscription<HADMapBin>("/map/vector_map", map_qos, on_map);
+  sub_image_ = create_subscription<Image>(
+    "/semseg/semantic_image", 10,
+    [this](Image::ConstSharedPtr msg) -> void { latest_image_msg_ = msg; });
 
   // Client
   ground_cli_ = create_client<Ground>(
@@ -38,35 +47,79 @@ CameraPoseInitializer::CameraPoseInitializer()
 
 void CameraPoseInitializer::on_initial_pose(const PoseCovStamped & initialpose)
 {
+  const rclcpp::Time stamp = initialpose.header.stamp;
   const auto query_pos = initialpose.pose.pose.position;
-  auto req = std::make_shared<Ground::Request>();
-  req->point.x = query_pos.x;
-  req->point.y = query_pos.y;
+  auto request = std::make_shared<Ground::Request>();
+  request->point.x = query_pos.x;
+  request->point.y = query_pos.y;
 
   using namespace std::chrono_literals;
-  auto result_future = ground_cli_->async_send_request(req);
+  auto result_future = ground_cli_->async_send_request(request);
   std::future_status status = result_future.wait_for(1000ms);
   if (status == std::future_status::ready) {
-    RCLCPP_INFO_STREAM(get_logger(), "get ground height service exited successfully");
   } else {
     RCLCPP_ERROR_STREAM(get_logger(), "get height service exited unexpectedly");
     return;
   }
 
-  //
-
+  // Retrieve 3d position
   const auto position = result_future.get()->pose.position;
   Eigen::Vector3f pos_vec3f;
   pos_vec3f << position.x, position.y, position.z;
+  RCLCPP_INFO_STREAM(get_logger(), "get initial position " << pos_vec3f.transpose());
 
-  std::cout << "get initial position" << pos_vec3f.transpose() << std::endl;
-
-  auto orientation = initialpose.pose.pose.orientation;
-  float theta = 2 * std::atan2(orientation.z, orientation.w);
+  // Estimate orientation
   Eigen::Vector3f tangent;
-  tangent << std::cos(theta), std::sin(theta), 0;
+  if (estimate_pose(pos_vec3f, tangent)) {
+    publish_rectified_initial_pose(pos_vec3f, tangent, stamp);
+  }
+}
 
-  publish_rectified_initial_pose(pos_vec3f, tangent, initialpose);
+bool CameraPoseInitializer::estimate_pose(
+  const Eigen::Vector3f & position, Eigen::Vector3f & tangent)
+{
+  if (info_.is_camera_info_nullopt()) {
+    RCLCPP_WARN_STREAM(get_logger(), "camera info is not ready");
+    return false;
+  }
+
+  std::optional<Eigen::Affine3f> camera_extrinsic =
+    tf_subscriber_.operator()(info_.get_frame_id(), "base_link");
+  if (!camera_extrinsic.has_value()) {
+    RCLCPP_WARN_STREAM(get_logger(), "camera tf_static is not ready");
+    return false;
+  }
+
+  // TODO: check time stamp
+  if (!latest_image_msg_.has_value()) {
+    RCLCPP_WARN_STREAM(get_logger(), "semantic image is not ready");
+    return false;
+  }
+
+  const Eigen::Vector3f t = camera_extrinsic->translation();
+  const Eigen::Quaternionf q(camera_extrinsic->rotation());
+
+  cv::Mat mask_image = common::decompress_to_cv_mat(*latest_image_msg_.value());
+  RCLCPP_INFO_STREAM(get_logger(), "semantic image is ready");
+
+  // TODO: project semantics on plane
+  std::vector<cv::Mat> masks;
+  cv::split(mask_image, masks);
+  std::vector<cv::Scalar> colors = {
+    cv::Scalar(0, 0, 255), cv::Scalar(0, 255, 0), cv::Scalar(255, 0, 0)};
+
+  cv::Mat show_image = cv::Mat::zeros(mask_image.size(), CV_8UC3);
+  for (int i = 0; i < 3; i++) {
+    std::vector<std::vector<cv::Point> > contours;
+    cv::Mat tmp;
+    // cv::erode(masks[i], tmp, cv::Mat(), cv::Point(-1, -1), 5);
+    cv::findContours(tmp, contours, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+    cv::drawContours(show_image, contours, -1, colors[i], -1);
+  }
+  cv::imshow("contours", show_image);
+  cv::waitKey(0);
+
+  return false;
 }
 
 void CameraPoseInitializer::on_map(const HADMapBin & msg)
@@ -87,10 +140,10 @@ void CameraPoseInitializer::on_map(const HADMapBin & msg)
 }
 
 void CameraPoseInitializer::publish_rectified_initial_pose(
-  const Eigen::Vector3f & pos, const Eigen::Vector3f & tangent,
-  const PoseCovStamped & raw_initialpose)
+  const Eigen::Vector3f & pos, const Eigen::Vector3f & tangent, const rclcpp::Time & stamp)
 {
-  PoseCovStamped msg = raw_initialpose;
+  PoseCovStamped msg;
+  msg.header.stamp = stamp;
   msg.header.frame_id = "map";
   msg.pose.pose.position.x = pos.x();
   msg.pose.pose.position.y = pos.y();
