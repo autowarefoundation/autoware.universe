154fe9aa1f9975766069e5f8d62b4e24e5be13f1
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-03-24 16:38:18 +0900

    feat: add change route service
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- planning/mission_planner/src/mission_planner/mission_planner.cpp
+++ planning/mission_planner/src/mission_planner/mission_planner.cpp
@@ -85,6 +85,8 @@ MissionPlanner::MissionPlanner(const rclcpp::NodeOptions & options)
   adaptor.init_srv(srv_clear_route_, this, &MissionPlanner::on_clear_route);
   adaptor.init_srv(srv_set_route_, this, &MissionPlanner::on_set_route);
   adaptor.init_srv(srv_set_route_points_, this, &MissionPlanner::on_set_route_points);
+  adaptor.init_srv(srv_change_route_, this, &MissionPlanner::on_change_route);
+  adaptor.init_srv(srv_change_route_points_, this, &MissionPlanner::on_change_route_points);
 
   change_state(RouteState::Message::UNSET);
 }
@@ -236,6 +238,23 @@ void MissionPlanner::on_set_route_points(
   change_route(route);
   change_state(RouteState::Message::SET);
   res->status.success = true;
+}
+
+// NOTE: The route services should be mutually exclusive by callback group.
+void MissionPlanner::on_change_route(
+  const SetRoute::Service::Request::SharedPtr req, const SetRoute::Service::Response::SharedPtr res)
+{
+  (void)req;
+  (void)res;
+}
+
+// NOTE: The route services should be mutually exclusive by callback group.
+void MissionPlanner::on_change_route_points(
+  const SetRoutePoints::Service::Request::SharedPtr req,
+  const SetRoutePoints::Service::Response::SharedPtr res)
+{
+  (void)req;
+  (void)res;
 }
 
 }  // namespace mission_planner
