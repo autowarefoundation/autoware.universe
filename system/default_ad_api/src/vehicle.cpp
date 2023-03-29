55a9941ec36ac47b62ea1e4396c7915e56ca5781
tkhmy <tkh.my.p@gmail.com>
2023-03-29 12:48:57 +0900

    update license
    Signed-off-by: tkhmy <tkh.my.p@gmail.com>

--- system/default_ad_api/src/vehicle.cpp
+++ system/default_ad_api/src/vehicle.cpp
@@ -1,11 +1,4 @@
-// Copyright 2022 TIER IV, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//// Copyright 2022 TIER IV, Inc.
+// Copyright 2023 TIER IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
