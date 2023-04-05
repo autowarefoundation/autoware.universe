779a4e0287a559aeff830f48b5d8ed355c955fa9
Shin-kyoto <58775300+Shin-kyoto@users.noreply.github.com>
2023-04-06 02:29:48 +0900

    feat(elevation_map_loader): add support for id_map_loader
    Signed-off-by: Shin-kyoto <58775300+Shin-kyoto@users.noreply.github.com>

--- launch/tier4_perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
+++ launch/tier4_perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
@@ -321,10 +321,12 @@ class GroundSegmentationPipeline:
                     ("output/elevation_map", "map"),
                     ("input/pointcloud_map", "/map/pointcloud_map"),
                     ("input/vector_map", "/map/vector_map"),
+                    ("service/get_id_pcd_map", "/map/get_id_pointcloud_map"),
                 ],
                 parameters=[
                     {
                         "use_lane_filter": False,
+                        "use_id_load": False,
                         "use_inpaint": True,
                         "inpaint_radius": 1.0,
                         "lane_margin": 2.0,
