c32e8d39124d477ec0d2d1cb661f731e8af74d8a
Mamoru Sobue <mamoru.sobue@tier4.jp>
2023-05-19 13:03:49 +0900

    feat(intersection): denoise occlusion by morphology open process (#3763)
    * apply morpholgoyEx to occlusion_mask_raw to remove noisy occlusion cell
    
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>
    
    * parameterize kernel size
    
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>
    
    * remove debug
    
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>
    
    ---------
    
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_velocity_intersection_module/src/scene_intersection.hpp
+++ planning/behavior_velocity_intersection_module/src/scene_intersection.hpp
@@ -122,6 +122,7 @@ public:
       double before_creep_stop_time;
       double min_vehicle_brake_for_rss;
       double max_vehicle_velocity_for_rss;
+      double denoise_kernel;
     } occlusion;
   };
 
