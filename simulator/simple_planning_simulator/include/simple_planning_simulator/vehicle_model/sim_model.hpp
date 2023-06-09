821a654d31c4505a6266e25ecece16af1ce89bff
lixianqiang <lxq243808918@gmail.com>
2023-06-09 15:19:44 +0800

    feat: add 4ws model for simple_plannig_simulator
    add an option ‘IDEAL_STEER_VEL_FOR_4WS’ for source code and some cpp file for model. if you want run it successfully, please modify simulator_model.param.yaml which locale in pixkit_launch/pixkit_description/config, there is ‘vehicle_model_type’ option and fill in as IDEAL_STEER_VEL_FOR_4WS, because the param loaded from there

--- simulator/simple_planning_simulator/include/simple_planning_simulator/vehicle_model/sim_model.hpp
+++ simulator/simple_planning_simulator/include/simple_planning_simulator/vehicle_model/sim_model.hpp
@@ -21,6 +21,7 @@
 #include "simple_planning_simulator/vehicle_model/sim_model_ideal_steer_acc.hpp"
 #include "simple_planning_simulator/vehicle_model/sim_model_ideal_steer_acc_geared.hpp"
 #include "simple_planning_simulator/vehicle_model/sim_model_ideal_steer_vel.hpp"
+#include "simple_planning_simulator/vehicle_model/sim_model_ideal_steer_vel_for_4ws.hpp"
 #include "simple_planning_simulator/vehicle_model/sim_model_interface.hpp"
 
 #endif  // SIMPLE_PLANNING_SIMULATOR__VEHICLE_MODEL__SIM_MODEL_HPP_
