36f89d1a090f4d4db19d6a77c68849e704e48bcd
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-06-11 13:21:50 +0900

    fix clang-format
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- planning/motion_velocity_smoother/include/motion_velocity_smoother/motion_velocity_smoother_node.hpp
+++ planning/motion_velocity_smoother/include/motion_velocity_smoother/motion_velocity_smoother_node.hpp
@@ -64,8 +64,8 @@ struct Motion
   double vel = 0.0;
   double acc = 0.0;
 
-  Motion(){};
-  Motion(const double v, const double a) : vel(v), acc(a){};
+  Motion() {}
+  Motion(const double v, const double a) : vel(v), acc(a) {}
 };
 
 class MotionVelocitySmootherNode : public rclcpp::Node
