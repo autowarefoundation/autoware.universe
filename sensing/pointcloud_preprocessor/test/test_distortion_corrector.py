e4d5ad74df8606e3ecc0adff42ecc973ed8952c3
kminoda <koji.minoda@tier4.jp>
2023-06-13 11:53:47 +0900

    update script
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- sensing/pointcloud_preprocessor/test/test_distortion_corrector.py
+++ sensing/pointcloud_preprocessor/test/test_distortion_corrector.py
@@ -15,33 +15,30 @@
 # limitations under the License.
 
 import os
-import struct
 import time
 import unittest
 
 from ament_index_python import get_package_share_directory
+from sensor_msgs.msg import Imu
+from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import TwistWithCovarianceStamped
 import launch
+from launch.logging import get_logger
 from launch.conditions import IfCondition
 from launch.conditions import UnlessCondition
-from launch.logging import get_logger
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 import launch_testing
-import numpy as np
 import pytest
 import rclpy
-from rclpy.qos import QoSDurabilityPolicy
-from rclpy.qos import QoSHistoryPolicy
-from rclpy.qos import QoSProfile
-from rclpy.qos import QoSReliabilityPolicy
-from rclpy.time import Time
-from sensor_msgs.msg import Imu
-from sensor_msgs.msg import PointCloud2
-from sensor_msgs.msg import PointField
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
+import numpy as np
+from sensor_msgs.msg import PointField, PointCloud2
 from std_msgs.msg import Header
+from rclpy.time import Time
+import struct
 
 logger = get_logger(__name__)
 
@@ -109,27 +106,27 @@ class TestDistortionCorrector(unittest.TestCase):
     def print_message(stat):
         logger.debug("===========================")
         logger.debug(stat)
+    
+    @staticmethod
+    def pointcloud2_to_xyz_array(cloud_msg):
+        cloud_arr = np.frombuffer(cloud_msg.data, dtype=np.float32)
+        cloud_arr = np.reshape(cloud_arr, (cloud_msg.width, int(cloud_msg.point_step / 4)))
+        return cloud_arr[:, :3]
 
     @staticmethod
     def generate_pointcloud(num_points, header, timestamp_offset_per_point=0.01):
         points_xyz = np.random.rand(num_points, 3).astype(np.float32)
         timestamps = np.array(
-            [
-                header.stamp.sec + header.stamp.nanosec * 1e-9 + timestamp_offset_per_point * i
-                for i in range(num_points)
-            ]
+            [header.stamp.sec + header.stamp.nanosec * 1e-9 + timestamp_offset_per_point * i for i in range(num_points)]
         ).astype(np.float64)
         xyz_data = points_xyz.tobytes()
         timestamp_data = timestamps.tobytes()
-        pointcloud_data = b"".join(
-            xyz_data[i * 12 : i * 12 + 12] + timestamp_data[i * 8 : i * 8 + 8]
-            for i in range(len(xyz_data))
-        )
+        pointcloud_data = b''.join(xyz_data[i*12:i*12+12] + timestamp_data[i*8:i*8+8] for i in range(len(xyz_data)))
         fields = [
-            PointField(name="x", offset=0, datatype=PointField.FLOAT32, count=1),
-            PointField(name="y", offset=4, datatype=PointField.FLOAT32, count=1),
-            PointField(name="z", offset=8, datatype=PointField.FLOAT32, count=1),
-            PointField(name="time_stamp", offset=12, datatype=PointField.FLOAT64, count=1),
+            PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+            PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+            PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+            PointField(name='time_stamp', offset=12, datatype=PointField.FLOAT64, count=1),
         ]
         pointcloud_msg = PointCloud2(
             header=header,
@@ -138,7 +135,7 @@ class TestDistortionCorrector(unittest.TestCase):
             is_dense=True,
             is_bigendian=False,
             point_step=20,  # 4 float32 fields * 4 bytes/field
-            row_step=20 * num_points,  # point_step * width
+            row_step=20*num_points,  # point_step * width
             fields=fields,
             data=pointcloud_data,
         )
@@ -154,30 +151,17 @@ class TestDistortionCorrector(unittest.TestCase):
         )
 
         # Publishers
-        imu_pub = self.test_node.create_publisher(Imu, "/test/sensing/imu/imu_data", 10)
-        velocity_pub = self.test_node.create_publisher(
-            TwistWithCovarianceStamped,
-            "/test/sensing/vehicle_velocity_converter/twist_with_covariance",
-            10,
-        )
-        pointcloud_pub = self.test_node.create_publisher(
-            PointCloud2,
-            "/test/sensing/lidar/top/mirror_cropped/pointcloud_ex",
-            qos_profile=sensor_qos,
-        )
+        imu_pub = self.test_node.create_publisher(Imu, '/test/sensing/imu/imu_data', 10)
+        velocity_pub = self.test_node.create_publisher(TwistWithCovarianceStamped, '/test/sensing/vehicle_velocity_converter/twist_with_covariance', 10)
+        pointcloud_pub = self.test_node.create_publisher(PointCloud2, '/test/sensing/lidar/top/mirror_cropped/pointcloud_ex', qos_profile=sensor_qos)
 
         # Subscribers
-        received_data = {"msg": None}
+        received_data = {'msg': None}
 
         def pointcloud_callback(msg):
-            received_data["msg"] = msg
+            received_data['msg'] = msg
 
-        self.test_node.create_subscription(
-            PointCloud2,
-            "/test/sensing/lidar/top/rectified/pointcloud_ex",
-            pointcloud_callback,
-            qos_profile=sensor_qos,
-        )
+        self.test_node.create_subscription(PointCloud2, '/test/sensing/lidar/top/rectified/pointcloud_ex', pointcloud_callback, qos_profile=sensor_qos)
 
         # Wait for composable node launches
         rclpy.spin_once(self.test_node, timeout_sec=1.0)
@@ -185,13 +169,13 @@ class TestDistortionCorrector(unittest.TestCase):
         # Prepare header
         header = Header()
         header.stamp = self.test_node.get_clock().now().to_msg()
-        header.frame_id = "base_link"
+        header.frame_id = 'base_link'
 
         # Publish IMU and velocity data
         for i in range(50):
             header_tmp = Header()
             header_tmp.stamp = self.test_node.get_clock().now().to_msg()
-            header_tmp.frame_id = "base_link"
+            header_tmp.frame_id = 'base_link'
 
             imu_msg = Imu()
             imu_msg.header = header_tmp
@@ -210,44 +194,22 @@ class TestDistortionCorrector(unittest.TestCase):
 
         # Wait for output with a timeout
         start_time = self.test_node.get_clock().now()
-        while self.test_node.get_clock().now() - start_time < rclpy.duration.Duration(
-            seconds=self.evaluation_time
-        ):
+        while self.test_node.get_clock().now() - start_time < rclpy.duration.Duration(seconds=self.evaluation_time):
             rclpy.spin_once(self.test_node, timeout_sec=0.1)
-            if received_data["msg"] is not None:
+            if received_data['msg'] is not None:
                 break
 
         # Check if the output was received
-        self.assertIsNotNone(received_data["msg"], "Did not receive output pointcloud data")
+        self.assertIsNotNone(received_data['msg'], 'Did not receive output pointcloud data')
 
         # Check that the received pointcloud data has same length as num_points
-        received_pointcloud = received_data["msg"]
-        self.assertEqual(
-            received_pointcloud.width,
-            num_points,
-            "The received pointcloud data has a different length than expected",
-        )
+        received_pointcloud = received_data['msg']
+        self.assertEqual(received_pointcloud.width, num_points, 'The received pointcloud data has a different length than expected')
 
         # Check that the received pointcloud data is different from the original one
-        # For this, let's convert the pointcloud data into numpy arrays and compare them
-
-        # Function to convert pointcloud2 to numpy array
-        def pointcloud2_to_xyz_array(cloud_msg):
-            cloud_arr = np.frombuffer(cloud_msg.data, dtype=np.float32)
-            cloud_arr = np.reshape(cloud_arr, (cloud_msg.width, int(cloud_msg.point_step / 4)))
-            return cloud_arr[:, :3]
-
-        original_pointcloud_arr = pointcloud2_to_xyz_array(pointcloud_msg)
-        received_pointcloud_arr = pointcloud2_to_xyz_array(received_pointcloud)
-
-        # You might want to allow some tolerance because of potential numerical errors
-        # Therefore, np.allclose() is used instead of np.array_equal()
-        # Adjust the tolerance according to your needs
-        self.assertFalse(
-            np.allclose(original_pointcloud_arr, received_pointcloud_arr, atol=1e-6),
-            "The received pointcloud data is not different from the original one",
-        )
-
+        original_pointcloud_arr = self.pointcloud2_to_xyz_array(pointcloud_msg)
+        received_pointcloud_arr = self.pointcloud2_to_xyz_array(received_pointcloud)
+        self.assertFalse(np.allclose(original_pointcloud_arr, received_pointcloud_arr, atol=1e-6), 'The received pointcloud data is not different from the original one')
 
 @launch_testing.post_shutdown_test()
 class TestProcessOutput(unittest.TestCase):
