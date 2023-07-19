c9a8b9201d03f1bd4ace0b3474839821ce04003c
kminoda <koji.minoda@tier4.jp>
2023-07-19 10:59:59 +0900

    move intended typo to ekf
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- localization/dummy_package/dummy_file_with_typo.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  // thisisatypo
-  return 0;
-}
--- localization/ekf_localizer/src/covariance.cpp
+++ localization/ekf_localizer/src/covariance.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// thisisanintendedtypo (This is an intended typo)
+
 #include "ekf_localizer/covariance.hpp"
 
 #include "ekf_localizer/state_index.hpp"
