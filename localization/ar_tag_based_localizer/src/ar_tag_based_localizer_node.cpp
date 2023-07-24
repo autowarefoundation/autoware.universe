5814ef35337e6f6946e2732aed6d3b6bc28a83ad
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-07-24 16:15:31 +0900

    Updated license statements
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- localization/ar_tag_based_localizer/src/ar_tag_based_localizer_node.cpp
+++ localization/ar_tag_based_localizer/src/ar_tag_based_localizer_node.cpp
@@ -1,3 +1,17 @@
+// Copyright 2023 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // This source code is derived from the https://github.com/pal-robotics/aruco_ros.
 // Here is the license statement.
 /*****************************
@@ -27,19 +41,6 @@
  authors and should not be interpreted as representing official policies, either expressed
  or implied, of Rafael Muñoz Salinas.
  ********************************/
-// Copyright 2023- Autoware Foundation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 #include "ar_tag_based_localizer/ar_tag_based_localizer_core.hpp"
 
--- localization/ar_tag_based_localizer/src/tag_tf_caster_core.cpp
+++ localization/ar_tag_based_localizer/src/tag_tf_caster_core.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023- Autoware Foundation
+// Copyright 2023 Autoware Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
