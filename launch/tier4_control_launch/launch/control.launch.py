c4975552b3b5cbece00ae0b8352c34498d08fb97
ismetatabay <ismet@leodrive.ai>
2023-07-26 20:28:47 +0300

    clean_commit
    Signed-off-by: ismetatabay <ismet@leodrive.ai>

--- launch/tier4_control_launch/launch/control.launch.py
+++ launch/tier4_control_launch/launch/control.launch.py
@@ -204,6 +204,7 @@ def launch_setup(context, *args, **kwargs):
                 "check_external_emergency_heartbeat": LaunchConfiguration(
                     "check_external_emergency_heartbeat"
                 ),
+                "enable_cmd_limit_filter": LaunchConfiguration("enable_cmd_limit_filter"),
             },
         ],
         extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
@@ -330,6 +331,7 @@ def generate_launch_description():
     add_launch_arg("enable_obstacle_collision_checker")
     add_launch_arg("lateral_controller_mode")
     add_launch_arg("longitudinal_controller_mode")
+    add_launch_arg("enable_cmd_limit_filter")
     # common param path
     add_launch_arg("nearest_search_param_path")
     # package param path
