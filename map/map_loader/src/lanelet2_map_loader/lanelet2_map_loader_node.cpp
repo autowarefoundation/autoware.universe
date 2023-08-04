7d56305d22fbc80883179122bf50c9c81283bc46
kminoda <koji.minoda@tier4.jp>
2023-08-04 15:45:11 +0900

    remove transverse mercator for now
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- map/map_loader/src/lanelet2_map_loader/lanelet2_map_loader_node.cpp
+++ map/map_loader/src/lanelet2_map_loader/lanelet2_map_loader_node.cpp
@@ -100,15 +100,6 @@ lanelet::LaneletMapPtr Lanelet2MapLoaderNode::load_map(
     if (errors.empty()) {
       return map;
     }
-  } else if (lanelet2_map_projector_type == "TransverseMercator") {
-    lanelet::GPSPoint position{map_origin_lat, map_origin_lon};
-    lanelet::Origin origin{position};
-
-    lanelet::projection::TransverseMercatorProjector projector{origin};
-    const lanelet::LaneletMapPtr map = lanelet::load(lanelet2_filename, projector, &errors);
-    if (errors.empty()) {
-      return map;
-    }
   } else if (lanelet2_map_projector_type == "local") {
     // Use MGRSProjector as parser
     lanelet::projection::MGRSProjector projector{};
