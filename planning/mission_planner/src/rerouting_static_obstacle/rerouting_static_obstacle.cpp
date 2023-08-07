ea20186fdd71c07513eceeaa4f7383b4754aad92
AhmedEbrahim <ahmed.a.d.ebrahim@gmail.com>
2023-08-07 14:39:43 +0300

    feat(rerouting-static-obstacle): proposed solution for rerouting static obstacle based on point publisher by the user
    Signed-off-by: AhmedEbrahim <ahmed.a.d.ebrahim@gmail.com>

---
+++ planning/mission_planner/src/rerouting_static_obstacle/rerouting_static_obstacle.cpp
@@ -0,0 +1,211 @@
+// Copyright 2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rerouting_static_obstacle.hpp"
+
+#include <lanelet2_extension/utility/message_conversion.hpp>
+#include <lanelet2_extension/utility/query.hpp>
+#include <lanelet2_extension/utility/utilities.hpp>
+
+namespace mission_planner
+{
+ReroutingStaticObstacle::ReroutingStaticObstacle(const rclcpp::NodeOptions & node_options)
+: Node("rerouting_static_obstacle", node_options)
+{
+  using std::placeholders::_1;
+
+  sub_odom_ = this->create_subscription<nav_msgs::msg::Odometry>(
+    "/localization/kinematic_state", rclcpp::QoS(1),
+    std::bind(&ReroutingStaticObstacle::onOdom, this, _1));
+
+  sub_route_ = create_subscription<autoware_planning_msgs::msg::LaneletRoute>(
+    "/planning/mission_planning/route", rclcpp::QoS{1}.transient_local(),
+    std::bind(&ReroutingStaticObstacle::route_callback, this, std::placeholders::_1));
+
+  map_subscriber_ = create_subscription<HADMapBin>(
+    "/map/vector_map", rclcpp::QoS{10}.transient_local(),
+    std::bind(&ReroutingStaticObstacle::map_callback, this, std::placeholders::_1));
+
+  sub_trigger_ = this->create_subscription<geometry_msgs::msg::PointStamped>(
+    "/clicked_point", rclcpp::QoS(1), std::bind(&ReroutingStaticObstacle::onTrigger, this, _1));
+
+  const auto adaptor = component_interface_utils::NodeAdaptor(this);
+  adaptor.init_cli(cli_change_route_);
+}
+
+void ReroutingStaticObstacle::onOdom(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+  current_pose = msg->pose.pose;
+}
+
+void ReroutingStaticObstacle::route_callback(
+  const autoware_planning_msgs::msg::LaneletRoute::ConstSharedPtr msg)
+{
+  goal_pose = msg->goal_pose;
+}
+
+void ReroutingStaticObstacle::map_callback(const HADMapBin::ConstSharedPtr msg)
+{
+  route_handler_.setMap(*msg);
+  lanelet_map_ptr_ = std::make_shared<lanelet::LaneletMap>();
+  lanelet::utils::conversion::fromBinMsg(
+    *msg, lanelet_map_ptr_, &traffic_rules_ptr_, &routing_graph_ptr_);
+  lanelet::ConstLanelets all_lanelets = lanelet::utils::query::laneletLayer(lanelet_map_ptr_);
+  road_lanelets_ = lanelet::utils::query::roadLanelets(all_lanelets);
+}
+
+void ReroutingStaticObstacle::onTrigger(const geometry_msgs::msg::PointStamped::SharedPtr msg)
+{
+  geometry_msgs::msg::Pose selected_point;
+  selected_point.position = msg->point;
+
+  lanelet::ConstLanelet selected_point_lanelet;
+  bool selected_point_lanelet_found{false};
+  selected_point_lanelet_found = getSelectedPointLanelet(selected_point, selected_point_lanelet);
+
+  lanelet::ConstLanelets remaining_route_lanelets;
+  bool remaining_route_lanelets_found{false};
+  remaining_route_lanelets_found = getRemainingRouteLanelets(remaining_route_lanelets);
+
+  bool selected_point_in_route{false};
+  if (selected_point_lanelet_found && remaining_route_lanelets_found) {
+    selected_point_in_route =
+      isSelectedPointInRoute(selected_point_lanelet, remaining_route_lanelets);
+  } else {
+    return;
+  }
+
+  if (selected_point_in_route) {
+    lanelet::routing::LaneletPath alternative_route_lanelets;
+    bool alternative_route_found{false};
+    alternative_route_found =
+      searchAlternativeRoute(selected_point_lanelet, alternative_route_lanelets);
+
+    if (alternative_route_found) {
+      changeRoute(alternative_route_lanelets);
+    }
+  }
+}
+
+bool ReroutingStaticObstacle::getSelectedPointLanelet(
+  const geometry_msgs::msg::Pose & selected_point,
+  lanelet::ConstLanelet & selected_point_lanelet) const
+{
+  lanelet::ConstLanelets selected_point_lanelets;
+  if (!lanelet::utils::query::getCurrentLanelets(
+        road_lanelets_, selected_point, &selected_point_lanelets)) {
+    RCLCPP_WARN_STREAM(this->get_logger(), "Failed to find selected_point current lanelet.");
+    return false;
+  }
+
+  if (selected_point_lanelets.size() > 1) {  // This will happen only if we have co-located lanes at
+                                             // the point the user selects
+    // TODO(AhmedEbrahim) find a way to deduce the selected point orientation
+    if (!lanelet::utils::query::getClosestLanelet(
+          road_lanelets_, selected_point, &selected_point_lanelet)) {
+      RCLCPP_WARN_STREAM(this->get_logger(), "Failed to find selected_point closest lanelet.");
+      return false;
+    }
+
+  } else {
+    selected_point_lanelet = selected_point_lanelets.at(0);
+  }
+
+  return true;
+}
+
+bool ReroutingStaticObstacle::getRemainingRouteLanelets(
+  lanelet::ConstLanelets & remaining_route_lanelets) const
+{
+  return route_handler_.planPathLaneletsBetweenCheckpoints(
+    current_pose, goal_pose, &remaining_route_lanelets);
+}
+
+bool ReroutingStaticObstacle::isSelectedPointInRoute(
+  const lanelet::ConstLanelet & selected_point_lanelet,
+  const lanelet::ConstLanelets & remaining_route_lanelets) const
+{
+  for (const auto & llt : remaining_route_lanelets) {
+    if (llt.id() == selected_point_lanelet.id()) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool ReroutingStaticObstacle::searchAlternativeRoute(
+  const lanelet::ConstLanelet & selected_point_lanelet,
+  lanelet::routing::LaneletPath & alternative_route_lanelets) const
+{
+  bool alternative_route_found{false};
+
+  lanelet::Optional<lanelet::routing::Route> optional_route;
+
+  lanelet::ConstLanelet current_lanelet;
+  double alternative_route_length2d = std::numeric_limits<double>::max();
+  if (!lanelet::utils::query::getClosestLanelet(road_lanelets_, current_pose, &current_lanelet)) {
+    RCLCPP_WARN_STREAM(this->get_logger(), "Failed to find current_lanelet.");
+    return false;
+  }
+
+  lanelet::ConstLanelet goal_lanelet;
+  if (!lanelet::utils::query::getClosestLanelet(road_lanelets_, goal_pose, &goal_lanelet)) {
+    RCLCPP_WARN_STREAM(this->get_logger(), "Failed to find goal_lanelet.");
+    return false;
+  }
+  for (const auto & llt : road_lanelets_) {
+    lanelet::ConstLanelets via_lanelet;
+    via_lanelet.push_back(llt);
+    optional_route = routing_graph_ptr_->getRouteVia(current_lanelet, via_lanelet, goal_lanelet, 0);
+
+    if (
+      (optional_route) && (!optional_route->contains(selected_point_lanelet)) &&
+      (optional_route->length2d() < alternative_route_length2d)) {
+      alternative_route_length2d = optional_route->length2d();
+      alternative_route_lanelets = optional_route->shortestPath();
+      alternative_route_found = true;
+    }
+    via_lanelet.clear();
+  }
+
+  return alternative_route_found;
+}
+
+void ReroutingStaticObstacle::changeRoute(const lanelet::routing::LaneletPath & lanelet_path)
+{
+  const auto req = std::make_shared<ChangeRoute::Service::Request>();
+  req->header.frame_id = "map";
+  req->header.stamp = this->get_clock()->now();
+  req->goal = goal_pose;
+  convertLaneletPathToRouteSegments(lanelet_path, req->segments);
+  cli_change_route_->async_send_request(req);
+}
+
+void ReroutingStaticObstacle::convertLaneletPathToRouteSegments(
+  const lanelet::routing::LaneletPath & lanelet_path,
+  std::vector<autoware_adapi_v1_msgs::msg::RouteSegment> & route_segments) const
+{
+  for (const auto & ll : lanelet_path) {
+    autoware_adapi_v1_msgs::msg::RouteSegment route_segment;
+    route_segment.preferred.id = ll.id();
+    route_segment.preferred.type = "lane";
+    route_segments.push_back(route_segment);
+  }
+}
+
+}  // namespace mission_planner
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(mission_planner::ReroutingStaticObstacle)
---
+++ planning/mission_planner/src/rerouting_static_obstacle/rerouting_static_obstacle.hpp
@@ -0,0 +1,79 @@
+// Copyright 2020 Tier IV, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef REROUTING_STATIC_OBSTACLE__REROUTING_STATIC_OBSTACLE_HPP_
+#define REROUTING_STATIC_OBSTACLE__REROUTING_STATIC_OBSTACLE_HPP_
+
+#include <component_interface_specs/planning.hpp>
+#include <component_interface_utils/rclcpp.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <route_handler/route_handler.hpp>
+
+#include <autoware_auto_mapping_msgs/msg/had_map_bin.hpp>
+#include <autoware_planning_msgs/msg/lanelet_route.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
+#include <lanelet2_routing/RoutingGraph.h>
+
+namespace mission_planner
+{
+using autoware_auto_mapping_msgs::msg::HADMapBin;
+class ReroutingStaticObstacle : public rclcpp::Node
+{
+  using ChangeRoute = planning_interface::ChangeRoute;
+
+public:
+  explicit ReroutingStaticObstacle(const rclcpp::NodeOptions & node_options);
+
+private:
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom_;
+  rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr sub_trigger_;
+  rclcpp::Subscription<autoware_planning_msgs::msg::LaneletRoute>::SharedPtr sub_route_;
+  rclcpp::Subscription<HADMapBin>::SharedPtr map_subscriber_;
+  component_interface_utils::Client<ChangeRoute>::SharedPtr cli_change_route_;
+
+  lanelet::LaneletMapPtr lanelet_map_ptr_;
+  lanelet::routing::RoutingGraphPtr routing_graph_ptr_;
+  lanelet::traffic_rules::TrafficRulesPtr traffic_rules_ptr_;
+  lanelet::ConstLanelets road_lanelets_;
+  route_handler::RouteHandler route_handler_;
+  geometry_msgs::msg::Pose current_pose;
+  geometry_msgs::msg::Pose goal_pose;
+
+  void onOdom(const nav_msgs::msg::Odometry::SharedPtr msg);
+  void route_callback(const autoware_planning_msgs::msg::LaneletRoute::ConstSharedPtr msg);
+  void map_callback(const HADMapBin::ConstSharedPtr msg);
+  void onTrigger(const geometry_msgs::msg::PointStamped::SharedPtr msg);
+  bool getSelectedPointLanelet(
+    const geometry_msgs::msg::Pose & selected_point,
+    lanelet::ConstLanelet & selected_point_lanelet) const;
+  bool getRemainingRouteLanelets(lanelet::ConstLanelets & remaining_route_lanelets) const;
+  bool isSelectedPointInRoute(
+    const lanelet::ConstLanelet & selected_point_lanelet,
+    const lanelet::ConstLanelets & remaining_route_lanelets) const;
+  bool searchAlternativeRoute(
+    const lanelet::ConstLanelet & selected_point_lanelet,
+    lanelet::routing::LaneletPath & alternative_route_lanelets) const;
+
+  void changeRoute(const lanelet::routing::LaneletPath & lanelet_path);
+
+  void convertLaneletPathToRouteSegments(
+    const lanelet::routing::LaneletPath & lanelet_path,
+    std::vector<autoware_adapi_v1_msgs::msg::RouteSegment> & route_segments) const;
+};
+
+}  // namespace mission_planner
+#endif  // REROUTING_STATIC_OBSTACLE__REROUTING_STATIC_OBSTACLE_HPP_
