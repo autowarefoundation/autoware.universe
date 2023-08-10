7acd51d610b6eeb2306a9cc06f60939f80217742
AhmedEbrahim <ahmed.a.d.ebrahim@gmail.com>
2023-08-10 17:43:59 +0300

    feat(rerouting-static-obstacle): adding dedicated plugin for publish rerouting point by the user
    Signed-off-by: AhmedEbrahim <ahmed.a.d.ebrahim@gmail.com>

---
+++ common/tier4_planning_rviz_plugin/include/reroute_static_obstacle/panel.hpp
@@ -0,0 +1,113 @@
+// Copyright 2022 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef TOOLS__REROUTE_STATIC_OBSTACLE_PANEL_HPP_
+#define TOOLS__REROUTE_STATIC_OBSTACLE_PANEL_HPP_
+
+#include "rviz_default_plugins/tools/point/point_tool.hpp"
+
+#include <sstream>
+
+#include <OgreVector.h>
+#include <OgreVector3.h>
+
+#include "rclcpp/qos.hpp"
+
+#include <rviz_common/display_context.hpp>
+#include "rviz_common/interaction/view_picker_iface.hpp"
+#include "rviz_common/load_resource.hpp"
+#include "rviz_common/msg_conversions.hpp"
+#include "rviz_common/properties/bool_property.hpp"
+#include "rviz_common/properties/string_property.hpp"
+#include "rviz_common/properties/qos_profile_property.hpp"
+#include "rviz_common/render_panel.hpp"
+#include "rviz_common/viewport_mouse_event.hpp"
+#include "rviz_common/view_controller.hpp"
+
+#include <optional>
+#include <algorithm>
+#include <memory>
+#include <vector>
+
+namespace rviz_plugins
+{
+
+class RerouteStaticObstacleTool : public rviz_default_plugins::tools::PointTool
+{
+
+public:
+  RerouteStaticObstacleTool();
+  void onInitialize() override;
+  
+  void activate() override;
+  void deactivate() override;
+
+  int processMouseEvent(rviz_common::ViewportMouseEvent & event) override;
+
+  public Q_SLOTS:
+  void updateTopic() ;
+  void updateAutoDeactivate() ;
+protected:
+  void publishPosition(const Ogre::Vector3 & position) const ;
+  void setStatusForPosition(const Ogre::Vector3 & position);
+
+//   QCursor std_cursor_;
+//   QCursor hit_cursor_;
+//   rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr publisher_;
+//   rclcpp::Clock::SharedPtr clock_;
+
+//   rviz_common::properties::StringProperty * topic_property_;
+//   rviz_common::properties::BoolProperty * auto_deactivate_property_;
+//   rviz_common::properties::QosProfileProperty * qos_profile_property_;
+
+
+  rclcpp::QoS qos_profile_;
+  private:
+  std::optional<Ogre::Vector3> get_point_from_mouse(rviz_common::ViewportMouseEvent & event);
+};
+}  // namespace rviz_plugins
+#endif  // TOOLS__REROUTE_STATIC_OBSTACLE_PANEL_HPP_
---
+++ common/tier4_planning_rviz_plugin/src/reroute_static_obstacle/panel.cpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2013, Willow Garage, Inc.
+ * Copyright (c) 2018, Bosch Software Innovations GmbH.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <reroute_static_obstacle/panel.hpp>
+#include <rviz_common/render_panel.hpp>
+#include <rviz_rendering/render_window.hpp>
+
+#include <OgreCamera.h>
+#include <OgreRay.h>
+#include <OgreViewport.h>
+
+namespace rviz_plugins
+{
+
+RerouteStaticObstacleTool::RerouteStaticObstacleTool()
+: qos_profile_(5)
+{
+  shortcut_key_ = 'u';
+
+  topic_property_ = new rviz_common::properties::StringProperty(
+    "Topic", "/simulation/planning/reroute_static_obstacle_point_publisher/point",
+    "The topic on which to publish points.",
+    getPropertyContainer(), SLOT(updateTopic()), this);
+
+  auto_deactivate_property_ = new rviz_common::properties::BoolProperty(
+    "Interfactive", false,
+    "Switch away from this tool after one click.",
+    getPropertyContainer(), SLOT(updateAutoDeactivate()), this);
+
+  qos_profile_property_ = new rviz_common::properties::QosProfileProperty(
+    topic_property_, qos_profile_);
+}
+
+void RerouteStaticObstacleTool::onInitialize()
+{
+  hit_cursor_ = cursor_;
+  std_cursor_ = rviz_common::getDefaultCursor();
+  qos_profile_property_->initialize(
+    [this](rclcpp::QoS profile) {this->qos_profile_ = profile;});
+  updateTopic();
+}
+
+void RerouteStaticObstacleTool::activate() {}
+
+void RerouteStaticObstacleTool::deactivate() {}
+
+void RerouteStaticObstacleTool::updateTopic()
+{
+  rclcpp::Node::SharedPtr raw_node =
+    context_->getRosNodeAbstraction().lock()->get_raw_node();
+  // TODO(anhosi, wjwwood): replace with abstraction for publishers once available
+  publisher_ = raw_node->
+    template create_publisher<geometry_msgs::msg::PointStamped>(
+    topic_property_->getStdString(), qos_profile_);
+  clock_ = raw_node->get_clock();
+}
+
+void RerouteStaticObstacleTool::updateAutoDeactivate() {}
+
+int RerouteStaticObstacleTool::processMouseEvent(rviz_common::ViewportMouseEvent & event)
+{
+  int flags = 0;
+  if (event.leftUp()) {
+    const auto point = get_point_from_mouse(event);
+    if(point){
+        setStatusForPosition(point.value());
+        publishPosition(point.value());
+        if (auto_deactivate_property_->getBool()) {
+        flags |= Finished;
+      }
+
+    }
+
+  }
+
+//   Ogre::Vector3 position;
+//   bool success = context_->getViewPicker()->get3DPoint(event.panel, event.x, event.y, position);
+//   setCursor(success ? hit_cursor_ : std_cursor_);
+
+//   if (success) {
+//     setStatusForPosition(position);
+
+//     if (event.leftUp()) {
+//       publishPosition(position);
+
+//       if (auto_deactivate_property_->getBool()) {
+//         flags |= Finished;
+//       }
+//     }
+//   } else {
+//     setStatus("Move over an object to select the target point.");
+//   }
+
+  return flags;
+}
+
+void RerouteStaticObstacleTool::setStatusForPosition(const Ogre::Vector3 & position)
+{
+  std::ostringstream s;
+  s << "<b>Left-Click:</b> Select this point.";
+  s.precision(3);
+  s << " [" << position.x << "," << position.y << "," << position.z << "]";
+  setStatus(s.str().c_str());
+}
+
+void RerouteStaticObstacleTool::publishPosition(const Ogre::Vector3 & position) const
+{
+  auto point = rviz_common::pointOgreToMsg(position);
+  geometry_msgs::msg::PointStamped point_stamped;
+  point_stamped.point = point;
+  point_stamped.header.frame_id = context_->getFixedFrame().toStdString();
+  point_stamped.header.stamp = clock_->now();
+  publisher_->publish(point_stamped);
+}
+
+std::optional<Ogre::Vector3> RerouteStaticObstacleTool::get_point_from_mouse(rviz_common::ViewportMouseEvent & event)
+{
+  using rviz_rendering::RenderWindowOgreAdapter;
+  const auto viewport = RenderWindowOgreAdapter::getOgreViewport(event.panel->getRenderWindow());
+  const auto w = viewport->getActualWidth();
+  const auto h = viewport->getActualHeight();
+  const auto x = static_cast<Ogre::Real>(event.x) / static_cast<Ogre::Real>(w);
+  const auto y = static_cast<Ogre::Real>(event.y) / static_cast<Ogre::Real>(h);
+
+  const auto plane = Ogre::Plane(Ogre::Vector3::UNIT_Z, 0.0);
+  const auto ray = viewport->getCamera()->getCameraToViewportRay(x, y);
+  const auto intersect = ray.intersects(plane);
+  return intersect.first ? std::optional(ray.getPoint(intersect.second)) : std::nullopt;
+}
+
+}  // namespace rviz_default_plugins
+
+#include <pluginlib/class_list_macros.hpp>  // NOLINT
+//PLUGINLIB_EXPORT_CLASS(rviz_default_plugins::tools::PointTool, rviz_common::Tool)
+PLUGINLIB_EXPORT_CLASS(rviz_plugins::RerouteStaticObstacleTool, rviz_common::Tool)
