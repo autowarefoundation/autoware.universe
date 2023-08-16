c6a819fa426c7e3b80edae7cf9996e8df70f4e05
yoshiri <yoshiyoshidetteiu@gmail.com>
2023-08-16 17:14:41 +0900

    create const function
    Signed-off-by: yoshiri <yoshiyoshidetteiu@gmail.com>

--- perception/tracking_object_merger/src/data_association/data_association.cpp
+++ perception/tracking_object_merger/src/data_association/data_association.cpp
@@ -293,3 +293,56 @@ double DataAssociation::calcScoreBetweenObjects(
   }
   return score;
 }
+
+double DataAssociation::calcScoreBetweenObjects(
+  const autoware_auto_perception_msgs::msg::TrackedObject & object0,
+  const autoware_auto_perception_msgs::msg::TrackedObject & object1) const
+{
+  const std::uint8_t object1_label =
+    object_recognition_utils::getHighestProbLabel(object1.classification);
+  const std::uint8_t object0_label =
+    object_recognition_utils::getHighestProbLabel(object0.classification);
+
+  std::vector<double> tracker_pose = {
+    object1.kinematics.pose_with_covariance.pose.position.x,
+    object1.kinematics.pose_with_covariance.pose.position.y};
+  std::vector<double> measurement_pose = {
+    object0.kinematics.pose_with_covariance.pose.position.x,
+    object0.kinematics.pose_with_covariance.pose.position.y};
+
+  double score = 0.0;
+  if (can_assign_matrix_(object1_label, object0_label)) {
+    const double max_dist = max_dist_matrix_(object1_label, object0_label);
+    const double dist = tier4_autoware_utils::calcDistance2d(
+      object0.kinematics.pose_with_covariance.pose.position,
+      object1.kinematics.pose_with_covariance.pose.position);
+
+    bool passed_gate = true;
+    // dist gate
+    if (passed_gate) {
+      if (max_dist < dist) passed_gate = false;
+    }
+    // angle gate
+    if (passed_gate) {
+      const double max_rad = max_rad_matrix_(object1_label, object0_label);
+      const double angle = getFormedYawAngle(
+        object0.kinematics.pose_with_covariance.pose.orientation,
+        object1.kinematics.pose_with_covariance.pose.orientation, false);
+      if (std::fabs(max_rad) < M_PI && std::fabs(max_rad) < std::fabs(angle)) passed_gate = false;
+    }
+    // 2d iou gate
+    if (passed_gate) {
+      const double min_iou = min_iou_matrix_(object1_label, object0_label);
+      const double min_union_iou_area = 1e-2;
+      const double iou = object_recognition_utils::get2dIoU(object0, object1, min_union_iou_area);
+      if (iou < min_iou) passed_gate = false;
+    }
+
+    // all gate is passed
+    if (passed_gate) {
+      score = (max_dist - std::min(dist, max_dist)) / max_dist;
+      if (score < score_threshold_) score = 0.0;
+    }
+  }
+  return score;
+}
