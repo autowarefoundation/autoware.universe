883dd3fdb6add9162f7348a76c5191e65ef9cc53
Mamoru Sobue <mamoru.sobue@tier4.jp>
2023-08-25 17:39:54 +0900

    fix cyclic/processing
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- perception/euclidean_cluster/src/voxel_grid_based_euclidean_cluster_node.cpp
+++ perception/euclidean_cluster/src/voxel_grid_based_euclidean_cluster_node.cpp
@@ -77,8 +77,8 @@ void VoxelGridBasedEuclideanClusterNode::onPointCloud(
     debug_pub_->publish(debug);
   }
   if (debug_publisher_) {
-    const double processing_time_ms = stop_watch_ptr_->toc("cyclic_time", true);
-    const double cyclic_time_ms = stop_watch_ptr_->toc("processing_time", true);
+    const double processing_time_ms = stop_watch_ptr_->toc("processing_time", true);
+    const double cyclic_time_ms = stop_watch_ptr_->toc("cyclic_time", true);
     debug_publisher_->publish<tier4_debug_msgs::msg::Float64Stamped>(
       "debug/cyclic_time_ms", cyclic_time_ms);
     debug_publisher_->publish<tier4_debug_msgs::msg::Float64Stamped>(
