31352ca1d0d79edee4d2db95739363c9e4f467e3
Cynthia Liu <cynthia.liu@autocore.ai>
2023-08-30 15:00:49 +0800

    feat(kalman_filter): add gtest
    Signed-off-by: Cynthia Liu <cynthia.liu@autocore.ai>

--- common/kalman_filter/test/test_kalman_filter.cpp
+++ common/kalman_filter/test/test_kalman_filter.cpp
@@ -49,32 +49,41 @@ TEST(kalman_filter, kf)
   u_t << 0.1, 0.1;
   EXPECT_TRUE(kf_.predict(u_t));
 
-  // Check the updated state
+  // Check the updated state and covariance matrix
   Eigen::MatrixXd x_predict_expected = A_t * x_t + B_t * u_t;
   Eigen::MatrixXd P_predict_expected = A_t * P_t * A_t.transpose() + Q_t;
 
   Eigen::MatrixXd x_predict;
   kf_.getX(x_predict);
+  Eigen::MatrixXd P_predict;
+  kf_.getP(P_predict); 
 
   EXPECT_NEAR(x_predict(0, 0), x_predict_expected(0, 0), 1e-5);
   EXPECT_NEAR(x_predict(1, 0), x_predict_expected(1, 0), 1e-5);
+  EXPECT_NEAR(P_predict(0, 0), P_predict_expected(0, 0), 1e-5);
+  EXPECT_NEAR(P_predict(1, 1), P_predict_expected(1, 1), 1e-5);
 
   // Perform update
   Eigen::MatrixXd y_t(2, 1);
   y_t << 1.05, 2.05;
   EXPECT_TRUE(kf_.update(y_t));
 
-  // Check the updated state
+  // Check the updated state and covariance matrix
   const Eigen::MatrixXd PCT_t = P_predict_expected * C_t.transpose();
   const Eigen::MatrixXd K_t = PCT_t * ((R_t + C_t * PCT_t).inverse());
   const Eigen::MatrixXd y_pred = C_t * x_predict_expected;
   Eigen::MatrixXd x_update_expected = x_predict_expected + K_t * (y_t - y_pred);
+  Eigen::MatrixXd P_update_expected = P_predict_expected - K_t * (C_t * P_predict_expected);
 
   Eigen::MatrixXd x_update;
   kf_.getX(x_update);
+  Eigen::MatrixXd P_update;
+  kf_.getP(P_update);
 
   EXPECT_NEAR(x_update(0, 0), x_update_expected(0, 0), 1e-5); 
   EXPECT_NEAR(x_update(1, 0), x_update_expected(1, 0), 1e-5);
+  EXPECT_NEAR(P_update(0, 0), P_update_expected(0, 0), 1e-5); 
+  EXPECT_NEAR(P_update(1, 1), P_update_expected(1, 1), 1e-5);
 
 }
 
