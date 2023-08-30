fd503610cad5f7bdf4ce53e88de68352fae4aa22
kminoda <koji.minoda@tier4.jp>
2023-08-30 15:03:33 +0900

    add test
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- common/tier4_geography_utils/test/test_height.cpp
+++ common/tier4_geography_utils/test/test_height.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Tier IV, Inc.
+// Copyright 2023 TIER IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,10 +12,55 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "tier4_geography_utils/geography/height.hpp"
+#include "tier4_geography_utils/height.hpp"
 
 #include <gtest/gtest.h>
 
+#include <stdexcept>
 #include <string>
 
-// TODO
+// Test case to verify if same source and target datums return original height
+TEST(Tier4GeographyUtils, SameSourceTargetDatum)
+{
+  const double height = 10.0;
+  const double latitude = 35.0;
+  const double longitude = 139.0;
+  const std::string datum = "WGS84";
+  
+  double converted_height = tier4_geography_utils::convert_height(height, latitude, longitude, datum, datum);
+  
+  EXPECT_DOUBLE_EQ(height, converted_height);
+}
+
+// Test case to verify valid source and target datums
+TEST(Tier4GeographyUtils, ValidSourceTargetDatum)
+{
+  const double height = 10.0;
+  const double latitude = 35.0;
+  const double longitude = 139.0;
+  
+  double converted_height = tier4_geography_utils::convert_height(height, latitude, longitude, "WGS84", "EGM2008");
+  
+  // As this is a conversion, just verify that it does not return the original height
+  // For precise testing, you'll need the exact expected values
+  EXPECT_NE(height, converted_height);
+}
+
+// Test case to verify invalid source and target datums
+TEST(Tier4GeographyUtils, InvalidSourceTargetDatum)
+{
+  const double height = 10.0;
+  const double latitude = 35.0;
+  const double longitude = 139.0;
+  
+  EXPECT_THROW(
+    tier4_geography_utils::convert_height(height, latitude, longitude, "INVALID1", "INVALID2"),
+    std::invalid_argument
+  );
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
