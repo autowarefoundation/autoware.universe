f0f9fb5cd635a44fef6683af5f205378bde3fd71
Maxime CLEMENT <78338830+maxime-clem@users.noreply.github.com>
2023-08-31 15:34:57 +0900

    fix typo

--- planning/behavior_path_planner/src/utils/drivable_area_expansion/expansion.cpp
+++ planning/behavior_path_planner/src/utils/drivable_area_expansion/expansion.cpp
@@ -59,7 +59,7 @@ polygon_t createExpansionPolygon(
   boost::geometry::buffer(
     base_ls, polygons, distance_strategy, strategy::side_straight(), strategy::join_miter(),
     strategy::end_flat(), strategy::point_square());
-  return polygons.empty() ? polyton_t{} : polygons.front();
+  return polygons.empty() ? polygon_t{} : polygons.front();
 }
 
 std::array<double, 3> calculate_arc_length_range_and_distance(
