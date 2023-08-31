75494214127467911d487a1add06298de3980b48
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-08-31 16:48:56 +0900

    Update planning/behavior_velocity_no_drivable_lane_module/src/manager.cpp
    Co-authored-by: Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>

--- planning/behavior_velocity_no_drivable_lane_module/src/manager.cpp
+++ planning/behavior_velocity_no_drivable_lane_module/src/manager.cpp
@@ -26,7 +26,7 @@ NoDrivableLaneModuleManager::NoDrivableLaneModuleManager(rclcpp::Node & node)
 : SceneModuleManagerInterface(node, getModuleName())
 {
   const std::string ns(getModuleName());
-  planner_param_.stop_margin = getOrDeclareParameter<double>(node, ns + ".stop_margin", 1.5);
+  planner_param_.stop_margin = getOrDeclareParameter<double>(node, ns + ".stop_margin");
   planner_param_.print_debug_info = getOrDeclareParameter<bool>(node, ns + ".print_debug_info", false);
 }
 
