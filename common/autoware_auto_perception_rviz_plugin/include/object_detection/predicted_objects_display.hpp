e836aefa7e37d64dc5b1e8d98f5f08f3e1cd593c
Owen-Liuyuxuan <uken.ryu@tier4.jp>
2023-09-01 16:16:54 +0800

    refactor(perception_rviz_plugin): use pthread to accelerate marker creation
    Signed-off-by: Owen-Liuyuxuan <uken.ryu@tier4.jp>

--- common/autoware_auto_perception_rviz_plugin/include/object_detection/predicted_objects_display.hpp
+++ common/autoware_auto_perception_rviz_plugin/include/object_detection/predicted_objects_display.hpp
@@ -28,6 +28,8 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <semaphore.h> 
+#include <pthread.h>
 
 namespace autoware
 {
@@ -45,6 +47,13 @@ public:
   using PredictedObjects = autoware_auto_perception_msgs::msg::PredictedObjects;
 
   PredictedObjectsDisplay();
+
+  // public methods for calling from detached pthreads. pthreads are detached from the "workingThread"
+  void pThreadJob(int rank);
+
+  // semaphores to control the starting and finishing of phthread tasks
+  sem_t* starting_semaphores;
+  sem_t* ending_semaphores;
 
 private:
   void processMessage(PredictedObjects::ConstSharedPtr msg) override;
@@ -96,11 +105,16 @@ private:
     return id_map.at(uuid);
   }
 
+  // createMarkers: create markers from the message with the single working process; activated when num_threads<=1;
   std::vector<visualization_msgs::msg::Marker::SharedPtr> createMarkers(
     PredictedObjects::ConstSharedPtr msg);
+  // Working thread is detached from the main to control the flow of messages
   void workerThread();
 
   void update(float wall_dt, float ros_dt) override;
+
+  // For pthreads to push to the same "tmp_maker" vector
+  void push_tmp_markers(std::vector<visualization_msgs::msg::Marker::SharedPtr> marker_ptrs);
 
   std::unordered_map<boost::uuids::uuid, int32_t, boost::hash<boost::uuids::uuid>> id_map;
   // std::unordered_map<boost::uuids::uuid, int32_t> id_map;
@@ -108,12 +122,33 @@ private:
   int32_t marker_id = 0;
   const int32_t PATH_ID_CONSTANT = 1e3;
 
+  // msg: latest message, passed from the main callback to workThread; 
   PredictedObjects::ConstSharedPtr msg;
+
+  // tmp_msg: latest message, maintained inside working threads and consumed by pthreads
+  PredictedObjects::ConstSharedPtr tmp_msg;
   bool consumed{false};
+
+  // mutex: mutex for accessing this->msg, handle conflicts between main process and the working thread;
   std::mutex mutex;
+
+  // tmp_mutex: mutex for accessing this->tmp_markers, handle conflicts between pthreads;
+  pthread_mutex_t tmp_marker_mutex;
+
+  // num_threads: number of threads going to be used, can be modifed on the fly in RVIZ parameters;
+  int num_threads;
+  // max_num_threads: number of threads to be created, hard-coded to be 8;
+  int max_num_threads;
+  // condition: condition variable for working thread to wait for main callback to get this->msg;
   std::condition_variable condition;
+  // marker: markers to be published, created by working thread and consumed by the "update" of main process; 
   std::vector<visualization_msgs::msg::Marker::SharedPtr> markers;
+  // tmp_markers: marker vectors being created by the pthreads, will be passed to marker when finished.
+  std::vector<visualization_msgs::msg::Marker::SharedPtr> tmp_markers;
+  // existing_marker_ids: marker ids that were maintained in the last frame.
   std::set<rviz_default_plugins::displays::MarkerID> existing_marker_ids;
+
+  rviz_common::properties::IntProperty * num_threads_property;
 };
 
 }  // namespace object_detection
--- common/autoware_auto_perception_rviz_plugin/src/object_detection/predicted_objects_display.cpp
+++ common/autoware_auto_perception_rviz_plugin/src/object_detection/predicted_objects_display.cpp
@@ -13,9 +13,11 @@
 // limitations under the License.
 
 #include <object_detection/predicted_objects_display.hpp>
-
+#include <rviz_common/properties/int_property.hpp>
 #include <memory>
 #include <set>
+#include <pthread.h>
+#include <semaphore.h> 
 
 namespace autoware
 {
@@ -23,29 +25,244 @@ namespace rviz_plugins
 {
 namespace object_detection
 {
+// PredictedObjectPThread: manager classes for pthreads
+class PredictedObjectPThread{
+  public:
+  PredictedObjectPThread(PredictedObjectsDisplay *_display,
+                         int _rank){
+    display = _display; // store the pointer of the main plugin class;
+    rank = _rank;
+  }
+  void start() {
+    pthread_create(&mThreadID, 0, PredictedObjectPThread::threadMethod, this);
+    pthread_detach(mThreadID); // keep pthread alive; avoid repeatly creating and destroying pthreads
+  };
+  static void *threadMethod(void *arg){
+    PredictedObjectPThread *_this = static_cast<PredictedObjectPThread *>(arg);
+    int rank = _this->rank;
+    sem_t* starting_semaphores = _this->display->starting_semaphores;
+    sem_t* ending_semaphores   = _this->display->ending_semaphores;
+    while (true){ // keep the pthread alive
+      sem_wait(&starting_semaphores[rank]); // wait for the signal to start
+      _this->display->pThreadJob(rank);
+      sem_post(&ending_semaphores[rank]); // signal the end of the job
+    }
+    return nullptr;    
+  };
+  pthread_t mThreadID;
+  int rank;
+  PredictedObjectsDisplay *display;
+};
+
 PredictedObjectsDisplay::PredictedObjectsDisplay() : ObjectPolygonDisplayBase("tracks")
 {
+  std::cout.rdbuf(out_file.rdbuf());
+  num_threads_property = new rviz_common::properties::IntProperty(
+    "Num Threads", 4, "Num of Threads to use", this); // by default, use 4 threads
+  max_num_threads = 8; // hard code the number of threads to be created
+  num_threads = max_num_threads; // this will be soon modified in the workerThread
   std::thread worker(&PredictedObjectsDisplay::workerThread, this);
   worker.detach();
 }
 
 void PredictedObjectsDisplay::workerThread()
 {
+  // PThread Initialization
+  std::vector<PredictedObjectPThread*> thread_handles;
+
+  starting_semaphores = (sem_t*)malloc(max_num_threads*sizeof(sem_t));
+  ending_semaphores   = (sem_t*)malloc(max_num_threads*sizeof(sem_t));
+  pthread_mutex_init(&tmp_marker_mutex, NULL);
+  for (int rank=0; rank < max_num_threads; rank++){
+    PredictedObjectPThread* pthread_object = new PredictedObjectPThread(this, rank);
+    thread_handles.push_back(pthread_object);
+    sem_init(&starting_semaphores[rank], 0, 0);  
+    sem_init(&ending_semaphores[rank], 0, 0);  
+    thread_handles[rank]->start();
+  }
+  
   while (true) {
     std::unique_lock<std::mutex> lock(mutex);
+    // waiting for the main process to get the message
     condition.wait(lock, [this] { return this->msg; });
 
-    auto tmp_msg = this->msg;
+    tmp_msg = this->msg;
     this->msg.reset();
 
     lock.unlock();
+    int N = tmp_msg->objects.size();
 
-    auto tmp_markers = createMarkers(tmp_msg);
+    // max_num_threads : Hard-coded for now. Define the pool of all threads.
+    // num_threads: dynamically change the number of threads based on the number of objects.
+    num_threads = std::min(max_num_threads, N/2);
+    num_threads = std::min(num_threads_property->getInt(), num_threads);
+    num_threads = std::max(1, num_threads);
+
+    if (num_threads > 1) {
+      update_id_map(tmp_msg);
+      tmp_markers.clear();
+      for (int rank=0; rank < num_threads; rank ++)
+      {
+        sem_post(&starting_semaphores[rank]);
+      }
+      for (int rank=0; rank < num_threads; rank++)
+      {
+        sem_wait(&ending_semaphores[rank]);
+      }
+    }
+    else {
+      tmp_markers = createMarkers(tmp_msg);
+    }
+
     lock.lock();
     markers = tmp_markers;
 
     consumed = true;
   }
+}
+
+void PredictedObjectsDisplay::push_tmp_markers(std::vector<visualization_msgs::msg::Marker::SharedPtr> marker_ptrs)
+{
+  pthread_mutex_lock(&tmp_marker_mutex);
+  for (auto marker_ptr : marker_ptrs)
+    tmp_markers.push_back(marker_ptr);
+  pthread_mutex_unlock(&tmp_marker_mutex);
+}
+
+void PredictedObjectsDisplay::pThreadJob(int rank)
+{
+  int length = tmp_msg->objects.size();
+  int num_object_per_thread = int(ceil(length / float(num_threads)));
+
+  int first_object_index = rank * num_object_per_thread;
+  int last_object_index = first_object_index + num_object_per_thread;
+  if (last_object_index > length) {last_object_index =length;}
+  std::vector<visualization_msgs::msg::Marker::SharedPtr> Thread_Markers;
+  for (int i = first_object_index; i < last_object_index; i++){
+    auto object = tmp_msg->objects[i];
+    // Get marker for shape
+    auto shape_marker = get_shape_marker_ptr(
+      object.shape, object.kinematics.initial_pose_with_covariance.pose.position,
+      object.kinematics.initial_pose_with_covariance.pose.orientation, object.classification,
+      get_line_width());
+    if (shape_marker) {
+      auto shape_marker_ptr = shape_marker.value();
+      shape_marker_ptr->header = tmp_msg->header;
+      shape_marker_ptr->id = uuid_to_marker_id(object.object_id);
+      Thread_Markers.push_back(shape_marker_ptr);
+    }
+
+    // Get marker for label
+    auto label_marker = get_label_marker_ptr(
+      object.kinematics.initial_pose_with_covariance.pose.position,
+      object.kinematics.initial_pose_with_covariance.pose.orientation, object.classification);
+    if (label_marker) {
+      auto label_marker_ptr = label_marker.value();
+      label_marker_ptr->header = tmp_msg->header;
+      label_marker_ptr->id = uuid_to_marker_id(object.object_id);
+      Thread_Markers.push_back(label_marker_ptr);
+    }
+
+    // Get marker for id
+    geometry_msgs::msg::Point uuid_vis_position;
+    uuid_vis_position.x = object.kinematics.initial_pose_with_covariance.pose.position.x - 0.5;
+    uuid_vis_position.y = object.kinematics.initial_pose_with_covariance.pose.position.y;
+    uuid_vis_position.z = object.kinematics.initial_pose_with_covariance.pose.position.z - 0.5;
+
+    auto id_marker =
+      get_uuid_marker_ptr(object.object_id, uuid_vis_position, object.classification);
+    if (id_marker) {
+      auto id_marker_ptr = id_marker.value();
+      id_marker_ptr->header = tmp_msg->header;
+      id_marker_ptr->id = uuid_to_marker_id(object.object_id);
+      Thread_Markers.push_back(id_marker_ptr);
+    }
+
+    // Get marker for pose with covariance
+    auto pose_with_covariance_marker =
+      get_pose_with_covariance_marker_ptr(object.kinematics.initial_pose_with_covariance);
+    if (pose_with_covariance_marker) {
+      auto pose_with_covariance_marker_ptr = pose_with_covariance_marker.value();
+      pose_with_covariance_marker_ptr->header = tmp_msg->header;
+      pose_with_covariance_marker_ptr->id = uuid_to_marker_id(object.object_id);
+      Thread_Markers.push_back(pose_with_covariance_marker_ptr);
+    }
+    // Get marker for velocity text
+    geometry_msgs::msg::Point vel_vis_position;
+    vel_vis_position.x = uuid_vis_position.x - 0.5;
+    vel_vis_position.y = uuid_vis_position.y;
+    vel_vis_position.z = uuid_vis_position.z - 0.5;
+    auto velocity_text_marker = get_velocity_text_marker_ptr(
+      object.kinematics.initial_twist_with_covariance.twist, vel_vis_position,
+      object.classification);
+    if (velocity_text_marker) {
+      auto velocity_text_marker_ptr = velocity_text_marker.value();
+      velocity_text_marker_ptr->header = tmp_msg->header;
+      velocity_text_marker_ptr->id = uuid_to_marker_id(object.object_id);
+      Thread_Markers.push_back(velocity_text_marker_ptr);
+    }
+
+    // Get marker for acceleration text
+    geometry_msgs::msg::Point acc_vis_position;
+    acc_vis_position.x = uuid_vis_position.x - 1.0;
+    acc_vis_position.y = uuid_vis_position.y;
+    acc_vis_position.z = uuid_vis_position.z - 1.0;
+    auto acceleration_text_marker = get_acceleration_text_marker_ptr(
+      object.kinematics.initial_acceleration_with_covariance.accel, acc_vis_position,
+      object.classification);
+    if (acceleration_text_marker) {
+      auto acceleration_text_marker_ptr = acceleration_text_marker.value();
+      acceleration_text_marker_ptr->header = tmp_msg->header;
+      acceleration_text_marker_ptr->id = uuid_to_marker_id(object.object_id);
+      Thread_Markers.push_back(acceleration_text_marker_ptr);
+    }
+
+    // Get marker for twist
+    auto twist_marker = get_twist_marker_ptr(
+      object.kinematics.initial_pose_with_covariance,
+      object.kinematics.initial_twist_with_covariance);
+    if (twist_marker) {
+      auto twist_marker_ptr = twist_marker.value();
+      twist_marker_ptr->header = tmp_msg->header;
+      twist_marker_ptr->id = uuid_to_marker_id(object.object_id);
+      Thread_Markers.push_back(twist_marker_ptr);
+    }
+
+    // Add marker for each candidate path
+    int32_t path_count = 0;
+    for (const auto & predicted_path : object.kinematics.predicted_paths) {
+      // Get marker for predicted path
+      auto predicted_path_marker =
+        get_predicted_path_marker_ptr(object.object_id, object.shape, predicted_path);
+      if (predicted_path_marker) {
+        auto predicted_path_marker_ptr = predicted_path_marker.value();
+        predicted_path_marker_ptr->header = tmp_msg->header;
+        predicted_path_marker_ptr->id =
+          uuid_to_marker_id(object.object_id) + path_count * PATH_ID_CONSTANT;
+        path_count++;
+        Thread_Markers.push_back(predicted_path_marker_ptr);
+      }
+    }
+
+    // Add confidence text marker for each candidate path
+    path_count = 0;
+    for (const auto & predicted_path : object.kinematics.predicted_paths) {
+      if (predicted_path.path.empty()) {
+        continue;
+      }
+      auto path_confidence_marker =
+        get_path_confidence_marker_ptr(object.object_id, predicted_path);
+      if (path_confidence_marker) {
+        auto path_confidence_marker_ptr = path_confidence_marker.value();
+        path_confidence_marker_ptr->header = tmp_msg->header;
+        path_confidence_marker_ptr->id =
+          uuid_to_marker_id(object.object_id) + path_count * PATH_ID_CONSTANT;
+        path_count++;
+        Thread_Markers.push_back(path_confidence_marker_ptr);
+      }
+    }
+  }
+  push_tmp_markers(Thread_Markers);
 }
 
 std::vector<visualization_msgs::msg::Marker::SharedPtr> PredictedObjectsDisplay::createMarkers(
