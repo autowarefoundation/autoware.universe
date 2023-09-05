4f83d0d2994f5979610db49efc1ca8d4d9f9d377
KAIPA SRI CHARAN <95237531+Kaipa-Charan@users.noreply.github.com>
2023-09-05 12:17:42 +0530

    Update cycle.hpp

--- common/tier4_perception_rviz_plugin/src/tools/cycle.hpp
+++ common/tier4_perception_rviz_plugin/src/tools/cycle.hpp
@@ -1,1 +1,23 @@
+#ifndef TOOLS__CYCLE_POSE_HPP_
+#define TOOLS__CYCLE_POSE_HPP_
+
 #include "interactive_object.hpp"
+
+namespace rviz_plugins
+{
+
+using autoware_auto_perception_msgs::msg::ObjectClassification;
+using autoware_auto_perception_msgs::msg::Shape;
+using dummy_perception_publisher::msg::Object;
+
+class PedestrianInitialPoseTool : public InteractiveObjectTool
+{
+public:
+  PedestrianInitialPoseTool();
+  void onInitialize() override;
+  [[nodiscard]] Object createObjectMsg() const override;
+};
+
+}  // namespace rviz_plugins
+
+#endif  // TOOLS__PEDESTRIAN_POSE_HPP_
