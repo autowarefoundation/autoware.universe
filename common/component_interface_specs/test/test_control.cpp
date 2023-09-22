7c3ead62cbccf62150e78aa93015939e9496d098
jack.song <jack.song@autocore.ai>
2023-09-22 11:30:58 +0800

    add interface specs test
    Signed-off-by: jack.song <jack.song@autocore.ai>

--- common/component_interface_specs/test/test_control.cpp
+++ common/component_interface_specs/test/test_control.cpp
@@ -5,28 +5,28 @@ TEST(controlinterface,interface)
 {
   {
     using control_interface::IsPaused;
-    IsPaused ispaused;    
+    IsPaused is_paused;    
     size_t depth=1;
-    EXPECT_EQ(ispaused.depth,depth);
-    EXPECT_EQ(ispaused.reliability,RMW_QOS_POLICY_RELIABILITY_RELIABLE);
-    EXPECT_EQ(ispaused.durability,RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
+    EXPECT_EQ(is_paused.depth,depth);
+    EXPECT_EQ(is_paused.reliability,RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+    EXPECT_EQ(is_paused.durability,RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
   }
 
   {
     using control_interface::IsStartRequested;
-    IsStartRequested isstartrequested;    
+    IsStartRequested is_start_requested;    
     size_t depth=1;
-    EXPECT_EQ(isstartrequested.depth,depth);
-    EXPECT_EQ(isstartrequested.reliability,RMW_QOS_POLICY_RELIABILITY_RELIABLE);
-    EXPECT_EQ(isstartrequested.durability,RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
+    EXPECT_EQ(is_start_requested.depth,depth);
+    EXPECT_EQ(is_start_requested.reliability,RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+    EXPECT_EQ(is_start_requested.durability,RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
   }
 
   {
     using control_interface::IsStopped;
-    IsStopped isstopped;    
+    IsStopped is_stopped;    
     size_t depth=1;
-    EXPECT_EQ(isstopped.depth,depth);
-    EXPECT_EQ(isstopped.reliability,RMW_QOS_POLICY_RELIABILITY_RELIABLE);
-    EXPECT_EQ(isstopped.durability,RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
+    EXPECT_EQ(is_stopped.depth,depth);
+    EXPECT_EQ(is_stopped.reliability,RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+    EXPECT_EQ(is_stopped.durability,RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
   }
 }
