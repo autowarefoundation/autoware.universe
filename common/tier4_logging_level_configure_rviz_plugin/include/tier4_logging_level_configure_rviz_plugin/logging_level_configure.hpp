d12f17284f39d24f18666e2a3a793d9bcadcb064
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-09-25 20:29:20 +0900

    change file names
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- common/tier4_logging_level_configure_rviz_plugin/include/tier4_logging_level_configure_rviz_plugin/logging_level_configure.hpp
+++ common/tier4_logging_level_configure_rviz_plugin/include/tier4_logging_level_configure_rviz_plugin/logging_level_configure.hpp
@@ -1,5 +1,20 @@
-#ifndef PLUGIN_HPP_
-#define PLUGIN_HPP_
+// Copyright 2023 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef TIER4_LOGGING_LEVEL_CONFIGURE_RVIZ_PLUGIN__LOGGING_LEVEL_CONFIGURE_HPP_
+#define TIER4_LOGGING_LEVEL_CONFIGURE_RVIZ_PLUGIN__LOGGING_LEVEL_CONFIGURE_HPP_
 
 #include <rviz_common/panel.hpp>
 #include <QPushButton>
@@ -50,4 +65,4 @@ private Q_SLOTS:
 
 } // namespace rviz_plugin
 
-#endif // PLUGIN_HPP_
+#endif // TIER4_LOGGING_LEVEL_CONFIGURE_RVIZ_PLUGIN__LOGGING_LEVEL_CONFIGURE_HPP_
