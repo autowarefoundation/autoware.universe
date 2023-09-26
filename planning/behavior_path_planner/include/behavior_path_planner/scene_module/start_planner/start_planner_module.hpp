ec373137b57dfc5de50603b43f4fc210ccfa8638
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2023-09-26 14:52:12 +0900

    add comment
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/behavior_path_planner/include/behavior_path_planner/scene_module/start_planner/start_planner_module.hpp
+++ planning/behavior_path_planner/include/behavior_path_planner/scene_module/start_planner/start_planner_module.hpp
@@ -136,6 +136,7 @@ private:
 
   std::unique_ptr<rclcpp::Time> last_route_received_time_;
   std::unique_ptr<rclcpp::Time> last_pull_out_start_update_time_;
+  // pose before approval of backward driving path
   std::unique_ptr<Pose> last_approved_pose_;
 
   // generate freespace pull out paths in a separate thread
