20aee3f88089f5a835cb304183bec8130ffbb61e
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2023-09-26 23:11:07 +0900

    delete waitApproval
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/behavior_path_planner/src/scene_module/start_planner/start_planner_module.cpp
+++ planning/behavior_path_planner/src/scene_module/start_planner/start_planner_module.cpp
@@ -669,8 +669,6 @@ void StartPlannerModule::updatePullOutStatus()
 void StartPlannerModule::updateStatusAfterBackwardDriving()
 {
   status_.back_finished = true;
-  // request start_planner approval
-  waitApproval();
   // To enable approval of the forward path, the RTC status is removed.
   removeRTCStatus();
   for (auto itr = uuid_map_.begin(); itr != uuid_map_.end(); ++itr) {
