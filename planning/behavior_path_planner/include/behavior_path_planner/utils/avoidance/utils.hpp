cc3e8ca8c288dc619fc9aa6f02a05b23bcc317a5
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2023-10-12 14:01:36 +0900

    fix(avoidance): expand detection area to prevent chattering (#5267)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/include/behavior_path_planner/utils/avoidance/utils.hpp
+++ planning/behavior_path_planner/include/behavior_path_planner/utils/avoidance/utils.hpp
@@ -166,7 +166,7 @@ std::vector<ExtendedPredictedObject> getSafetyCheckTargetObjects(
 std::pair<PredictedObjects, PredictedObjects> separateObjectsByPath(
   const PathWithLaneId & path, const std::shared_ptr<const PlannerData> & planner_data,
   const AvoidancePlanningData & data, const std::shared_ptr<AvoidanceParameters> & parameters,
-  DebugData & debug);
+  const bool is_running, DebugData & debug);
 }  // namespace behavior_path_planner::utils::avoidance
 
 #endif  // BEHAVIOR_PATH_PLANNER__UTILS__AVOIDANCE__UTILS_HPP_
