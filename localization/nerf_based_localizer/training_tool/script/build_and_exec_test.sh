ab27685cd81d22e5d93a678797d914d99ca7b125
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-10-17 21:47:02 +0900

    Updated test script
    Signed-off-by: Shintaro SAKODA <shintaro.sakoda@tier4.jp>

--- localization/nerf_based_localizer/training_tool/script/build_and_exec_test.sh
+++ localization/nerf_based_localizer/training_tool/script/build_and_exec_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
 set -eux
 
+ROOT_DIR=$(readlink -f $(dirname $0)/../)
 TRAIN_RESULT_DIR=$(readlink -f $1)
 DATASET_DIR=$(readlink -f $2)
 
-cd $(dirname $0)/../build/
+cd ${ROOT_DIR}
+cmake . -B build
+cmake --build build --config RelWithDebInfo -j8
 
-make -j $(nproc)
-
+cd ${ROOT_DIR}/build
 ./main test ${TRAIN_RESULT_DIR} ${DATASET_DIR}
