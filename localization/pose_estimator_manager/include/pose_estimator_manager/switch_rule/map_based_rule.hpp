ea6941f4132ea1436a768cf2bced165573870b3d
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-10-19 17:45:06 +0900

    add license description
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- localization/pose_estimator_manager/include/pose_estimator_manager/switch_rule/map_based_rule.hpp
+++ localization/pose_estimator_manager/include/pose_estimator_manager/switch_rule/map_based_rule.hpp
@@ -1,9 +1,23 @@
+// Copyright 2023 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include "pose_estimator_manager/pose_estimator_name.hpp"
-#include "pose_estimator_manager/switch_rule/ar_tag_position.hpp"
+#include "pose_estimator_manager/rule_helper/ar_tag_position.hpp"
+#include "pose_estimator_manager/rule_helper/eagleye_area.hpp"
 #include "pose_estimator_manager/switch_rule/base_switch_rule.hpp"
-#include "pose_estimator_manager/switch_rule/eagleye_area.hpp"
 
 #include <rclcpp/logger.hpp>
 
--- localization/pose_estimator_manager/test/map_based_rule.cpp
+++ localization/pose_estimator_manager/test/map_based_rule.cpp
@@ -1,7 +1,21 @@
+// Copyright 2023 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "pose_estimator_manager/switch_rule/map_based_rule.hpp"
 
-#include "pose_estimator_manager/switch_rule/eagleye_area.hpp"
-#include "pose_estimator_manager/switch_rule/grid_info.hpp"
+#include "pose_estimator_manager/rule_helper/eagleye_area.hpp"
+#include "pose_estimator_manager/rule_helper/grid_info.hpp"
 
 #include <lanelet2_extension/utility/message_conversion.hpp>
 
