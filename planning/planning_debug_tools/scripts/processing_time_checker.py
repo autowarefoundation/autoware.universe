e146c7d964f8b4c058397c5b55453a49f1571231
Takamasa Horibe <horibe.takamasa@gmail.com>
2023-10-23 15:08:09 +0900

    update readme
    Signed-off-by: Takamasa Horibe <horibe.takamasa@gmail.com>

--- planning/planning_debug_tools/scripts/processing_time_checker.py
+++ planning/planning_debug_tools/scripts/processing_time_checker.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 TIER IV, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import argparse
 import os
 import sys
@@ -72,13 +89,17 @@ def main(args=None):
     )
     args = parser.parse_args()
 
-    rclpy.init(args=cmd_args)  # Use the original command line arguments here
-    subscriber = ProcessingTimeSubscriber(
-        max_display_time=args.max_display_time, display_frequency=args.display_frequency
-    )
-    rclpy.spin(subscriber)
-    subscriber.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.init(args=cmd_args)  # Use the original command line arguments here
+        subscriber = ProcessingTimeSubscriber(
+            max_display_time=args.max_display_time, display_frequency=args.display_frequency
+        )
+        rclpy.spin(subscriber)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        subscriber.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
