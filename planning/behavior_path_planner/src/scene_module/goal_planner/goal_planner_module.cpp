0f5ea967c29a002f950b97de0dd9bafce7bc1260
Yuki Takagi <yuki.takagi@tier4.jp>
2023-10-23 19:21:46 +0900

    fix clang-format
    Signed-off-by: Yuki Takagi <yuki.takagi@tier4.jp>

--- planning/behavior_path_planner/src/scene_module/goal_planner/goal_planner_module.cpp
+++ planning/behavior_path_planner/src/scene_module/goal_planner/goal_planner_module.cpp
@@ -22,8 +22,8 @@
 #include "behavior_path_planner/utils/path_utils.hpp"
 #include "behavior_path_planner/utils/start_goal_planner_common/utils.hpp"
 #include "behavior_path_planner/utils/utils.hpp"
-#include "tier4_autoware_utils/math/unit_conversion.hpp"
 #include "tier4_autoware_utils/geometry/boost_polygon_utils.hpp"
+#include "tier4_autoware_utils/math/unit_conversion.hpp"
 
 #include <lanelet2_extension/utility/message_conversion.hpp>
 #include <lanelet2_extension/utility/query.hpp>
