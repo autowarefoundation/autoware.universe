b2be2b6b4370075eabecfeeef90f34bc3b835ef4
keiota <k3robot@gmail.com>
2023-11-02 12:06:57 +0900

    fix(test_objects_filtering): fix unittest of objects_filtering

--- planning/behavior_path_planner/test/test_objects_filtering.cpp
+++ planning/behavior_path_planner/test/test_objects_filtering.cpp
@@ -30,29 +30,37 @@ TEST(BehaviorPathPlanningObjectsFiltering, filterObjectsByVelocity)
     using autoware_auto_perception_msgs::msg::PredictedObjects;
     using behavior_path_planner::utils::path_safety_checker::filterObjectsByVelocity;
     
-    const PredictedObjects & predicted_objects;
-    const PredictedObjects expected_objects;
-    predicted_objects[0].kinematics.initial_twist_with_covariance.twist.linear.x = 1.0;
-    predicted_objects[0].kinematics.initial_twist_with_covariance.twist.linear.y = 1.0;
+    PredictedObjects predicted_objects;
+    PredictedObject predicted_object;
+    PredictedObjects expected_ans[2];
 
-    predicted_objects[1].kinematics.initial_twist_with_covariance.twist.linear.x = 0.5;
-    predicted_objects[1].kinematics.initial_twist_with_covariance.twist.linear.y = 0.5;
+
+    predicted_object.kinematics.initial_twist_with_covariance.twist.linear.x = 1.0;
+    predicted_object.kinematics.initial_twist_with_covariance.twist.linear.y = 1.0;
+
+    expected_ans[0].objects.push_back(predicted_object);
+    predicted_objects.objects.push_back(predicted_object);
+
+    predicted_object.kinematics.initial_twist_with_covariance.twist.linear.x = 0.5;
+    predicted_object.kinematics.initial_twist_with_covariance.twist.linear.y = 0.5;
+
+    expected_ans[1].objects.push_back(predicted_object);
+    predicted_objects.objects.push_back(predicted_object);
 
     // case function has boolian flag "remove_above_threshold" = true
-    const double velocity_threshold = 1.0; // eg 0.5
-    const bool remove_threshold = true; 
+    double velocity_threshold = 1.0; // eg 0.5
+    bool remove_threshold = true; 
 
-    expected_objects = {predicted_objects[1]};
-    const PredictedObjects ans;
+    PredictedObjects ans;
     ans = filterObjectsByVelocity(predicted_objects, velocity_threshold, remove_threshold);
-    EXPECT_EQ(ans, expected_objects);
-
+    EXPECT_EQ(ans, expected_ans[1]);
 
     // another case
-    // const double velocity_threshold = 1.0; // eg 0.5
-    // bool remove_threshold = false; 
+    remove_threshold = false; 
 
-    // expected_objects = [predicted_objects[1]];
-    // ans = filterObjectsByVelocity(predicted_objects, velocity_threshold, remove_threshold);
-    // EXPECT_EQ(ans, expected_objects);
+    ans = filterObjectsByVelocity(predicted_objects, velocity_threshold, remove_threshold);
+    EXPECT_EQ(ans, expected_ans[0]);
+
+
+
 }
