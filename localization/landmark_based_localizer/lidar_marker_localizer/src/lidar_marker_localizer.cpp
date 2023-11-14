219abe9cc62620c645666f0c024724548113bda9
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-11-14 21:52:22 +0900

    Removed include dir
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- localization/landmark_based_localizer/lidar_marker_localizer/src/lidar_marker_localizer.cpp
+++ localization/landmark_based_localizer/lidar_marker_localizer/src/lidar_marker_localizer.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "lidar_marker_localizer/lidar_marker_localizer.hpp"
+#include "lidar_marker_localizer.hpp"
 
 #include "localization_util/pose_array_interpolator.hpp"
 
