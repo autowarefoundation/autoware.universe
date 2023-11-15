5811c72aee3b0900fa5d3693e664235e0b210006
keiota <k3robot@gmail.com>
2023-11-15 19:19:45 +0900

    add(behavior_path_planner): add unittest for filterObjectsByPosition fuction

--- planning/behavior_path_planner/test/test_objects_filtering.cpp
+++ planning/behavior_path_planner/test/test_objects_filtering.cpp
@@ -73,19 +73,64 @@ TEST(BehaviorPathPlanningObjectsFiltering, filterObjectsByPosition)
     using autoware_auto_planning_msgs::msg::PathPoint;
     using geometry_msgs::msg::Pose;
 
+    std::vector<PathPointWithLaneId> sample_path_points;
+
+    float initial_pose_value = 0.0; 
+    float pose_increment = 1.0;
+    size_t point_sample = 10;
+    for (size_t idx = 0; idx < point_sample; ++idx) {
+        PathPoint point;
+        point.pose.position.x = std::exchange(initial_pose_value, initial_pose_value + pose_increment);
+        point.pose.position.y = 0.0;
+        point.pose.position.z = 0.0;
+        point.longitudinal_velocity_mps = 0.1;  // [m/s]
+        point.heading_rate_rps = 0.0;           // [rad/s]
+        point.is_final = (idx == point_sample - 1);
+
+        PathPointWithLaneId path_point_with_lane_id;
+        path_point_with_lane_id.point = point;
+        path_point_with_lane_id.lane_ids = std::vector<int64_t>();
+
+        sample_path_points.push_back(path_point_with_lane_id);
+    }
+
     PredictedObjects predicted_objects;
-    PredictedObject predicted_object;
+    PredictedObject predicted_object[2];
     PredictedObjects ans_objects;
-    PredictedObjects ans_object;
 
-    const std::vector<PathPointWithLaneId> path_points;
-    tier4_autoware_utils::createPoint(0, 0, 0);
-    
-    const geometry_msgs::msg::Point current_pose = tier4_autoware_utils::createPoint(1, 0, 0);
-    const double forward_distance = 1.0;
-    const double backward_distance = 1.0;
+    geometry_msgs::msg::Point current_pose = tier4_autoware_utils::createPoint(0, 0, 0);
+    double forward_distance = 1.0;
+    double backward_distance = 1.0;
 
-    
-    filterObjectsByPosition(predicted_objects, path_points, current_pose, forward_distance, backward_distance);
+    //no objects case
+    filterObjectsByPosition(predicted_objects, sample_path_points, current_pose, forward_distance, backward_distance);
     EXPECT_EQ(predicted_objects, ans_objects);
+
+
+    current_pose = tier4_autoware_utils::createPoint(0, 0, 0);
+    forward_distance = 5.0;
+    backward_distance = 5.0;
+    
+    //objects passed case
+    predicted_object[0].kinematics.initial_pose_with_covariance.pose.position.x = 2.0;
+    predicted_object[0].kinematics.initial_pose_with_covariance.pose.position.y = 0.0;
+    predicted_objects.objects.push_back(predicted_object[0]);
+    ans_objects.objects.push_back(predicted_object[0]);
+    
+    filterObjectsByPosition(predicted_objects, sample_path_points, current_pose, forward_distance, backward_distance);
+    EXPECT_EQ(predicted_objects, ans_objects);
+    
+
+    //objects didn't pass case
+    predicted_object[0].kinematics.initial_pose_with_covariance.pose.position.x = 6.0;
+    predicted_object[0].kinematics.initial_pose_with_covariance.pose.position.y = 0.0;
+    predicted_objects.objects.push_back(predicted_object[0]);
+    
+    filterObjectsByPosition(predicted_objects, sample_path_points, current_pose, forward_distance, backward_distance);
+    EXPECT_EQ(predicted_objects, ans_objects);
+    
+    //std::cout << "number of objects: " << predicted_objects.objects.size() << std::endl;
+
+
+    
 }
