4db999418e8c9f675bf75d72b25f71b09067e8fe
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2023-11-17 13:56:26 +0900

    fix(avoidance): fix avoidance path chattering (#1012)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
+++ planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
@@ -2312,8 +2312,7 @@ AvoidLineArray AvoidanceModule::findNewShiftLine(const AvoidLineArray & candidat
 
     // new shift points must exist in front of Ego
     // this value should be larger than -eps consider path shifter calculation error.
-    const double eps = 0.01;
-    if (candidate.start_longitudinal < -eps) {
+    if (candidate.start_idx < avoid_data_.ego_closest_path_index) {
       break;
     }
 
