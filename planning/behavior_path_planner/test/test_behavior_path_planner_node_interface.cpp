cd10b8bf38dd97edc6cb92fa176ed3f2a5983c70
satoshi-ota <satoshi.ota928@gmail.com>
2023-12-08 17:05:28 +0900

    refactor(bpp): remove side shift module
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/test/test_behavior_path_planner_node_interface.cpp
+++ planning/behavior_path_planner/test/test_behavior_path_planner_node_interface.cpp
@@ -50,7 +50,6 @@ std::shared_ptr<BehaviorPathPlannerNode> generateNode()
 
   std::vector<std::string> module_names;
   module_names.emplace_back("behavior_path_planner::DynamicAvoidanceModuleManager");
-  module_names.emplace_back("behavior_path_planner::SideShiftModuleManager");
   module_names.emplace_back("behavior_path_planner::LaneChangeRightModuleManager");
   module_names.emplace_back("behavior_path_planner::LaneChangeLeftModuleManager");
   module_names.emplace_back("behavior_path_planner::ExternalRequestLaneChangeRightModuleManager");
@@ -69,8 +68,7 @@ std::shared_ptr<BehaviorPathPlannerNode> generateNode()
      behavior_path_planner_dir + "/config/drivable_area_expansion.param.yaml",
      behavior_path_planner_dir + "/config/scene_module_manager.param.yaml",
      behavior_path_planner_dir + "/config/dynamic_avoidance/dynamic_avoidance.param.yaml",
-     behavior_path_planner_dir + "/config/lane_change/lane_change.param.yaml",
-     behavior_path_planner_dir + "/config/side_shift/side_shift.param.yaml"});
+     behavior_path_planner_dir + "/config/lane_change/lane_change.param.yaml"});
 
   return std::make_shared<BehaviorPathPlannerNode>(node_options);
 }
