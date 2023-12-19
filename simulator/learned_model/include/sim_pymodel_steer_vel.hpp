9e5c33cd0d38083f46756a299bff966e8c21f615
Tomas Nagy <nagytom4@fel.cvut.cz>
2023-12-19 11:18:44 +0100

    Changes from Maxime

--- simulator/learned_model/include/sim_pymodel_steer_vel.hpp
+++ simulator/learned_model/include/sim_pymodel_steer_vel.hpp
@@ -5,11 +5,15 @@
 #include <pybind11/stl.h>
 namespace py = pybind11;
 
-class SimPymodelSteerVel {
-  public:             
-    int test_variable = 123;  
+class SimPymodelSteerVel
+{
+public:
+  int test_variable = 123;
+  SimPymodelSteerVel()
+  {
     py::scoped_interpreter guard{};
-    py::print("Testing py::print LIB"); // use the Python API
+    py::print("Testing py::print LIB");  // use the Python API
+  }
 };
 
 #endif  // LEARNED_MODEL__SIM_PYMODEL_STEER_VEL_HPP_
