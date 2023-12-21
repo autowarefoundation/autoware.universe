483c243060c97d5d0e06d1aebb6d33b3d38a0e44
Tao Zhong <55872497+tzhong518@users.noreply.github.com>
2023-12-21 23:08:33 +0900

    Update perception/traffic_light_occlusion_predictor/src/nodelet.cpp
    Co-authored-by: Yusuke Muramatsu <yukke42@users.noreply.github.com>

--- perception/traffic_light_occlusion_predictor/src/nodelet.cpp
+++ perception/traffic_light_occlusion_predictor/src/nodelet.cpp
@@ -156,7 +156,7 @@ void TrafficLightOcclusionPredictorNodelet::syncCallback(
   size_t predicted_num = out_msg_.signals.size();
 
   for (size_t i = 0; i < occlusion_ratios.size(); i++) {
-    out_msg_.signals.push_back(in_signal_msg->signals[i]);
+    out_msg_.signals.push_back(in_signal_msg->signals.at(i));
 
     if (occlusion_ratios[i] >= config_.max_occlusion_ratio) {
       traffic_light_utils::setSignalUnknown(out_msg_.signals.at(predicted_num + i));
