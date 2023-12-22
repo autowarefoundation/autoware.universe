2e53ec511a0a146c6163b4a57d4a9a4dbd4c76a2
Zhe Shen <lucidshenzhe@gmail.com>
2023-12-22 17:31:52 +0900

    change the include from other .cpp or .hpp (not finished...)
    Signed-off-by: Zhe Shen <lucidshenzhe@gmail.com>

--- planning/behavior_velocity_planner_common/include/behavior_velocity_planner_common/utilization/arc_lane_util.hpp
+++ planning/behavior_velocity_planner_common/include/behavior_velocity_planner_common/utilization/arc_lane_util.hpp
@@ -15,7 +15,7 @@
 #ifndef BEHAVIOR_VELOCITY_PLANNER_COMMON__UTILIZATION__ARC_LANE_UTIL_HPP_
 #define BEHAVIOR_VELOCITY_PLANNER_COMMON__UTILIZATION__ARC_LANE_UTIL_HPP_
 
-#include <behavior_velocity_planner_common/utilization/boost_geometry_helper.hpp>
+#include "tier4_autoware_utils/geometry/boost_polygon_utils.hpp"
 #include <tier4_autoware_utils/geometry/geometry.hpp>
 
 #include <autoware_auto_planning_msgs/msg/path_with_lane_id.hpp>
