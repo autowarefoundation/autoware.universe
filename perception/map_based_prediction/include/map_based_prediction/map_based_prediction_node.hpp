69e59fe62ce9c61f99663393f5900af2f5a1c0f3
Daniel Sanchez <danielsanchezaran@gmail.com>
2023-12-22 15:00:52 +0900

    Delete debug print
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- perception/map_based_prediction/include/map_based_prediction/map_based_prediction_node.hpp
+++ perception/map_based_prediction/include/map_based_prediction/map_based_prediction_node.hpp
@@ -346,14 +346,12 @@ private:
       }
       double v_curvature_max = std::sqrt(max_lateral_accel_abs / std::max(curvature, 1.0E-5));
       // v_curvature_max = std::max(v_curvature_max, min_curve_velocity);
-      min_curv_thing = std::min(v_curvature_max, min_curv_thing);
       if (trajectory.at(i).longitudinal_velocity_mps > v_curvature_max) {
         std::cerr << "False Min curvature thing " << min_curv_thing << "\n";
         std::cerr << "Velocity " << trajectory.at(i).longitudinal_velocity_mps << "\n";
         return false;
       }
     }
-    std::cerr << "True Min curvature thing " << min_curv_thing << "\n";
     std::cerr << "Velocity " << trajectory.back().longitudinal_velocity_mps << "\n";
 
     return true;
