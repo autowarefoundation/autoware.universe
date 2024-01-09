1c1d47c66bbd85833f6e0c8acf627a21d5cf8eed
Taekjin LEE <taekjin.lee@tier4.jp>
2024-01-09 15:14:23 +0900

    feat(autoware_auto_perception_rviz_plugin): visualize object heading
    Signed-off-by: Taekjin LEE <taekjin.lee@tier4.jp>

--- common/autoware_auto_perception_rviz_plugin/src/object_detection/object_polygon_detail.cpp
+++ common/autoware_auto_perception_rviz_plugin/src/object_detection/object_polygon_detail.cpp
@@ -327,158 +327,103 @@ void calc_bounding_box_line_list(
   const autoware_auto_perception_msgs::msg::Shape & shape,
   std::vector<geometry_msgs::msg::Point> & points)
 {
+  const double length_half = shape.dimensions.x / 2.0;
+  const double width_half = shape.dimensions.y / 2.0;
+  const double height_half = shape.dimensions.z / 2.0;
   geometry_msgs::msg::Point point;
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
 
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
+  // bounding box corner points
+  // top and bottom surface, clockwise
+  const double corner_points[8][3] = {
+    {length_half, width_half, height_half},   {length_half, -width_half, height_half},
+    {-length_half, -width_half, height_half},  {-length_half, width_half, height_half},
+    {length_half, width_half, -height_half},  {length_half, -width_half, -height_half},
+    {-length_half, -width_half, -height_half}, {-length_half, width_half, -height_half},
+  }; 
+  const int bounding_box_pairs[12][2] = {
+    {0, 1}, {1, 2}, {2, 3}, {3, 0}, 
+    {4, 5}, {5, 6}, {6, 7}, {7, 4}, 
+    {0, 4}, {1, 5}, {2, 6}, {3, 7},
+  };
+  for (int i = 0; i < 12; ++i) {
+    point.x = corner_points[bounding_box_pairs[i][0]][0];
+    point.y = corner_points[bounding_box_pairs[i][0]][1];
+    point.z = corner_points[bounding_box_pairs[i][0]][2];
+    points.push_back(point);
+    point.x = corner_points[bounding_box_pairs[i][1]][0];
+    point.y = corner_points[bounding_box_pairs[i][1]][1];
+    point.z = corner_points[bounding_box_pairs[i][1]][2];
+    points.push_back(point);
+  }
 
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  // up surface
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  // down surface
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
+  // direction triangle
+  const double triangle_size_half = shape.dimensions.y / 1.4;
+  const double triangle_points[6][3] = {
+    {length_half, 0, height_half}, {length_half - triangle_size_half, width_half, height_half}, 
+    {length_half - triangle_size_half, -width_half, height_half},
+    {length_half, 0, -height_half}, {length_half - triangle_size_half, width_half, -height_half}, 
+    {length_half - triangle_size_half, -width_half, -height_half},
+  };
+  const int triangle_pairs[6][2] = {
+    {0, 1}, {1, 2}, {0, 2}, 
+    {3, 4}, {4, 5}, {3, 5},
+  };
+  for (int i = 0; i < 6; ++i) {
+    point.x = triangle_points[triangle_pairs[i][0]][0];
+    point.y = triangle_points[triangle_pairs[i][0]][1];
+    point.z = triangle_points[triangle_pairs[i][0]][2];
+    points.push_back(point);
+    point.x = triangle_points[triangle_pairs[i][1]][0];
+    point.y = triangle_points[triangle_pairs[i][1]][1];
+    point.z = triangle_points[triangle_pairs[i][1]][2];
+    points.push_back(point);
+  }
 }
 
 void calc_2d_bounding_box_bottom_line_list(
   const autoware_auto_perception_msgs::msg::Shape & shape,
   std::vector<geometry_msgs::msg::Point> & points)
 {
+  const double length_half = shape.dimensions.x / 2.0;
+  const double width_half = shape.dimensions.y / 2.0;
+  const double height_half = shape.dimensions.z / 2.0;
   geometry_msgs::msg::Point point;
+
   // down surface
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
+  point.x = length_half;
+  point.y = width_half;
+  point.z = -height_half;
   points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-
-  point.x = shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
-  points.push_back(point);
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
+  point.x = -length_half;
+  point.y = width_half;
+  point.z = -height_half;
   points.push_back(point);
 
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
+  point.x = length_half;
+  point.y = width_half;
+  point.z = -height_half;
   points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
+  point.x = length_half;
+  point.y = -width_half;
+  point.z = -height_half;
   points.push_back(point);
 
-  point.x = shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
+  point.x = -length_half;
+  point.y = width_half;
+  point.z = -height_half;
   points.push_back(point);
-  point.x = -shape.dimensions.x / 2.0;
-  point.y = -shape.dimensions.y / 2.0;
-  point.z = -shape.dimensions.z / 2.0;
+  point.x = -length_half;
+  point.y = -width_half;
+  point.z = -height_half;
+  points.push_back(point);
+
+  point.x = length_half;
+  point.y = -width_half;
+  point.z = -height_half;
+  points.push_back(point);
+  point.x = -length_half;
+  point.y = -width_half;
+  point.z = -height_half;
   points.push_back(point);
 }
 
