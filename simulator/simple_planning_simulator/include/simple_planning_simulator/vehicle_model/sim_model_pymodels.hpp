0f4715c54e08d44e0e69585b2d10d5c99985cbae
Tomas Nagy <nagytom4@fel.cvut.cz>
2024-01-18 18:30:18 +0100

    Add python model to simulation models

--- simulator/simple_planning_simulator/include/simple_planning_simulator/vehicle_model/sim_model_pymodels.hpp
+++ simulator/simple_planning_simulator/include/simple_planning_simulator/vehicle_model/sim_model_pymodels.hpp
@@ -16,6 +16,7 @@
 #define SIMPLE_PLANNING_SIMULATOR__VEHICLE_MODEL__SIM_MODEL_PYMODELS_HPP_
 
 #include "simple_planning_simulator/vehicle_model/sim_model_interface.hpp"
+#include "pymodel_interconnected_model.hpp"
 
 #include <Eigen/Core>
 #include <Eigen/LU>
@@ -60,25 +61,23 @@ public:
   ~SimModelPymodels() = default;
 
 private:
-  const double MIN_TIME_CONSTANT;  //!< @brief minimum time constant   --> TODO what is this?
+  const double MIN_TIME_CONSTANT;  //!< @brief minimum time constant
 
   /* 
   Specify string names for states and inputs. So we can automatically map states and 
-  inputs of this model to states and inputs of the python submodels.
-  In order not to compare strings each iteration, we compute mappings between the states 
-  and inputs of this model and python submodels.
+  inputs of this model to states and inputs of the python model.
   */
-  // const char* STATE_NAMES[5] = {"POS_X", "POS_Y", "YAW", "VX", "STEER"};
-  // const char* INPUT_NAMES[2] = {"VX_DES", "STEER_DES"};
 
-  /* 
-  Index means state or input in this model. First 5 indexes are states, last 2 indexes are inputs.
-  ----------------------------  {      STATES      }{INPUTS} */
-  // int MAP_TO_PYSTEER_INPUT[7] = {-1, -1, -1, -1, -1, -1, -1};  // <<- Value means index of the input in python steer model. -1 means not used.
-  /* 
-  Index means state in this model.
-  -------------------------------  {      STATES      }*/
-  // int MAP_FROM_PYSTEER_OUTPUT[5] = {-1, -1, -1, -1, -1};  // <<- Value means index of the output in python steer model. -1 means not used.
+  std::vector<char*> state_names = {(char*)"POS_X", 
+                                    (char*)"POS_Y", 
+                                    (char*)"YAW", 
+                                    (char*)"VX", 
+                                    (char*)"STEER"
+                                    };
+
+  std::vector<char*> input_names = {(char*)"VX_DES", 
+                                    (char*)"STEER_DES"
+                                    };
 
   enum IDX {
     X = 0,
@@ -92,6 +91,7 @@ private:
     STEER_DES,
   };
 
+  InterconnectedModel vehicle;
   // py::scoped_interpreter guard{};
   
 
@@ -112,26 +112,7 @@ private:
   const double
     steer_time_constant_;  //!< @brief time constant for 1D model of angular-velocity dynamics
   const double steer_dead_band_;  //!< @brief dead band for steering angle [rad]
-
-
-  //PyObject *steer_base_model;
-
-  // int num_states_steering;
-  // int num_inputs_steering;
-
-  // Create vectors to store system input and state (C++)
-  // Eigen::VectorXd state_steering;
-  // Eigen::VectorXd input_steering;
-
-  // Create vectors to store system input and state (python)
-  // PyObject *py_state_steering, *py_input_steering;
-
-  // std::vector<double> state_steering;
-  // std::vector<double> input_steering;
-
-  // py::object steering_module_class;
   
-
   /**
    * @brief set queue buffer for input command
    * @param [in] dt delta time
