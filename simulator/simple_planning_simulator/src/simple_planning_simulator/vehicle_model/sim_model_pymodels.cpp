e697eea25284db4f4f1855e99b3545bb034a8bec
Tomas Nagy <nagytom4@fel.cvut.cz>
2024-01-18 18:30:18 +0100

    Add python model to simulation models

--- simulator/simple_planning_simulator/src/simple_planning_simulator/vehicle_model/sim_model_pymodels.cpp
+++ simulator/simple_planning_simulator/src/simple_planning_simulator/vehicle_model/sim_model_pymodels.cpp
@@ -15,13 +15,8 @@
 #include "simple_planning_simulator/vehicle_model/sim_model_pymodels.hpp"
 
 #include <algorithm>
-// #include <Python.h>
-// #include <dlfcn.h>
-// #include <pybind11/embed.h>
-// #include <pybind11/stl.h>
-// namespace py = pybind11;
 
-#include "sim_pymodel_steer_vel.hpp"
+#include "pymodel_interconnected_model.hpp"
 
 SimModelPymodels::SimModelPymodels(
   double vx_lim, double steer_lim, double vx_rate_lim, double steer_rate_lim, double wheelbase,
@@ -41,77 +36,52 @@ SimModelPymodels::SimModelPymodels(
   steer_dead_band_(steer_dead_band)
 {
   
-  // Initialize python library
-  // dlopen("libpython3.10.so", RTLD_GLOBAL | RTLD_NOW); // Manually load libpython3.10.so as we need it for python.h.
-  /*
-  More about the line above here:
-    https://stackoverflow.com/questions/60719987/embedding-python-which-uses-numpy-in-c-doesnt-work-in-library-dynamically-loa
-    https://mail.python.org/pipermail/new-bugs-announce/2008-November/003322.html
-    https://stackoverflow.com/questions/67891197/ctypes-cpython-39-x86-64-linux-gnu-so-undefined-symbol-pyfloat-type-in-embedd
-    https://man7.org/linux/man-pages/man3/dlopen.3.html
-  */
- // start the interpreter and keep it alive
+  // TODO this should be in config file not hardcoded here
+  // Think of a way how to differentiate between "simple" model and "base + error" model
+  std::vector<std::tuple<char*, char*, char*>> model_desc = {
+    {
+      (char*)"control_analysis_pipeline.model.base_model.kinematic_bicycle_steer_vel",
+      (char*)nullptr,
+      (char*)"KinematicBicycleSteerVel"
+    },
+    // {
+    //   (char*)"control_analysis_pipeline.model.base_model.base_model_simple_steering_hysteresis",
+    //   (char*)"$HOME/f1tenth/ws_testing/base_model_save",
+    //   (char*)"SimpleSteeringHyst"
+    // },
+    {
+      (char*)"control_analysis_pipeline.model.base_model.base_model_simple_velocity",
+      (char*)nullptr,
+      (char*)"SimpleVelocity"
+    }
+  };
   
-  // char *model_save_path = (char*)"/home/tomas/f1tenth/ws_testing/base_model_save"; 
-
-  // // Import python module
-  // py::module_ steering_model_module = py::module_::import("control_analysis_pipeline.model.base_model.base_model_simple_steering_hysteresis");
-
-  // steering_module_class = steering_model_module.attr("SimpleSteeringHyst")(nullptr);
-
-
-  // // TODO call function to get model signal names
-  // // From the names create a mapping table for correctly pass arguments to the python models
-  // py::object sig_state_names = steering_module_class.attr("get_sig_state_names")();
-
-  // char *my_result;
-  // PyObject *temp_bytes = PyUnicode_AsEncodedString(PyList_GetItem(sig_state_names.ptr(), 0), "UTF-8", "strict");
-  // my_result = PyBytes_AS_STRING(temp_bytes);
-  // my_result = strdup(my_result);
-  // Py_DECREF(temp_bytes);
-
-  // // wcstombs(buffer, PyList_GetItem(py_sig_state_names, 0), sizeof(buffer));
-
-  // // int len_ = PyList_Size(py_sig_state_names);
-  // std::cout << "STATE NAMES:  -" <<  my_result << "-" << std::endl;
-  // // std::cout << "STEER:  " << len_ << std::endl;
-  // // std::cout << "STEER:  " << strcmp(my_result, "STEER") << std::endl;
-  // // std::cout << "STEER:  " << strcmp(my_result, STATE_NAMES[4]) << std::endl;
-  // // std::cout << "STEERR:  " << strcmp(my_result, "STEERR") << std::endl;
-
-
-  // // Load model params
-  // py::object load_params_succ = steering_module_class.attr("load_params")(model_save_path);
-
-
-  // // Reset the python model after loading the parameter file
-  // steering_module_class.attr("reset")();
+  vehicle.addSubmodel(model_desc[0]);
+  vehicle.addSubmodel(model_desc[1]); 
   
 
-  // // Set number model states and inputs
-  // num_states_steering = 1;
-  // num_inputs_steering = 1;
-
-  // // Initialize state and input vectors
-  // for (int i = 0; i < num_inputs_steering; i++){
-  //   input_steering.push_back(0.0);
-  // }
-
-  // for (int i = 0; i < num_states_steering; i++){
-  //   state_steering.push_back(0.0);
-  // }
 
 
-  // std::vector<double> state_steering(num_states_steering, 0);
-  // std::vector<double> input_steering(num_inputs_steering, 0);
+  std::vector<std::tuple<char*, char*, char*>> error_model_desc = {
+    {
+      (char*)"control_analysis_pipeline.model.base_model.base_model_simple_steering_hysteresis",
+      (char*)"$HOME/f1tenth/ws_testing/base_model_save",
+      (char*)"SimpleSteeringHyst"
+    },
+    {
+      (char*)"control_analysis_pipeline.model.error_model.error_debug_model",
+      (char*)nullptr,
+      (char*)"ErrorDebugModel"
+    },
+  };
   
-  // std::cout << action_delayed[0]<< std::endl;
- 
-  SimPymodelSteerVel test_;
-  std::cout << "----->  " << test_.test_variable << std::endl;
+  // vehicle.AddSubmodel(model_desc[1]);
+  vehicle.addSubmodelBaseError(error_model_desc[0], error_model_desc[1]);
 
 
-  // py::print("Testing py::print"); // use the Python API
+
+
+  vehicle.generateConnections(input_names, state_names);
 
   std::cout << dt << std::endl;
   std::cout << "OKAAAAAYYYYYYY" << std::endl;
@@ -151,45 +121,19 @@ double SimModelPymodels::getSteer()
 }
 void SimModelPymodels::update(const double & dt)
 {
-  Eigen::VectorXd delayed_input = Eigen::VectorXd::Zero(dim_u_);
-  //std::cout << dt << std::endl;
+  // Eigen::VectorXd to std::vector<double> for model input
+  std::vector<double> vehicle_input_(input_.data(), input_.data() + input_.size());
 
-  // Model the delay of speed
-  vx_input_queue_.push_back(input_(IDX_U::VX_DES));
-  delayed_input(IDX_U::VX_DES) = vx_input_queue_.front();
-  vx_input_queue_.pop_front();
+  // Eigen::VectorXd to std::vector<double> for model state
+  std::vector<double> new_state(state_.data(), state_.data() + state_.size());
+  // set model state
+  vehicle.initState(new_state);
 
-  // Simulate the system (car kinematics and dynamics)
-  // do not use deadzone_delta_steer (Steer IF does not exist in this model)
-  updateRungeKutta(dt, delayed_input);
+  // model forward
+  std::vector<double> vehicle_state_ = vehicle.updatePymodel(vehicle_input_);
 
-  // Simulate the steering system using python model
-
-  // TODO: define inputs to the model in a more general way
-  /*
-  Following inputs to the steering system can be used:
-
-    - IDX_U::STEER_DES
-    - IDX::VX
-  */
-  // input_steering[0] = input_(IDX_U::STEER_DES);
-  
-
-  // // Call forward() of the python model
-  // py::tuple res = steering_module_class.attr("forward")(state_steering, input_steering);
-  
-  // std::vector<double> next_state = res[0].cast<std::vector<std::vector<double>>>()[0];
-  // state_steering[0] = next_state[0];
-  // std::vector<double> action_delayed = res[1].cast<std::vector<std::vector<double>>>()[0];
-  // std::cout << next_state[0]<< std::endl;
-  
-  // TODO: define outputs of the model in a more general way
-  /*
-  Following outputs from the steering system can be used:
-    - IDX::STEER
-  */
-  // state_(IDX::STEER) = next_state[0];
-  state_(IDX::STEER) = 0;
+  // std::vector<double> to Eigen::VectorXd
+  for (size_t i = 0; i < vehicle_state_.size(); i++) state_[i] = vehicle_state_[i];
 
   // Calculate
   current_ax_ = (input_(IDX_U::VX_DES) - prev_vx_) / dt;
@@ -199,6 +143,7 @@ void SimModelPymodels::update(const double & dt)
 Eigen::VectorXd SimModelPymodels::calcModel(
   const Eigen::VectorXd & state, const Eigen::VectorXd & input)
 {
+  // Not used for this model -> we can get rid of this later
   auto sat = [](double val, double u, double l) { return std::max(std::min(val, u), l); };
 
   const double vx = sat(state(IDX::VX), vx_lim_, -vx_lim_);
@@ -210,10 +155,10 @@ Eigen::VectorXd SimModelPymodels::calcModel(
 
 
   Eigen::VectorXd d_state = Eigen::VectorXd::Zero(dim_x_);
-  d_state(IDX::X) = vx * cos(yaw);
-  d_state(IDX::Y) = vx * sin(yaw);
-  d_state(IDX::YAW) = vx * std::tan(steer) / wheelbase_;
-  d_state(IDX::VX) = vx_rate;
+  d_state(IDX::X) = 0 * vx * cos(yaw);
+  d_state(IDX::Y) = 0 * vx * sin(yaw);
+  d_state(IDX::YAW) = 0 * vx * std::tan(steer) / wheelbase_;
+  d_state(IDX::VX) = 0 * vx_rate;
   d_state(IDX::STEER) = 0.0;  //Use python models to update steer
 
   return d_state;
