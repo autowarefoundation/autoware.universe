767f045c797f89cb33ac43a033916af4c9cccfab
Tomas Nagy <nagytom4@fel.cvut.cz>
2024-01-30 14:22:45 +0100

    Split implementation between .hpp and .cpp files

--- simulator/learned_model/include/learned_model/pymodel_interconnected_model.hpp
+++ simulator/learned_model/include/learned_model/pymodel_interconnected_model.hpp
@@ -51,76 +51,28 @@ public:
   }
 
 private:
-  std::vector<int> createConnectionsMap(
-    std::vector<char *> connection_names_1, std::vector<char *> connection_names_2)
-  {
-    std::vector<int> connection_map;
-    // index in "connection_map" is index in "connection_names_2" and value in "connection_map" is
-    // index in "connection_names_1"
-    for (auto name_2 : connection_names_2) {
-      int mapped_idx = -1;  // -1 means that we cannot create a connection between some signals
-      for (size_t NAME_1_IDX = 0; NAME_1_IDX < connection_names_1.size(); NAME_1_IDX++) {
-        if (strcmp(name_2, connection_names_1[NAME_1_IDX]) == 0) {  // 0 means strings are the same
-                                                                    // and we can create connection
-          mapped_idx = NAME_1_IDX;
-          break;
-        }
-      }
-      connection_map.push_back(mapped_idx);
-    }
-    return connection_map;
-  }
-
   /**
    * @brief create a mapping between vector of signal input names from PSIM to vector of signals
    * @param [in] in_names vector of signal input names from PSIM
    */
-  void mapInputs(std::vector<char *> in_names)
-  {
-    // index in "map_in_to_sig_vec" is index in "in_names" and value in "map_in_to_sig_vec" is index
-    // in "signals_vec_names"
-    map_in_to_sig_vec = createConnectionsMap(signals_vec_names, in_names);
-  }
+  void mapInputs(std::vector<char *> in_names);
 
   /**
    * @brief create a mapping between vector of signal output names from PSIM to vector of signals
    * @param [in] out_names vector of signal output names from PSIM
    */
-  void mapOutputs(std::vector<char *> out_names)
-  {
-    // index in "map_sig_vec_to_out" is index in "out_names" and value in "map_sig_vec_to_out" is
-    // index in "signals_vec_names"
-    map_sig_vec_to_out = createConnectionsMap(signals_vec_names, out_names);
-  }
+  void mapOutputs(std::vector<char *> out_names);
 
   /**
    * @brief add unique names to the vector of signal names
    * @param [in] names vector of signal names
    */
-  void addNamesToSigVec(const std::vector<char *> & names)
-  {
-    // Check if the name is already in the vector. If not add it.
-    for (char * name : names) {
-      if (
-        std::find(signals_vec_names.begin(), signals_vec_names.end(), name) ==
-        signals_vec_names.end()) {
-        signals_vec_names.push_back(name);
-      }
-    }
-  }
+  void addNamesToSigVec(const std::vector<char *> & names);
 
   /**
    * @brief create of signal names from all sub-models and PSIM signal names
    */
-  void getSignalNames(std::vector<char *> in_names, std::vector<char *> out_names)
-  {
-    addNamesToSigVec(in_names);
-    addNamesToSigVec(out_names);
-    for (auto & submodel : submodels) {
-      addNamesToSigVec(submodel->getInputNames());
-      addNamesToSigVec(submodel->getStateNames());
-    }
-  }
+  void getSignalNames(std::vector<char *> in_names, std::vector<char *> out_names);
 
 public:
   /**
@@ -128,108 +80,32 @@ public:
    * @param [in] in_names string names of inputs available from PSIM
    * @param [in] out_names string names of outputs required by PSIM
    */
-  void generateConnections(std::vector<char *> in_names, std::vector<char *> out_names)
-  {
-    // Create vector of signal names
-    getSignalNames(in_names, out_names);
-    num_signals = signals_vec_names.size();
-    // Init vector of signal values
-    for (int i = 0; i < num_signals; i++) model_signals_vec.push_back(0);
-
-    // For every sub-model create mapping from vector of signals to inputs and outputs
-    for (auto & submodel : submodels) {
-      submodel->mapInputs(signals_vec_names);
-      submodel->mapOutputs(signals_vec_names);
-    }
-
-    // Create mapping from vector of signals to inputs and outputs of PSIM
-    mapInputs(in_names);
-    mapOutputs(out_names);
-  }
+  void generateConnections(std::vector<char *> in_names, std::vector<char *> out_names);
 
   /**
    * @brief add a sub-model consisting of base + error model
    * @param [in] submodel_desc descriptor of the sub-model
    */
-  void addSubmodel(std::tuple<std::string, std::string, std::string> submodel_desc)
-  {
-    const auto [lib_path, param_path, class_name] = submodel_desc;
-    auto new_model = new PymodelSimpleModel(lib_path, param_path, class_name);
-    submodels.push_back(std::unique_ptr<PymodelSimpleModel>(new_model));
-  }
+  void addSubmodel(std::tuple<std::string, std::string, std::string> submodel_desc);
 
   /**
    * @brief set a new model state if it was changed using PSIM interface (mainly position and
    * orientation)
    * @param [in] new_state new state set by PSIM
    */
-  void initState(std::vector<double> new_state)
-  {
-    bool state_changed_externally = false;
-
-    // Check if some state was changed externally
-    for (size_t PSIM_STATE_IDX = 0; PSIM_STATE_IDX < new_state.size(); PSIM_STATE_IDX++) {
-      if (
-        abs(model_signals_vec[map_sig_vec_to_out[PSIM_STATE_IDX]] - new_state[PSIM_STATE_IDX]) >
-        1e-6) {
-        state_changed_externally = true;
-        break;
-      }
-    }
-
-    if (state_changed_externally) {
-      // Reinitialize model
-      std::cout << "Reseting model" << std::endl;
-
-      // Currently initializing model to zero -> TODO find a way how to initialize them to some
-      // other default values
-      std::fill(model_signals_vec.begin(), model_signals_vec.end(), 0.0);
-
-      for (size_t PSIM_STATE_IDX = 0; PSIM_STATE_IDX < new_state.size(); PSIM_STATE_IDX++) {
-        model_signals_vec[map_sig_vec_to_out[PSIM_STATE_IDX]] = new_state[PSIM_STATE_IDX];
-      }
-    }
-  }
+  void initState(std::vector<double> new_state);
 
   /**
    * @brief set time step for all the models
    * @param [in] dt time step
    */
-  void dtSet(double dt)
-  {
-    for (auto & submodel : submodels) {
-      submodel->dtSet(dt);
-    }
-  }
+  void dtSet(double dt);
 
   /**
    * @brief compute next step of the PSIM model using python sub-models
    * @param [in] psim_input vector of input values provided by PSIM
    */
-  std::vector<double> updatePymodel(std::vector<double> psim_input)
-  {
-    // map input to vector of all variables
-    for (size_t PSIM_INPUT_IDX = 0; PSIM_INPUT_IDX < psim_input.size(); PSIM_INPUT_IDX++) {
-      model_signals_vec[map_in_to_sig_vec[PSIM_INPUT_IDX]] = psim_input[PSIM_INPUT_IDX];
-    }
-
-    // Compute forward pass through all models (order should not matter)
-    std::vector<double> model_signals_vec_next = model_signals_vec;
-    for (auto & submodel : submodels) {
-      model_signals_vec_next = submodel->getNextState(model_signals_vec, model_signals_vec_next);
-    }
-
-    // Map vector of all variables to
-    std::vector<double> psim_next_state;
-    for (size_t PSIM_STATE_IDX = 0; PSIM_STATE_IDX < map_sig_vec_to_out.size(); PSIM_STATE_IDX++) {
-      psim_next_state.push_back(model_signals_vec_next[map_sig_vec_to_out[PSIM_STATE_IDX]]);
-    }
-
-    // Update vector of all variables
-    model_signals_vec = model_signals_vec_next;
-
-    return psim_next_state;
-  }
+  std::vector<double> updatePymodel(std::vector<double> psim_input);
 };
 
 #endif  // LEARNED_MODEL__PYMODEL_INTERCONNECTED_MODEL_HPP_
--- simulator/learned_model/include/learned_model/pymodel_simple_model.hpp
+++ simulator/learned_model/include/learned_model/pymodel_simple_model.hpp
@@ -40,144 +40,43 @@ public:
    * @param [in] param_file_path path to saved parameter file of the python sub-model
    * @param [in] py_class_name name of the python class
    */
-  PymodelSimpleModel(std::string pymodel_import_name_, std::string param_file_path, std::string py_class_name)
-  {
-    // Import model class
-    pymodel_import_name = pymodel_import_name_;
-    if (!pymodel_import_name.empty()) {
-      // Import python module
-      py::module_ imported_module = py::module_::import(pymodel_import_name.c_str());
-      // Initialize model class from imported module
-      py_model_class = imported_module.attr(py_class_name.c_str())();
-    } else {
-      // TODO throw exception/error
-      return;
-    }
-
-    // Load model parameters and reset the model
-    if (!param_file_path.empty()) {
-      py::object load_params_succ = py_model_class.attr("load_params")(param_file_path.c_str());
-      py_model_class.attr("reset")();
-      std::cout << "Params loaded" << std::endl;
-    } else {
-      // TODO warning that using default model params
-    }
-
-    // Get string names of states of python model, convert them to C++ string and store them in
-    // pymodel_state_names
-    py::list pymodel_state_names_ = py_model_class.attr("get_state_names")();
-    num_outputs_py = pymodel_state_names_.size();
-    for (int STATE_IDX = 0; STATE_IDX < num_outputs_py; STATE_IDX++) {
-      pymodel_state_names.push_back(PyBytes_AS_STRING(
-        PyUnicode_AsEncodedString(pymodel_state_names_[STATE_IDX].ptr(), "UTF-8", "strict")));
-    }
-
-    // Get string names of actions (inputs) of python model, convert them to C++ string and store
-    // them in pymodel_input_names
-    py::list pymodel_input_names_ = py_model_class.attr("get_action_names")();
-    num_inputs_py = pymodel_input_names_.size();
-    for (int INPUT_IDX = 0; INPUT_IDX < num_inputs_py; INPUT_IDX++) {
-      pymodel_input_names.push_back(PyBytes_AS_STRING(
-        PyUnicode_AsEncodedString(pymodel_input_names_[INPUT_IDX].ptr(), "UTF-8", "strict")));
-    }
-
-    std::cout << "PymodelInterface import name:  " << pymodel_import_name << std::endl;
-  }
+  PymodelSimpleModel(std::string pymodel_import_name_, std::string param_file_path, std::string py_class_name);
 
   /**
    * @brief calculate the next state of a python model
    * @param [in] model_signals_vec all available inputs from PSIM
    */
   std::vector<double> getNextState(
-    std::vector<double> model_signals_vec, std::vector<double> model_signals_vec_next) override
-  {
-    // get inputs and states of the python model from the vector of signals
-    std::vector<double> py_inputs(num_inputs_py);
-    std::vector<double> py_state(num_outputs_py);
-    py_inputs = fillVectorUsingMap(py_inputs, model_signals_vec, map_sig_vec_to_pyin, true);
-    py_state = fillVectorUsingMap(py_state, model_signals_vec, map_pyout_to_sig_vec, true);
-
-    // forward pass through the base model
-    py::tuple res = py_model_class.attr("forward")(py_inputs, py_state);
-    std::vector<double> py_state_next = res.cast<std::vector<double>>();
-
-    // map outputs from python model to required outputs
-    std::vector<double> next_state =
-      fillVectorUsingMap(py_state_next, model_signals_vec_next, map_pyout_to_sig_vec, false);
-
-    return next_state;
-  }
+    std::vector<double> model_signals_vec, std::vector<double> model_signals_vec_next) override;
 
   /**
    * @brief set time step of the model
    * @param [in] dt time step
    */
-  void dtSet(double dt) override { py_model_class.attr("dtSet")(dt); }
+  void dtSet(double dt) override;
 
   /**
    * @brief get names of inputs of python model
    */
-  std::vector<char *> getInputNames() override { return pymodel_input_names; }
+  std::vector<char *> getInputNames() override;
 
   /**
    * @brief get names of states of python model
    */
-  std::vector<char *> getStateNames() override { return pymodel_state_names; }
+  std::vector<char *> getStateNames() override;
 
   /**
    * @brief create a map from model signal vector to python model inputs
    * @param [in] signal_vec_names names of signals in model signal vector
    */
-  void mapInputs(std::vector<char *> signals_vec_names) override
-  {
-    // index in "map_sig_vec_to_pyin" is index in "py_inputs" and value in "map_sig_vec_to_pyin" is
-    // index in "signals_vec_names"
-    map_sig_vec_to_pyin = createConnectionsMap(signals_vec_names, pymodel_input_names);
-  }
+  void mapInputs(std::vector<char *> signals_vec_names) override;
 
   /**
    * @brief create a map from python outputs to model signal vector
    * @param [in] signal_vec_names names of signals in model signal vector
    */
-  void mapOutputs(std::vector<char *> signals_vec_names) override
-  {
-    // index in "map_pyout_to_sig_vec" is index in "pymodel_outputs" and value in
-    // "map_pyout_to_sig_vec" is index in "signals_vec_names"
-    map_pyout_to_sig_vec = createConnectionsMap(signals_vec_names, pymodel_state_names);
-  }
+  void mapOutputs(std::vector<char *> signals_vec_names) override;
 
-private:
-  std::vector<int> createConnectionsMap(
-    std::vector<char *> connection_names_1, std::vector<char *> connection_names_2)
-  {
-    std::vector<int> connection_map;
-    // index in "connection_map" is index in "connection_names_2" and value in "connection_map" is
-    // index in "connection_names_1"
-    for (auto name_2 : connection_names_2) {
-      int mapped_idx = -1;  // -1 means that we cannot create a connection between some signals
-      for (size_t NAME_1_IDX = 0; NAME_1_IDX < connection_names_1.size(); NAME_1_IDX++) {
-        if (strcmp(name_2, connection_names_1[NAME_1_IDX]) == 0) {  // 0 means strings are the same
-                                                                    // and we can create connection
-          mapped_idx = NAME_1_IDX;
-          break;
-        }
-      }
-      connection_map.push_back(mapped_idx);
-    }
-    return connection_map;
-  }
-
-  std::vector<double> fillVectorUsingMap(
-    std::vector<double> vector1, std::vector<double> vector2, std::vector<int> map, bool inverse)
-  {
-    // index in "map" is index in "vector1" and value in "map" is index in "vector2"
-    // inverse = 0 from 1 -> 2; inverse = 1 from 2 -> 1
-    for (size_t idx = 0; idx < map.size(); idx++) {
-      if (map[idx] == -1) continue;
-      inverse ? vector1[idx] = vector2[map[idx]] : vector2[map[idx]] = vector1[idx];
-    }
-    return inverse ? vector1 : vector2;
-  }
 };
 
 #endif  // LEARNED_MODEL__PYMODEL_SIMPLE_MODEL_HPP_
