ae93f779b95261bd1bcd1cc5ff65f4723c46ed54
Takumi Ito <takumi.ito@tier4.jp>
2024-02-19 09:37:32 +0900

    - Add method getWaypoints() into the astar class. - No need to convert to serial when user passes ros2 message to methods.
    Signed-off-by: Takumi Ito <takumi.ito@tier4.jp>

--- planning/freespace_planning_algorithms/scripts/example/example.py
+++ planning/freespace_planning_algorithms/scripts/example/example.py
@@ -1,13 +1,9 @@
-import freespace_planning_algorithms.freespace_planning_algorithms_python as fp
-
+import freespace_planning_algorithms.astar_search as fp
+from geometry_msgs.msg import Pose
+from nav_msgs.msg import OccupancyGrid
 from pyquaternion import Quaternion
 
-from nav_msgs.msg import OccupancyGrid
-from geometry_msgs.msg import Pose
-from rclpy.serialization import serialize_message
-
-
-# -- Vehicle Shape -- 
+# -- Vehicle Shape --
 vehicle_shape = fp.VehicleShape()
 vehicle_shape.length = 2.0
 vehicle_shape.width = 1.0
@@ -17,26 +13,26 @@ vehicle_shape.base2back = 1.0
 # -- Planner Common Parameter --
 planner_param = fp.PlannerCommonParam()
 # base configs
-planner_param.time_limit= 30000.0
-planner_param.minimum_turning_radius= 9.0
-planner_param.maximum_turning_radius= 9.0
-planner_param.turning_radius_size= 1
+planner_param.time_limit = 30000.0
+planner_param.minimum_turning_radius = 9.0
+planner_param.maximum_turning_radius = 9.0
+planner_param.turning_radius_size = 1
 # search configs
-planner_param.theta_size= 144
-planner_param.angle_goal_range= 6.0
-planner_param.curve_weight= 1.2
-planner_param.reverse_weight= 2.0
-planner_param.lateral_goal_range= 0.5
-planner_param.longitudinal_goal_range= 2.0
+planner_param.theta_size = 144
+planner_param.angle_goal_range = 6.0
+planner_param.curve_weight = 1.2
+planner_param.reverse_weight = 2.0
+planner_param.lateral_goal_range = 0.5
+planner_param.longitudinal_goal_range = 2.0
 # costmap configs
-planner_param.obstacle_threshold= 100
+planner_param.obstacle_threshold = 100
 
 
 # -- A* search Configurations --
 astar_param = fp.AstarParam()
-astar_param.only_behind_solutions= False
+astar_param.only_behind_solutions = False
 astar_param.use_back = True
-astar_param.distance_heuristic_weight= 1.0
+astar_param.distance_heuristic_weight = 1.0
 
 astar = fp.AstarSearch(planner_param, vehicle_shape, astar_param)
 
@@ -49,11 +45,11 @@ costmap = OccupancyGrid()
 costmap.info.resolution = resolution
 costmap.info.height = size
 costmap.info.width = size
-costmap.info.origin.position.x = -size*resolution/2
-costmap.info.origin.position.y = -size*resolution/2
-costmap.data = [0 for i in range(size**2) ]
+costmap.info.origin.position.x = -size * resolution / 2
+costmap.info.origin.position.y = -size * resolution / 2
+costmap.data = [0 for i in range(size**2)]
 
-astar.setMap(serialize_message(costmap))
+astar.setMap(costmap)
 
 
 # -- Start and Goal Pose
@@ -76,8 +72,8 @@ goal_pose.orientation.z = quaterinon.z
 
 
 # -- Search --
-if astar.makePlan(serialize_message(start_pose),\
-                  serialize_message(goal_pose)):
-    print('Success to find path.')
+if astar.makePlan(start_pose, goal_pose):
+    print("Success to find path.")
+    print("  Path length:", astar.getWaypoints().length)
 else:
-    print('Fail to find path.')
+    print("Fail to find path.")
