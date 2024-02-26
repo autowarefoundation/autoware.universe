df33eb0273d134891540f1f25deeeb7bc1b7cd13
Mamoru Sobue <hilo.soblin@gmail.com>
2024-02-26 22:12:33 +0900

    fix(start_planner): start prepare blinker when autonomous mode is available (#6470)
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_path_start_planner_module/include/behavior_path_start_planner_module/start_planner_module.hpp
+++ planning/behavior_path_start_planner_module/include/behavior_path_start_planner_module/start_planner_module.hpp
@@ -71,7 +71,7 @@ struct PullOutStatus
   bool has_stop_point{false};
   std::optional<Pose> stop_pose{std::nullopt};
   //! record the first time when the state changed from !isActivated() to isActivated()
-  std::optional<rclcpp::Time> first_approved_time{std::nullopt};
+  std::optional<rclcpp::Time> first_engaged_time{std::nullopt};
 
   PullOutStatus() {}
 };
