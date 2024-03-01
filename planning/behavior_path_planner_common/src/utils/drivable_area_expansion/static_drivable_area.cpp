181ce6e59590ef4a3fcbbfeb185c823dfa3bd11b
YoshihiroMichishita-PT <150212429+YoshihiroMichishita-PT@users.noreply.github.com>
2024-03-01 12:34:39 +0900

    Update static_drivable_area.cpp (postProcess)

--- planning/behavior_path_planner_common/src/utils/drivable_area_expansion/static_drivable_area.cpp
+++ planning/behavior_path_planner_common/src/utils/drivable_area_expansion/static_drivable_area.cpp
@@ -1546,11 +1546,7 @@ std::vector<geometry_msgs::msg::Point> postProcess(
     }
   }
 
-  if (!is_driving_forward) {
-    std::reverse(tmp_bound.begin(), tmp_bound.end());
-  }
-
-  const auto start_idx = [&]() {
+  const auto [start_idx, start_point] = [&]() {
     const size_t current_seg_idx = planner_data->findEgoSegmentIndex(path.points);
     const auto cropped_path_points = motion_utils::cropPoints(
       path.points, current_pose.position, current_seg_idx,
@@ -1566,11 +1562,12 @@ std::vector<geometry_msgs::msg::Point> postProcess(
       calcLongitudinalOffsetStartPoint(tmp_bound, front_pose, front_start_idx, -front_length);
 
     // Insert a start point
-    processed_bound.push_back(start_point);
+    //processed_bound.push_back(start_point);
 
     const auto p_tmp =
       geometry_msgs::build<Pose>().position(start_point).orientation(front_pose.orientation);
-    return findNearestSegmentIndexFromLateralDistance(tmp_bound, p_tmp, M_PI_2);
+    //return findNearestSegmentIndexFromLateralDistance(tmp_bound, p_tmp, M_PI_2);
+    return std::make_pair(findNearestSegmentIndexFromLateralDistance(tmp_bound, p_tmp, M_PI_2), start_point);
   }();
 
   // Get Closest segment for the goal point
@@ -1582,25 +1579,39 @@ std::vector<geometry_msgs::msg::Point> postProcess(
       calcLongitudinalOffsetGoalPoint(tmp_bound, goal_pose, goal_start_idx, vehicle_length);
     const auto p_tmp =
       geometry_msgs::build<Pose>().position(goal_point).orientation(goal_pose.orientation);
-    const size_t goal_idx = std::max(
-      goal_start_idx, findNearestSegmentIndexFromLateralDistance(tmp_bound, p_tmp, M_PI_2));
-
+    const size_t goal_nearest_idx = findNearestSegmentIndexFromLateralDistance(tmp_bound, p_tmp, M_PI_2);
+    
+    const size_t goal_idx = ((goal_start_idx - start_idx)*(goal_start_idx - start_idx) > (goal_nearest_idx - start_idx)*(goal_nearest_idx - start_idx)) ? goal_start_idx : goal_nearest_idx;
     return std::make_pair(goal_idx, goal_point);
   }();
 
-  // Insert middle points
-  for (size_t i = start_idx + 1; i <= goal_idx; ++i) {
-    const auto & next_point = tmp_bound.at(i);
-    const double dist = tier4_autoware_utils::calcDistance2d(processed_bound.back(), next_point);
-    if (dist > overlap_threshold) {
-      processed_bound.push_back(next_point);
+    //ForwardDriving
+  if(start_idx < goal_idx){
+    processed_bound.push_back(start_point);
+    for (size_t i = start_idx + 1; i <= goal_idx; ++i) {
+    //for (size_t i = st + 1; i <= ed; ++i) {
+      const auto & next_point = tmp_bound.at(i);
+      const double dist = tier4_autoware_utils::calcDistance2d(processed_bound.back(), next_point);
+      if (dist > overlap_threshold) {
+        processed_bound.push_back(next_point);
+      }
+    }
+    if (tier4_autoware_utils::calcDistance2d(processed_bound.back(), goal_point) > overlap_threshold) {
+      processed_bound.push_back(goal_point);
     }
   }
-
-  // Insert a goal point
-  if (
-    tier4_autoware_utils::calcDistance2d(processed_bound.back(), goal_point) > overlap_threshold) {
-    processed_bound.push_back(goal_point);
+  else{//BackwardDriving
+      processed_bound.push_back(start_point);
+      for (size_t i = start_idx-1; i >= goal_idx; --i) {
+      const auto & next_point = tmp_bound.at(i);
+      const double dist = tier4_autoware_utils::calcDistance2d(processed_bound.back(), next_point);
+      if (dist > overlap_threshold) {
+        processed_bound.push_back(next_point);
+      }
+    }
+    if (tier4_autoware_utils::calcDistance2d(processed_bound.back(), goal_point) > overlap_threshold) {
+      processed_bound.push_back(goal_point);
+    }
   }
 
   return removeSharpPoints(processed_bound);
