da6fd2e14806d2bcf827edd10de656d3a7957f57
KhalilSelyan <khalil@leodrive.ai>
2024-03-11 17:41:15 +0300

    Add CustomToggleSwitch class to tier4_state_rviz_plugin

---
+++ common/tier4_state_rviz_plugin/src/custom_toggle_switch.hpp
@@ -0,0 +1,26 @@
+// CustomToggleSwitch.h
+#ifndef CUSTOMTOGGLESWITCH_H
+#define CUSTOMTOGGLESWITCH_H
+
+#include <QCheckBox>
+#include <QMouseEvent>
+
+class CustomToggleSwitch : public QCheckBox
+{
+  Q_OBJECT
+
+public:
+  explicit CustomToggleSwitch(QWidget * parent = nullptr);
+  QSize sizeHint() const override;  // Declare the sizeHint method
+
+protected:
+  void paintEvent(QPaintEvent * event) override;
+  void mousePressEvent(QMouseEvent * event) override;
+  void mouseReleaseEvent(QMouseEvent * event) override;
+
+private:
+  bool isDragging = false;
+  QPoint dragStartPoint;
+};
+
+#endif  // CUSTOMTOGGLESWITCH_H
