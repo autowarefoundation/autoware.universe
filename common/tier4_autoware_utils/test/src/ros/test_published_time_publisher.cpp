5882726aeedf932b4b54445d903ec891011ea44b
Berkay Karaman <brkay54@gmail.com>
2024-03-13 15:59:41 +0300

    feat: simplify node and topic names
    Signed-off-by: Berkay Karaman <brkay54@gmail.com>

--- common/tier4_autoware_utils/test/src/ros/test_published_time_publisher.cpp
+++ common/tier4_autoware_utils/test/src/ros/test_published_time_publisher.cpp
@@ -34,19 +34,27 @@ protected:
   {
     ASSERT_TRUE(rclcpp::ok());
 
+    // Simplify node and topic names for brevity and uniqueness
+    const std::string test_name = ::testing::UnitTest::GetInstance()->current_test_info()->name();
+    const std::string base_name =
+      "published_time_publisher_" + test_name;  // Base name for node and topics
+    const std::string suffix = "/debug/published_time";
+
     // Create a node
-    node_ = std::make_shared<rclcpp::Node>("PublishedTimePublisher_test_node");
+    node_ = std::make_shared<rclcpp::Node>(base_name + "_node");
+
     // Create a publisher
-    test_publisher_ =
-      node_->create_publisher<std_msgs::msg::Header>("PublishedTimePublisher_test_topic", 1);
+    test_publisher_ = node_->create_publisher<std_msgs::msg::Header>(base_name, 1);
+
     // Create a PublishedTimePublisher
     published_time_publisher_ =
       std::make_shared<tier4_autoware_utils::PublishedTimePublisher>(node_.get());
+
     // Create a subscriber
     test_subscriber_ = node_->create_subscription<autoware_internal_msgs::msg::PublishedTime>(
-      "PublishedTimePublisher_test_topic/debug/published_time", 1,
-      [this](autoware_internal_msgs::msg::PublishedTime::SharedPtr msg) {
-        this->published_time_ = msg;
+      base_name + suffix, 1,
+      [this](autoware_internal_msgs::msg::PublishedTime::ConstSharedPtr msg) {
+        this->published_time_ = std::move(msg);
       });
     rclcpp::spin_some(node_);
   }
@@ -56,12 +64,17 @@ protected:
 
 TEST_F(PublishedTimePublisherTest, PublishMsgWithHeader)
 {
+  // Check if the PublishedTimePublisher is created
+  ASSERT_TRUE(published_time_publisher_ != nullptr);
+
   std_msgs::msg::Header header;
   header.stamp = rclcpp::Time(1234);
 
   // Use Published Time Publisher with a timestamp
   published_time_publisher_->publish(test_publisher_, header);
   rclcpp::spin_some(node_);
+
+  // Check if the published_time_ is created
   ASSERT_TRUE(published_time_ != nullptr);
 
   // Check if the published time is the same as the header
@@ -70,12 +83,17 @@ TEST_F(PublishedTimePublisherTest, PublishMsgWithHeader)
 
 TEST_F(PublishedTimePublisherTest, PublishMsgWithTimestamp)
 {
+  // Check if the PublishedTimePublisher is created
+  ASSERT_TRUE(published_time_publisher_ != nullptr);
+
   std_msgs::msg::Header header;
   header.stamp = rclcpp::Time(4321);
 
   // Use Published Time Publisher with a timestamp
   published_time_publisher_->publish(test_publisher_, header.stamp);
   rclcpp::spin_some(node_);
+
+  // Check if the published_time_ is created
   ASSERT_TRUE(published_time_ != nullptr);
 
   // Check if the published time is the same as the header
