0cb5ca47e1a4546cf3ea59b3aa747b2894e63872
mraditya01 <mraditya01@yahoo.com>
2024-03-15 17:42:53 +0900

    remove redundancy and added remap

--- simulator/CARLA_Autoware/carla_pointcloud_interface/include/carla_pointcloud_interface/carla_pointcloud_interface_node.hpp
+++ simulator/CARLA_Autoware/carla_pointcloud_interface/include/carla_pointcloud_interface/carla_pointcloud_interface_node.hpp
@@ -1,4 +1,16 @@
-// Copyright 2024 Raditya.
+// Copyright 2024 Autoware Foundation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef CARLA_POINTCLOUD_INTERFACE__CARLA_POINTCLOUD_INTERFACE_NODE_HPP_
 #define CARLA_POINTCLOUD_INTERFACE__CARLA_POINTCLOUD_INTERFACE_NODE_HPP_
@@ -23,10 +35,8 @@ public:
 
 private:
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr velodyne_points_raw;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr velodyne_points_top;
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr velodyne_points_con;
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr velodyne_points_raw;
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr carla_cloud_;
   std::string tf_output;
   void processScan(const sensor_msgs::msg::PointCloud2::SharedPtr scanMsg);
