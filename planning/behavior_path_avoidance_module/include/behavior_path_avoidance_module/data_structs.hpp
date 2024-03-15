5b597a7b761cb930837f32ad00a4c5dd21d11037
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2024-03-15 11:01:06 +0900

    fix(avoidance): the module ignored merging objects unexpectedly (#6601)
    * feat(avoidance): output overhang lanelet
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    * fix(avoidance): avoid merging vehicle if it's NOT on ego lane.
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    * fix(avoidance): add flag to identify ambiguous vehicle
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    * refactor(avoidance): add helper function
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    * refactor(avoidance): rename param
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    * chore(avoidance): update comment
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>
    
    ---------
    
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_avoidance_module/include/behavior_path_avoidance_module/data_structs.hpp
+++ planning/behavior_path_avoidance_module/include/behavior_path_avoidance_module/data_structs.hpp
@@ -381,7 +381,8 @@ struct ObjectData  // avoidance target
   rclcpp::Time last_move;
   double stop_time{0.0};
 
-  // store the information of the lanelet which the object's overhang is currently occupying
+  // It is one of the ego driving lanelets (closest lanelet to the object) and used in the logic to
+  // check whether the object is on the ego lane.
   lanelet::ConstLanelet overhang_lanelet;
 
   // the position at the detected moment
@@ -416,6 +417,12 @@ struct ObjectData  // avoidance target
 
   // is parked vehicle on road shoulder
   bool is_parked{false};
+
+  // is driving on ego current lane
+  bool is_on_ego_lane{false};
+
+  // is ambiguous stopped vehicle.
+  bool is_ambiguous{false};
 
   // object direction.
   Direction direction{Direction::NONE};
