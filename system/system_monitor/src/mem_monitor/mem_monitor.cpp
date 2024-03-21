0d6bdcc990a31430ee080b7e4a35d91413fd45a2
TetsuKawa <kawaguchitnon@icloud.com>
2024-03-21 15:04:34 +0900

    feat: replace free command with reading meminfo
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- system/system_monitor/src/mem_monitor/mem_monitor.cpp
+++ system/system_monitor/src/mem_monitor/mem_monitor.cpp
@@ -50,101 +50,100 @@ void MemMonitor::update()
   updater_.force_update();
 }
 
+std::unordered_map<std::string, size_t> readMemInfo() {
+    std::unordered_map<std::string, size_t> memInfo;
+    std::ifstream file("/proc/meminfo");
+
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open /proc/meminfo");
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::size_t pos = line.find(':');
+        if (pos != std::string::npos) {
+            std::string key = line.substr(0, pos);
+            try {
+                size_t value = std::stoll(line.substr(pos + 1)) * 1024;
+                memInfo[key] = value;
+            } catch (const std::invalid_argument& e) {
+                throw std::runtime_error("Invalid value in /proc/meminfo: " + line);
+            } catch (const std::out_of_range& e) {
+                throw std::runtime_error("Value out of range in /proc/meminfo: " + line);
+            }
+        } else {
+            throw std::runtime_error("Invalid line in /proc/meminfo: " + line);
+        }
+    }
+
+    return memInfo;
+}
+
 void MemMonitor::checkUsage(diagnostic_updater::DiagnosticStatusWrapper & stat)
 {
   // Remember start time to measure elapsed time
   const auto t_start = SystemMonitorUtility::startMeasurement();
 
   // Get total amount of free and used memory
-
-  // boost::process create file descriptor without O_CLOEXEC required for multithreading.
-  // So create file descriptor with O_CLOEXEC and pass it to boost::process.
-  int out_fd[2];
-  if (pipe2(out_fd, O_CLOEXEC) != 0) {
-    stat.summary(DiagStatus::ERROR, "pipe2 error");
-    stat.add("pipe2", strerror(errno));
-    return;
-  }
-  bp::pipe out_pipe{out_fd[0], out_fd[1]};
-  bp::ipstream is_out{std::move(out_pipe)};
-
-  int err_fd[2];
-  if (pipe2(err_fd, O_CLOEXEC) != 0) {
-    stat.summary(DiagStatus::ERROR, "pipe2 error");
-    stat.add("pipe2", strerror(errno));
-    return;
-  }
-  bp::pipe err_pipe{err_fd[0], err_fd[1]};
-  bp::ipstream is_err{std::move(err_pipe)};
-
-  bp::child c("free -tb", bp::std_out > is_out, bp::std_err > is_err);
-  c.wait();
-  if (c.exit_code() != 0) {
-    std::ostringstream os;
-    is_err >> os.rdbuf();
-    stat.summary(DiagStatus::ERROR, "free error");
-    stat.add("free", os.str().c_str());
-    return;
-  }
-
-  std::string line;
-  int index = 0;
-  std::vector<std::string> list;
-  float usage;
+  std::unordered_map<std::string, size_t> memInfo;
   size_t mem_total = 0;
+  size_t mem_free = 0;
   size_t mem_shared = 0;
   size_t mem_available = 0;
-  size_t used_plus = 0;
+  size_t slab_reclaimable = 0;
+  size_t buffers = 0;
+  size_t cached = 0;
+  size_t swap_total = 0;
+  size_t swap_free = 0;
 
-  /*
-   Output example of `free -tb`
-
-             list[0]     list[1]     list[2]     list[3]     list[4]     list[5]     list[6]
-   index 0 |               total        used        free      shared  buff/cache   available
-   index 1 | Mem:       32809744    12554780    13090376      292840     7164588    19622092
-   index 2 | Swap:      33554428     1767680    31786748
-   index 3 | Total:     66364172    14322460    44877124
-  */
-  while (std::getline(is_out, line) && !line.empty()) {
-    // Skip header
-    if (index <= 0) {
-      ++index;
-      continue;
-    }
-
-    boost::split(list, line, boost::is_space(), boost::token_compress_on);
-
-    // Physical memory
-    if (index == 1) {
-      mem_total = std::atoll(list[1].c_str());
-      mem_shared = std::atoll(list[4].c_str());
-      mem_available = std::atoll(list[6].c_str());
-
-      // available divided by total is available memory including calculation for buff/cache,
-      // so the subtraction of this from 1 gives real usage.
-      usage = 1.0f - static_cast<double>(mem_available) / mem_total;
-      stat.addf(fmt::format("{} usage", list[0]), "%.2f%%", usage * 1e+2);
-    }
-
-    stat.add(fmt::format("{} total", list[0]), toHumanReadable(list[1]));
-    stat.add(fmt::format("{} used", list[0]), toHumanReadable(list[2]));
-    stat.add(fmt::format("{} free", list[0]), toHumanReadable(list[3]));
-
-    // Add an additional information for physical memory
-    if (index == 1) {
-      stat.add(fmt::format("{} shared", list[0]), toHumanReadable(list[4]));
-      stat.add(fmt::format("{} buff/cache", list[0]), toHumanReadable(list[5]));
-      stat.add(fmt::format("{} available", list[0]), toHumanReadable(list[6]));
-    } else if (index == 3) {
-      // Total:used + Mem:shared
-      used_plus = std::atoll(list[2].c_str()) + mem_shared;
-      double giga = static_cast<double>(used_plus) / (1024 * 1024 * 1024);
-      stat.add(fmt::format("{} used+", list[0]), fmt::format("{:.1f}{}", giga, "G"));
-    } else {
-      /* nothing */
-    }
-    ++index;
+  try{
+    memInfo = readMemInfo();
+  } catch (const std::exception& e) {
+    stat.summary(DiagStatus::ERROR, e.what());
+    stat.add("ifstream", strerror(errno));
+    return;
   }
+
+  try {
+    mem_total = memInfo.at("MemTotal");
+    mem_free = memInfo.at("MemFree");
+    mem_shared = memInfo.at("Shmem");
+    mem_available = memInfo.at("MemAvailable");
+    slab_reclaimable = memInfo.at("SReclaimable");
+    buffers = memInfo.at("Buffers");
+    cached = memInfo.at("Cached");
+    swap_total = memInfo.at("SwapTotal");
+    swap_free = memInfo.at("SwapFree");
+  } catch (const std::out_of_range& e) {
+    stat.summary(DiagStatus::ERROR, e.what());
+    stat.add("unordered_map::at", strerror(errno));
+    return;
+  } 
+
+  float usage = 1.0f - static_cast<double>(mem_available) / mem_total;
+  size_t mem_buff_and_cache = buffers + cached + slab_reclaimable;
+  size_t mem_used = mem_total - mem_free - mem_buff_and_cache;
+  stat.addf("Mem: usage", "%.2f%%", usage * 1e+2);
+  stat.add("Mem: total", toHumanReadable(std::to_string(mem_total)));
+  stat.add("Mem: used", toHumanReadable(std::to_string(mem_used)));
+  stat.add("Mem: free", toHumanReadable(std::to_string(mem_free)));
+  stat.add("Mem: shared", toHumanReadable(std::to_string(mem_shared)));
+  stat.add("Mem: buff/cache", toHumanReadable(std::to_string(mem_buff_and_cache)));
+  stat.add("Mem: available", toHumanReadable(std::to_string(mem_available)));
+
+  size_t swap_used = swap_total - swap_free;
+  stat.add("Swap: total", toHumanReadable(std::to_string(swap_total)));
+  stat.add("Swap: used", toHumanReadable(std::to_string(swap_used)));
+  stat.add("Swap: free", toHumanReadable(std::to_string(swap_free)));
+
+  size_t total_total = mem_total + swap_total;
+  size_t total_used = mem_used + swap_used;
+  size_t total_free = mem_free + swap_free;
+  size_t used_plus = total_used + mem_shared;
+  stat.add("Total: total", toHumanReadable(std::to_string(total_total)));
+  stat.add("Total: used", toHumanReadable(std::to_string(total_used)));
+  stat.add("Total: free", toHumanReadable(std::to_string(total_free)));
+  stat.add("Total: used+", toHumanReadable(std::to_string(used_plus)));
 
   int level;
   if (mem_total > used_plus) {
