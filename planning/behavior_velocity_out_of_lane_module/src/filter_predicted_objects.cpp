238a373876653ebebcf34ef5628366939574a861
Maxime CLEMENT <maxime.clement@tier4.jp>
2024-03-22 11:41:39 +0900

    smaller extended stop line
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/behavior_velocity_out_of_lane_module/src/filter_predicted_objects.cpp
+++ planning/behavior_velocity_out_of_lane_module/src/filter_predicted_objects.cpp
@@ -86,10 +86,11 @@ void cut_predicted_path_beyond_red_lights(
 {
   const auto stop_line = find_next_stop_line(predicted_path, planner_data);
   if (stop_line) {
+    // we use a longer stop line to also cut predicted paths that slightly go around the stop line
     auto longer_stop_line = *stop_line;
     const auto diff = stop_line->back() - stop_line->front();
-    longer_stop_line.front() += diff * -2.0;
-    longer_stop_line.back() += diff * 2.0;
+    longer_stop_line.front() -= diff * 0.5;
+    longer_stop_line.back() += diff * 0.5;
     cut_predicted_path_beyond_line(predicted_path, longer_stop_line, object_front_overhang);
   }
 }
