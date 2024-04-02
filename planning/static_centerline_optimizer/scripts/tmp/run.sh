d5a4acde99a90b1891b45b59897164c7a5711981
Takayuki Murooka <takayuki5168@gmail.com>
2024-04-02 18:09:37 +0900

    fix
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/static_centerline_optimizer/scripts/tmp/run.sh
+++ planning/static_centerline_optimizer/scripts/tmp/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# ros2 launch static_centerline_optimizer static_centerline_optimizer.launch.xml run_background:=false lanelet2_input_file_path:="$HOME/autoware_map/sample_map/lanelet2_map.osm" start_lanelet_id:=125 end_lanelet_id:=132 vehicle_model:=lexus
-ros2 launch static_centerline_optimizer static_centerline_optimizer.launch.xml run_background:=false lanelet2_input_file_path:="$HOME/autoware_map/odaiba_beta/lanelet2_map.osm" start_lanelet_id:=168 end_lanelet_id:=74 vehicle_model:=lexus
+ros2 launch static_centerline_optimizer static_centerline_optimizer.launch.xml run_background:=false lanelet2_input_file_path:="$HOME/autoware_map/sample_map/lanelet2_map.osm" start_lanelet_id:=125 end_lanelet_id:=132 vehicle_model:=lexus
