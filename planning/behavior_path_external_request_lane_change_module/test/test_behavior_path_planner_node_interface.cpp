cb0508aefc8dccc9729591fc793ad6415618b821
Esteve Fernandez <esteve.fernandez@tier4.jp>
2024-04-18 14:53:10 +0200

    build(autoware_behavior_path_external_request_lane_change_module): revert performance-related clang-tidy fixes
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>

--- planning/behavior_path_external_request_lane_change_module/test/test_behavior_path_planner_node_interface.cpp
+++ planning/behavior_path_external_request_lane_change_module/test/test_behavior_path_planner_node_interface.cpp
@@ -75,8 +75,8 @@ std::shared_ptr<BehaviorPathPlannerNode> generateNode()
 }
 
 void publishMandatoryTopics(
-  const std::shared_ptr<PlanningInterfaceTestManager> & test_manager,
-  const std::shared_ptr<BehaviorPathPlannerNode> & test_target_node)
+  std::shared_ptr<PlanningInterfaceTestManager> test_manager,
+  std::shared_ptr<BehaviorPathPlannerNode> test_target_node)
 {
   // publish necessary topics from test_manager
   test_manager->publishInitialPose(test_target_node, "behavior_path_planner/input/odometry");
