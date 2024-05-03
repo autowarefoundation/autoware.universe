20e39864e12997a44eee28b6608be301759387cd
M. Fatih Cırıt <mfc@leodrive.ai>
2024-05-03 16:49:23 +0300

    cpp-changes
    Signed-off-by: M. Fatih Cırıt <mfc@leodrive.ai>

--- localization/autoware_pose_covariance_modifier_node/src/autoware_pose_covariance_modifier_node.cpp
+++ localization/autoware_pose_covariance_modifier_node/src/autoware_pose_covariance_modifier_node.cpp
@@ -21,19 +21,15 @@ AutowarePoseCovarianceModifierNode::AutowarePoseCovarianceModifierNode()
   gnss_pose_last_received_time_(this->now()),
   pose_source_(AutowarePoseCovarianceModifierNode::PoseSource::NDT)
 {
-  try {
-    gnss_stddev_reliable_max_ =
-      this->declare_parameter<double>("stddev_thresholds.gnss_stddev_reliable_max");
-    gnss_stddev_unreliable_min_ =
-      this->declare_parameter<double>("stddev_thresholds.gnss_stddev_unreliable_min");
-    yaw_stddev_deg_threshold_ =
-      this->declare_parameter<double>("stddev_thresholds.yaw_stddev_deg_threshold");
-    gnss_pose_timeout_sec_ = this->declare_parameter<double>("validation.gnss_pose_timeout_sec");
-    debug_ = this->declare_parameter<bool>("debug.enable_debug_topics");
-  } catch (const std::exception & e) {
-    RCLCPP_ERROR(this->get_logger(), "Failed to declare parameters: %s", e.what());
-    throw;
-  }
+  gnss_stddev_reliable_max_ =
+    this->declare_parameter<double>("stddev_thresholds.gnss_stddev_reliable_max");
+  gnss_stddev_unreliable_min_ =
+    this->declare_parameter<double>("stddev_thresholds.gnss_stddev_unreliable_min");
+  yaw_stddev_deg_threshold_ =
+    this->declare_parameter<double>("stddev_thresholds.yaw_stddev_deg_threshold");
+  gnss_pose_timeout_sec_ = this->declare_parameter<double>("validation.gnss_pose_timeout_sec");
+  debug_ = this->declare_parameter<bool>("debug.enable_debug_topics");
+
   gnss_pose_with_cov_sub_ =
     this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
       "input_gnss_pose_with_cov_topic", 10,
@@ -46,12 +42,14 @@ AutowarePoseCovarianceModifierNode::AutowarePoseCovarianceModifierNode()
     std::bind(
       &AutowarePoseCovarianceModifierNode::ndt_pose_with_cov_callback, this,
       std::placeholders::_1));
+
   output_pose_with_covariance_stamped_pub_ =
     this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>(
       "output_pose_with_covariance_topic", 10);
 
   pose_source_pub_ = this->create_publisher<std_msgs::msg::String>(
     "autoware_pose_covariance_modifier/selected_pose_type", 10);
+
   if (debug_) {
     out_ndt_position_stddev_pub_ = this->create_publisher<std_msgs::msg::Float32>(
       "/autoware_pose_covariance_modifier/output/ndt_position_stddev", 10);
@@ -59,15 +57,16 @@ AutowarePoseCovarianceModifierNode::AutowarePoseCovarianceModifierNode()
       "/autoware_pose_covariance_modifier/output/gnss_position_stddev", 10);
   }
 }
+
 void AutowarePoseCovarianceModifierNode::check_gnss_pose_timeout()
 {
   auto time_diff = this->now() - gnss_pose_last_received_time_;
   if (time_diff.seconds() > gnss_pose_timeout_sec_) {
     RCLCPP_WARN(this->get_logger(), "GNSS Pose Timeout");
     pose_source_ = AutowarePoseCovarianceModifierNode::PoseSource::NDT;
-    std_msgs::msg::String selected_pose_type;
-    selected_pose_type.data = "NDT";
-    pose_source_pub_->publish(selected_pose_type);
+    std_msgs::msg::String msg_str_pose_selected;
+    msg_str_pose_selected.data = "NDT";
+    pose_source_pub_->publish(msg_str_pose_selected);
   }
 }
 
@@ -80,14 +79,32 @@ std::array<double, 36> AutowarePoseCovarianceModifierNode::ndt_covariance_modifi
    * explained in the section
    * (https://github.com/meliketanrikulu/autoware.universe/tree/feat/add_autoware_pose_covariance_modifier_node/localization/autoware_pose_covariance_modifier_node#how-does-the-interpolate-gnss-and-ndt-pose-part-work-)
    */
+
+  auto interpolate = [](double x, double x_min, double x_max, double y_min, double y_max) {
+    return y_min + (y_max - y_min) * ((x - x_min) / (x_max - x_min));
+  };
+
   auto modify_covariance = [&](double ndt_covariance, double gnss_covariance) {
     // calculate NDT covariance value based on gnss covariance
-    double sqrt_ndt_cov = std::sqrt(ndt_covariance);
-    double calculated_covariance = std::pow(
-      ((sqrt_ndt_cov * 2) - ((sqrt_ndt_cov * 2) - (sqrt_ndt_cov)) *
-                              ((std::sqrt(gnss_covariance) - gnss_stddev_reliable_max_) /
-                               (gnss_stddev_unreliable_min_ - gnss_stddev_reliable_max_))),
-      2);
+    const double ndt_std_dev = std::sqrt(ndt_covariance);
+    const double gnss_std_dev = std::sqrt(gnss_covariance);
+
+    const double ndt_std_dev_min = ndt_std_dev;
+    const double ndt_std_dev_max = ndt_std_dev * 5;
+
+    const double delta_ndt_std_dev = ndt_std_dev_max - ndt_std_dev_min;
+    const double delta_gnss_std_dev = gnss_stddev_reliable_max_ - gnss_stddev_unreliable_min_;
+
+    const double calculated_std_dev =
+      ndt_std_dev_max -
+      delta_ndt_std_dev * ((gnss_stddev_reliable_max_ - gnss_std_dev) / delta_gnss_std_dev);
+
+    const double interpolated_std_dev = interpolate(
+      gnss_std_dev, gnss_stddev_unreliable_min_, gnss_stddev_reliable_max_, ndt_std_dev_min,
+      ndt_std_dev_max);
+
+    const double calculated_covariance = std::pow(calculated_std_dev, 2);
+
     // Make sure the ndt covariance is not below the input ndt covariance value and return
     return (std::max(calculated_covariance, ndt_covariance));
   };
@@ -129,6 +146,7 @@ void AutowarePoseCovarianceModifierNode::ndt_pose_with_cov_callback(
     out_ndt_position_stddev_pub_->publish(out_ndt_stddev);
   }
 }
+
 void AutowarePoseCovarianceModifierNode::gnss_pose_with_cov_callback(
   const geometry_msgs::msg::PoseWithCovarianceStamped::ConstSharedPtr & msg)
 {
