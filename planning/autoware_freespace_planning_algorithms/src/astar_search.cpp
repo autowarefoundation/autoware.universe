0461798029e254325460d8579d06e3fa1295d583
Takumi Ito <takumi.ito@tier4.jp>
2024-05-20 13:52:40 +0900

    modify astar for standalone running move clearNoe() from setMap to makePlan().
    Signed-off-by: Takumi Ito <takumi.ito@tier4.jp>

--- planning/autoware_freespace_planning_algorithms/src/astar_search.cpp
+++ planning/autoware_freespace_planning_algorithms/src/astar_search.cpp
@@ -130,9 +130,7 @@ AstarSearch::AstarSearch(
 void AstarSearch::setMap(const nav_msgs::msg::OccupancyGrid & costmap)
 {
   AbstractPlanningAlgorithm::setMap(costmap);
-
-  clearNodes();
-
+  
   x_scale_ = costmap_.info.height;
   graph_.reserve(100000);
 }
@@ -142,6 +140,8 @@ bool AstarSearch::makePlan(
 {
   start_pose_ = global2local(costmap_, start_pose);
   goal_pose_ = global2local(costmap_, goal_pose);
+
+  clearNodes();
 
   if (!setStartNode()) {
     return false;
