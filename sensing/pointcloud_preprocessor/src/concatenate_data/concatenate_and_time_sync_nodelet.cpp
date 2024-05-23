2a1dad597c37e23bd52953f509aa9ee5aa67fe09
Shumpei Wakabayashi <42209144+shmpwk@users.noreply.github.com>
2024-05-24 08:25:31 +0900

    fix(pointcloud_preprocessor): typo consecutive_concatenate_failures (#1310)

--- sensing/pointcloud_preprocessor/src/concatenate_data/concatenate_and_time_sync_nodelet.cpp
+++ sensing/pointcloud_preprocessor/src/concatenate_data/concatenate_and_time_sync_nodelet.cpp
@@ -705,7 +705,7 @@ void PointCloudConcatenateDataSynchronizerComponent::checkConcatStatus()
   {
     diagnostic_msgs::msg::KeyValue key_value_msg;
     key_value_msg.key = "consecutiveConcatenateFailures";
-    key_value_msg.value = std::to_string(consecutive_concatenate_failures_);
+    key_value_msg.value = std::to_string(consecutive_concatenate_failures);
     diag_status_msg.values.push_back(key_value_msg);
   }
 
