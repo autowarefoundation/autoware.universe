6ef95d3b6d013cc75c0184c537aefaba13543b44
amadeuszsz <amadeusz.szymko@tier4.jp>
2024-05-23 18:11:49 +0900

    refactor(lidar_transfusion): consts & uniform initialization
    Signed-off-by: amadeuszsz <amadeusz.szymko@tier4.jp>

--- perception/lidar_transfusion/include/lidar_transfusion/transfusion_config.hpp
+++ perception/lidar_transfusion/include/lidar_transfusion/transfusion_config.hpp
@@ -73,55 +73,57 @@ public:
       yaw_norm_threshold =
         (yaw_norm_threshold >= 0.0 && yaw_norm_threshold < 1.0) ? yaw_norm_threshold : 0.0;
     }
+    grid_x_size_ = static_cast<std::size_t>((max_x_range_ - min_x_range_) / voxel_x_size_);
+    grid_y_size_ = static_cast<std::size_t>((max_y_range_ - min_y_range_) / voxel_y_size_);
+    grid_z_size_ = static_cast<std::size_t>((max_z_range_ - min_z_range_) / voxel_z_size_);
   }
 
   ///// INPUT PARAMETERS /////
-  std::size_t cloud_capacity_ = 1000000;
+  const std::size_t cloud_capacity_{1000000};
   ///// KERNEL PARAMETERS /////
-  std::size_t threads_for_voxel_ = 256;  // threads number for a block
-  std::size_t points_per_voxel_ = 20;
-  std::size_t warp_size_ = 32;          // one warp(32 threads) for one pillar
-  std::size_t features_size_ = 10;      // features maps number
-  std::size_t pillars_per_block_ = 64;  // one thread deals with one pillar
-                                        // and a block has pillars_per_block threads
-  std::size_t pillar_feature_size_ = 64;
-  std::size_t max_voxels_ = 60000;
+  const std::size_t threads_for_voxel_{256};   // threads number for a block
+  const std::size_t points_per_voxel_{20};
+  const std::size_t warp_size_{32};            // one warp(32 threads) for one pillar
+  const std::size_t pillars_per_block_{64};    // one thread deals with one pillar
+                                               // and a block has pillars_per_block threads
+  const std::size_t pillar_feature_size_{64};
+  std::size_t max_voxels_{60000};
 
   ///// NETWORK PARAMETERS /////
-  std::size_t num_classes_ = 10;
-  std::size_t num_point_feature_size_ = 5;  // x, y, z, intensity, lag
+  const std::size_t num_classes_{10};
+  const std::size_t num_point_feature_size_{5};  // x, y, z, intensity, lag
   // the dimension of the input cloud
-  float min_x_range_ = -76.8;
-  float max_x_range_ = 76.8;
-  float min_y_range_ = -76.8;
-  float max_y_range_ = 76.8;
-  float min_z_range_ = -3.0;
-  float max_z_range_ = 5.0;
+  float min_x_range_{-76.8};
+  float max_x_range_{76.8};
+  float min_y_range_{-76.8};
+  float max_y_range_{76.8};
+  float min_z_range_{-3.0};
+  float max_z_range_{5.0};
   // the size of a pillar
-  float voxel_x_size_ = 0.3;
-  float voxel_y_size_ = 0.3;
-  float voxel_z_size_ = 8.0;
-  std::size_t out_size_factor_ = 4;
-  std::size_t max_num_points_per_pillar_ = points_per_voxel_;
-  std::size_t num_point_values_ = 4;
-  std::size_t num_proposals_ = 200;
+  float voxel_x_size_{0.3};
+  float voxel_y_size_{0.3};
+  float voxel_z_size_{8.0};
+  const std::size_t out_size_factor_{4};
+  const std::size_t max_num_points_per_pillar_{points_per_voxel_};
+  const std::size_t num_point_values_{4};
+  const std::size_t num_proposals_{200};
   // the number of feature maps for pillar scatter
-  std::size_t num_feature_scatter_ = pillar_feature_size_;
+  const std::size_t num_feature_scatter_{pillar_feature_size_};
   // the score threshold for classification
-  float score_threshold_ = 0.2;
-  float circle_nms_dist_threshold_ = 0.5;
+  float score_threshold_{0.2};
+  float circle_nms_dist_threshold_{0.5};
   std::vector<float> yaw_norm_thresholds_{0.3, 0.3, 0.3, 0.3, 0.0};
-  std::size_t max_num_pillars_ = max_voxels_;
-  std::size_t pillar_points_bev_ = max_num_points_per_pillar_ * max_num_pillars_;
+  std::size_t max_num_pillars_{max_voxels_};
+  const std::size_t pillar_points_bev_{max_num_points_per_pillar_ * max_num_pillars_};
   // the detected boxes result decode by (x, y, z, w, l, h, yaw)
-  std::size_t num_box_values_ = 8;
+  const std::size_t num_box_values_{8};
   // the input size of the 2D backbone network
-  std::size_t grid_x_size_ = (max_x_range_ - min_x_range_) / voxel_x_size_;
-  std::size_t grid_y_size_ = (max_y_range_ - min_y_range_) / voxel_y_size_;
-  std::size_t grid_z_size_ = (max_z_range_ - min_z_range_) / voxel_z_size_;
+  std::size_t grid_x_size_{512};
+  std::size_t grid_y_size_{512};
+  std::size_t grid_z_size_{1};
   // the output size of the 2D backbone network
-  std::size_t feature_x_size_ = grid_x_size_ / out_size_factor_;
-  std::size_t feature_y_size_ = grid_y_size_ / out_size_factor_;
+  std::size_t feature_x_size_{grid_x_size_ / out_size_factor_};
+  std::size_t feature_y_size_{grid_y_size_ / out_size_factor_};
 
   ///// RUNTIME DIMENSIONS /////
   std::vector<std::size_t> voxels_num_{5000, 30000, 60000};
