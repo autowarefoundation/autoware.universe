b4f2efb760a1ad0fe0407c3e4c63e530e03379c5
Esteve Fernandez <esteve.fernandez@tier4.jp>
2024-06-05 15:17:22 +0200

    refactor(behavior_velocity_stop_line_module): prefix package and namespace with autoware_
    Signed-off-by: Esteve Fernandez <esteve.fernandez@tier4.jp>

--- planning/autoware_behavior_velocity_planner/test/src/test_node_interface.cpp
+++ planning/autoware_behavior_velocity_planner/test/src/test_node_interface.cpp
@@ -105,7 +105,7 @@ std::shared_ptr<BehaviorVelocityPlannerNode> generateNode()
                    get_behavior_velocity_module_config_no_prefix("occlusion_spot"),
                    get_behavior_velocity_module_config("run_out"),
                    get_behavior_velocity_module_config_no_prefix("speed_bump"),
-                   get_behavior_velocity_module_config_no_prefix("stop_line"),
+                   get_behavior_velocity_module_config("stop_line"),
                    get_behavior_velocity_module_config_no_prefix("traffic_light"),
                    get_behavior_velocity_module_config("virtual_traffic_light"),
                    get_behavior_velocity_module_config_no_prefix("out_of_lane"),
