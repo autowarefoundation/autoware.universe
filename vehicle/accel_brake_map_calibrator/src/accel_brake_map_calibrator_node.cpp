fedba851c51f6c79c65f8c8cb81847fe75fc3dbf
N-Eiki <eiki.nagata.2@tier4.jp>
2024-06-06 10:46:11 +0900

    undo copyright change.\n move to new autoware msg from old autoware_auto msg
    Signed-off-by: N-Eiki <eiki.nagata.2@tier4.jp>

--- vehicle/accel_brake_map_calibrator/src/accel_brake_map_calibrator_node.cpp
+++ vehicle/accel_brake_map_calibrator/src/accel_brake_map_calibrator_node.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright 2024 Tier IV, Inc. All rights reserved.
+// Copyright 202 Tier IV, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
