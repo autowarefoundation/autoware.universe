a25e42dd18c5813c7d314a8f9be7e6577a85ebf4
Daniel Sanchez <danielsanchezaran@gmail.com>
2024-06-20 16:47:35 +0900

    add minimum cluster height threshold
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- control/autoware_autonomous_emergency_braking/src/node.cpp
+++ control/autoware_autonomous_emergency_braking/src/node.cpp
@@ -147,6 +147,7 @@ AEB::AEB(const rclcpp::NodeOptions & node_options)
   a_obj_min_ = declare_parameter<double>("a_obj_min");
 
   cluster_tolerance_ = declare_parameter<double>("cluster_tolerance");
+  cluster_minimum_height_ = declare_parameter<double>("cluster_minimum_height");
   minimum_cluster_size_ = declare_parameter<int>("minimum_cluster_size");
   maximum_cluster_size_ = declare_parameter<int>("maximum_cluster_size");
 
@@ -725,9 +726,13 @@ void AEB::createObjectDataUsingPointCloudClusters(
   PointCloud::Ptr points_belonging_to_cluster_hulls(new PointCloud);
   for (const auto & indices : cluster_indices) {
     PointCloud::Ptr cluster(new PointCloud);
+    bool cluster_surpasses_threshold_height{false};
     for (const auto & index : indices.indices) {
-      cluster->push_back((*obstacle_points_ptr)[index]);
+      const auto & p = (*obstacle_points_ptr)[index];
+      cluster_surpasses_threshold_height = (p.z > 0.0) ? true : cluster_surpasses_threshold_height;
+      cluster->push_back(p);
     }
+    if (!cluster_surpasses_threshold_height) continue;
     // Make a 2d convex hull for the objects
     pcl::ConvexHull<pcl::PointXYZ> hull;
     hull.setDimension(2);
