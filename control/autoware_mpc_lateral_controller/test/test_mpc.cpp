1204fcecb60701f25f11c9f7db8b55a2b2ffc3a8
kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
2024-06-25 03:04:55 +0900

    feat: update autoware_mpc_lateral_controller dependencies
    The autoware_mpc_lateral_controller package.xml file was modified to update the dependency from `planning_test_utils` to `autoware_test_utils`. Additionally, the test_mpc.cpp file was updated to include the `autoware_test_utils` header.
    
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- control/autoware_mpc_lateral_controller/test/test_mpc.cpp
+++ control/autoware_mpc_lateral_controller/test/test_mpc.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "ament_index_cpp/get_package_share_directory.hpp"
 #include "autoware/mpc_lateral_controller/mpc.hpp"
 #include "autoware/mpc_lateral_controller/qp_solver/qp_solver_osqp.hpp"
 #include "autoware/mpc_lateral_controller/qp_solver/qp_solver_unconstraint_fast.hpp"
@@ -20,6 +21,7 @@
 #include "autoware/mpc_lateral_controller/vehicle_model/vehicle_model_bicycle_kinematics_no_delay.hpp"
 #include "gtest/gtest.h"
 #include "rclcpp/rclcpp.hpp"
+#include "autoware_test_utils/autoware_test_utils.hpp"
 
 #include "autoware_control_msgs/msg/lateral.hpp"
 #include "autoware_planning_msgs/msg/trajectory.hpp"
