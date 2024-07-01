71f5e0bf72c047b567a458cfaab44f463b8d2284
Ryuta Kambe <ryuta.kambe@tier4.jp>
2024-07-02 08:35:29 +0900

    restore and suppress inline
    Signed-off-by: Ryuta Kambe <ryuta.kambe@tier4.jp>

--- system/diagnostic_graph_aggregator/src/common/graph/units.hpp
+++ system/diagnostic_graph_aggregator/src/common/graph/units.hpp
@@ -137,6 +137,9 @@ class ShortCircuitMaxUnit : public MaxUnit
 public:
   using MaxUnit::MaxUnit;
   std::string type() const override { return unit_name::short_circuit_max; }
+
+private:
+  void update_status() override; // cppcheck-suppress uselessOverride
 };
 
 class MinUnit : public NodeUnit
