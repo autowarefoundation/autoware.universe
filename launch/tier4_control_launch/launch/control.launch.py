eefe356b62c9bf274571819db1e814eb9bdcd1ca
veqcc <ryuta.kambe@tier4.jp>
2024-07-04 12:37:39 +0900

    test
    Signed-off-by: veqcc <ryuta.kambe@tier4.jp>

--- launch/tier4_control_launch/launch/control.launch.py
+++ launch/tier4_control_launch/launch/control.launch.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+## TODO(veqcc) delete after test
+
 import launch
 from launch.actions import DeclareLaunchArgument
 from launch.actions import GroupAction
