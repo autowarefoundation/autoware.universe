86e54f44193103eb28360b778112721f1c7e397e
Y.Hisaki <yhisaki31@gmail.com>
2024-07-17 09:58:19 +0900

    refactor(tier4_autoware_utils): Changed the API to be more intuitive and added documentation.
    Signed-off-by: Y.Hisaki <yhisaki31@gmail.com>

--- control/autoware_lane_departure_checker/src/lane_departure_checker_node/lane_departure_checker_node.cpp
+++ control/autoware_lane_departure_checker/src/lane_departure_checker_node/lane_departure_checker_node.cpp
@@ -261,11 +261,11 @@ void LaneDepartureCheckerNode::onTimer()
   reference_trajectory_ = sub_reference_trajectory_.takeData();
   predicted_trajectory_ = sub_predicted_trajectory_.takeData();
 
-  const auto lanelet_map_bin_msg = sub_lanelet_map_bin_.takeNewData();
+  const auto lanelet_map_bin_msg = sub_lanelet_map_bin_.takeData();
   if (lanelet_map_bin_msg) {
     lanelet_map_ = std::make_shared<lanelet::LaneletMap>();
     lanelet::utils::conversion::fromBinMsg(
-      *lanelet_map_bin_msg, lanelet_map_, &traffic_rules_, &routing_graph_);
+      **lanelet_map_bin_msg, lanelet_map_, &traffic_rules_, &routing_graph_);
 
     // get all shoulder lanes
     lanelet::ConstLanelets all_lanelets = lanelet::utils::query::laneletLayer(lanelet_map_);
