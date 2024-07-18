bdf8e381d0799b144d1ea49d1360d21dacee5baf
Go Sakayori <gsakayori@gmail.com>
2024-07-18 18:08:19 +0900

    fix
    Signed-off-by: Go Sakayori <gsakayori@gmail.com>

--- planning/behavior_path_planner/autoware_behavior_path_static_obstacle_avoidance_module/src/scene.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_static_obstacle_avoidance_module/src/scene.cpp
@@ -602,8 +602,8 @@ void StaticObstacleAvoidanceModule::fillEgoStatus(
     return;
   }
 
-  if (left_shift_array_.empty()){
-    if(isActivated() && candidate_uuid_ == uuid_map_.at("left")) {
+  if (left_shift_array_.empty()) {
+    if (isActivated() && candidate_uuid_ == uuid_map_.at("left")) {
       if (rtc_interface_ptr_map_.at("left")->isForceActivated(candidate_uuid_)) {
         data.yield_required = false;
         data.safe_shift_line = data.new_shift_line;
@@ -620,8 +620,8 @@ void StaticObstacleAvoidanceModule::fillEgoStatus(
     }
   }
 
-  if (right_shift_array_.empty()){
-    if(isActivated() && candidate_uuid_ == uuid_map_.at("right")) {
+  if (right_shift_array_.empty()) {
+    if (isActivated() && candidate_uuid_ == uuid_map_.at("right")) {
       if (rtc_interface_ptr_map_.at("right")->isForceActivated(candidate_uuid_)) {
         data.yield_required = false;
         data.safe_shift_line = data.new_shift_line;
