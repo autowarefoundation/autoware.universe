c6f7b0255f8de9c559f8c14ea6a19cfc4ea1755c
Daniel Sanchez <danielsanchezaran@gmail.com>
2024-07-18 19:32:41 +0900

    add initial tests
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

---
+++ control/autoware_autonomous_emergency_braking/test/test.cpp
@@ -0,0 +1,91 @@
+// Copyright 2024 TIER IV
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "test.hpp"
+
+#include "autoware/universe_utils/geometry/geometry.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <gtest/gtest.h>
+
+namespace autoware::motion::control::autonomous_emergency_braking::test
+{
+using autoware::universe_utils::Polygon2d;
+using autoware_perception_msgs::msg::PredictedObject;
+using autoware_perception_msgs::msg::PredictedObjects;
+using geometry_msgs::msg::Point;
+using geometry_msgs::msg::Pose;
+using geometry_msgs::msg::TransformStamped;
+using geometry_msgs::msg::Vector3;
+
+std::shared_ptr<AEB> generateNode()
+{
+  auto node_options = rclcpp::NodeOptions{};
+
+  const auto aeb_dir =
+    ament_index_cpp::get_package_share_directory("autoware_autonomous_emergency_braking");
+  const auto vehicle_info_util_dir =
+    ament_index_cpp::get_package_share_directory("autoware_vehicle_info_utils");
+
+  node_options.arguments(
+    {"--ros-args", "--params-file", aeb_dir + "/config/autonomous_emergency_braking.param.yaml",
+     "--ros-args", "--params-file", vehicle_info_util_dir + "/config/vehicle_info.param.yaml"});
+  return std::make_shared<AEB>(node_options);
+};
+
+TEST_F(TestAEB, checkCollision)
+{
+  constexpr double longitudinal_velocity = 3.0;
+  ObjectData object_collision;
+  object_collision.distance_to_object = 0.5;
+  object_collision.velocity = 0.1;
+  ASSERT_TRUE(aeb_node_->hasCollision(longitudinal_velocity, object_collision));
+
+  ObjectData object_no_collision;
+  object_no_collision.distance_to_object = 10.0;
+  object_no_collision.velocity = 0.1;
+  ASSERT_FALSE(aeb_node_->hasCollision(longitudinal_velocity, object_no_collision));
+}
+
+TEST_F(TestAEB, checkImuPathGeneration)
+{
+  constexpr double longitudinal_velocity = 3.0;
+  constexpr double yaw_rate = 0.05;
+  const auto imu_path = aeb_node_->generateEgoPath(longitudinal_velocity, yaw_rate);
+  ASSERT_FALSE(imu_path.empty());
+
+  const double dt = aeb_node_->imu_prediction_time_interval_;
+  const double horizon = aeb_node_->imu_prediction_time_horizon_;
+  ASSERT_TRUE(imu_path.size() >= static_cast<size_t>(horizon / dt));
+}
+
+TEST_F(TestAEB, checkIncompleteImuPathGeneration)
+{
+  const double dt = aeb_node_->imu_prediction_time_interval_;
+  const double horizon = aeb_node_->imu_prediction_time_horizon_;
+  const double min_generated_path_length = aeb_node_->min_generated_path_length_;
+  const double slow_velocity = min_generated_path_length / (2.0 * horizon);
+  constexpr double yaw_rate = 0.05;
+  const auto imu_path = aeb_node_->generateEgoPath(slow_velocity, yaw_rate);
+
+  ASSERT_FALSE(imu_path.empty());
+  ASSERT_TRUE(imu_path.size() >= static_cast<size_t>(horizon / dt));
+  ASSERT_TRUE(autoware::motion_utils::calcArcLength(imu_path) >= min_generated_path_length);
+
+  const auto footprint = aeb_node_->generatePathFootprint(imu_path, 0.0);
+  ASSERT_FALSE(footprint.empty());
+}
+
+}  // namespace autoware::motion::control::autonomous_emergency_braking::test
