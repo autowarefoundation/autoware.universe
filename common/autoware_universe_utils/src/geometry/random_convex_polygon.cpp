8e4952fb69293b474f5bb340301f4cd0cf8bd292
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-07-23 11:41:29 +0900

    fix:clang format
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- common/autoware_universe_utils/src/geometry/random_convex_polygon.cpp
+++ common/autoware_universe_utils/src/geometry/random_convex_polygon.cpp
@@ -30,10 +30,12 @@ struct VectorsWithMin
   double min;
 };
 
-
 VectorsWithMin prepare_coordinate_vectors(
-  const size_t nb_vertices, std::uniform_real_distribution<double> & random_double,// cppcheck-suppress constParameterReference
-  std::uniform_int_distribution<int> & random_bool, std::default_random_engine & random_engine)// cppcheck-suppress constParameterReference
+  const size_t nb_vertices,
+  std::uniform_real_distribution<double> &
+    random_double,                                   // cppcheck-suppress constParameterReference
+  std::uniform_int_distribution<int> & random_bool,  // cppcheck-suppress constParameterReference
+  std::default_random_engine & random_engine)
 {
   std::vector<double> v;
   for (auto i = 0UL; i < nb_vertices; ++i) {
