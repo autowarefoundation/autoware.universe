d1262ef2f7447b389fab54c32fce3ab1201935cc
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-07-23 10:23:56 +0900

    fix:constParameterReference
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- common/autoware_universe_utils/src/geometry/random_convex_polygon.cpp
+++ common/autoware_universe_utils/src/geometry/random_convex_polygon.cpp
@@ -31,8 +31,8 @@ struct VectorsWithMin
 };
 
 VectorsWithMin prepare_coordinate_vectors(
-  const size_t nb_vertices, std::uniform_real_distribution<double> & random_double,
-  std::uniform_int_distribution<int> & random_bool, std::default_random_engine & random_engine)
+  const size_t nb_vertices, const std::uniform_real_distribution<double> & random_double,
+  std::uniform_int_distribution<int> & random_bool, const std::default_random_engine & random_engine)
 {
   std::vector<double> v;
   for (auto i = 0UL; i < nb_vertices; ++i) {
