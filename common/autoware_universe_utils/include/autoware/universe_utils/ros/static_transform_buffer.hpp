d31481c4f32485a6e5a7a62a7eed715fa3df8d1a
amadeuszsz <amadeusz.szymko.2@tier4.jp>
2024-07-26 10:15:28 +0900

    perf(autoware_universe_utils): skip redundant transform
    Signed-off-by: amadeuszsz <amadeusz.szymko.2@tier4.jp>

--- common/autoware_universe_utils/include/autoware/universe_utils/ros/static_transform_buffer.hpp
+++ common/autoware_universe_utils/include/autoware/universe_utils/ros/static_transform_buffer.hpp
@@ -113,6 +113,10 @@ public:
     rclcpp::Node * node, const std::string & target_frame,
     const sensor_msgs::msg::PointCloud2 & cloud_in, sensor_msgs::msg::PointCloud2 & cloud_out)
   {
+    if (target_frame == cloud_in.header.frame_id) {
+      cloud_out = cloud_in;
+      return true;
+    }
     if (
       pcl::getFieldIndex(cloud_in, "x") == -1 || pcl::getFieldIndex(cloud_in, "y") == -1 ||
       pcl::getFieldIndex(cloud_in, "z") == -1) {
