0555ea54cec056d80840515a5a92ee328643a93b
Zhe Shen <lucidshenzhe@gmail.com>
2024-07-29 09:44:07 +0900

    change back to enable_integration
    Signed-off-by: Zhe Shen <lucidshenzhe@gmail.com>

--- control/autoware_pid_longitudinal_controller/test/test_pid.cpp
+++ control/autoware_pid_longitudinal_controller/test/test_pid.cpp
@@ -24,18 +24,18 @@ TEST(TestPID, calculate_pid_output)
   const double dt = 1.0;
   double target = 10.0;
   double current = 0.0;
-  bool erase_integral = true;
+  bool enable_integration = false;
 
   PIDController pid;
 
   // Cannot calculate before initializing gains and limits
-  EXPECT_THROW(pid.calculate(0.0, dt, erase_integral, contributions), std::runtime_error);
+  EXPECT_THROW(pid.calculate(0.0, dt, enable_integration, contributions), std::runtime_error);
 
   pid.setGains(1.0, 1.0, 1.0);
   pid.setLimits(10.0, 0.0, 10.0, 0.0, 10.0, 0.0, 10.0, 0.0);
   double error = target - current;
   while (current != target) {
-    current = pid.calculate(error, dt, erase_integral, contributions);
+    current = pid.calculate(error, dt, enable_integration, contributions);
     EXPECT_EQ(contributions[0], 0.0);  // integral is erased
     EXPECT_EQ(contributions[1], 0.0);  // double integral is erased
     EXPECT_EQ(contributions[2], error);
@@ -45,12 +45,12 @@ TEST(TestPID, calculate_pid_output)
 
   pid.setGains(100.0, 100.0, 100.0);
   pid.setLimits(10.0, -10.0, 10.0, -10.0, 10.0, -10.0, 10.0, -10.0);
-  erase_integral = false;
+  enable_integration = true;
 
   // High errors to force each component to its upper limit
-  EXPECT_EQ(pid.calculate(0.0, dt, erase_integral, contributions), 0.0);
+  EXPECT_EQ(pid.calculate(0.0, dt, enable_integration, contributions), 0.0);
   for (double error = 100.0; error < 1000.0; error += 100.0) {
-    EXPECT_EQ(pid.calculate(error, dt, erase_integral, contributions), 10.0);
+    EXPECT_EQ(pid.calculate(error, dt, enable_integration, contributions), 10.0);
     EXPECT_EQ(contributions[0], 10.0);  // integral is not erased
     EXPECT_EQ(contributions[1], 10.0);  // double integral is not erased
     EXPECT_EQ(contributions[2], 10.0);
@@ -58,9 +58,9 @@ TEST(TestPID, calculate_pid_output)
   pid.reset();
 
   // Low errors to force each component to its lower limit
-  EXPECT_EQ(pid.calculate(0.0, dt, erase_integral, contributions), 0.0);
+  EXPECT_EQ(pid.calculate(0.0, dt, enable_integration, contributions), 0.0);
   for (double error = -100.0; error > -1000.0; error -= 100.0) {
-    EXPECT_EQ(pid.calculate(error, dt, erase_integral, contributions), -10.0);
+    EXPECT_EQ(pid.calculate(error, dt, enable_integration, contributions), -10.0);
     EXPECT_EQ(contributions[0], -10.0);  // integral is not erased
     EXPECT_EQ(contributions[1], -10.0);  // double integral is not erased
     EXPECT_EQ(contributions[2], -10.0);
