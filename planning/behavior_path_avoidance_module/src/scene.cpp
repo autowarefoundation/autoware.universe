paulsohn/autoware_universe_planning_2

feat(static_obstacle_avoidance): show markers when system requests operator support (#7994)
Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

planning/behavior_path_planner/autoware_behavior_path_static_obstacle_avoidance_module/src/scene.cpp
@@ -1386,6 +1386,7 @@ void StaticObstacleAvoidanceModule::updateRTCData()
 
 void StaticObstacleAvoidanceModule::updateInfoMarker(const AvoidancePlanningData & data) const
 {
+  using utils::static_obstacle_avoidance::createAmbiguousObjectsMarkerArray;
   using utils::static_obstacle_avoidance::createStopTargetObjectMarkerArray;
   using utils::static_obstacle_avoidance::createTargetObjectsMarkerArray;
 
@@ -1393,6 +1394,10 @@ void StaticObstacleAvoidanceModule::updateInfoMarker(const AvoidancePlanningData
   appendMarkerArray(
     createTargetObjectsMarkerArray(data.target_objects, "target_objects"), &info_marker_);
   appendMarkerArray(createStopTargetObjectMarkerArray(data), &info_marker_);
+  appendMarkerArray(
+    createAmbiguousObjectsMarkerArray(
+      data.target_objects, getEgoPose(), parameters_->policy_ambiguous_vehicle),
+    &info_marker_);
 }
 
 void StaticObstacleAvoidanceModule::updateDebugMarker(

autowarefoundation/autoware.universe

planning/behavior_path_avoidance_module/src/scene.cpp
@@ -1385,6 +1385,7 @@ void AvoidanceModule::updateRTCData()
 
 void AvoidanceModule::updateInfoMarker(const AvoidancePlanningData & data) const
 {
+  using utils::avoidance::createAmbiguousObjectsMarkerArray;
   using utils::avoidance::createStopTargetObjectMarkerArray;
   using utils::avoidance::createTargetObjectsMarkerArray;
 
@@ -1392,6 +1393,10 @@ void AvoidanceModule::updateInfoMarker(const AvoidancePlanningData & data) const
   appendMarkerArray(
     createTargetObjectsMarkerArray(data.target_objects, "target_objects"), &info_marker_);
   appendMarkerArray(createStopTargetObjectMarkerArray(data), &info_marker_);
+  appendMarkerArray(
+    createAmbiguousObjectsMarkerArray(
+      data.target_objects, getEgoPose(), parameters_->policy_ambiguous_vehicle),
+    &info_marker_);
 }
 
 void AvoidanceModule::updateDebugMarker(