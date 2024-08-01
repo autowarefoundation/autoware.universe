0c8d1a32d0cfeae7b28ba36b36874c087be64b87
Maxime CLEMENT <maxime.clement@tier4.jp>
2024-08-02 07:47:13 +0900

    small cleanup
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- common/autoware_universe_utils/src/geometry/sat_2d.cpp
+++ common/autoware_universe_utils/src/geometry/sat_2d.cpp
@@ -19,25 +19,19 @@ namespace autoware::universe_utils::sat
 
 namespace
 {
-/// @brief calculate the dot product between two points
-double dot_product(const Point2d & p1, const Point2d & p2)
-{
-  return p1.x() * p2.x() + p1.y() * p2.y();
-}
-
 /// @brief calculate the edge normal of two points
 Point2d edge_normal(const Point2d & p1, const Point2d & p2)
 {
-  return Point2d(p2.y() - p1.y(), p1.x() - p2.x());
+  return {p2.y() - p1.y(), p1.x() - p2.x()};
 }
 
 /// @brief project a polygon onto an axis and return the minimum and maximum values
 std::pair<double, double> project_polygon(const Polygon2d & poly, const Point2d & axis)
 {
-  double min = dot_product(poly.outer()[0], axis);
+  double min = poly.outer()[0].dot(axis);
   double max = min;
   for (const auto & point : poly.outer()) {
-    double projection = dot_product(point, axis);
+    double projection = point.dot(axis);
     if (projection < min) {
       min = projection;
     }
@@ -66,10 +60,10 @@ bool intersects(const Polygon2d & convex_polygon1, const Polygon2d & convex_poly
 {
   // check all edges of polygon1
   for (size_t i = 0; i < convex_polygon1.outer().size(); ++i) {
-    size_t next_i = (i + 1) % convex_polygon1.outer().size();
-    Point2d edge = edge_normal(convex_polygon1.outer()[i], convex_polygon1.outer()[next_i]);
-    auto projection1 = project_polygon(convex_polygon1, edge);
-    auto projection2 = project_polygon(convex_polygon2, edge);
+    const size_t next_i = (i + 1) % convex_polygon1.outer().size();
+    const Point2d edge = edge_normal(convex_polygon1.outer()[i], convex_polygon1.outer()[next_i]);
+    const auto projection1 = project_polygon(convex_polygon1, edge);
+    const auto projection2 = project_polygon(convex_polygon2, edge);
     if (!projections_overlap(projection1, projection2)) {
       return false;
     }
@@ -77,10 +71,10 @@ bool intersects(const Polygon2d & convex_polygon1, const Polygon2d & convex_poly
 
   // check all edges of polygon2
   for (size_t i = 0; i < convex_polygon2.outer().size(); ++i) {
-    size_t next_i = (i + 1) % convex_polygon2.outer().size();
-    Point2d edge = edge_normal(convex_polygon2.outer()[i], convex_polygon2.outer()[next_i]);
-    auto projection1 = project_polygon(convex_polygon1, edge);
-    auto projection2 = project_polygon(convex_polygon2, edge);
+    const size_t next_i = (i + 1) % convex_polygon2.outer().size();
+    const Point2d edge = edge_normal(convex_polygon2.outer()[i], convex_polygon2.outer()[next_i]);
+    const auto projection1 = project_polygon(convex_polygon1, edge);
+    const auto projection2 = project_polygon(convex_polygon2, edge);
     if (!projections_overlap(projection1, projection2)) {
       return false;
     }
