960fbc3e5b9113d79b272dc6cf64d35e2a7e29fa
Takayuki Murooka <takayuki5168@gmail.com>
2024-08-02 11:39:33 +0900

    Update control/autoware_pure_pursuit/src/autoware_pure_pursuit_core/interpolate.cpp

--- control/autoware_pure_pursuit/src/autoware_pure_pursuit_core/interpolate.cpp
+++ control/autoware_pure_pursuit/src/autoware_pure_pursuit_core/interpolate.cpp
@@ -114,7 +114,7 @@ void SplineInterpolate::generateSpline(const std::vector<double> & x)
   w_.push_back(0.0);
 
   for (int i = 1; i < N - 1; i++) {
-    double tmp = 1.0 / (4.0 - w_[i - 1]);
+    const double tmp = 1.0 / (4.0 - w_[i - 1]);
     c_[i] = (c_[i] - c_[i - 1]) * tmp;
     w_.push_back(tmp);
   }
