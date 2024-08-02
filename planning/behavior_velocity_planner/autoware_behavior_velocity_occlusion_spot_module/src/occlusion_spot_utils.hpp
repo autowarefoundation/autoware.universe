9f5a0bc4c5780023c736f4022fe5a83513fdb89b
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-08-02 13:33:49 +0900

    fix:uninitMemberVar
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_occlusion_spot_module/src/occlusion_spot_utils.hpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_occlusion_spot_module/src/occlusion_spot_utils.hpp
@@ -158,12 +158,15 @@ struct ObstacleInfo
  */
 struct PossibleCollisionInfo
 {
-  ObstacleInfo obstacle_info{};                        // For hidden obstacle
+  ObstacleInfo obstacle_info;                        // For hidden obstacle
   PathPoint collision_with_margin;                     // For baselink at collision point
   Pose collision_pose;                                 // only use this for debugging
   Pose intersection_pose;                              // For egp path and hidden obstacle
   lanelet::ArcCoordinates arc_lane_dist_at_collision;  // For ego distance to obstacle in s-d
-  PossibleCollisionInfo() = default;
+  PossibleCollisionInfo()
+    : obstacle_info(), collision_with_margin(), collision_pose(), intersection_pose(), arc_lane_dist_at_collision()
+  {
+  }
   PossibleCollisionInfo(
     const ObstacleInfo & obstacle_info, const PathPoint & collision_with_margin,
     const Pose & intersection_pose, const lanelet::ArcCoordinates & arc_lane_dist_to_occlusion)
