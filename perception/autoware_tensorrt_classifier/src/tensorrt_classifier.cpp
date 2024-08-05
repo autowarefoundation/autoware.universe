f8e68c9c55d3ef41d8327aa63dcc1a3bc47a0506
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-08-05 19:12:53 +0900

    fix:unreadVariable
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- perception/autoware_tensorrt_classifier/src/tensorrt_classifier.cpp
+++ perception/autoware_tensorrt_classifier/src/tensorrt_classifier.cpp
@@ -123,8 +123,6 @@ TrtClassifier::TrtClassifier(
     autoware::tensorrt_classifier::ImageStream stream(
       max_batch_size, input_dims, calibration_images);
     fs::path calibration_table{model_path};
-    // cppcheck-suppress unreadVariable
-    std::string calibName = "";
     std::string ext = "";
     if (build_config.calib_type_str == "Entropy") {
       ext = "EntropyV2-";
