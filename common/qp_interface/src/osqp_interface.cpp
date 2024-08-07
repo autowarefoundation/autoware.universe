0cfc79d1cd57d10d4a97331ab73736957ff9c2c7
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-08-08 07:41:35 +0900

    fix:unreadVariable
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- common/qp_interface/src/osqp_interface.cpp
+++ common/qp_interface/src/osqp_interface.cpp
@@ -354,7 +354,6 @@ std::vector<double> OSQPInterface::optimizeImpl()
   osqp_solve(work_.get());
 
   double * sol_x = work_->solution->x;
-  double * sol_y = work_->solution->y;
   std::vector<double> sol_primal(sol_x, sol_x + param_n_);
 
   latest_work_info_ = *(work_->info);
