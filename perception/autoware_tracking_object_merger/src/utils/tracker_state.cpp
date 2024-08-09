7159c1208c9dd9049f203ed67d50ff0a6a0ecd80
Kaan Çolak <kaancolak95@gmail.com>
2024-08-09 10:19:26 +0900

    fix(autoware_tracking_object_merger): add merge frame
    Signed-off-by: Kaan Çolak <kaancolak95@gmail.com>

--- perception/autoware_tracking_object_merger/src/utils/tracker_state.cpp
+++ perception/autoware_tracking_object_merger/src/utils/tracker_state.cpp
@@ -304,7 +304,7 @@ TrackerState::~TrackerState()
 }
 
 TrackedObjects getTrackedObjectsFromTrackerStates(
-  std::vector<TrackerState> & tracker_states, const rclcpp::Time & current_time)
+  std::vector<TrackerState> & tracker_states, const rclcpp::Time & current_time, const std::string & frame_id)
 {
   TrackedObjects tracked_objects;
 
@@ -325,7 +325,7 @@ TrackedObjects getTrackedObjectsFromTrackerStates(
 
   // update header
   tracked_objects.header.stamp = current_time;
-  tracked_objects.header.frame_id = "map";  // TODO(yoshiri): get frame_id from input
+  tracked_objects.header.frame_id = frame_id;
   return tracked_objects;
 }
 
