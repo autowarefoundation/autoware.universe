43d311868703b583cf161e9daca994b80692a129
liu cui <cynthia.liu@autocore.ai>
2024-08-09 16:32:34 +0800

    feat(autoware_tensorrt_bevdet): add new 3d object detection method
    Signed-off-by: liu cui <cynthia.liu@autocore.ai>

--- perception/autoware_tensorrt_bevdet/src/data.cpp
+++ perception/autoware_tensorrt_bevdet/src/data.cpp
@@ -79,7 +79,7 @@ int read_image(const std::string & image_names, std::vector<char> & raw_data)
   return EXIT_SUCCESS;
 }
 
-int read_sample(std::vector<std::string> & imgs_file, std::vector<std::vector<char>> & imgs_data)
+int read_sample(const std::vector<std::string> & imgs_file, std::vector<std::vector<char>> & imgs_data)
 {
   imgs_data.resize(imgs_file.size());
 
