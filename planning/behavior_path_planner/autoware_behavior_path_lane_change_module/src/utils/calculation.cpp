3a211b930e04090af034bfb3239c187026cc003b
Muhammad Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
2024-08-16 20:29:13 +0900

    modify lane change requested condition
    Signed-off-by: Muhammad Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/utils/calculation.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/utils/calculation.cpp
@@ -42,4 +42,111 @@ double calc_dist_from_pose_to_terminal_end(
   }
   return utils::getDistanceToEndOfLane(src_pose, lanes);
 }
+
+double calc_stopping_distance(const LCParamPtr & lc_param_ptr)
+{
+  // v^2 = u^2 + 2ad
+  const auto min_lc_vel = lc_param_ptr->minimum_lane_changing_velocity;
+  const auto min_lon_acc = lc_param_ptr->min_longitudinal_acc;
+  const auto min_back_dist = std::abs((min_lc_vel * min_lc_vel) / (2 * min_lon_acc));
+
+  const auto param_back_dist = lc_param_ptr->backward_length_buffer_for_end_of_lane;
+  return std::max(min_back_dist, param_back_dist);
+}
+
+double calc_dist_to_last_fit_width(
+  const lanelet::ConstLanelets lanelets, const Pose & src_pose,
+  const BehaviorPathPlannerParameters & bpp_param, const double margin)
+{
+  if (lanelets.empty()) return 0.0;
+
+  const auto lane_polygon = lanelets.back().polygon2d().basicPolygon();
+  const auto center_line = lanelet::utils::generateFineCenterline(lanelets.back(), 1.0);
+
+  if (center_line.size() <= 1) return 0.0;
+
+  universe_utils::LineString2d line_string;
+  line_string.reserve(center_line.size() - 1);
+  std::for_each(center_line.begin() + 1, center_line.end(), [&line_string](const auto & point) {
+    boost::geometry::append(line_string, universe_utils::Point2d(point.x(), point.y()));
+  });
+
+  const double buffer_distance = 0.5 * bpp_param.vehicle_width + margin;
+  universe_utils::MultiPolygon2d center_line_polygon;
+  namespace strategy = boost::geometry::strategy::buffer;
+  boost::geometry::buffer(
+    line_string, center_line_polygon, strategy::distance_symmetric<double>(buffer_distance),
+    strategy::side_straight(), strategy::join_miter(), strategy::end_flat(),
+    strategy::point_square());
+
+  if (center_line_polygon.empty()) return 0.0;
+
+  std::vector<universe_utils::Point2d> intersection_points;
+  boost::geometry::intersection(lane_polygon, center_line_polygon, intersection_points);
+
+  if (intersection_points.empty()) {
+    return utils::getDistanceToEndOfLane(src_pose, lanelets);
+  }
+
+  Pose pose;
+  double distance = std::numeric_limits<double>::max();
+  for (const auto & point : intersection_points) {
+    pose.position.x = boost::geometry::get<0>(point);
+    pose.position.y = boost::geometry::get<1>(point);
+    distance = std::min(distance, utils::getSignedDistance(src_pose, pose, lanelets));
+  }
+
+  return std::max(distance - (bpp_param.base_link2front + margin), 0.0);
+}
+
+double calc_maximum_prepare_length(const CommonDataPtr & common_data_ptr)
+{
+  const auto max_prepare_duration = common_data_ptr->lc_param_ptr->lane_change_prepare_duration;
+  const auto ego_max_speed = common_data_ptr->bpp_param_ptr->max_vel;
+
+  return max_prepare_duration * ego_max_speed;
+}
+
+double calc_ego_dist_to_lanes_start(
+  const CommonDataPtr & common_data_ptr, const lanelet::ConstLanelets & current_lanes,
+  const lanelet::ConstLanelets & target_lanes)
+{
+  if (target_lanes.empty()) {
+    return 0.0;
+  }
+
+  const auto & target_left_bound = target_lanes.front().leftBound();
+  const auto & target_right_bound = target_lanes.front().rightBound();
+  if (target_left_bound.empty() || target_right_bound.empty()) {
+    return 0.0;
+  }
+
+  const auto & route_handler_ptr = common_data_ptr->route_handler_ptr;
+
+  if (!route_handler_ptr) {
+    return 0.0;
+  }
+
+  if (current_lanes.empty()) {
+    return 0.0;
+  }
+
+  const auto path =
+    route_handler_ptr->getCenterLinePath(current_lanes, 0.0, std::numeric_limits<double>::max());
+
+  if (path.points.empty()) {
+    return 0.0;
+  }
+
+  const auto left_front_pt = lanelet::utils::conversion::toGeomMsgPt(target_left_bound.front());
+  const auto right_front_pt = lanelet::utils::conversion::toGeomMsgPt(target_right_bound.front());
+
+  const auto ego_position = common_data_ptr->get_ego_pose().position;
+  const auto left_dist =
+    motion_utils::calcSignedArcLength(path.points, ego_position, left_front_pt);
+  const auto right_dist =
+    motion_utils::calcSignedArcLength(path.points, ego_position, right_front_pt);
+
+  return std::min(left_dist, right_dist);
+}
 }  // namespace autoware::behavior_path_planner::utils::lane_change::calculation
