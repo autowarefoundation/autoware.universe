9d3fb7ff15c1fbd004c546b3f6db3e361b99a2dc
vividf <yihsiang.fang@tier4.jp>
2024-08-20 17:13:18 +0900

    chore: add explicit cast
    Signed-off-by: vividf <yihsiang.fang@tier4.jp>

--- sensing/autoware_pointcloud_preprocessor/src/downsample_filter/random_downsample_filter_node.cpp
+++ sensing/autoware_pointcloud_preprocessor/src/downsample_filter/random_downsample_filter_node.cpp
@@ -58,7 +58,7 @@ RandomDownsampleFilterComponent::RandomDownsampleFilterComponent(
 {
   // set initial parameters
   {
-    sample_num_ = declare_parameter<int64_t>("sample_num");
+    sample_num_ = static_cast<size_t>(declare_parameter<int64_t>("sample_num"));
   }
 
   using std::placeholders::_1;
