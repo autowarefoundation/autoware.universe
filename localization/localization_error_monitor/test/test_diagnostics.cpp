3a34c68014588378ce51c0b7cb4e4a176923f7cd
RyuYamamoto <ryu.yamamoto@tier4.jp>
2024-08-21 13:04:13 +0900

    fix: rename include file
    Signed-off-by: RyuYamamoto <ryu.yamamoto@tier4.jp>

--- localization/localization_error_monitor/test/test_diagnostics.cpp
+++ localization/localization_error_monitor/test/test_diagnostics.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "localization_error_monitor/diagnostics.hpp"
+#include "localization_error_monitor/diagnostics_helper.hpp"
 
 #include <gtest/gtest.h>
 
