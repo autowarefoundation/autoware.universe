3ada5c0f92c53de570f9be4b2c0627dcaa37c6eb
bathteayo <105347690+bathteayo@users.noreply.github.com>
2024-08-26 19:07:33 +0900

    Revert "fix: unusedFunction"
    This reverts commit abe836e3556fc9738a4e7c2144b73fbefb040452.
    
    Signed-off-by: bathteayo <105347690+bathteayo@users.noreply.github.com>

--- system/autoware_component_monitor/src/unit_conversions.hpp
+++ system/autoware_component_monitor/src/unit_conversions.hpp
@@ -27,6 +27,42 @@ std::uint64_t kib_to_bytes(T kibibytes)
   return static_cast<std::uint64_t>(kibibytes * 1024);
 }
 
+template <typename T>
+std::uint64_t mib_to_bytes(T mebibytes)
+{
+  static_assert(std::is_arithmetic<T>::value, "Template parameter must be a numeric type");
+  return static_cast<std::uint64_t>(mebibytes * 1024 * 1024);
+}
+
+template <typename T>
+std::uint64_t gib_to_bytes(T gibibytes)
+{
+  static_assert(std::is_arithmetic<T>::value, "Template parameter must be a numeric type");
+  return static_cast<std::uint64_t>(gibibytes * 1024ULL * 1024ULL * 1024ULL);
+}
+
+template <typename T>
+std::uint64_t tib_to_bytes(T tebibytes)
+{
+  static_assert(std::is_arithmetic<T>::value, "Template parameter must be a numeric type");
+  return static_cast<std::uint64_t>(tebibytes * 1024ULL * 1024ULL * 1024ULL * 1024ULL);
+}
+
+template <typename T>
+std::uint64_t pib_to_bytes(T pebibytes)
+{
+  static_assert(std::is_arithmetic<T>::value, "Template parameter must be a numeric type");
+  return static_cast<std::uint64_t>(pebibytes * 1024ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL);
+}
+
+template <typename T>
+std::uint64_t eib_to_bytes(T exbibytes)
+{
+  static_assert(std::is_arithmetic<T>::value, "Template parameter must be a numeric type");
+  return static_cast<std::uint64_t>(
+    exbibytes * 1024ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL);
+}
+
 }  // namespace autoware::component_monitor::unit_conversions
 
 #endif  // UNIT_CONVERSIONS_HPP_
