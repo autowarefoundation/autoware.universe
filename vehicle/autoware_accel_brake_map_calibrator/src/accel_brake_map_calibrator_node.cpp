a383a8a5d23726986c066a9e321f4f223675ee7c
N-Eiki <eiki.nagata.2@tier4.jp>
2024-08-26 12:02:11 +0900

    add take validation
    Signed-off-by: N-Eiki <eiki.nagata.2@tier4.jp>

--- vehicle/autoware_accel_brake_map_calibrator/src/accel_brake_map_calibrator_node.cpp
+++ vehicle/autoware_accel_brake_map_calibrator/src/accel_brake_map_calibrator_node.cpp
@@ -266,15 +266,16 @@ bool AccelBrakeMapCalibrator::get_current_pitch_from_tf(double * pitch)
 bool AccelBrakeMapCalibrator::take_data()
 {
   // take data from subscribers
-  // take actuation data
-
   if (accel_brake_value_source_ == ACCEL_BRAKE_SOURCE::STATUS) {
-    ActuationStatusStamped::ConstSharedPtr actuation_status_ptr = actuation_status_sub_.takeData();
-    take_actuation_status(actuation_status_ptr);
+      ActuationStatusStamped::ConstSharedPtr actuation_status_ptr = actuation_status_sub_.takeData();
+      if (!actuation_status_ptr) return false;
+      take_actuation_status(actuation_status_ptr);
   }
+  // take actuation data
   if (accel_brake_value_source_ == ACCEL_BRAKE_SOURCE::COMMAND) {
-    ActuationCommandStamped::ConstSharedPtr actuation_cmd_ptr = actuation_cmd_sub_.takeData();
-    take_actuation_command(actuation_cmd_ptr);
+      ActuationCommandStamped::ConstSharedPtr actuation_cmd_ptr = actuation_cmd_sub_.takeData();
+      if (!actuation_cmd_ptr) return false;
+      take_actuation_command(actuation_cmd_ptr);
   }
 
   // take velocity data
