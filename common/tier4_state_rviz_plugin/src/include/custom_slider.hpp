a5ed6451720289519a416bd0dd28b6dd1d6df9a3
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-08-27 16:43:51 +0900

    fix:revert custom slider
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- common/tier4_state_rviz_plugin/src/include/custom_slider.hpp
+++ common/tier4_state_rviz_plugin/src/include/custom_slider.hpp
@@ -27,6 +27,9 @@ class CustomSlider : public QSlider
 
 public:
   explicit CustomSlider(Qt::Orientation orientation, QWidget * parent = nullptr);
+
+protected:
+  void paintEvent(QPaintEvent * event) override;
 };
 
 #endif  // CUSTOM_SLIDER_HPP_
