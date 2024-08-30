ad17e074b545d51e5a0b3ca7da7fa1cc299f3072
amadeuszsz <amadeusz.szymko.2@tier4.jp>
2024-08-30 10:37:12 +0900

    fix(autoware_pointcloud_preprocessor): vector map TFs are dynamic
    Signed-off-by: amadeuszsz <amadeusz.szymko.2@tier4.jp>

--- sensing/autoware_pointcloud_preprocessor/src/vector_map_filter/vector_map_inside_area_filter_node.cpp
+++ sensing/autoware_pointcloud_preprocessor/src/vector_map_filter/vector_map_inside_area_filter_node.cpp
@@ -65,7 +65,7 @@ namespace autoware::pointcloud_preprocessor
 {
 VectorMapInsideAreaFilterComponent::VectorMapInsideAreaFilterComponent(
   const rclcpp::NodeOptions & node_options)
-: Filter("VectorMapInsideAreaFilter", node_options, true)
+: Filter("VectorMapInsideAreaFilter", node_options, false)
 {
   polygon_type_ = declare_parameter<std::string>("polygon_type");
 
