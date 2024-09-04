4a449d1187adff44fc03bb3db0549c4609a197ff
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-09-04 11:38:51 +0900

    fix:copyright
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- perception/autoware_shape_estimation/lib/corrector/reference_shape_size_corrector.cpp
+++ perception/autoware_shape_estimation/lib/corrector/reference_shape_size_corrector.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Autoware Foundation. All rights reserved.
+// Copyright 2024 TIER IV, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
