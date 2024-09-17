332b4a55b62f94303114445e3f5ffd99801515e7
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-09-17 16:23:43 +0900

    fix:cppcheck CI
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

---
+++ cppcheck_macros.hpp
@@ -0,0 +1,12 @@
+#ifndef CPPCHECK_MACROS_HPP
+#define CPPCHECK_MACROS_HPP
+
+// Empty macro definition (so that cppcheck ignores the macro)
+#define PLUGINLIB_EXPORT_CLASS(class_type, base_class)
+
+// Empty macro definition for Qt
+#define slots
+#define Q_SLOTS
+
+#endif // CPPCHECK_MACROS_HPP
+
