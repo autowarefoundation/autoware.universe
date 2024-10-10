39f989e001d4fd50d655a9c7f9bc27369f707b97
Barış Zeren <baris@leodrive.ai>
2024-10-10 18:21:34 +0300

    chore: update fusion node
    Signed-off-by: Barış Zeren <baris@leodrive.ai>

--- perception/autoware_image_projection_based_fusion/include/autoware/image_projection_based_fusion/mask_cluster_fusion/node.hpp
+++ perception/autoware_image_projection_based_fusion/include/autoware/image_projection_based_fusion/mask_cluster_fusion/node.hpp
@@ -7,40 +7,28 @@
 #include <string>
 #include <vector>
 
-namespace autoware::image_projection_based_fusion {
-    const std::map<std::string, uint8_t> IOU_MODE_MAP{{"iou",   0},
-                                                      {"iou_x", 1},
-                                                      {"iou_y", 2}};
+namespace autoware::image_projection_based_fusion
+{
+class MaskClusterFusionNode
+: public FusionNode<DetectedObjectsWithFeature, DetectedObjectWithFeature, SegmentationMask>
+{
+public:
+  explicit MaskClusterFusionNode(const rclcpp::NodeOptions & options);
 
-    class MaskClusterFusionNode
-            : public FusionNode<DetectedObjectsWithFeature, DetectedObjectWithFeature, SegmentationMask> {
-    public:
-        explicit MaskClusterFusionNode(const rclcpp::NodeOptions &options);
+protected:
+  void preprocess(DetectedObjectsWithFeature & output_msg) override;
 
-    protected:
-        void preprocess(DetectedObjectsWithFeature &output_msg) override;
+  void postprocess(DetectedObjectsWithFeature & output_msg) override;
 
-        void postprocess(DetectedObjectsWithFeature &output_msg) override;
+  void fuseOnSingleImage(
+    const DetectedObjectsWithFeature & input_cluster_msg, const std::size_t image_id,
+    const SegmentationMask & input_mask_msg, const sensor_msgs::msg::CameraInfo & camera_info,
+    DetectedObjectsWithFeature & output_object_msg) override;
 
-        void fuseOnSingleImage(
-                const DetectedObjectsWithFeature &input_cluster_msg, const std::size_t image_id,
-                const SegmentationMask &input_mask_msg,
-                const sensor_msgs::msg::CameraInfo &camera_info,
-                DetectedObjectsWithFeature &output_object_msg) override;
+  bool out_of_scope(const DetectedObjectWithFeature & obj) override;
 
-        bool out_of_scope(const DetectedObjectWithFeature & obj) override;
-
-        std::string trust_object_iou_mode_{"iou"};
-        bool use_cluster_semantic_type_{false};
-        bool only_allow_inside_cluster_{false};
-        double roi_scale_factor_{1.1};
-        double iou_threshold_{0.0};
-        double unknown_iou_threshold_{0.0};
-        const float min_roi_existence_prob_ =
-                0.1;  // keep small value to lessen affect on merger object stage
-        bool remove_unknown_;
-        double fusion_distance_;
-        double trust_object_distance_;
-        std::string non_trust_object_iou_mode_{"iou_x"};
-    };
-} // namespace autoware::image_projection_based_fusion
+  double fusion_distance_;
+  double fusion_ratio_;
+  bool remove_unknown_;
+};
+}  // namespace autoware::image_projection_based_fusion
