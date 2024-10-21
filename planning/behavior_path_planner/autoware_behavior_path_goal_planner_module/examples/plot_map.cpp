3a380cfec0fe4ec2bb910dc12bab30a5e0c05009
Mamoru Sobue <mamoru.sobue@tier4.jp>
2024-10-21 16:40:08 +0900

    plot samples
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/examples/plot_map.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/examples/plot_map.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "autoware/behavior_path_goal_planner_module/goal_searcher.hpp"
+
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <autoware/behavior_path_goal_planner_module/manager.hpp>
 #include <autoware/behavior_path_goal_planner_module/pull_over_planner/shift_pull_over.hpp>
@@ -82,24 +84,39 @@ void plot_lanelet(
     Kwargs("color"_a = "black", "linewidth"_a = linewidth, "linestyle"_a = "dashed"));
 }
 
-int main(int argc, char ** argv)
+std::shared_ptr<const autoware::behavior_path_planner::PlannerData> instantiate_planner_data(
+  rclcpp::Node::SharedPtr node, const std::string & map_path,
+  const autoware_planning_msgs::msg::LaneletRoute & route_msg)
 {
-  rclcpp::init(argc, argv);
-
-  const auto road_shoulder_test_map_path =
-    ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner_common") +
-    "/test_map/road_shoulder/lanelet2_map.osm";
-
   lanelet::ErrorMessages errors{};
   lanelet::projection::MGRSProjector projector{};
-  const lanelet::LaneletMapPtr lanelet_map_ptr =
-    lanelet::load(road_shoulder_test_map_path, projector, &errors);
+  const lanelet::LaneletMapPtr lanelet_map_ptr = lanelet::load(map_path, projector, &errors);
   if (!errors.empty()) {
     for (const auto & error : errors) {
       std::cout << error << std::endl;
     }
-    return 1;
+    return nullptr;
   }
+  autoware_map_msgs::msg::LaneletMapBin map_bin;
+  lanelet::utils::conversion::toBinMsg(
+    lanelet_map_ptr, &map_bin);  // TODO(soblin): pass lanelet_map_ptr to RouteHandler
+
+  auto planner_data = std::make_shared<autoware::behavior_path_planner::PlannerData>();
+  planner_data->init_parameters(*node);
+  planner_data->route_handler->setMap(map_bin);
+  planner_data->route_handler->setRoute(route_msg);
+
+  nav_msgs::msg::Odometry odom;
+  odom.pose.pose = route_msg.start_pose;
+  auto odometry = std::make_shared<const nav_msgs::msg::Odometry>(odom);
+  planner_data->self_odometry = odometry;
+
+  return planner_data;
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
 
   autoware_planning_msgs::msg::LaneletRoute route_msg;
   route_msg.start_pose = geometry_msgs::build<geometry_msgs::msg::Pose>()
@@ -122,15 +139,6 @@ int main(int argc, char ** argv)
                                          .y(0.0)
                                          .z(-0.3361155457734493)
                                          .w(0.9418207578352774));
-  /*
-  route_msg.goal_pose =
-    geometry_msgs::build<geometry_msgs::msg::Pose>()
-      .position(
-        geometry_msgs::build<geometry_msgs::msg::Point>().x(568.836731).y(437.871904).z(100.0))
-      .orientation(
-        geometry_msgs::build<geometry_msgs::msg::Quaternion>().x(0.0).y(0.0).z(-0.292125).w(
-          0.956380));
-  */
 
   route_msg.segments = std::vector<autoware_planning_msgs::msg::LaneletSegment>{
     autoware_planning_msgs::build<autoware_planning_msgs::msg::LaneletSegment>()
@@ -174,43 +182,7 @@ int main(int argc, char ** argv)
       }),
   };
   route_msg.allow_modification = false;
-  autoware_map_msgs::msg::LaneletMapBin map_bin;
-  lanelet::utils::conversion::toBinMsg(
-    lanelet_map_ptr, &map_bin);  // TODO(soblin): pass lanelet_map_ptr to RouteHandler
 
-  /*
-    reference:
-    https://github.com/ros2/rclcpp/blob/ee94bc63e4ce47a502891480a2796b53d54fcdfc/rclcpp/test/rclcpp/test_parameter_client.cpp#L927
-   */
-  /*
-  auto node = rclcpp::Node::make_shared(
-    "node", "namespace", rclcpp::NodeOptions().allow_undeclared_parameters(true));
-  auto param_node = std::make_shared<rclcpp::AsyncParametersClient>(node, "");
-  {
-    auto future = param_node->load_parameters(
-      ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner") +
-      "/config/behavior_path_planner.param.yaml");
-    if (rclcpp::spin_until_future_complete(node, future) != rclcpp::FutureReturnCode::SUCCESS) {
-      RCLCPP_INFO(node->get_logger(), "failed to load behavior_path_planner.param.yaml");
-    }
-  }
-  {
-    auto future = param_node->load_parameters(
-      ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner") +
-      "/config/drivable_area_expansion.param.yaml");
-    if (rclcpp::spin_until_future_complete(node, future) != rclcpp::FutureReturnCode::SUCCESS) {
-      RCLCPP_INFO(node->get_logger(), "failed to load drivable_area_expansion.param.yaml");
-    }
-  }
-  {
-    auto future = param_node->load_parameters(
-      ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner") +
-      "/config/scene_module_manager.param.yaml");
-    if (rclcpp::spin_until_future_complete(node, future) != rclcpp::FutureReturnCode::SUCCESS) {
-      RCLCPP_INFO(node->get_logger(), "failed to load scene_module_manager.param.yaml");
-    }
-  }
-  */
   auto node_options = rclcpp::NodeOptions{};
   node_options.parameter_overrides(
     std::vector<rclcpp::Parameter>{{"launch_modules", std::vector<std::string>{}}});
@@ -238,18 +210,15 @@ int main(int argc, char ** argv)
       "/config/goal_planner.param.yaml"});
   auto node = rclcpp::Node::make_shared("plot_map", node_options);
 
-  auto planner_data = std::make_shared<autoware::behavior_path_planner::PlannerData>();
-  planner_data->init_parameters(*node);
-  planner_data->route_handler->setMap(map_bin);
-  planner_data->route_handler->setRoute(route_msg);
-  nav_msgs::msg::Odometry odom;
-  odom.pose.pose = route_msg.start_pose;
-  auto odometry = std::make_shared<const nav_msgs::msg::Odometry>(odom);
-  planner_data->self_odometry = odometry;
+  auto planner_data = instantiate_planner_data(
+    node,
+    ament_index_cpp::get_package_share_directory("autoware_behavior_path_planner_common") +
+      "/test_map/road_shoulder/lanelet2_map.osm",
+    route_msg);
+
   lanelet::ConstLanelet current_route_lanelet;
   planner_data->route_handler->getClosestLaneletWithinRoute(
     route_msg.start_pose, &current_route_lanelet);
-  std::cout << "current_route_lanelet is " << current_route_lanelet.id() << std::endl;
   const auto reference_path =
     autoware::behavior_path_planner::utils::getReferencePath(current_route_lanelet, planner_data);
   auto goal_planner_parameter =
@@ -262,12 +231,9 @@ int main(int argc, char ** argv)
   lane_departure_checker_params.footprint_extra_margin =
     goal_planner_parameter.lane_departure_check_expansion_margin;
   lane_departure_checker.setParam(lane_departure_checker_params);
-  autoware::behavior_path_planner::GoalCandidate goal_candidate;
-  goal_candidate.goal_pose = route_msg.goal_pose;
-  auto shift_pull_over_planner = autoware::behavior_path_planner::ShiftPullOver(
-    *node, goal_planner_parameter, lane_departure_checker);
-  const auto pull_over_path_opt =
-    shift_pull_over_planner.plan(goal_candidate, 0, planner_data, reference_path);
+  autoware::behavior_path_planner::GoalSearcher goal_searcher(
+    goal_planner_parameter, vehicle_info.createFootprint());
+  const auto goal_candidates = goal_searcher.search(planner_data);
 
   pybind11::scoped_interpreter guard{};
   auto plt = matplotlibcpp17::pyplot::import();
@@ -276,16 +242,21 @@ int main(int argc, char ** argv)
   const std::vector<lanelet::Id> ids{17756, 17757, 18493, 18494, 18496, 18497,
                                      18492, 18495, 18498, 18499, 18500};
   for (const auto & id : ids) {
-    const auto lanelet = lanelet_map_ptr->laneletLayer.get(id);
+    const auto lanelet = planner_data->route_handler->getLaneletMapPtr()->laneletLayer.get(id);
     plot_lanelet(ax, lanelet);
   }
 
   plot_path_with_lane_id(ax, reference_path.path);
-  std::cout << pull_over_path_opt.has_value() << std::endl;
-  if (pull_over_path_opt) {
-    const auto & pull_over_path = pull_over_path_opt.value();
-    const auto & full_path = pull_over_path.full_path();
-    plot_path_with_lane_id(ax, full_path);
+  for (const auto & goal_candidate : goal_candidates) {
+    auto shift_pull_over_planner = autoware::behavior_path_planner::ShiftPullOver(
+      *node, goal_planner_parameter, lane_departure_checker);
+    const auto pull_over_path_opt =
+      shift_pull_over_planner.plan(goal_candidate, 0, planner_data, reference_path);
+    if (pull_over_path_opt) {
+      const auto & pull_over_path = pull_over_path_opt.value();
+      const auto & full_path = pull_over_path.full_path();
+      plot_path_with_lane_id(ax, full_path);
+    }
   }
   ax.set_aspect(Args("equal"));
   plt.show();
