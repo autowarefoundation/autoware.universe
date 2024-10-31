2fce034399c1d85e04f02c75c19c84eb2be8cae8
Mamoru Sobue <hilo.soblin@gmail.com>
2024-10-31 15:57:03 +0900

    Apply suggestions from code review

--- perception/autoware_map_based_prediction/include/map_based_prediction/predictor_vru.hpp
+++ perception/autoware_map_based_prediction/include/map_based_prediction/predictor_vru.hpp
@@ -109,7 +109,7 @@ lanelet::LaneletMapUPtr fence_layer_{nullptr};
   std::map<std::pair<std::string, lanelet::Id>, rclcpp::Time> stopped_times_against_green_;
   std::unordered_map<std::string, std::string> known_matches_;
   std::unordered_map<lanelet::Id, TrafficLightGroup> traffic_signal_id_map_;
-  lanelet::LaneletMapUPtr fence_layer_{nullptr};
+  
 
   // Parameters
   double prediction_time_horizon_;
