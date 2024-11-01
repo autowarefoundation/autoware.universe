2c35de8989eb96508070ac9ba4ad5a5d785f3f27
Yuki Takagi <yuki.takagi@tier4.jp>
2024-11-01 19:37:51 +0900

    po
    Signed-off-by: Yuki Takagi <yuki.takagi@tier4.jp>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_crosswalk_module/src/scene_crosswalk.hpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_crosswalk_module/src/scene_crosswalk.hpp
@@ -62,6 +62,10 @@ using tier4_planning_msgs::msg::PathWithLaneId;
 
 namespace
 {
+/**
+ * @param x_vec strictly monotone increasing is required
+ * @param y_vec same number of elements as key_map is reqired
+ */
 double interpolateEgoPassMargin(
   const std::vector<double> & x_vec, const std::vector<double> & y_vec, const double target_x)
 {
@@ -77,6 +81,10 @@ double interpolateEgoPassMargin(
   return y_vec.back();
 }
 
+/**
+ * @param key_map strictly monotone increasing is required
+ * @param value_map same number of elements as key_map is reqired
+ */
 double InterpolateMap(
   const std::vector<double> & key_map, const std::vector<double> & value_map, const double query)
 {
