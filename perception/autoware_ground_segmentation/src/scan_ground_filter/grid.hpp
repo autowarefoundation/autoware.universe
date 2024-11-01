c944db9ff812fd16d360e02460abb30bfdb437cc
Taekjin LEE <taekjin.lee@tier4.jp>
2024-11-01 17:40:41 +0900

    feat: Add Cell class and Grid class for grid-based segmentation
    Signed-off-by: Taekjin LEE <taekjin.lee@tier4.jp>

--- perception/autoware_ground_segmentation/src/scan_ground_filter/grid.hpp
+++ perception/autoware_ground_segmentation/src/scan_ground_filter/grid.hpp
@@ -19,6 +19,7 @@
 #include <autoware/universe_utils/math/normalization.hpp>
 
 #include <cmath>
+#include <vector>
 
 namespace autoware::ground_segmentation
 {
@@ -104,6 +105,175 @@ private:
   float mode_switch_grid_id_ = 0.0f;
   float mode_switch_angle_rad_ = 0.0f;
   float grid_id_offset_ = 0.0f;
+};
+
+class Cell
+{
+public:
+  // list of point indices
+  std::vector<size_t> point_indices_;
+
+  // statistics of the points in the cell
+  float avg_height_;
+  float max_height_;
+  float min_height_;
+  float std_dev_height_;
+
+  // method to calculate the statistics
+};
+
+class Grid
+{
+public:
+  Grid(const float origin_x, const float origin_y, const float origin_z)
+  : origin_x_(origin_x), origin_y_(origin_y), origin_z_(origin_z)
+  {
+  }
+  ~Grid() = default;
+
+  // given parameters
+  float origin_x_;
+  float origin_y_;
+  float origin_z_;
+  float grid_dist_size_;                // meters
+  float grid_azimuth_size_;             // meters
+  float grid_linearity_switch_radius_;  // meters
+
+  // calculated parameters
+  float grid_radial_limit_;   // meters
+  float grid_dist_size_rad_;  // radians
+  bool is_initialized_ = false;
+
+  void initialize(
+    const float grid_dist_size, const float grid_azimuth_size,
+    const float grid_linearity_switch_radius)
+  {
+    grid_dist_size_ = grid_dist_size;
+    grid_azimuth_size_ = grid_azimuth_size;
+    grid_linearity_switch_radius_ = grid_linearity_switch_radius;
+
+    // calculate grid parameters
+    constexpr float grid_radial_limit_angle = 80.0f;  // degrees
+    grid_radial_limit_ = origin_z_ * tan(grid_radial_limit_angle * M_PI / 180.0f);
+    grid_dist_size_rad_ = std::atan2(grid_linearity_switch_radius_ + grid_dist_size_, origin_z_) -
+                          std::atan2(grid_linearity_switch_radius_, origin_z_);
+
+    // generate grid geometry
+    setGridBoundaries();
+
+    //
+
+    // set initialized flag
+    is_initialized_ = true;
+  }
+
+  // the grid is cylindrical mesh grid
+  // azimuth interval: constant angle
+  // radial interval: constant distance within mode switch radius
+  //                  constant elevation angle outside mode switch radius
+
+  // array of grid boundaries
+  std::vector<float> grid_radial_boundaries_;
+  std::vector<int> azimuth_grids_per_radial_;
+  std::vector<float> grid_azimuth_boundaries_;
+  std::vector<int> grid_id_offsets_;
+
+  // generate grid geometry
+  void setGridBoundaries()
+  {
+    // radial boundaries
+    {
+      int idx = 0;
+      float radius = 0.0f;
+      // 1. within mode switch radius, constant distance
+      while (radius < grid_linearity_switch_radius_) {
+        grid_radial_boundaries_.push_back(radius);
+        idx++;
+        radius = static_cast<float>(idx) * grid_dist_size_;
+      }
+      // 2. outside mode switch radius, constant elevation angle
+      while (radius < grid_radial_limit_ && radius > 0) {
+        grid_radial_boundaries_.push_back(radius);
+        const float angle = std::atan2(radius, origin_z_);
+        radius = tan(angle + grid_dist_size_rad_) * origin_z_;
+      }
+    }
+
+    // azimuth boundaries
+    {
+      int idx = 0;
+      float azimuth = 0.0f;
+      const int azimuth_grid_num = std::ceil(360 / grid_azimuth_size_);
+      const float azimuth_interval_evened = 2.0f * M_PI / azimuth_grid_num;
+      grid_azimuth_boundaries_.push_back(azimuth);
+      // constant angle interval
+      while (azimuth < 2.0f * M_PI) {
+        idx++;
+        azimuth = static_cast<float>(idx) * azimuth_interval_evened;
+        grid_azimuth_boundaries_.push_back(azimuth);
+      }
+
+      // number of azimuth grids per radial grid, which is constant
+      azimuth_grids_per_radial_.resize(grid_radial_boundaries_.size());
+      for (size_t i = 0; i < grid_radial_boundaries_.size(); ++i) {
+        azimuth_grids_per_radial_[i] = azimuth_grid_num;
+      }
+    }
+
+    // accumulate the number of azimuth grids per radial grid, set offset for each radial grid
+    grid_id_offsets_.resize(grid_radial_boundaries_.size());
+    grid_id_offsets_[0] = 0;
+    for (size_t i = 1; i < grid_radial_boundaries_.size(); ++i) {
+      grid_id_offsets_[i] = grid_id_offsets_[i - 1] + azimuth_grids_per_radial_[i - 1];
+    }
+  }
+
+  // method to determine the grid id of a point
+  // -1 means out of range
+  // range limit is horizon angle
+  int getGridIdx(const float radius, const float azimuth) const
+  {
+    // check if initialized
+    if (!is_initialized_) {
+      throw std::runtime_error("Grid is not initialized.");
+    }
+
+    // check if the point is within the grid
+    if (radius > grid_radial_limit_) {
+      return -1;
+    }
+    if (radius < 0) {
+      return -1;
+    }
+
+    // normalize azimuth, make sure it is within [0, 2pi)
+    float azimuth_norm = universe_utils::normalizeRadian(azimuth, 0.0f);
+
+    // determine the grid id
+    int grid_rad_idx = 0;
+    // radial grid id
+    for (size_t i = 0; i < grid_radial_boundaries_.size(); ++i) {
+      if (radius < grid_radial_boundaries_[i]) {
+        grid_rad_idx = i;
+        break;
+      }
+    }
+    int grid_az_idx = 0;
+    // azimuth grid id
+    for (size_t i = 0; i < grid_azimuth_boundaries_.size(); ++i) {
+      if (azimuth_norm < grid_azimuth_boundaries_[i]) {
+        grid_az_idx = i;
+        break;
+      }
+    }
+
+    const int grid_id = grid_id_offsets_[grid_rad_idx] + grid_az_idx;
+
+    return grid_id;
+  }
+
+  // list of cells
+  std::vector<Cell> cells_;
 };
 
 }  // namespace autoware::ground_segmentation
