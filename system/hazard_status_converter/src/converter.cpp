c065dbb8f624ca5412680e76cc6511d5314e8714
TetsuKawa <kawaguchitnon@icloud.com>
2024-11-07 17:57:34 +0900

    feat: add subscriber for emergency_holding
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- system/hazard_status_converter/src/converter.cpp
+++ system/hazard_status_converter/src/converter.cpp
@@ -117,7 +117,9 @@ void Converter::on_update(DiagGraph::ConstSharedPtr graph)
   hazard.stamp = graph->updated_stamp();
   hazard.status.level = get_system_level(hazard.status);
   hazard.status.emergency = hazard.status.level == HazardStatus::SINGLE_POINT_FAULT;
-  hazard.status.emergency_holding = false;
+
+  auto is_emergency_holding = sub_emergency_holding_.takeData();
+  hazard.status.emergency_holding = is_emergency_holding == nullptr ? false : is_emergency_holding->holding;
   pub_hazard_->publish(hazard);
 }
 
--- system/hazard_status_converter/src/converter.hpp
+++ system/hazard_status_converter/src/converter.hpp
@@ -16,9 +16,11 @@
 #define CONVERTER_HPP_
 
 #include <diagnostic_graph_utils/subscription.hpp>
+#include <autoware/universe_utils/ros/polling_subscriber.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 #include <autoware_system_msgs/msg/hazard_status_stamped.hpp>
+#include <tier4_system_msgs/msg/emergency_holding_state.hpp>
 
 #include <unordered_set>
 
@@ -38,6 +40,8 @@ private:
   void on_update(DiagGraph::ConstSharedPtr graph);
   diagnostic_graph_utils::DiagGraphSubscription sub_graph_;
   rclcpp::Publisher<HazardStatusStamped>::SharedPtr pub_hazard_;
+  autoware::universe_utils::InterProcessPollingSubscriber<tier4_system_msgs::msg::EmergencyHoldingState>
+    sub_emergency_holding_{this, "~/input/emergency_holding"};
 
   DiagUnit * auto_mode_root_;
   std::unordered_set<DiagUnit *> auto_mode_tree_;
