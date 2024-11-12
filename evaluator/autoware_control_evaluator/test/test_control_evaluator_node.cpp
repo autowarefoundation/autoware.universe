5cd47a78bcd7879811952724a49ff101b55e8eed
Kem (TiankuiXian) <1041084556@qq.com>
2024-11-12 17:24:37 +0900

    feat(tier4_metric_msgs): apply tier4_metric_msgs for scenario_simulator_v2_adapter, control_evaluator, planning_evaluator, autonomous_emergency_braking, obstacle_cruise_planner, motion_velocity_planner, processing_time_checker (#9180)
    * first commit
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * fix building errs.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * change diagnostic messages to metric messages for publishing decision.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * fix bug about motion_velocity_planner
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * change the diagnostic msg to metric msg in autoware_obstacle_cruise_planner.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * tmp save for planning_evaluator
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * change the topic to which metrics published to.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * fix typo.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * remove unnesessary publishing of metrics.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * mke planning_evaluator publish msg of MetricArray instead of Diags.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * update aeb with metric type for decision.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * fix some bug
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * remove autoware_evaluator_utils package.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * remove diagnostic_msgs dependency of planning_evaluator
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * use metric_msgs for autoware_processing_time_checker.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * rewrite diagnostic_convertor to scenario_simulator_v2_adapter, supporting metric_msgs.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * pre-commit and fix typo
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * publish metrics even if there is no metric in the MetricArray.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * modify the metric name of processing_time.
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * update unit test for test_planning/control_evaluator
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    * manual pre-commit
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>
    
    ---------
    
    Signed-off-by: xtk8532704 <1041084556@qq.com>

--- evaluator/autoware_control_evaluator/test/test_control_evaluator_node.cpp
+++ evaluator/autoware_control_evaluator/test/test_control_evaluator_node.cpp
@@ -21,8 +21,8 @@
 #include <autoware/control_evaluator/control_evaluator_node.hpp>
 
 #include "autoware_planning_msgs/msg/trajectory.hpp"
-#include "diagnostic_msgs/msg/diagnostic_array.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
+#include <tier4_metric_msgs/msg/metric_array.hpp>
 
 #include "boost/lexical_cast.hpp"
 
@@ -36,7 +36,7 @@
 using EvalNode = control_diagnostics::ControlEvaluatorNode;
 using Trajectory = autoware_planning_msgs::msg::Trajectory;
 using TrajectoryPoint = autoware_planning_msgs::msg::TrajectoryPoint;
-using diagnostic_msgs::msg::DiagnosticArray;
+using MetricArrayMsg = tier4_metric_msgs::msg::MetricArray;
 using nav_msgs::msg::Odometry;
 
 constexpr double epsilon = 1e-6;
@@ -77,14 +77,15 @@ protected:
 
   void setTargetMetric(const std::string & metric_str)
   {
-    const auto is_target_metric = [metric_str](const auto & status) {
-      return status.name == metric_str;
+    const auto is_target_metric = [metric_str](const auto & metric) {
+      return metric.name == metric_str;
     };
-    metric_sub_ = rclcpp::create_subscription<DiagnosticArray>(
-      dummy_node, "/control_evaluator/metrics", 1, [=](const DiagnosticArray::ConstSharedPtr msg) {
-        const auto it = std::find_if(msg->status.begin(), msg->status.end(), is_target_metric);
-        if (it != msg->status.end()) {
-          metric_value_ = boost::lexical_cast<double>(it->values[0].value);
+    metric_sub_ = rclcpp::create_subscription<MetricArrayMsg>(
+      dummy_node, "/control_evaluator/metrics", 1, [=](const MetricArrayMsg::ConstSharedPtr msg) {
+        const auto it =
+          std::find_if(msg->metric_array.begin(), msg->metric_array.end(), is_target_metric);
+        if (it != msg->metric_array.end()) {
+          metric_value_ = boost::lexical_cast<double>(it->value);
           metric_updated_ = true;
         }
       });
@@ -153,7 +154,7 @@ protected:
   // Trajectory publishers
   rclcpp::Publisher<Trajectory>::SharedPtr traj_pub_;
   rclcpp::Publisher<Odometry>::SharedPtr odom_pub_;
-  rclcpp::Subscription<DiagnosticArray>::SharedPtr metric_sub_;
+  rclcpp::Subscription<MetricArrayMsg>::SharedPtr metric_sub_;
   // TF broadcaster
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 };
