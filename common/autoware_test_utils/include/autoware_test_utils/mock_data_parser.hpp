e9e4cffb467a84422996b67b04bd0e9c4c40d2ea
Mamoru Sobue <mamoru.sobue@tier4.jp>
2024-11-15 20:18:27 +0900

    add TrackedObjects
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- common/autoware_test_utils/include/autoware_test_utils/mock_data_parser.hpp
+++ common/autoware_test_utils/include/autoware_test_utils/mock_data_parser.hpp
@@ -50,6 +50,7 @@ using autoware_perception_msgs::msg::PredictedPath;
 using autoware_perception_msgs::msg::Shape;
 using autoware_perception_msgs::msg::TrackedObject;
 using autoware_perception_msgs::msg::TrackedObjectKinematics;
+using autoware_perception_msgs::msg::TrackedObjects;
 using autoware_perception_msgs::msg::TrafficLightElement;
 using autoware_perception_msgs::msg::TrafficLightGroup;
 using autoware_perception_msgs::msg::TrafficLightGroupArray;
@@ -166,6 +167,9 @@ TrackedObjectKinematics parse(const YAML::Node & node);
 
 template <>
 TrackedObject parse(const YAML::Node & node);
+
+template <>
+TrackedObjects parse(const YAML::Node & node);
 
 template <>
 TrafficLightGroupArray parse(const YAML::Node & node);
