3d969677decf12fdaeca15016ac9c82e8b1fdba0
Zulfaqar Azmi <93502286+zulfaqar-azmi-t4@users.noreply.github.com>
2024-11-22 10:35:13 +0900

    Update planning/behavior_path_planner/autoware_behavior_path_planner_common/include/autoware/behavior_path_planner_common/utils/path_safety_checker/objects_filtering.hpp
    Co-authored-by: mkquda <168697710+mkquda@users.noreply.github.com>
    Signed-off-by: Zulfaqar Azmi <zulfaqar.azmi@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_planner_common/include/autoware/behavior_path_planner_common/utils/path_safety_checker/objects_filtering.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_planner_common/include/autoware/behavior_path_planner_common/utils/path_safety_checker/objects_filtering.hpp
@@ -42,7 +42,7 @@ using tier4_planning_msgs::msg::PathPointWithLaneId;
  * @param max_velocity Upper bound
  * @return Returns true when the object is within a certain velocity range.
  */
-bool velocity_filter(const Twist & object, double velocity_threshold, double max_velocity);
+bool velocity_filter(const Twist & object_twist, double velocity_threshold, double max_velocity);
 
 /**
  * @brief Filters object based on position.
