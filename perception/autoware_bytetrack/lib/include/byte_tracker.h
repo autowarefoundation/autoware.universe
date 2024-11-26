0c1cdb4142f00e016a04b563c61c5cbb054c1fe1
kobayu858 <yutaro.kobayashi@tier4.jp>
2024-11-26 14:10:24 +0900

    fix: fmt
    Signed-off-by: kobayu858 <yutaro.kobayashi@tier4.jp>

--- perception/autoware_bytetrack/lib/include/byte_tracker.h
+++ perception/autoware_bytetrack/lib/include/byte_tracker.h
@@ -40,8 +40,8 @@
 
 #include "strack.h"
 
-#include <vector>
 #include <limits>
+#include <vector>
 
 struct ByteTrackObject
 {
@@ -84,8 +84,8 @@ private:
 
   double lapjv(
     const std::vector<std::vector<float>> & cost, std::vector<int> & rowsol,
-    std::vector<int> & colsol, bool extend_cost = false, float cost_limit = std::numeric_limits<float>::max(),
-    bool return_cost = true);
+    std::vector<int> & colsol, bool extend_cost = false,
+    float cost_limit = std::numeric_limits<float>::max(), bool return_cost = true);
 
 private:
   float track_thresh;
