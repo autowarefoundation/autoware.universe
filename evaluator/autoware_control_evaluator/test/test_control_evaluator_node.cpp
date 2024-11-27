3b9d82da78614decc1790c3e2190828814cb0c06
xtk8532704 <1041084556@qq.com>
2024-11-27 15:07:04 +0900

    fix unit test bug.
    Signed-off-by: xtk8532704 <1041084556@qq.com>

--- evaluator/autoware_control_evaluator/test/test_control_evaluator_node.cpp
+++ evaluator/autoware_control_evaluator/test/test_control_evaluator_node.cpp
@@ -53,7 +53,8 @@ protected:
     const auto share_dir =
       ament_index_cpp::get_package_share_directory("autoware_control_evaluator");
     options.arguments(
-      {"--ros-args", "--params-file", share_dir + "/param/control_evaluator.defaults.yaml"});
+      {"--ros-args", "--params-file", share_dir + "/param/control_evaluator.defaults.yaml", "-p",
+       "output_metrics:=false"});
 
     dummy_node = std::make_shared<rclcpp::Node>("control_evaluator_test_node");
     eval_node = std::make_shared<EvalNode>(options);
