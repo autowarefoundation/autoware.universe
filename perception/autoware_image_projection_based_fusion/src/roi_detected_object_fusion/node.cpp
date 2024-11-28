26cdeea81bdc996c0e4bf9e7a7d0cb75f4ff13e3
Taekjin LEE <taekjin.lee@tier4.jp>
2024-11-28 17:55:43 +0900

    fix: fix roi range calculation in RoiDetectedObjectFusionNode
    Improve the calculation of the region of interest (ROI) in the RoiDetectedObjectFusionNode. The previous code had an issue where the ROI range was not correctly limited to the image edges. This fix ensures that the ROI is within the image boundaries by using the correct comparison operators for the x and y coordinates.
    
    Signed-off-by: Taekjin LEE <taekjin.lee@tier4.jp>

--- perception/autoware_image_projection_based_fusion/src/roi_detected_object_fusion/node.cpp
+++ perception/autoware_image_projection_based_fusion/src/roi_detected_object_fusion/node.cpp
@@ -158,8 +158,8 @@ RoiDetectedObjectFusionNode::generateDetectedObjectRoIs(
       max_y = std::max(proj_point.y(), max_y);
 
       if (
-        proj_point.x() >= 0 && proj_point.x() <= image_width && proj_point.y() >= 0 &&
-        proj_point.y() <= image_height) {
+        proj_point.x() >= 0 && proj_point.x() < image_width && proj_point.y() >= 0 &&
+        proj_point.y() < image_height) {
         point_on_image_cnt++;
 
         if (debugger_) {
@@ -171,18 +171,16 @@ RoiDetectedObjectFusionNode::generateDetectedObjectRoIs(
       continue;
     }
 
-    min_x = std::max(min_x, 0.0);
-    min_y = std::max(min_y, 0.0);
-    max_x = std::min(max_x, image_width);
-    max_y = std::min(max_y, image_height);
+    const uint32_t idx_min_x = std::floor(std::max(min_x, 0.0));
+    const uint32_t idx_min_y = std::floor(std::max(min_y, 0.0));
+    const uint32_t idx_max_x = std::ceil(std::min(max_x, image_width));
+    const uint32_t idx_max_y = std::ceil(std::min(max_y, image_height));
 
     DetectedObjectWithFeature object_roi;
-    object_roi.feature.roi.x_offset = static_cast<std::uint32_t>(min_x);
-    object_roi.feature.roi.y_offset = static_cast<std::uint32_t>(min_y);
-    object_roi.feature.roi.width =
-      static_cast<std::uint32_t>(max_x) - static_cast<std::uint32_t>(min_x);
-    object_roi.feature.roi.height =
-      static_cast<std::uint32_t>(max_y) - static_cast<std::uint32_t>(min_y);
+    object_roi.feature.roi.x_offset = idx_min_x;
+    object_roi.feature.roi.y_offset = idx_min_y;
+    object_roi.feature.roi.width = idx_max_x - idx_min_x;
+    object_roi.feature.roi.height = idx_max_y - idx_min_y;
     object_roi.object = object;
     object_roi_map.insert(std::make_pair(obj_i, object_roi));
 
