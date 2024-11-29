656736114f072da2d4466d357b768c5418ef8559
M. Fatih Cırıt <mfc@autoware.org>
2024-11-29 21:29:08 +0300

    clock_shrd_ptr -> clock_ptr
    Signed-off-by: M. Fatih Cırıt <mfc@autoware.org>

--- planning/autoware_freespace_planning_algorithms/test/src/test_freespace_planning_algorithms.cpp
+++ planning/autoware_freespace_planning_algorithms/test/src/test_freespace_planning_algorithms.cpp
@@ -231,9 +231,9 @@ std::unique_ptr<fpa::AbstractPlanningAlgorithm> configure_astar(bool use_multi)
     obstacle_distance_weight,
     goal_lat_distance_weight};
 
-  auto clock_shrd_ptr = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
+  auto clock_ptr = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
   auto algo = std::make_unique<fpa::AstarSearch>(
-    planner_common_param, vehicle_shape, astar_param, clock_shrd_ptr);
+    planner_common_param, vehicle_shape, astar_param, clock_ptr);
   return algo;
 }
 
@@ -247,9 +247,9 @@ std::unique_ptr<fpa::AbstractPlanningAlgorithm> configure_rrtstar(bool informed,
   const double max_planning_time = 200;
   const auto rrtstar_param = fpa::RRTStarParam{update, informed, max_planning_time, mu, margin};
 
-  auto clock_shrd_ptr = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
+  auto clock_ptr = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
   auto algo = std::make_unique<fpa::RRTStar>(
-    planner_common_param, vehicle_shape, rrtstar_param, clock_shrd_ptr);
+    planner_common_param, vehicle_shape, rrtstar_param, clock_ptr);
   return algo;
 }
 
