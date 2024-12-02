815abd93a997812a853c8b75c435d76696a51942
Mamoru Sobue <hilo.soblin@gmail.com>
2024-12-02 12:01:22 +0900

    refactor(goal_planner): move unnecessary member functions (#9522)
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/include/autoware/behavior_path_goal_planner_module/goal_planner_module.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_goal_planner_module/include/autoware/behavior_path_goal_planner_module/goal_planner_module.hpp
@@ -135,6 +135,43 @@ struct PullOverContextData
   std::optional<rclcpp::Time> last_path_idx_increment_time;
 };
 
+bool isOnModifiedGoal(
+  const Pose & current_pose, const std::optional<GoalCandidate> & modified_goal_opt,
+  const GoalPlannerParameters & parameters);
+
+bool hasPreviousModulePathShapeChanged(
+  const BehaviorModuleOutput & previous_module_output,
+  const BehaviorModuleOutput & last_previous_module_output);
+bool hasDeviatedFromLastPreviousModulePath(
+  const PlannerData & planner_data, const BehaviorModuleOutput & last_previous_module_output);
+bool hasDeviatedFromCurrentPreviousModulePath(
+  const PlannerData & planner_data, const BehaviorModuleOutput & previous_module_output);
+
+bool needPathUpdate(
+  const Pose & current_pose, const double path_update_duration, const rclcpp::Time & now,
+  const std::optional<GoalCandidate> & modified_goal,
+  const std::optional<rclcpp::Time> & selected_time, const GoalPlannerParameters & parameters);
+
+bool checkOccupancyGridCollision(
+  const PathWithLaneId & path,
+  const std::shared_ptr<OccupancyGridBasedCollisionDetector> occupancy_grid_map);
+
+bool isStopped(
+  std::deque<nav_msgs::msg::Odometry::ConstSharedPtr> & odometry_buffer,
+  const nav_msgs::msg::Odometry::ConstSharedPtr self_odometry, const double duration_lower);
+
+// Flag class for managing whether a certain callback is running in multi-threading
+class ScopedFlag
+{
+public:
+  explicit ScopedFlag(std::atomic<bool> & flag) : flag_(flag) { flag_.store(true); }
+
+  ~ScopedFlag() { flag_.store(false); }
+
+private:
+  std::atomic<bool> & flag_;
+};
+
 class GoalPlannerModule : public SceneModuleInterface
 {
 public:
@@ -193,18 +230,6 @@ public:
   void processOnExit() override;
   void updateData() override;
 
-  void updateEgoPredictedPathParams(
-    std::shared_ptr<EgoPredictedPathParams> & ego_predicted_path_params,
-    const std::shared_ptr<GoalPlannerParameters> & goal_planner_params);
-
-  void updateSafetyCheckParams(
-    std::shared_ptr<SafetyCheckParams> & safety_check_params,
-    const std::shared_ptr<GoalPlannerParameters> & goal_planner_params);
-
-  void updateObjectsFilteringParams(
-    std::shared_ptr<ObjectsFilteringParams> & objects_filtering_params,
-    const std::shared_ptr<GoalPlannerParameters> & goal_planner_params);
-
   void postProcess() override;
   void setParameters(const std::shared_ptr<GoalPlannerParameters> & parameters);
   void acceptVisitor(
@@ -256,18 +281,6 @@ private:
   };
   std::optional<GoalPlannerData> gp_planner_data_{std::nullopt};
   std::mutex gp_planner_data_mutex_;
-
-  // Flag class for managing whether a certain callback is running in multi-threading
-  class ScopedFlag
-  {
-  public:
-    explicit ScopedFlag(std::atomic<bool> & flag) : flag_(flag) { flag_.store(true); }
-
-    ~ScopedFlag() { flag_.store(false); }
-
-  private:
-    std::atomic<bool> & flag_;
-  };
 
   /*
    * state transitions and plan function used in each state
@@ -359,11 +372,6 @@ private:
   mutable GoalPlannerDebugData debug_data_;
   mutable PoseWithString debug_stop_pose_with_info_;
 
-  // collision check
-  bool checkOccupancyGridCollision(
-    const PathWithLaneId & path,
-    const std::shared_ptr<OccupancyGridBasedCollisionDetector> occupancy_grid_map) const;
-
   // goal seach
   GoalCandidates generateGoalCandidates() const;
 
@@ -380,18 +388,8 @@ private:
   double calcSignedArcLengthFromEgo(const PathWithLaneId & path, const Pose & pose) const;
 
   // status
-  bool isStopped();
-  bool isStopped(
-    std::deque<nav_msgs::msg::Odometry::ConstSharedPtr> & odometry_buffer, const double time);
   bool hasFinishedCurrentPath(const PullOverContextData & ctx_data);
-  bool isOnModifiedGoal(
-    const Pose & current_pose, const std::optional<GoalCandidate> & modified_goal_opt,
-    const GoalPlannerParameters & parameters) const;
   double calcModuleRequestLength() const;
-  bool needPathUpdate(
-    const Pose & current_pose, const double path_update_duration,
-    const std::optional<GoalCandidate> & modified_goal_opt,
-    const GoalPlannerParameters & parameters) const;
   bool isStuck(
     const PredictedObjects & static_target_objects, const PredictedObjects & dynamic_target_objects,
     const std::shared_ptr<const PlannerData> planner_data,
@@ -442,16 +440,6 @@ private:
   // new turn signal
   TurnSignalInfo calcTurnSignalInfo(const PullOverContextData & context_data);
   std::optional<lanelet::Id> ignore_signal_{std::nullopt};
-
-  bool hasPreviousModulePathShapeChanged(
-    const BehaviorModuleOutput & previous_module_output,
-    const BehaviorModuleOutput & last_previous_module_output) const;
-  bool hasDeviatedFromLastPreviousModulePath(
-    const std::shared_ptr<const PlannerData> planner_data,
-    const BehaviorModuleOutput & last_previous_module_output) const;
-  bool hasDeviatedFromCurrentPreviousModulePath(
-    const std::shared_ptr<const PlannerData> planner_data,
-    const BehaviorModuleOutput & previous_module_output) const;
 
   // timer for generating pull over path candidates in a separate thread
   void onTimer();
