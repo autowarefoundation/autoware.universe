a7e0cf25c41fd8c5c42825f0031f14e3b6d0fedb
Mamoru Sobue <mamoru.sobue@tier4.jp>
2024-12-03 14:55:04 +0900

    test(blind_spot): add unit tests
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_blind_spot_module/src/scene.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_blind_spot_module/src/scene.cpp
@@ -101,8 +101,9 @@ BlindSpotDecision BlindSpotModule::modifyPathVelocityDetail(PathWithLaneId * pat
   }
 
   if (!blind_spot_lanelets_) {
+    const auto lane_ids_upto_intersection = find_lane_ids_upto(input_path, lane_id_);
     const auto blind_spot_lanelets = generateBlindSpotLanelets(
-      planner_data_->route_handler_, turn_direction_, lane_id_, input_path,
+      planner_data_->route_handler_, turn_direction_, lane_ids_upto_intersection,
       planner_param_.ignore_width_from_center_line, planner_param_.adjacent_extend_width,
       planner_param_.opposite_adjacent_extend_width);
     if (!blind_spot_lanelets.empty()) {
@@ -177,27 +178,6 @@ bool BlindSpotModule::modifyPathVelocity(PathWithLaneId * path)
   reactRTCApproval(decision, path);
 
   return true;
-}
-
-static std::optional<size_t> getFirstPointIntersectsLineByFootprint(
-  const lanelet::ConstLineString2d & line, const InterpolatedPathInfo & interpolated_path_info,
-  const autoware::universe_utils::LinearRing2d & footprint, const double vehicle_length)
-{
-  const auto & path_ip = interpolated_path_info.path;
-  const auto [lane_start, lane_end] = interpolated_path_info.lane_id_interval.value();
-  const size_t vehicle_length_idx = static_cast<size_t>(vehicle_length / interpolated_path_info.ds);
-  const size_t start =
-    static_cast<size_t>(std::max<int>(0, static_cast<int>(lane_start) - vehicle_length_idx));
-  const auto line2d = line.basicLineString();
-  for (auto i = start; i <= lane_end; ++i) {
-    const auto & base_pose = path_ip.points.at(i).point.pose;
-    const auto path_footprint = autoware::universe_utils::transformVector(
-      footprint, autoware::universe_utils::pose2transform(base_pose));
-    if (bg::intersects(path_footprint, line2d)) {
-      return std::make_optional<size_t>(i);
-    }
-  }
-  return std::nullopt;
 }
 
 static std::optional<size_t> getDuplicatedPointIdx(
