3246696af7cdeffba64f60a3a9fa1ec25f444656
Mamoru Sobue <mamoru.sobue@tier4.jp>
2024-12-05 11:34:33 +0900

    adding test
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_blind_spot_module/test/test_util.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_blind_spot_module/test/test_util.cpp
@@ -15,22 +15,51 @@
 #include <autoware/behavior_velocity_blind_spot_module/util.hpp>
 #include <autoware/behavior_velocity_planner_common/planner_data.hpp>
 #include <autoware_test_utils/autoware_test_utils.hpp>
+#include <autoware_test_utils/mock_data_parser.hpp>
+
+#include <autoware_perception_msgs/msg/detail/predicted_objects__struct.hpp>
 
 #include <gtest/gtest.h>
 
-using autoware::behavior_velocity_planner::PlannerData;
+using autoware::test_utils::parse;
 
-class TestUtilWithMap : public ::testing::Test
+class TestWithAdjLaneData : public ::testing::Test
 {
 protected:
   void SetUp() override
   {
-    const std::string intersection_map_file =
-      autoware::test_utils::get_absolute_path_to_lanelet_map(
-        "autoware_test_utils", "test_map/intersection/lanelet2_map.osm");
-    return;
+    const auto test_data_file =
+      ament_index_cpp::get_package_share_directory("autoware_behavior_velocity_blind_spot_module") +
+      "/test_data/object_on_adj_lane.yaml";
+    const auto config = YAML::LoadFile(test_data_file);
+    const auto route = parse<autoware_planning_msgs::msg::LaneletRoute>(config["route"]);
+    const auto map_path =
+      autoware::test_utils::resolve_pkg_share_uri(config["map_path_uri"].as<std::string>());
+    if (!map_path) {
+      ASSERT_DEATH({ assert(false); }, "invalid map path");
+    }
+    const auto intersection_map = autoware::test_utils::make_map_bin_msg(map_path.value());
+    route_handler = std::make_shared<autoware::route_handler::RouteHandler>();
+    route_handler->setMap(intersection_map);
+    route_handler->setRoute(route);
+    self_odometry = autoware::test_utils::create_const_shared_ptr(
+      parse<nav_msgs::msg::Odometry>(config["self_odometry"]));
+    dynamic_object = autoware::test_utils::create_const_shared_ptr(
+      parse<autoware_perception_msgs::msg::PredictedObjects>(config["dynamic_object"]));
   }
 
-private:
-  std::shared_ptr<PlannerData> planner_data;
+  std::shared_ptr<autoware::route_handler::RouteHandler> route_handler{};
+  std::shared_ptr<const nav_msgs::msg::Odometry> self_odometry{};
+  std::shared_ptr<const autoware_perception_msgs::msg::PredictedObjects> dynamic_object{};
+  const lanelet::Id lane_id_{2200};
 };
+
+TEST_F(TestWithAdjLaneData, getSiblingStraightLanelet)
+{
+  const auto sibling_straight_lanelet_opt =
+    autoware::behavior_velocity_planner::getSiblingStraightLanelet(
+      route_handler->getLaneletMapPtr()->laneletLayer.get(lane_id_),
+      route_handler->getRoutingGraphPtr());
+  ASSERT_NO_FATAL_FAILURE({ ASSERT_TRUE(sibling_straight_lanelet_opt.has_value()); });
+  EXPECT_EQ(sibling_straight_lanelet_opt.value().id(), 2100);
+}
