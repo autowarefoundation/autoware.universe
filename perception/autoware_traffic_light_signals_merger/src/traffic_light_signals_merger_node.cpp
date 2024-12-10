d799516a7489bf09c23f2bb5b00fe27e6e27b29d
badai-nguyen <dai.nguyen@tier4.jp>
2024-12-10 16:25:45 +0900

    feat: init traffic light signal merger
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

---
+++ perception/autoware_traffic_light_signals_merger/src/traffic_light_signals_merger_node.cpp
@@ -0,0 +1,53 @@
+// Copyright 2023 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "traffic_light_signals_merger_node.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace autoware::traffic_light
+{
+
+TrafficLightSignalsMergerNode::TrafficLightSignalsMergerNode(const rclcpp::NodeOptions & node_options)
+: rclcpp::Node("traffic_light_signals_merger_node", node_options),
+  tf_buffer_(get_clock()),
+  tf_listener_(tf_buffer_),
+  car_signal_sub_(this, "input/car_signals", rclcpp::QoS{1}.get_rmw_qos_profile()),
+  pedestrian_signal_sub_(this, "input/pedestrian_signals", rclcpp::QoS{1}.get_rmw_qos_profile()),
+  sync_(SyncPolicy(10), car_signal_sub_, pedestrian_signal_sub_)
+{
+  using std::placeholders::_1;
+  using std::placeholders::_2;
+  sync_.registerCallback(std::bind(&TrafficLightSignalsMergerNode::signalsCallback, this, _1, _2));
+  pub_traffic_light_signals_ = create_publisher<TrafficLightArray>("output/traffic_light_signals", rclcpp::QoS{1});
+}
+
+void TrafficLightSignalsMergerNode::signalsCallback(
+  const TrafficLightArray::ConstSharedPtr & car_signals_msg,
+  const TrafficLightArray::ConstSharedPtr & pedestrian_signals_msg)
+{
+  TrafficLightArray output;
+  output.header = car_signals_msg->header;
+  output.signals.insert(output.signals.end(), car_signals_msg->signals.begin(), car_signals_msg->signals.end());
+  output.signals.insert(
+    output.signals.end(), pedestrian_signals_msg->signals.begin(), pedestrian_signals_msg->signals.end());
+  pub_traffic_light_signals_->publish(output);
+}
+
+}  // namespace autoware::traffic_light
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(autoware::traffic_light::TrafficLightSignalsMergerNode)
---
+++ perception/autoware_traffic_light_signals_merger/src/traffic_light_signals_merger_node.hpp
@@ -0,0 +1,64 @@
+// Copyright 2023 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TRAFFIC_LIGHT_SIGNALS_MERGER_NODE_HPP_
+#define TRAFFIC_LIGHT_SIGNALS_MERGER_NODE_HPP_
+
+#include "autoware/universe_utils/ros/transform_listener.hpp"
+#include "message_filters/subscriber.h"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/synchronizer.h"
+#include "rclcpp/rclcpp.hpp"
+
+#include "tier4_perception_msgs/msg/traffic_light_array.hpp"
+
+#include <chrono>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace autoware::traffic_light
+{
+using tier4_perception_msgs::msg::TrafficLightArray;
+
+class TrafficLightSignalsMergerNode : public rclcpp::Node
+{
+public:
+  explicit TrafficLightSignalsMergerNode(const rclcpp::NodeOptions & node_options);
+
+private:
+  // Subscriber
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
+
+  message_filters::Subscriber<TrafficLightArray> car_signal_sub_;
+  message_filters::Subscriber<TrafficLightArray> pedestrian_signal_sub_;
+
+  typedef message_filters::sync_policies::ApproximateTime<
+    TrafficLightArray, TrafficLightArray>
+    SyncPolicy;
+  message_filters::Synchronizer<SyncPolicy> sync_;
+
+  void signalsCallback(
+    const TrafficLightArray::ConstSharedPtr & car_signals_msg,
+    const TrafficLightArray::ConstSharedPtr & pedestrian_signals_msg);
+
+  rclcpp::Publisher<TrafficLightArray>::SharedPtr  pub_traffic_light_signals_;
+
+
+};
+
+}  // namespace autoware::traffic_light
+
+#endif  // TRAFFIC_LIGHT_SELECTOR_NODE_HPP_
