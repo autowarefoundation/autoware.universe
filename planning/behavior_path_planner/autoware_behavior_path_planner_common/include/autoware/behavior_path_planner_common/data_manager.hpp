6fa1a723af4307f70964f79bc0a8705ebd9c7ccf
Felix F Xu <84662027+felixf4xu@users.noreply.github.com>
2024-12-11 00:55:31 +0800

    build(behavior_path_planner_common): fix #include <rclcpp/clock.hpp> (#6297)
    Signed-off-by: Felix Xavier <felixfxu@gmail.com>

--- planning/behavior_path_planner/autoware_behavior_path_planner_common/include/autoware/behavior_path_planner_common/data_manager.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_planner_common/include/autoware/behavior_path_planner_common/data_manager.hpp
@@ -23,8 +23,8 @@
 #include <autoware/route_handler/route_handler.hpp>
 #include <autoware_lanelet2_extension/regulatory_elements/Forward.hpp>
 #include <autoware_lanelet2_extension/utility/utilities.hpp>
-#include <rclcpp/rclcpp/clock.hpp>
-#include <rclcpp/rclcpp/time.hpp>
+#include <rclcpp/clock.hpp>
+#include <rclcpp/time.hpp>
 
 #include <autoware_adapi_v1_msgs/msg/operation_mode_state.hpp>
 #include <autoware_perception_msgs/msg/predicted_objects.hpp>
