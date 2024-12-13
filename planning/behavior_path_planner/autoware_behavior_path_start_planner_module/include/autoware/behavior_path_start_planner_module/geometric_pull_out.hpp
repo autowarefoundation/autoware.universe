86ba21f14dc31b53c7c06aba8644651ce120812a
Kyoichi Sugahara <32741405+kyoichi-sugahara@users.noreply.github.com>
2024-12-13 15:17:05 +0900

    test(autoware_behavior_path_start_planner_module): add unit tests for geometric shift pull out planner (#9640)
    * feat(behavior_path_planner): add unit tests for geometric pull-out planner and improve collision check
    
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
    
    * feat(behavior_path_planner): add boolean parameter for divide_pull_out_path and update tests
    
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>
    
    ---------
    
    Signed-off-by: kyoichi-sugahara <kyoichi.sugahara@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_start_planner_module/include/autoware/behavior_path_start_planner_module/geometric_pull_out.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_start_planner_module/include/autoware/behavior_path_start_planner_module/geometric_pull_out.hpp
@@ -45,6 +45,8 @@ public:
   GeometricParallelParking planner_;
   ParallelParkingParameters parallel_parking_parameters_;
   std::shared_ptr<autoware::lane_departure_checker::LaneDepartureChecker> lane_departure_checker_;
+
+  friend class TestGeometricPullOut;
 };
 }  // namespace autoware::behavior_path_planner
 
