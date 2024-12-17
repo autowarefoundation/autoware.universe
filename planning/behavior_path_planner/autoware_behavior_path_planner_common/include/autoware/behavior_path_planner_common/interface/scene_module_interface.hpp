6ffc46e0c1cf7644693b3ee575cdf57ce9dd1268
satoshi-ota <satoshi.ota928@gmail.com>
2024-12-17 13:08:34 +0900

    fix(bpp): don't use velocity/steering factor
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/autoware_behavior_path_planner_common/include/autoware/behavior_path_planner_common/interface/scene_module_interface.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_planner_common/include/autoware/behavior_path_planner_common/interface/scene_module_interface.hpp
@@ -22,8 +22,6 @@
 
 #include <autoware/behavior_path_planner_common/turn_signal_decider.hpp>
 #include <autoware/motion_utils/factor/planning_factor_interface.hpp>
-#include <autoware/motion_utils/factor/steering_factor_interface.hpp>
-#include <autoware/motion_utils/factor/velocity_factor_interface.hpp>
 #include <autoware/motion_utils/marker/marker_helper.hpp>
 #include <autoware/motion_utils/trajectory/path_with_lane_id.hpp>
 #include <autoware/motion_utils/trajectory/trajectory.hpp>
@@ -37,7 +35,6 @@
 #include <magic_enum.hpp>
 #include <rclcpp/rclcpp.hpp>
 
-#include <autoware_adapi_v1_msgs/msg/steering_factor.hpp>
 #include <tier4_planning_msgs/msg/avoidance_debug_msg_array.hpp>
 #include <tier4_planning_msgs/msg/path_with_lane_id.hpp>
 #include <tier4_rtc_msgs/msg/state.hpp>
@@ -56,16 +53,11 @@
 namespace autoware::behavior_path_planner
 {
 using autoware::motion_utils::PlanningFactorInterface;
-using autoware::motion_utils::SteeringFactorInterface;
-using autoware::motion_utils::VelocityFactorInterface;
 using autoware::objects_of_interest_marker_interface::ColorName;
 using autoware::objects_of_interest_marker_interface::ObjectsOfInterestMarkerInterface;
 using autoware::rtc_interface::RTCInterface;
 using autoware::universe_utils::calcOffsetPose;
 using autoware::universe_utils::generateUUID;
-using autoware_adapi_v1_msgs::msg::PlanningBehavior;
-using autoware_adapi_v1_msgs::msg::SteeringFactor;
-using autoware_adapi_v1_msgs::msg::VelocityFactor;
 using tier4_planning_msgs::msg::AvoidanceDebugMsgArray;
 using tier4_planning_msgs::msg::PathWithLaneId;
 using tier4_planning_msgs::msg::PlanningFactor;
@@ -99,26 +91,6 @@ public:
     objects_of_interest_marker_interface_ptr_map_(
       std::move(objects_of_interest_marker_interface_ptr_map)),
     planning_factor_interface_{planning_factor_interface},
-    time_keeper_(std::make_shared<universe_utils::TimeKeeper>())
-  {
-    for (const auto & [module_name, ptr] : rtc_interface_ptr_map_) {
-      uuid_map_.emplace(module_name, generateUUID());
-    }
-  }
-
-  // TODO(satoshi-ota): remove this constructor after all planning factors have been migrated.
-  SceneModuleInterface(
-    const std::string & name, rclcpp::Node & node,
-    std::unordered_map<std::string, std::shared_ptr<RTCInterface>> rtc_interface_ptr_map,
-    std::unordered_map<std::string, std::shared_ptr<ObjectsOfInterestMarkerInterface>>
-      objects_of_interest_marker_interface_ptr_map)
-  : name_{name},
-    logger_{node.get_logger().get_child(name)},
-    clock_{node.get_clock()},
-    rtc_interface_ptr_map_(std::move(rtc_interface_ptr_map)),
-    objects_of_interest_marker_interface_ptr_map_(
-      std::move(objects_of_interest_marker_interface_ptr_map)),
-    planning_factor_interface_{std::make_shared<PlanningFactorInterface>(&node, "tmp")},
     time_keeper_(std::make_shared<universe_utils::TimeKeeper>())
   {
     for (const auto & [module_name, ptr] : rtc_interface_ptr_map_) {
@@ -210,8 +182,6 @@ public:
     unlockNewModuleLaunch();
     unlockOutputPath();
 
-    reset_factor();
-
     processOnExit();
   }
 
@@ -283,16 +253,6 @@ public:
   virtual MarkerArray getModuleVirtualWall() { return MarkerArray(); }
 
   ModuleStatus getCurrentStatus() const { return current_state_; }
-
-  void reset_factor()
-  {
-    steering_factor_interface_.reset();
-    velocity_factor_interface_.reset();
-  }
-
-  auto get_steering_factor() const -> SteeringFactor { return steering_factor_interface_.get(); }
-
-  auto get_velocity_factor() const -> VelocityFactor { return velocity_factor_interface_.get(); }
 
   std::string name() const { return name_; }
 
@@ -584,22 +544,6 @@ protected:
     }
   }
 
-  void setVelocityFactor(const PathWithLaneId & path)
-  {
-    if (stop_pose_.has_value()) {
-      velocity_factor_interface_.set(
-        path.points, getEgoPose(), stop_pose_.value().pose, VelocityFactor::APPROACHING, "stop");
-      return;
-    }
-
-    if (!slow_pose_.has_value()) {
-      return;
-    }
-
-    velocity_factor_interface_.set(
-      path.points, getEgoPose(), slow_pose_.value().pose, VelocityFactor::APPROACHING, "slow down");
-  }
-
   void set_longitudinal_planning_factor(const PathWithLaneId & path)
   {
     if (stop_pose_.has_value()) {
@@ -672,10 +616,6 @@ protected:
     objects_of_interest_marker_interface_ptr_map_;
 
   mutable std::shared_ptr<PlanningFactorInterface> planning_factor_interface_;
-
-  mutable SteeringFactorInterface steering_factor_interface_;
-
-  mutable VelocityFactorInterface velocity_factor_interface_;
 
   mutable PoseWithDetailOpt stop_pose_{std::nullopt};
 
