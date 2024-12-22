bc6e05118482e131813540c9fe8ad7423f8ac07b
M. Fatih Cırıt <mfc@autoware.org>
2024-12-23 02:13:24 +0300

    cuda package - bad change
    Signed-off-by: M. Fatih Cırıt <mfc@autoware.org>

--- perception/autoware_lidar_apollo_instance_segmentation/src/node.cpp
+++ perception/autoware_lidar_apollo_instance_segmentation/src/node.cpp
@@ -17,11 +17,10 @@
 #include "autoware/lidar_apollo_instance_segmentation/detector.hpp"
 
 #include <autoware/universe_utils/ros/debug_publisher.hpp>
-#include <autoware/universe_utils/system/stop_watch.hpp>
+#include <autoware/universe_utils/system/stop_watchbadchange.hpp>
 
 #include <memory>
 
-// good change
 namespace autoware
 {
 namespace lidar_apollo_instance_segmentation
