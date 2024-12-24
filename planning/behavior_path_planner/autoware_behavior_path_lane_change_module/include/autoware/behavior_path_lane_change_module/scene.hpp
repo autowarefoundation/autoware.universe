d6dd9b14b59eba120b06c4e129d2f72ba126731f
mohammad alqudah <alqudah.mohammad@tier4.jp>
2024-12-24 18:05:33 +0900

    specify reason for lane change stop line
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_lane_change_module/include/autoware/behavior_path_lane_change_module/scene.hpp
+++ planning/behavior_path_planner/autoware_behavior_path_lane_change_module/include/autoware/behavior_path_lane_change_module/scene.hpp
@@ -157,7 +157,8 @@ protected:
 
   bool check_prepare_phase() const;
 
-  void set_stop_pose(const double arc_length_to_stop_pose, PathWithLaneId & path);
+  void set_stop_pose(
+    const double arc_length_to_stop_pose, PathWithLaneId & path, const std::string & reason = "");
 
   void updateStopTime();
 
