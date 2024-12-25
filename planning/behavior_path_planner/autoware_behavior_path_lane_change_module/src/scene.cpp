1bfa911904c35d6329b5eec4b6fc33ad8855ad00
mohammad alqudah <alqudah.mohammad@tier4.jp>
2024-12-25 13:49:17 +0900

    consider stopped front objects
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/scene.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/scene.cpp
@@ -947,8 +947,6 @@ FilteredLanesObjects NormalLaneChange::filter_objects() const
   filtered_objects.target_lane_trailing.reserve(reserve_size);
   filtered_objects.others.reserve(reserve_size);
 
-  const auto stopped_obj_vel_th = lane_change_parameters_->safety.th_stopped_object_velocity;
-
   for (const auto & object : objects.objects) {
     auto ext_object = utils::lane_change::transform(object, *lane_change_parameters_);
     const auto & ext_obj_pose = ext_object.initial_pose;
@@ -967,12 +965,8 @@ FilteredLanesObjects NormalLaneChange::filter_objects() const
       continue;
     }
 
-    // TODO(Azu): We have to think about how to remove is_within_vel_th without breaking AW behavior
-    const auto is_moving = velocity_filter(
-      ext_object.initial_twist, stopped_obj_vel_th, std::numeric_limits<double>::max());
-
     if (
-      ahead_of_ego && is_moving && is_before_terminal &&
+      ahead_of_ego && is_before_terminal &&
       !boost::geometry::disjoint(ext_object.initial_polygon, lanes_polygon.current)) {
       // check only the objects that are in front of the ego vehicle
       filtered_objects.current_lane.push_back(ext_object);
