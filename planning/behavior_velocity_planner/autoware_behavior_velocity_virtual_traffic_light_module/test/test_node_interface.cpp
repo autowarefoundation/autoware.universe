b531b4b5402c28459c8cb36cb925ce55b0dc9d80
Takayuki Murooka <takayuki5168@gmail.com>
2024-12-26 17:34:50 +0900

    fix test
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_virtual_traffic_light_module/test/test_node_interface.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_virtual_traffic_light_module/test/test_node_interface.cpp
@@ -30,6 +30,11 @@ TEST(PlanningModuleInterfaceTest, NodeTestWithExceptionPathWithLaneID)
   auto test_target_node = autoware::behavior_velocity_planner::generateNode({});
 
   autoware::behavior_velocity_planner::publishMandatoryTopics(test_manager, test_target_node);
+<<<<<<< HEAD
+=======
+  test_manager->publishVirtualTrafficLightState(
+    test_target_node, "behavior_velocity_planner_node/input/virtual_traffic_light_states");
+>>>>>>> 383f6d25fd (fix test)
 
   // test with nominal path_with_lane_id
   ASSERT_NO_THROW_WITH_ERROR_MSG(test_manager->testWithNominalPathWithLaneId(test_target_node));
@@ -51,6 +56,11 @@ TEST(PlanningModuleInterfaceTest, NodeTestWithOffTrackEgoPose)
   auto test_manager = autoware::behavior_velocity_planner::generateTestManager();
   auto test_target_node = autoware::behavior_velocity_planner::generateNode(plugin_info_vec);
   autoware::behavior_velocity_planner::publishMandatoryTopics(test_manager, test_target_node);
+<<<<<<< HEAD
+=======
+  test_manager->publishVirtualTrafficLightState(
+    test_target_node, "behavior_velocity_planner_node/input/virtual_traffic_light_states");
+>>>>>>> 383f6d25fd (fix test)
 
   // test for normal trajectory
   ASSERT_NO_THROW_WITH_ERROR_MSG(test_manager->testWithNominalPathWithLaneId(test_target_node));
