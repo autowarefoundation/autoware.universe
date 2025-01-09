18d26cd2c4b4c23fd03b9e56dc277bcd24f0de38
vish0012 <vishalchhn42@gmail.com>
2025-01-09 11:18:05 +0900

    feat: tier4_debug_msgs changed to autoware_internal_debug_msgs in files perception/autoware_euclidean_cluster
    Signed-off-by: vish0012 <vishalchhn42@gmail.com>

--- perception/autoware_euclidean_cluster/src/voxel_grid_based_euclidean_cluster_node.cpp
+++ perception/autoware_euclidean_cluster/src/voxel_grid_based_euclidean_cluster_node.cpp
@@ -81,11 +81,11 @@ void VoxelGridBasedEuclideanClusterNode::onPointCloud(
       std::chrono::duration<double, std::milli>(
         std::chrono::nanoseconds((this->get_clock()->now() - output.header.stamp).nanoseconds()))
         .count();
-    debug_publisher_->publish<tier4_debug_msgs::msg::Float64Stamped>(
+    debug_publisher_->publish<autoware_internal_debug_msgs::msg::Float64Stamped>(
       "debug/cyclic_time_ms", cyclic_time_ms);
-    debug_publisher_->publish<tier4_debug_msgs::msg::Float64Stamped>(
+    debug_publisher_->publish<autoware_internal_debug_msgs::msg::Float64Stamped>(
       "debug/processing_time_ms", processing_time_ms);
-    debug_publisher_->publish<tier4_debug_msgs::msg::Float64Stamped>(
+    debug_publisher_->publish<autoware_internal_debug_msgs::msg::Float64Stamped>(
       "debug/pipeline_latency_ms", pipeline_latency_ms);
   }
 }
