8b1445ac8d7429dd636ac7a85622eac99699eb70
TetsuKawa <kawaguchitnon@icloud.com>
2025-01-09 16:29:05 +0900

    feat: add subscriber
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- system/autoware_topic_relay_controller/src/topic_relay_controller_node.cpp
+++ system/autoware_topic_relay_controller/src/topic_relay_controller_node.cpp
@@ -17,7 +17,50 @@ namespace autoware::topic_relay_controller
 {
 TopicRelayController::TopicRelayController(const rclcpp::NodeOptions & options) : Node("topic_relay_controller", options)
 {
-  RCLCPP_INFO(get_logger(), "topic_relay_controller_node started.");
+  // Parameter
+  node_param_.topic = declare_parameter<std::string>("topic");
+  node_param_.remap_topic = declare_parameter<std::string>("remap_topic");
+	node_param_.qos = declare_parameter("qos", 1);
+  node_param_.transient_local = declare_parameter("transient_local", false);
+  node_param_.best_effort = declare_parameter("best_effort", false);
+  node_param_.is_transform = (node_param_.topic == "/tf" || node_param_.topic == "/tf_static");
+
+  if (node_param_.is_transform) {
+    node_param_.frame_id = declare_parameter<std::string>("frame_id");
+    node_param_.child_frame_id = declare_parameter<std::string>("child_frame_id");
+  } else {
+    node_param_.topic_type = declare_parameter<std::string>("topic_type");
+  }
+
+  // Subscriber
+  rclcpp::QoS qos = rclcpp::QoS{node_param_.qos};
+  if (node_param_.transient_local) {
+    qos.transient_local();
+  }
+  if (node_param_.best_effort) {
+    qos.best_effort();
+  }
+
+  if (node_param_.is_transform) {
+    sub_transform_ = this->create_subscription<tf2_msgs::msg::TFMessage>(
+      node_param_.topic, qos, [this](tf2_msgs::msg::TFMessage::ConstSharedPtr msg) {
+        for (const auto & transform : msg->transforms) {
+          if (
+            transform.header.frame_id == node_param_.frame_id &&
+            transform.child_frame_id == node_param_.child_frame_id) {
+            RCLCPP_INFO (
+              this->get_logger(), "Received transform from %s to %s",
+              transform.header.frame_id.c_str(), transform.child_frame_id.c_str());
+          }
+        }
+      });
+  } else {
+    sub_topic_ = this->create_generic_subscription(
+      node_param_.topic, node_param_.topic_type, qos,
+      [this]([[maybe_unused]] std::shared_ptr<rclcpp::SerializedMessage> msg) {
+        RCLCPP_INFO(this->get_logger(), "Received message");
+      });
+  }
 }
 } // namespace autoware::topic_relay_controller
 
--- system/autoware_topic_relay_controller/src/topic_relay_controller_node.hpp
+++ system/autoware_topic_relay_controller/src/topic_relay_controller_node.hpp
@@ -18,14 +18,35 @@
 // ROS 2 core
 #include <rclcpp/rclcpp.hpp>
 
+#include <tf2_msgs/msg/tf_message.hpp>
+
 namespace autoware::topic_relay_controller
 {
+struct NodeParam
+{
+  std::string topic;
+  std::string remap_topic;
+  std::string topic_type;
+  size_t qos;
+  std::string frame_id;
+  std::string child_frame_id;
+  bool transient_local;
+  bool best_effort;
+  bool is_transform;
+};
+
 class TopicRelayController : public rclcpp::Node
 {
 public:
   explicit TopicRelayController(const rclcpp::NodeOptions & options);
 
 private:
+  // Parameter
+  NodeParam node_param_;
+
+  // Subscriber
+  rclcpp::GenericSubscription::SharedPtr sub_topic_;
+  rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr sub_transform_;
 };
 }  // namespace autoware::topic_relay_controller
 
