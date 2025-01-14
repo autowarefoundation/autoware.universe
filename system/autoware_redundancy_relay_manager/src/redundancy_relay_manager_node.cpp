3a111302e268fce72244c9edda2eda25a70d398a
TetsuKawa <kawaguchitnon@icloud.com>
2025-01-15 00:29:20 +0900

    feat: add service
    Signed-off-by: TetsuKawa <kawaguchitnon@icloud.com>

--- system/autoware_redundancy_relay_manager/src/redundancy_relay_manager_node.cpp
+++ system/autoware_redundancy_relay_manager/src/redundancy_relay_manager_node.cpp
@@ -13,11 +13,20 @@
 
 #include "redundancy_relay_manager_node.hpp"
 
+#include <chrono>
+#include <memory>
+#include <string>
+
 namespace autoware::redundancy_relay_manager
 {
 RedundancyRelayManager::RedundancyRelayManager(const rclcpp::NodeOptions & options)
-: Node("redundancy_relay_manager", options)
+: Node("redundancy_relay_manager", options),
+  is_relaying_(true),
+  is_stopped_by_main_(true)
 {
+  // Params
+  node_param_.service_timeout_ms = declare_parameter<int>("service_timeout_ms");
+
   // Subscribers
   sub_operation_mode_state_ = create_subscription<autoware_adapi_v1_msgs::msg::OperationModeState>(
     "~/input/operation_mode/state", rclcpp::QoS{1},
@@ -28,24 +37,94 @@ RedundancyRelayManager::RedundancyRelayManager(const rclcpp::NodeOptions & optio
   sub_sub_election_status_ = create_subscription<tier4_system_msgs::msg::ElectionStatus>(
     "~/input/sub/election/status", rclcpp::QoS{1},
     std::bind(&RedundancyRelayManager::onSubElectionStatus, this, std::placeholders::_1));
+  
+  // Clients
+  client_relay_trajectory_group_ = create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  client_relay_trajectory_ = create_client<tier4_system_msgs::srv::ChangeTopicRelayControl>(
+    "~/output/topic_relay_controller_trajectory/operate", rmw_qos_profile_services_default,
+    client_relay_trajectory_group_);
+  client_relay_pose_with_covariance_group_ =
+    create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  client_relay_pose_with_covariance_ = create_client<tier4_system_msgs::srv::ChangeTopicRelayControl>(
+    "~/output/topic_relay_controller_pose_with_covariance/operate", rmw_qos_profile_services_default,
+    client_relay_pose_with_covariance_group_);
 }
 
 void RedundancyRelayManager::onOperationModeState(
   const autoware_adapi_v1_msgs::msg::OperationModeState::SharedPtr msg)
 {
-  RCLCPP_INFO(get_logger(), "Received operation mode state: %d", msg->mode);
+  operation_mode_state_ = msg;
 }
 
 void RedundancyRelayManager::onMainElectionStatus(
   const tier4_system_msgs::msg::ElectionStatus::SharedPtr msg)
 {
-  RCLCPP_INFO(get_logger(), "Received main election state: %d", msg->path_info);
+  const auto tmp_election_status = main_election_status_;
+  main_election_status_ = msg;
+
+  if (is_relaying_) {
+    if (tmp_election_status == nullptr || operation_mode_state_ == nullptr) return;
+    if (operation_mode_state_->mode != autoware_adapi_v1_msgs::msg::OperationModeState::AUTONOMOUS) return;
+    if (((msg->path_info >> 3) & 0x01) == 1 || ((tmp_election_status->path_info >> 3) & 0x01) == 0) return;
+
+    requestTopicRelayControl(false, client_relay_trajectory_, "topic_relay_control_trajectory");
+    requestTopicRelayControl(false, client_relay_pose_with_covariance_, "topic_relay_control_pose_with_covariance");
+    is_relaying_ = false;
+    is_stopped_by_main_ = true;
+  } else {
+    if (((msg->path_info >> 3) & 0x01) == 1 && is_stopped_by_main_) {
+      requestTopicRelayControl(true, client_relay_trajectory_, "topic_relay_control_trajectory");
+      requestTopicRelayControl(true, client_relay_pose_with_covariance_, "topic_relay_control_pose_with_covariance");
+      is_relaying_ = true;
+    }
+  }
 }
 
 void RedundancyRelayManager::onSubElectionStatus(
   const tier4_system_msgs::msg::ElectionStatus::SharedPtr msg)
 {
-  RCLCPP_INFO(get_logger(), "Received sub election state: %d", msg->path_info);
+  const auto tmp_election_status = sub_election_status_;
+  sub_election_status_ = msg;
+
+  if (is_relaying_) {
+    if (tmp_election_status == nullptr || operation_mode_state_ == nullptr) return;
+    if (operation_mode_state_->mode != autoware_adapi_v1_msgs::msg::OperationModeState::AUTONOMOUS) return;
+    if (((msg->path_info >> 3) & 0x01) == 1 || ((tmp_election_status->path_info >> 3) & 0x01) == 0) return;
+
+    requestTopicRelayControl(false, client_relay_trajectory_, "topic_relay_control_trajectory");
+    requestTopicRelayControl(false, client_relay_pose_with_covariance_, "topic_relay_control_pose_with_covariance");
+    is_relaying_ = false;
+    is_stopped_by_main_ = false;
+  } else {
+    if (((msg->path_info >> 3) & 0x01) == 1 && !is_stopped_by_main_) {
+      requestTopicRelayControl(true, client_relay_trajectory_, "topic_relay_control_trajectory");
+      requestTopicRelayControl(true, client_relay_pose_with_covariance_, "topic_relay_control_pose_with_covariance");
+      is_relaying_ = true;
+    }
+  }
+}
+
+void RedundancyRelayManager::requestTopicRelayControl(
+  const bool relay_on, 
+  rclcpp::Client<tier4_system_msgs::srv::ChangeTopicRelayControl>::SharedPtr client,
+  std::string srv_name)
+{
+  auto request = std::make_shared<tier4_system_msgs::srv::ChangeTopicRelayControl::Request>();
+  request->relay_on = relay_on;
+
+  const auto duration = std::chrono::milliseconds(node_param_.service_timeout_ms);
+  auto future = client->async_send_request(request).future.share();
+
+  if (future.wait_for(duration) == std::future_status::ready) {
+    auto response = future.get();
+    if (response->status.success) {
+      RCLCPP_INFO(get_logger(), "Changed %s relay control: %s", srv_name.c_str(), relay_on ? "ON" : "OFF");
+    } else {
+      RCLCPP_ERROR(get_logger(), "Failed to change %s relay control: %s", srv_name.c_str(), relay_on ? "ON" : "OFF");
+    }
+  } else {
+    RCLCPP_ERROR(get_logger(), "Service timeout %s relay control: %s", srv_name.c_str(), relay_on ? "ON" : "OFF");
+  }
 }
 }  // namespace autoware::redundancy_relay_manager
 
--- system/autoware_redundancy_relay_manager/src/redundancy_relay_manager_node.hpp
+++ system/autoware_redundancy_relay_manager/src/redundancy_relay_manager_node.hpp
@@ -20,26 +20,54 @@
 
 #include <autoware_adapi_v1_msgs/msg/operation_mode_state.hpp>
 #include <tier4_system_msgs/msg/election_status.hpp>
+#include <tier4_system_msgs/srv/change_topic_relay_control.hpp>
+
+#include <string>
 
 namespace autoware::redundancy_relay_manager
 {
+struct NodeParam
+{
+  int service_timeout_ms;
+};
+
 class RedundancyRelayManager : public rclcpp::Node
 {
 public:
   explicit RedundancyRelayManager(const rclcpp::NodeOptions & options);
 
 private:
+  // Params
+  NodeParam node_param_;
   // Subscribers
   rclcpp::Subscription<autoware_adapi_v1_msgs::msg::OperationModeState>::SharedPtr
     sub_operation_mode_state_;
   rclcpp::Subscription<tier4_system_msgs::msg::ElectionStatus>::SharedPtr sub_main_election_status_;
   rclcpp::Subscription<tier4_system_msgs::msg::ElectionStatus>::SharedPtr sub_sub_election_status_;
 
+  // Clients
+  rclcpp::CallbackGroup::SharedPtr client_relay_trajectory_group_;
+  rclcpp::Client<tier4_system_msgs::srv::ChangeTopicRelayControl>::SharedPtr client_relay_trajectory_;
+  rclcpp::CallbackGroup::SharedPtr client_relay_pose_with_covariance_group_;
+  rclcpp::Client<tier4_system_msgs::srv::ChangeTopicRelayControl>::SharedPtr client_relay_pose_with_covariance_;
+
   // Callbacks
   void onOperationModeState(
     const autoware_adapi_v1_msgs::msg::OperationModeState::SharedPtr msg);
   void onMainElectionStatus(const tier4_system_msgs::msg::ElectionStatus::SharedPtr msg);
   void onSubElectionStatus(const tier4_system_msgs::msg::ElectionStatus::SharedPtr msg);
+
+  // State
+  bool is_relaying_;
+  bool is_stopped_by_main_;
+  autoware_adapi_v1_msgs::msg::OperationModeState::SharedPtr operation_mode_state_;
+  tier4_system_msgs::msg::ElectionStatus::SharedPtr main_election_status_;
+  tier4_system_msgs::msg::ElectionStatus::SharedPtr sub_election_status_;
+
+  // Functions
+  void requestTopicRelayControl(
+    const bool relay_on, const rclcpp::Client<tier4_system_msgs::srv::ChangeTopicRelayControl>::SharedPtr client,
+    std::string topic_name);
 };
 }  // namespace autoware::redundancy_relay_manager
 
