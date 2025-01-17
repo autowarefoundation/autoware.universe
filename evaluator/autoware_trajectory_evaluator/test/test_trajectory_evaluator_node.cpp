c5f7a1158cb086a55678c80333dab50e343ecbab
mraditya01 <mraditya01@yahoo.com>
2025-01-17 15:51:09 +0900

    fixed evaluator and added test

--- evaluator/autoware_trajectory_evaluator/test/test_trajectory_evaluator_node.cpp
+++ evaluator/autoware_trajectory_evaluator/test/test_trajectory_evaluator_node.cpp
@@ -1,262 +1,152 @@
-// // Copyright 2021 Tier IV, Inc.
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// #include "ament_index_cpp/get_package_share_directory.hpp"
-// #include "gtest/gtest.h"
-// #include "rclcpp/rclcpp.hpp"
-// #include "rclcpp/time.hpp"
-// #include "tf2_ros/transform_broadcaster.h"
+#include <gtest/gtest.h>
+#include <rclcpp/rclcpp.hpp>
+#include <cmath>
+#include <sstream>
+#include <nav_msgs/msg/odometry.hpp>
+#include "autoware/trajectory_evaluator/trajectory_evaluator_node.hpp"
+#include "autoware_planning_msgs/msg/trajectory.hpp"
+#include "autoware_planning_msgs/msg/trajectory_point.hpp"
+#include "tier4_metric_msgs/msg/metric_array.hpp"  
 
-// #include <autoware/trajectory_evaluator/trajectory_evaluator_node.hpp>
+using MetricArrayMsg = tier4_metric_msgs::msg::MetricArray;
+using MetricMsg = tier4_metric_msgs::msg::Metric;
 
-// #include "autoware_perception_msgs/msg/predicted_objects.hpp"
-// #include "autoware_planning_msgs/msg/trajectory.hpp"
-// #include "diagnostic_msgs/msg/diagnostic_array.hpp"
-// #include "geometry_msgs/msg/transform_stamped.hpp"
+class TrajectoryEvaluatorTests : public ::testing::Test {
+protected:
+    rclcpp::Node::SharedPtr node;
+    rclcpp::Publisher<autoware_planning_msgs::msg::Trajectory>::SharedPtr trajectory_publisher;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odometry_publisher;
+    rclcpp::Subscription<MetricArrayMsg>::SharedPtr metric_subscription;
+    MetricArrayMsg::SharedPtr received_metrics;
+    std::shared_ptr<trajectory_evaluator::TrajectoryEvaluatorNode> evaluator_node;
 
-// #include "boost/lexical_cast.hpp"
+    virtual void SetUp() {
+        rclcpp::init(0, nullptr);
+        node = rclcpp::Node::make_shared("trajectory_evaluator_test_node");
 
-// #include <memory>
-// #include <string>
-// #include <utility>
-// #include <vector>
+        trajectory_publisher = node->create_publisher<autoware_planning_msgs::msg::Trajectory>("/input_trajectory", 10);
+        odometry_publisher = node->create_publisher<nav_msgs::msg::Odometry>("/localization/kinematic_state", 10);
+        metric_subscription = node->create_subscription<MetricArrayMsg>(
+            "/trajectory_metrics", 10,
+            [this](MetricArrayMsg::SharedPtr msg) { this->received_metrics = msg; });
 
-// using EvalNode = planning_diagnostics::PlanningEvaluatorNode;
-// using Trajectory = autoware_planning_msgs::msg::Trajectory;
-// using TrajectoryPoint = autoware_planning_msgs::msg::TrajectoryPoint;
-// using Objects = autoware_perception_msgs::msg::PredictedObjects;
-// using autoware_planning_msgs::msg::PoseWithUuidStamped;
-// using diagnostic_msgs::msg::DiagnosticArray;
-// using nav_msgs::msg::Odometry;
+        evaluator_node = std::make_shared<trajectory_evaluator::TrajectoryEvaluatorNode>(rclcpp::NodeOptions());
+    }
 
-// constexpr double epsilon = 1e-6;
+    virtual void TearDown() {
+        rclcpp::shutdown();
+    }
 
-// class EvalTest : public ::testing::Test
-// {
-// protected:
-//   void SetUp() override
-//   {
-//     rclcpp::init(0, nullptr);
+    void publishTrajectoryAndWait(const autoware_planning_msgs::msg::Trajectory &trajectory, std::chrono::milliseconds timeout) {
+        // received_metrics.reset();
+        trajectory_publisher->publish(trajectory);
 
-//     rclcpp::NodeOptions options;
-//     const auto share_dir =
-//       ament_index_cpp::get_package_share_directory("autoware_planning_evaluator");
-//     options.arguments(
-//       {"--ros-args", "--params-file", share_dir + "/param/planning_evaluator.defaults.yaml"});
+        auto start_time = std::chrono::steady_clock::now();
+        while (std::chrono::steady_clock::now() - start_time < timeout) {
+            rclcpp::spin_some(node);
+            rclcpp::spin_some(evaluator_node);
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        }
+    }
 
-//     dummy_node = std::make_shared<rclcpp::Node>("planning_evaluator_test_node");
-//     eval_node = std::make_shared<EvalNode>(options);
-//     // Enable all logging in the node
-//     auto ret = rcutils_logging_set_logger_level(
-//       dummy_node->get_logger().get_name(), RCUTILS_LOG_SEVERITY_DEBUG);
-//     if (ret != RCUTILS_RET_OK) {
-//       std::cerr << "Failed to set logging severity to DEBUG\n";
-//     }
-//     ret = rcutils_logging_set_logger_level(
-//       eval_node->get_logger().get_name(), RCUTILS_LOG_SEVERITY_DEBUG);
-//     if (ret != RCUTILS_RET_OK) {
-//       std::cerr << "Failed to set logging severity to DEBUG\n";
-//     }
+    void simulateEgoVehicle(
+        const autoware_planning_msgs::msg::Trajectory &trajectory, 
+        double interval_seconds) 
+    {
+        nav_msgs::msg::Odometry odometry_msg;
+        odometry_msg.header.frame_id = "map";
+        odometry_msg.child_frame_id = "base_link";
 
-//     traj_pub_ =
-//       rclcpp::create_publisher<Trajectory>(dummy_node, "/planning_evaluator/input/trajectory",
-//       1);
-//     ref_traj_pub_ = rclcpp::create_publisher<Trajectory>(
-//       dummy_node, "/planning_evaluator/input/reference_trajectory", 1);
-//     objects_pub_ =
-//       rclcpp::create_publisher<Objects>(dummy_node, "/planning_evaluator/input/objects", 1);
-//     odom_pub_ =
-//       rclcpp::create_publisher<Odometry>(dummy_node, "/planning_evaluator/input/odometry", 1);
-//     modified_goal_pub_ = rclcpp::create_publisher<PoseWithUuidStamped>(
-//       dummy_node, "/planning_evaluator/input/modified_goal", 1);
+        rclcpp::Time current_time = node->now(); 
 
-//     tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(dummy_node);
-//     publishEgoPose(0.0, 0.0, 0.0);
-//   }
+        for (size_t i = 1; i < trajectory.points.size(); ++i) {
+            const auto &previous_point = trajectory.points[i - 1];
+            const auto &current_point = trajectory.points[i];
 
-//   ~EvalTest() override { rclcpp::shutdown(); }
+            double dx = current_point.pose.position.x - previous_point.pose.position.x;
+            double dy = current_point.pose.position.y - previous_point.pose.position.y;
+            double distance = std::sqrt(dx * dx + dy * dy);
 
-//   void setTargetMetric(planning_diagnostics::Metric metric)
-//   {
-//     const auto metric_str = planning_diagnostics::metric_to_str.at(metric);
-//     const auto is_target_metric = [metric_str](const auto & status) {
-//       return status.name == metric_str;
-//     };
-//     metric_sub_ = rclcpp::create_subscription<DiagnosticArray>(
-//       dummy_node, "/planning_evaluator/metrics", 1, [=](const DiagnosticArray::ConstSharedPtr
-//       msg) {
-//         const auto it = std::find_if(msg->status.begin(), msg->status.end(), is_target_metric);
-//         if (it != msg->status.end()) {
-//           metric_value_ = boost::lexical_cast<double>(it->values[2].value);
-//           metric_updated_ = true;
-//         }
-//       });
-//   }
+            double velocity = current_point.longitudinal_velocity_mps;
 
-//   Trajectory makeTrajectory(const std::vector<std::pair<double, double>> & traj)
-//   {
-//     Trajectory t;
-//     t.header.frame_id = "map";
-//     TrajectoryPoint p;
-//     for (const std::pair<double, double> & point : traj) {
-//       p.pose.position.x = point.first;
-//       p.pose.position.y = point.second;
-//       t.points.push_back(p);
-//     }
-//     return t;
-//   }
+            double travel_time = distance / velocity;
 
-//   void publishTrajectory(const Trajectory & traj)
-//   {
-//     traj_pub_->publish(traj);
-//     rclcpp::spin_some(eval_node);
-//     rclcpp::spin_some(dummy_node);
-//     rclcpp::sleep_for(std::chrono::milliseconds(100));
-//   }
-//   void publishReferenceTrajectory(const Trajectory & traj)
-//   {
-//     ref_traj_pub_->publish(traj);
-//     rclcpp::spin_some(eval_node);
-//     rclcpp::spin_some(dummy_node);
-//     rclcpp::sleep_for(std::chrono::milliseconds(100));
-//   }
-//   void publishObjects(const Objects & obj)
-//   {
-//     objects_pub_->publish(obj);
-//     rclcpp::spin_some(eval_node);
-//     rclcpp::spin_some(dummy_node);
-//     rclcpp::sleep_for(std::chrono::milliseconds(100));
-//   }
-//   double publishTrajectoryAndGetMetric(const Trajectory & traj)
-//   {
-//     metric_updated_ = false;
-//     traj_pub_->publish(traj);
-//     while (!metric_updated_) {
-//       rclcpp::spin_some(eval_node);
-//       rclcpp::spin_some(dummy_node);
-//       rclcpp::sleep_for(std::chrono::milliseconds(100));
-//     }
-//     return metric_value_;
-//   }
+            int num_steps = static_cast<int>(std::ceil(travel_time / interval_seconds));
+            for (int step = 0; step <= num_steps; ++step) {
+                double t = static_cast<double>(step) / num_steps; 
 
-//   double publishModifiedGoalAndGetMetric(const double x, const double y, const double yaw)
-//   {
-//     metric_updated_ = false;
+                odometry_msg.pose.pose.position.x = previous_point.pose.position.x + t * dx;
+                odometry_msg.pose.pose.position.y = previous_point.pose.position.y + t * dy;
+                odometry_msg.pose.pose.position.z = previous_point.pose.position.z;
 
-//     PoseWithUuidStamped goal;
-//     goal.header.frame_id = "map";
-//     goal.header.stamp = dummy_node->now();
-//     goal.pose.position.x = x;
-//     goal.pose.position.y = y;
-//     goal.pose.position.z = 0.0;
-//     tf2::Quaternion q;
-//     q.setRPY(0.0, 0.0, yaw);
-//     goal.pose.orientation.x = q.x();
-//     goal.pose.orientation.y = q.y();
-//     goal.pose.orientation.z = q.z();
-//     goal.pose.orientation.w = q.w();
-//     modified_goal_pub_->publish(goal);
+                odometry_msg.twist.twist.linear.x = velocity;
 
-//     while (!metric_updated_) {
-//       rclcpp::spin_some(eval_node);
-//       rclcpp::spin_some(dummy_node);
-//       rclcpp::sleep_for(std::chrono::milliseconds(100));
-//     }
-//     return metric_value_;
-//   }
+                current_time += rclcpp::Duration::from_seconds(interval_seconds);
+                odometry_msg.header.stamp = current_time;
 
-//   void publishEgoPose(const double x, const double y, const double yaw)
-//   {
-//     Odometry odom;
-//     odom.header.frame_id = "map";
-//     odom.header.stamp = dummy_node->now();
-//     odom.pose.pose.position.x = x;
-//     odom.pose.pose.position.y = y;
-//     odom.pose.pose.position.z = 0.0;
-//     tf2::Quaternion q;
-//     q.setRPY(0.0, 0.0, yaw);
-//     odom.pose.pose.orientation.x = q.x();
-//     odom.pose.pose.orientation.y = q.y();
-//     odom.pose.pose.orientation.z = q.z();
-//     odom.pose.pose.orientation.w = q.w();
+                odometry_publisher->publish(odometry_msg);
 
-//     odom_pub_->publish(odom);
-//     rclcpp::spin_some(eval_node);
-//     rclcpp::spin_some(dummy_node);
-//     rclcpp::sleep_for(std::chrono::milliseconds(100));
-//   }
+                rclcpp::spin_some(node);
+                rclcpp::spin_some(evaluator_node);
+                std::this_thread::sleep_for(std::chrono::milliseconds(
+                    static_cast<int>(interval_seconds * 1000)));
+            }
+        }
+    }
 
-//   // Latest metric value
-//   bool metric_updated_ = false;
-//   double metric_value_;
-//   // Node
-//   rclcpp::Node::SharedPtr dummy_node;
-//   EvalNode::SharedPtr eval_node;
-//   // Trajectory publishers
-//   rclcpp::Publisher<Trajectory>::SharedPtr traj_pub_;
-//   rclcpp::Publisher<Trajectory>::SharedPtr ref_traj_pub_;
-//   rclcpp::Publisher<Objects>::SharedPtr objects_pub_;
-//   rclcpp::Publisher<PoseWithUuidStamped>::SharedPtr modified_goal_pub_;
-//   rclcpp::Publisher<Odometry>::SharedPtr odom_pub_;
-//   rclcpp::Subscription<DiagnosticArray>::SharedPtr metric_sub_;
-//   // TF broadcaster
-//   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-// };
+    double extractMetricValue(const MetricArrayMsg &metrics, const std::string &name) {
+        for (const auto &metric : metrics.metric_array) {
+            if (metric.name == name) {
+                return std::stod(metric.value);
+            }
+        }
+        throw std::runtime_error("Metric not found: " + name);
+    }
 
-// TEST_F(EvalTest, TestCurvature)
-// {
-//   setTargetMetric(planning_diagnostics::Metric::curvature);
-//   Trajectory t = makeTrajectory({{0.0, 0.0}, {1.0, 1.0}, {2.0, 0.0}});
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), -1.0);
-//   t = makeTrajectory({{0.0, 0.0}, {2.0, -2.0}, {4.0, 0.0}});
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), 0.5);
-// }
+    autoware_planning_msgs::msg::Trajectory generateLinearTrajectory(double length, int num_points, double velocity) {
+        autoware_planning_msgs::msg::Trajectory trajectory;
+        for (int i = 0; i < num_points; ++i) {
+            autoware_planning_msgs::msg::TrajectoryPoint point;
+            point.pose.position.x = i * (length / (num_points - 1));
+            point.pose.position.y = 0.0;
+            point.pose.position.z = 0.0;
+            point.longitudinal_velocity_mps = velocity;
+            trajectory.points.push_back(point);
+        }
+        return trajectory;
+    }
+};
 
-// TEST_F(EvalTest, TestPointInterval)
-// {
-//   setTargetMetric(planning_diagnostics::Metric::point_interval);
-//   Trajectory t = makeTrajectory({{0.0, 0.0}, {0.0, 1.0}, {0.0, 2.0}});
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), 1.0);
-//   // double the average interval
-//   TrajectoryPoint p;
-//   p.pose.position.x = 0.0;
-//   p.pose.position.y = 6.0;
-//   t.points.push_back(p);
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), 2.0);
-// }
+TEST_F(TrajectoryEvaluatorTests, TestLinearTrajectoryWithEgoVehicleOdometry) {
+    double length = 10.0;
+    int num_points = 20;
+    double velocity = 5.0;
 
-// TEST_F(EvalTest, TestRelativeAngle)
-// {
-//   setTargetMetric(planning_diagnostics::Metric::relative_angle);
-//   Trajectory t = makeTrajectory({{0.0, 0.0}, {0.0, 1.0}, {1.0, 1.0}});
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), -M_PI_4);
-//   // add an angle of PI/4 to bring the average to 0
-//   TrajectoryPoint p;
-//   p.pose.position.x = 1.0;
-//   p.pose.position.y = 2.0;
-//   t.points.push_back(p);
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), 0.0);
-// }
+    auto trajectory = generateLinearTrajectory(length, num_points, velocity);
+    publishTrajectoryAndWait(trajectory, std::chrono::milliseconds(5000));
 
-// TEST_F(EvalTest, TestLength)
-// {
-//   setTargetMetric(planning_diagnostics::Metric::length);
-//   Trajectory t = makeTrajectory({{0.0, 0.0}, {0.0, 1.0}, {0.0, 2.0}, {0.0, 3.0}});
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), 3.0);
-//   TrajectoryPoint p;
-//   p.pose.position.x = 3.0;
-//   p.pose.position.y = 3.0;
-//   t.points.push_back(p);
-//   EXPECT_DOUBLE_EQ(publishTrajectoryAndGetMetric(t), 6.0);
-// }
+    double interval_seconds = 0.1;  
+    simulateEgoVehicle(trajectory, interval_seconds);
+
+    double expected_time = extractMetricValue(*received_metrics, "trajectory_metrics/expected_time");
+    double actual_time = extractMetricValue(*received_metrics, "trajectory_metrics/actual_time");
+    double time_error = extractMetricValue(*received_metrics, "trajectory_metrics/time_error");
+
+    double tolerance = 0.1;
+    EXPECT_NEAR(actual_time, expected_time, tolerance);
+    EXPECT_GE(time_error, 0.0);
+}
