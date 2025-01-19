1d37e374d8392d0ecba1a8675d8c89c44ac16da8
Junya Sasaki <junya.sasaki@tier4.jp>
2025-01-19 21:21:14 +0900

    fix(mrm_handler): fix include header path
      * To follow the previous commit
    
    Signed-off-by: Junya Sasaki <junya.sasaki@tier4.jp>

--- system/mrm_handler/src/mrm_handler/mrm_handler_core.cpp
+++ system/mrm_handler/src/mrm_handler/mrm_handler_core.cpp
@@ -11,7 +11,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
 // governing permissions and limitations under the License.
 
-#include "mrm_handler/mrm_handler_core.hpp"
+#include "autoware/mrm_handler/mrm_handler_core.hpp"
 
 #include <chrono>
 #include <memory>
