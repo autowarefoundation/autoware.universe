6f2a984fd2b63bd70afa566ef8e0b7b34e44c554
Junya Sasaki <junya.sasaki@tier4.jp>
2025-01-22 14:25:13 +0900

    bug(autoware_dummy_infrastructure): revert wrongly updated copyrights
    Signed-off-by: Junya Sasaki <junya.sasaki@tier4.jp>

--- system/autoware_dummy_infrastructure/include/autoware/dummy_infrastructure/dummy_infrastructure_node.hpp
+++ system/autoware_dummy_infrastructure/include/autoware/dummy_infrastructure/dummy_infrastructure_node.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Tier IV
+// Copyright 2021 Tier IV
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
