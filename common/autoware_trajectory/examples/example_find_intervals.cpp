eb030483e2bd835f003bb155164f2e3d81cb339f
Y.Hisaki <yhisaki31@gmail.com>
2025-01-30 15:54:34 +0900

    fix copyright
    Signed-off-by: Y.Hisaki <yhisaki31@gmail.com>

--- common/autoware_trajectory/examples/example_find_intervals.cpp
+++ common/autoware_trajectory/examples/example_find_intervals.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Tier IV, Inc.
+// Copyright 2025 Tier IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include "autoware/trajectory/path_point_with_lane_id.hpp"
 #include "autoware/trajectory/utils/find_intervals.hpp"
 
