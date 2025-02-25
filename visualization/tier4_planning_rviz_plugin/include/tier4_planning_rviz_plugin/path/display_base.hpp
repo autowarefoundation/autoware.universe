a07047f2a7eb39aa17791484a3c3ac1d7d01c631
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2025-02-26 02:12:24 +0900

    feat: replace autoware_universe_utils with autoware_utils in visualization folder
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

--- visualization/tier4_planning_rviz_plugin/include/tier4_planning_rviz_plugin/path/display_base.hpp
+++ visualization/tier4_planning_rviz_plugin/include/tier4_planning_rviz_plugin/path/display_base.hpp
@@ -91,8 +91,8 @@ bool validateFloats(const typename T::ConstSharedPtr & msg_ptr)
 {
   for (auto && path_point : msg_ptr->points) {
     if (
-      !rviz_common::validateFloats(autoware::universe_utils::getPose(path_point)) &&
-      !rviz_common::validateFloats(autoware::universe_utils::getLongitudinalVelocity(path_point))) {
+      !rviz_common::validateFloats(autoware_utils::get_pose(path_point)) &&
+      !rviz_common::validateFloats(autoware_utils::get_longitudinal_velocity(path_point))) {
       return false;
     }
   }
@@ -371,10 +371,10 @@ protected:
       alphas.back() = 0.0f;
       float cumulative_distance = 0.0f;
       for (size_t point_idx = msg_ptr->points.size() - 1; point_idx > 0; point_idx--) {
-        const auto & curr_point = autoware::universe_utils::getPose(msg_ptr->points.at(point_idx));
+        const auto & curr_point = autoware_utils::get_pose(msg_ptr->points.at(point_idx));
         const auto & prev_point =
-          autoware::universe_utils::getPose(msg_ptr->points.at(point_idx - 1));
-        float distance = std::sqrt(autoware::universe_utils::calcSquaredDistance2d(
+          autoware_utils::get_pose(msg_ptr->points.at(point_idx - 1));
+        float distance = std::sqrt(autoware_utils::calc_squared_distance2d(
           prev_point.position, curr_point.position));
         cumulative_distance += distance;
 
@@ -393,8 +393,8 @@ protected:
     // Forward iteration to visualize path
     for (size_t point_idx = 0; point_idx < msg_ptr->points.size(); point_idx++) {
       const auto & path_point = msg_ptr->points.at(point_idx);
-      const auto & pose = autoware::universe_utils::getPose(path_point);
-      const auto & velocity = autoware::universe_utils::getLongitudinalVelocity(path_point);
+      const auto & pose = autoware_utils::get_pose(path_point);
+      const auto & velocity = autoware_utils::get_longitudinal_velocity(path_point);
 
       // path
       if (property_path_view_.getBool()) {
@@ -489,9 +489,9 @@ protected:
           (point_idx != msg_ptr->points.size() - 1) ? point_idx + 1 : point_idx;
 
         const auto & prev_path_pos =
-          autoware::universe_utils::getPose(msg_ptr->points.at(prev_idx)).position;
+          autoware_utils::get_pose(msg_ptr->points.at(prev_idx)).position;
         const auto & next_path_pos =
-          autoware::universe_utils::getPose(msg_ptr->points.at(next_idx)).position;
+          autoware_utils::get_pose(msg_ptr->points.at(next_idx)).position;
 
         Ogre::Vector3 position;
         position.x = pose.position.x;
@@ -502,7 +502,7 @@ protected:
 
         rviz_rendering::MovableText * text = slope_texts_.at(point_idx);
         const double slope =
-          autoware::universe_utils::calcElevationAngle(prev_path_pos, next_path_pos);
+          autoware_utils::calc_elevation_angle(prev_path_pos, next_path_pos);
 
         std::stringstream ss;
         ss << std::fixed << std::setprecision(2) << slope;
@@ -546,7 +546,7 @@ protected:
 
     for (size_t p_idx = 0; p_idx < msg_ptr->points.size(); p_idx++) {
       const auto & point = msg_ptr->points.at(p_idx);
-      const auto & pose = autoware::universe_utils::getPose(point);
+      const auto & pose = autoware_utils::get_pose(point);
       // footprint
       if (property_footprint_view_.getBool()) {
         Ogre::ColourValue color;
