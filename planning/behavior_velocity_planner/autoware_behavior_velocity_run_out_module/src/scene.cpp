fe4028484c1b648e6aad9726091bae0e3cdb16b7
liuXinGangChina <lxg19892021@gmail.com>
2025-02-28 19:30:52 +0800

    feat(autoware_planning_factor_interface): modify multiple  plugins under motion_velocity_planner folder, autoware_planning_factor_interface, : v0.6
    Signed-off-by: liuXinGangChina <lxg19892021@gmail.com>

--- planning/behavior_velocity_planner/autoware_behavior_velocity_run_out_module/src/scene.cpp
+++ planning/behavior_velocity_planner/autoware_behavior_velocity_run_out_module/src/scene.cpp
@@ -769,7 +769,7 @@ bool RunOutModule::insertStopPoint(
 
   planning_factor_interface_->add(
     path.points, planner_data_->current_odometry->pose, stop_point.value(), stop_point.value(),
-    tier4_planning_msgs::msg::PlanningFactor::STOP, tier4_planning_msgs::msg::SafetyFactorArray{},
+    autoware_internal_planning_msgs::msg::PlanningFactor::STOP, autoware_internal_planning_msgs::msg::SafetyFactorArray{},
     true /*is_driving_forward*/, 0.0 /*velocity*/, 0.0 /*shift_distance*/, "run_out_stop");
 
   return true;
@@ -883,8 +883,8 @@ void RunOutModule::insertApproachingVelocity(
 
   planning_factor_interface_->add(
     output_path.points, planner_data_->current_odometry->pose, stop_point.value(),
-    stop_point.value(), tier4_planning_msgs::msg::PlanningFactor::STOP,
-    tier4_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0 /*velocity*/,
+    stop_point.value(), autoware_internal_planning_msgs::msg::PlanningFactor::STOP,
+    autoware_internal_planning_msgs::msg::SafetyFactorArray{}, true /*is_driving_forward*/, 0.0 /*velocity*/,
     0.0 /*shift_distance*/, "run_out_approaching_velocity");
 
   // debug
