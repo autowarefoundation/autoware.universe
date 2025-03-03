11d6b0c50a7ab58ba11c845c8b5fe2df3bdf4112
ktro2828 <kotaro.uetake@tier4.jp>
2025-03-03 23:16:31 +0900

    feat: update diagnostics when harsh backlight is detected
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- perception/autoware_traffic_light_classifier/src/traffic_light_classifier_node.cpp
+++ perception/autoware_traffic_light_classifier/src/traffic_light_classifier_node.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 #include "traffic_light_classifier_node.hpp"
 
+#include <diagnostic_msgs/msg/diagnostic_status.hpp>
 #include <tier4_perception_msgs/msg/traffic_light_element.hpp>
 
 #include <iostream>
@@ -60,6 +61,9 @@ TrafficLightClassifierNodelet::TrafficLightClassifierNodelet(const rclcpp::NodeO
       this->get_logger(), "please install CUDA, CUDNN and TensorRT to use cnn classifier");
 #endif
   }
+
+  diagnostics_interface_ptr_ =
+    std::make_unique<autoware_utils::DiagnosticsInterface>(this, "traffic_light_classifier");
 }
 
 void TrafficLightClassifierNodelet::connectCb()
@@ -165,6 +169,17 @@ void TrafficLightClassifierNodelet::imageRoiCallback(
 
   output_msg.header = input_image_msg->header;
   traffic_signal_array_pub_->publish(output_msg);
+
+  // publish diagnostics
+  diagnostics_interface_ptr_->clear();
+  bool found_harsh_backlight = backlight_indices.size() != 0;
+  diagnostics_interface_ptr_->add_key_value("found_harsh_backlight", found_harsh_backlight);
+  if (found_harsh_backlight) {
+    diagnostics_interface_ptr_->update_level_and_message(
+      diagnostic_msgs::msg::DiagnosticStatus::WARN,
+      "Found harsh backlight in ROI(s) and corresponding ROI(s) were overwritten by UNKNOWN");
+  }
+  diagnostics_interface_ptr_->publish(output_msg.header.stamp);
 }
 
 bool TrafficLightClassifierNodelet::is_harsh_backlight(const cv::Mat & img) const
