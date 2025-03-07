ded5f5b903e4f177939a0fe336674a3853ba3faf
mohammad alqudah <alqudah.mohammad@tier4.jp>
2025-03-07 13:38:00 +0900

    dont insert stop point on target lane if next lc dist buffer is zero
    Signed-off-by: mohammad alqudah <alqudah.mohammad@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/scene.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_lane_change_module/src/scene.cpp
@@ -523,6 +523,9 @@ void NormalLaneChange::insert_stop_point(
 
   // if input is not current lane, we can just insert the points at terminal.
   if (!is_current_lane) {
+    if(common_data_ptr_->transient_data.next_dist_buffer.min < calculation::eps) {
+      return;
+    }
     const auto arc_length_to_stop_pose = motion_utils::calcArcLength(path.points) -
                                          common_data_ptr_->transient_data.next_dist_buffer.min;
     set_stop_pose(arc_length_to_stop_pose, path, "next lc terminal");
