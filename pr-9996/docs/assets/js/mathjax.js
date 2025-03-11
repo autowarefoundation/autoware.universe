158d8a0ec5833a1b0db505017f97f5d5ead2320a
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2025-03-11 12:12:42 +0900

    chore: reset commit history
    Signed-off-by: Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>

---
+++ pr-9996/docs/assets/js/mathjax.js
@@ -0,0 +1,20 @@
+// This file is automatically synced from:
+// https://github.com/autowarefoundation/sync-file-templates
+// To make changes, update the source repository and follow the guidelines in its README.
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
