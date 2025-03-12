1ca8c8e136fcba5ca5aab557754c4920d461e085
Maxime CLEMENT <maxime.clement@tier4.jp>
2025-03-12 14:19:16 +0900

    add unit test for new metric
    Signed-off-by: Maxime CLEMENT <maxime.clement@tier4.jp>

--- planning/autoware_planning_validator/test/src/test_planning_validator_helper.cpp
+++ planning/autoware_planning_validator/test/src/test_planning_validator_helper.cpp
@@ -211,6 +211,7 @@ rclcpp::NodeOptions getNodeOptionsWithDefaultParams()
   node_options.append_parameter_override(
     "thresholds.longitudinal_distance_deviation", THRESHOLD_LONGITUDINAL_DISTANCE_DEVIATION);
   node_options.append_parameter_override("thresholds.nominal_latency", THRESHOLD_NOMINAL_LATENCY);
+  node_options.append_parameter_override("thresholds.yaw_deviation", THRESHOLD_YAW_DEVIATION);
   node_options.append_parameter_override(
     "parameters.forward_trajectory_length_acceleration",
     PARAMETER_FORWARD_TRAJECTORY_LENGTH_ACCELERATION);
