4083051863b39fdd59cb92452b1ace72bc9c5a6e
NorahXiong <norah.xiong@autocore.ai>
2025-03-13 14:34:45 +0800

    feat: adaptation to ROS nodes guidelines about directory structure
    Signed-off-by: NorahXiong <norah.xiong@autocore.ai>

--- planning/behavior_path_planner/autoware_behavior_path_start_planner_module/test/test_freespace_pull_out.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_start_planner_module/test/test_freespace_pull_out.cpp
@@ -20,7 +20,7 @@
 #include <autoware/behavior_path_start_planner_module/util.hpp>
 #include <autoware/route_handler/route_handler.hpp>
 #include <autoware_lanelet2_extension/utility/query.hpp>
-#include <autoware_planning_test_manager/autoware_planning_test_manager_utils.hpp>
+#include <autoware/planning_test_manager/autoware_planning_test_manager_utils.hpp>
 #include <autoware_test_utils/autoware_test_utils.hpp>
 
 #include <gtest/gtest.h>
