de7ad759c2391ec0c115acbdb0ff6e6eb7875385
Arjun Jagdish Ram <arjun.ram@tier4.jp>
2025-03-19 11:43:03 +0900

    feat(path_optimizer): additional failure logging and failure mode handling (#10276)
    MRM when MPT fails
    
    Signed-off-by: Arjun Jagdish Ram <arjun.ram@tier4.jp>

--- planning/autoware_path_optimizer/include/autoware/path_optimizer/state_equation_generator.hpp
+++ planning/autoware_path_optimizer/include/autoware/path_optimizer/state_equation_generator.hpp
@@ -37,6 +37,16 @@ public:
     Eigen::VectorXd W;
   };
 
+  friend std::ostream & operator<<(std::ostream & os, const Matrix & matrix)
+  {
+    os << "Matrix: {\n";
+    os << "\tA:\n" << matrix.A << "\n";
+    os << "\tB:\n" << matrix.B << "\n";
+    os << "\tW:\n" << matrix.W << "\n";
+    os << "}\n";
+    return os;
+  }
+
   StateEquationGenerator() = default;
   StateEquationGenerator(
     const double wheel_base, const double max_steer_rad,
